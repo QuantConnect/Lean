efd4a69bf24bce3b47b2eb370acabca7d6e777db
snugs <mhandschuh@gmail.com>
2015-01-20 11:52:12 -0300

    Adds config value to use test data feed endpoint

--- Engine/Setup/ConsoleSetupHandler.cs
+++ Engine/Setup/ConsoleSetupHandler.cs
@@ -151,7 +151,9 @@ namespace QuantConnect.Lean.Engine.Setup
                     //Endpoints:
                     liveJob.TransactionEndpoint = TransactionHandlerEndpoint.Backtesting;
                     liveJob.ResultEndpoint = ResultHandlerEndpoint.LiveTrading;
-                    liveJob.DataEndpoint = Log.DebuggingEnabled ? DataFeedEndpoint.Test : DataFeedEndpoint.LiveTrading;
+
+                    bool testLiveTradingEnabled = Config.GetBool("test-live-trading-enabled", defaultValue: false);
+                    liveJob.DataEndpoint = testLiveTradingEnabled ? DataFeedEndpoint.Test : DataFeedEndpoint.LiveTrading;
                     liveJob.RealTimeEndpoint = RealTimeEndpoint.LiveTrading;
                     liveJob.SetupEndpoint = SetupHandlerEndpoint.Console;
 
