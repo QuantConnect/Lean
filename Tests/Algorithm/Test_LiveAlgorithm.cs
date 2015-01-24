bc908e4d9488ecc535b8c75230307581f8aed242
snugs <mhandschuh@gmail.com>
2015-01-24 19:12:21 -0300

    Brokerage tidy up

--- Tests/Algorithm/Test_LiveAlgorithm.cs
+++ Tests/Algorithm/Test_LiveAlgorithm.cs
@@ -103,7 +103,6 @@ namespace QuantConnect
                     break;
 
                 //Live socket for bitcoin prices:
-                case DataFeedEndpoint.Tradier:
                 case DataFeedEndpoint.LiveTrading:
                     //Live refreshing endpoint.
                     source = "https://www.bitstamp.net/api/ticker/";
@@ -162,7 +161,6 @@ namespace QuantConnect
 
                 //Example Line Format:
                 //{"high": "441.00", "last": "421.86", "timestamp": "1411606877", "bid": "421.96", "vwap": "428.58", "volume": "14120.40683975", "low": "418.83", "ask": "421.99"}
-                case DataFeedEndpoint.Tradier:
                 case DataFeedEndpoint.LiveTrading:
                     try
                     {
