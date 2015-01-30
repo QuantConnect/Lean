041946a28296cfd461194e91baa238d25729fb3f
snugs <mhandschuh@gmail.com>
2015-01-30 10:45:52 -0300

    Stash custom data testing

---
+++ Algorithm/Examples/CustomDataWithFillForward.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantConnect.Data.Test;
+
+namespace QuantConnect.Algorithm.Examples
+{
+    public class CustomDataWithFillForward : QCAlgorithm
+    {
+        public List<string> CustomSymbols = new List<string>(); 
+        public override void Initialize()
+        {
+            SetStartDate(2013, 01, 01);
+            SetEndDate(2014, 01, 01);
+
+            int count = 100;
+            for (int i = 0; i < count; i++)
+            {
+                CustomSymbols.Add("SYM" + i.ToString("000"));
+            }
+
+            foreach (var symbol in CustomSymbols)
+            {
+                AddData<FakeTradeBarCustom>(symbol, Resolution.Minute);
+            }
+        }
+
+        public void OnData(FakeTradeBarCustom custom)
+        {
+            
+        }
+    }
+}
---
+++ Common/Data/Test/FakeTradeBarCustom.cs
@@ -0,0 +1,11 @@
+﻿using QuantConnect.Data.Market;
+
+namespace QuantConnect.Data.Test
+{
+    /// <summary>
+    /// This type is used to simulate custom data moving through the system. It grabs its data
+    /// </summary>
+    public class FakeTradeBarCustom : TradeBar
+    {
+    }
+}
