d8bde63f626b1fc06c9fab50ceb6912885ff73ac
Michael Ourednik <michael.ourednik@gmail.com>
2015-01-31 13:03:12 +1300

    StandardDeviation indicator

---
+++ Indicators/StandardDeviation.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using MathNetStatistics = MathNet.Numerics.Statistics.Statistics;
+
+namespace QuantConnect.Indicators
+{
+    public class StandardDeviation : WindowIndicator<IndicatorDataPoint>
+    {
+        /// <summary>
+        /// Initializes a new instance of the StandardDeviation class with the specified period.
+        /// 
+        /// Estimates the unbiased population standard deviation.
+        /// On a dataset of size N will use an N-1 normalizer (Bessel's correction).
+        /// </summary>
+        /// <param name="period">The sample size of the standard deviation</param>
+        public StandardDeviation(int period)
+            : this("STD" + period, period)
+        {
+        }
+
+        public StandardDeviation(string name, int period)
+            : base(name, period)
+        {
+        }
+
+        /// <summary>
+        /// Gets a flag indicating when this indicator is ready and fully initialized
+        /// </summary>
+        public override bool IsReady {
+            get { return Samples >= Period; }
+        }
+
+        protected override decimal ComputeNextValue(IReadOnlyWindow<IndicatorDataPoint> window, IndicatorDataPoint input)
+        {
+            if (!IsReady) {
+                return input;
+            }
+            IEnumerable<double> doubleValues = window.Select(i => Convert.ToDouble(i.Value));
+            double std = MathNetStatistics.StandardDeviation(doubleValues);
+            return Convert.ToDecimal(std);
+        }
+    }
+}
---
+++ Tests/Indicators/StandardDeviationTests.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using NUnit.Framework;
+using QuantConnect.Indicators;
+
+namespace QuantConnect.Tests.Indicators
+{
+    [TestFixture]
+    public class StandardDeviationTests
+    {
+        [Test]
+        public void ComputesCorrectly()
+        {
+            // Indicator output was compared against values from "std" function in Julia.
+            var std = new StandardDeviation(3);
+            var reference = DateTime.MinValue;
+
+            std.Update(reference.AddDays(1), 1m);
+            Assert.AreEqual(1m, std.Current.Value);
+
+            std.Update(reference.AddDays(2), -1m);
+            Assert.AreEqual(-1m, std.Current.Value);
+
+            std.Update(reference.AddDays(3), 1m);
+            Assert.AreEqual(1.15470053837925m, std.Current.Value);
+
+            std.Update(reference.AddDays(4), -2m);
+            Assert.AreEqual(1.52752523165195m, std.Current.Value);
+
+            std.Update(reference.AddDays(5), 3m);
+            Assert.AreEqual(2.51661147842358m, std.Current.Value);
+        }
+    }
+}
