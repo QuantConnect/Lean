9546aedce73e0ad81312931c49457574da47f764
bdilber <baran.dilber@gmail.com>
2015-02-20 19:49:51 +0200

    Refactored the Log class into ILogHandler interface and the ConsoleLogHandler implementation.

---
+++ Logging/ILogHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace QuantConnect.Logging
+{
+    /// <summary>
+    /// Interface for redirecting log output
+    /// </summary>
+    public interface ILogHandler
+    {
+        /// <summary>
+        /// Write error message to log
+        /// </summary>
+        /// <param name="text"></param>
+        void Error(string text);
+       
+        /// <summary>
+        /// Write debug message to log
+        /// </summary>
+        /// <param name="text"></param>
+        void Debug(string text);
+       
+        /// <summary>
+        /// Write debug message to log
+        /// </summary>
+        /// <param name="text"></param>
+        void Trace(string text);
+    }
+}
