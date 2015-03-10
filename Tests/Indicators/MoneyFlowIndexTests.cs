c4af1e2a5b98ffcfc81b9467a5595bcc6d9ccd93
mattmast <mastm17@gmail.com>
2015-03-09 22:46:27 -0400

    Add MFI tests for zero volume trade bars and Resets properly

--- Tests/Indicators/MoneyFlowIndexTests.cs
+++ Tests/Indicators/MoneyFlowIndexTests.cs
@@ -13,8 +13,10 @@
  * limitations under the License.
 */
 
+using System;
 using NUnit.Framework;
 using QuantConnect.Indicators;
+using QuantConnect.Data.Market;
 
 namespace QuantConnect.Tests.Indicators {
     [TestFixture]
@@ -23,6 +25,49 @@ namespace QuantConnect.Tests.Indicators {
         public void ComparesAgainstExternalData() {
             var mfi = new MoneyFlowIndex(20);
             TestHelper.TestIndicator(mfi, "spy_mfi.txt", "Money Flow Index 20");
+        }
+
+        [Test]
+        public void TestTradeBarsWithNoVolume() {
+            var mfi = new MoneyFlowIndex(3);
+            foreach (var data in TestHelper.GetDataStream(4)) {
+                var tradeBar = new TradeBar {
+                    Open = data.Value,
+                    Close = data.Value,
+                    High = data.Value,
+                    Low = data.Value,
+                    Volume = 0
+                };
+                mfi.Update(tradeBar);
+            }
+
+            Assert.AreEqual(mfi.Current.Value, 100.0m);
+        }
+
+        [Test]
+        public void ResetsProperly() {
+            var mfi = new MoneyFlowIndex(3);
+            foreach (var data in TestHelper.GetDataStream(4)) {
+                var tradeBar = new TradeBar {
+                    Open = data.Value,
+                    Close = data.Value,
+                    High = data.Value,
+                    Low = data.Value,
+                    Volume = Decimal.ToInt64(data.Value)
+                };
+                mfi.Update(tradeBar);
+            }
+            Assert.IsTrue(mfi.IsReady);
+            Assert.IsTrue(mfi.PositiveMoneyFlow.IsReady);
+            Assert.IsTrue(mfi.NegativeMoneyFlow.IsReady);
+            Assert.AreNotEqual(mfi.PreviousTypicalPrice, 0.0m);
+
+            mfi.Reset();
+
+            Assert.AreEqual(mfi.PreviousTypicalPrice, 0.0m);
+            TestHelper.AssertIndicatorIsInDefaultState(mfi);
+            TestHelper.AssertIndicatorIsInDefaultState(mfi.PositiveMoneyFlow);
+            TestHelper.AssertIndicatorIsInDefaultState(mfi.NegativeMoneyFlow);
         }
     }
 }
