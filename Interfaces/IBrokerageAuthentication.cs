a44f770da37cac366c8ac8a303851d24652254d0
QANTau <adam@qant.com>
2015-04-01 16:33:45 +1000

    Added IBrokerateAuthentication

---
+++ Interfaces/IBrokerageAuthentication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using QuantConnect.Orders;
+using QuantConnect.Securities;
+
+namespace QuantConnect.Interfaces
+{
+
+    public interface IBrokerageAuthentication
+    {
+        /// <summary>
+        /// Validate Brokerage Authentication Parameters
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        bool Validate(out string message);
+    }
+}
