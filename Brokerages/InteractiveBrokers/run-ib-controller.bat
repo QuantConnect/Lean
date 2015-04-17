f4ace5d2f7cf1d6fcf541f358f41e4afd59cfbbf
snugs <mhandschuh@gmail.com>
2015-04-17 11:35:23 -0400

    Clear cmd history in run-ib-controller
    Add manual test for IB connection resiliency

--- Brokerages/InteractiveBrokers/run-ib-controller.bat
+++ Brokerages/InteractiveBrokers/run-ib-controller.bat
@@ -15,9 +15,14 @@ if /i "%5" EQU "-tws" set CONTROLLER=ibcontroller.IBController
 set TWSCP=jts.jar;total.2012.jar
 set JAVAOPTS=-Dsun.java2d.noddraw=false -Dswing.boldMetal=false -Dsun.locale.formatasdefault=true -Xmx768M
 
+:: start ib controller
 pushd %TWSDIR%
 start java.exe -cp  %TWSCP%;%IBCDIR%\IBController.jar %JAVAOPTS% %CONTROLLER% %IBCINI% %3 %4
 popd
+
+:: clear out our history and the screen
+doskey /reinstall
+cls
 
 :: leave this window running so we can find it later and close its child processes
 pause
