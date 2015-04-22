1bd1d751afb72eca2ec8d1751ea2cfd8717693f7
QuantConnect <contact@quantconnect.com>
2015-04-21 21:30:35 -0400

    Initial implementation of MYSQL datafeed

---
+++ Engine/DataFeeds/DatabaseDataFeed.cs
@@ -0,0 +1,333 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+/**********************************************************
+* USING NAMESPACES
+**********************************************************/
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+using QuantConnect.Packets;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using QuantConnect.Configuration;
+using QuantConnect.Logging;
+
+namespace QuantConnect.Lean.Engine.DataFeeds
+{
+    /// <summary>
+    /// ODBC Database Feed Connection
+    /// </summary>
+    public class DatabaseDataFeed : IDataFeed
+    {
+        /******************************************************** 
+        * CLASS VARIABLES
+        *********************************************************/
+        // Set types in public area to speed up:
+        private IAlgorithm _algorithm;
+        private BacktestNodePacket _job;
+        private bool _endOfStreams = false;
+        private int _subscriptions = 0;
+        private int _bridgeMax = 500000;
+        private bool _exitTriggered = false;
+        private MySqlConnection _connection;
+
+        /******************************************************** 
+        * CLASS PROPERTIES
+        *********************************************************/
+        /// <summary>
+        /// List of the subscription the algorithm has requested. Subscriptions contain the type, sourcing information and manage the enumeration of data.
+        /// </summary>
+        public List<SubscriptionDataConfig> Subscriptions { get; private set; }
+
+        /// <summary>
+        /// Prices of the datafeed this instant for dynamically updating security values (and calculation of the total portfolio value in realtime).
+        /// </summary>
+        /// <remarks>Indexed in order of the subscriptions</remarks>
+        public List<decimal> RealtimePrices { get; private set; }
+
+        /// <summary>
+        /// Cross-threading queues so the datafeed pushes data into the queue and the primary algorithm thread reads it out.
+        /// </summary>
+        public ConcurrentQueue<List<BaseData>>[] Bridge { get; set; }
+
+        /// <summary>
+        /// Stream created from the configuration settings.
+        /// </summary>
+        public SubscriptionDataReader[] SubscriptionReaderManagers { get; set; }
+
+        /// <summary>
+        /// Set the source of the data we're requesting for the type-readers to know where to get data from.
+        /// </summary>
+        /// <remarks>Live or Backtesting Datafeed</remarks>
+        public DataFeedEndpoint DataFeed { get; set; }
+
+        /// <summary>
+        /// Flag indicating the hander thread is completely finished and ready to dispose.
+        /// </summary>
+        public bool IsActive { get; private set; }
+
+        /// <summary>
+        /// Flag indicating the file system has loaded all files.
+        /// </summary>
+        public bool LoadingComplete { get; private set; }
+
+        /// <summary>
+        /// Furthest point in time that the data has loaded into the bridges.
+        /// </summary>
+        public DateTime LoadedDataFrontier { get; private set; }
+
+        /// <summary>
+        /// Signifying no more data across all bridges
+        /// </summary>
+        public bool EndOfBridges
+        {
+            get
+            {
+                for (var i = 0; i < Bridge.Length; i++)
+                {
+                    if (Bridge[i].Count != 0 || EndOfBridge[i] != true || _endOfStreams != true)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// End of Stream for Each Bridge:
+        /// </summary>
+        public bool[] EndOfBridge { get; set; }
+
+
+
+        /// <summary>
+        /// Flag for when we're connected:
+        /// </summary>
+        private bool Connected
+        {
+            get
+            {
+                if (_connection == null) return false;
+                if (ConnectionState == ConnectionState.Open) return true;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Current State of the DB Connection.
+        /// </summary>
+        private ConnectionState ConnectionState
+        {
+            get { return _connection.State; }
+        }
+
+        /******************************************************** 
+        * CLASS CONSTRUCTOR
+        *********************************************************/
+        /// <summary>
+        /// Prepare and create the new OBDC Database connection datafeed.
+        /// </summary>
+        /// <param name="algorithm"></param>
+        /// <param name="job"></param>
+        public DatabaseDataFeed(IAlgorithm algorithm, BacktestNodePacket job)
+        {
+            //Save the data subscriptions
+            Subscriptions = algorithm.SubscriptionManager.Subscriptions;
+            _subscriptions = Subscriptions.Count;
+
+            //Public Properties:
+            DataFeed = DataFeedEndpoint.FileSystem;
+            IsActive = true;
+            Bridge = new ConcurrentQueue<List<BaseData>>[_subscriptions];
+            EndOfBridge = new bool[_subscriptions];
+            SubscriptionReaderManagers = new SubscriptionDataReader[_subscriptions];
+            RealtimePrices = new List<decimal>(_subscriptions);
+
+            //Class Privates:
+            _job = job;
+            _algorithm = algorithm;
+            _endOfStreams = false;
+            _bridgeMax = _bridgeMax / _subscriptions; //Set the bridge maximum count:
+        }
+
+        /// <summary>
+        /// Connect and pull required data from MYSQL
+        /// </summary>
+        public void Run()
+        {
+            //Initialize MYSQL Connection:
+            Connect();
+            IsActive = true;
+            
+            while (!_exitTriggered && IsActive)
+            {
+                for (var i = 0; i < Subscriptions.Count; i++)
+                {
+                    //With each subscription; fetch the next increment of data from the queues:
+                    var subscription = Subscriptions[i];
+
+                    if (Bridge[i].Count < 10000 && !EndOfBridge[i])
+                    {
+                        //Fetch our data from mysql
+                        List<BaseData> data = Query("SELECT * FROM my_data WHERE symbol = ? AND time > ? ORDER BY time ASC LIMIT 100", subscription.Symbol, time);
+                        //Insert data into bridge, each list is time-grouped. Assume all different time-groups.
+                        data.ForEach(bar => Bridge[i].Enqueue(new List<BaseData>() { bar }));
+                        //Record the furthest moment in time.
+                        _mySQLBridgeTime[i] = data.Max(bar => bar.Time);
+                    }
+                }
+                //Set the most backward moment in time we've loaded
+                LoadedDataFrontier = _mySQLBridgeTime.Min();
+            }
+
+            _connection.Close();
+            IsActive = false;
+        }
+
+        /// <summary>
+        /// Send an exit signal to the thread.
+        /// </summary>
+        public void Exit()
+        {
+            _exitTriggered = true;
+            PurgeData();
+        }
+
+
+        /// <summary>
+        /// Loop over all the queues and clear them to fast-quit this thread and return to main.
+        /// </summary>
+        public void PurgeData()
+        {
+            foreach (var t in Bridge)
+            {
+                t.Clear();
+            }
+        }
+
+
+        /// <summary>
+        /// Connect to the database: if we're running locally connect to local host.
+        /// </summary>
+        private void Connect()
+        {
+            //Default to local for ease:
+            var serverAddress = Config.Get("database-address");
+            var databaseName = Config.Get("database-name");
+            var userId = Config.Get("database-user");
+            var password = Config.Get("database-password");
+            var connectionString = "";
+
+            try
+            {
+                //Create Connection String:
+                connectionString = GetConnectionString(serverAddress, databaseName, userId, password);
+
+                //Set and Open Connection:
+                _connection = new MySqlConnection(connectionString);
+                _connection.StateChange += ConnectionOnStateChange;
+                _connection.Open();
+
+                //Place a pause here if want to display graph,
+                while (_connection.State != ConnectionState.Open) { Thread.Sleep(100); };
+            }
+            catch (Exception err)
+            {
+                Log.Error("QC.Database.Connect(): " + connectionString + " | " + err.Message);
+            }
+        }
+
+        /// <summary>
+        /// Handler for state change:
+        /// </summary>
+        private void ConnectionOnStateChange(object sender, StateChangeEventArgs e)
+        {
+            if (e.CurrentState == ConnectionState.Broken || e.CurrentState == ConnectionState.Closed)
+            {
+                Connect();
+            }
+        }
+
+        /// <summary>
+        /// Generate a connection string to connect to the database:
+        /// </summary>
+        /// <returns>string completed for connection</returns>
+        private string GetConnectionString(string serverAddress, string databaseName, string userId, string password)
+        {
+            return "Server=" + serverAddress + ";" +
+                    "Database=" + databaseName + ";" +
+                    "User ID=" + userId + ";" +
+                    "Password=" + password + ";" +
+                    "Pooling=false";
+        }
+
+        /// <summary>
+        /// Send a SQL COmmand to the server:
+        /// </summary>
+        /// <returns>List of Key Arrays </returns>
+        public List<Dictionary<string, string>> Query(string sql)
+        {
+            var results = new List<Dictionary<string, string>>();
+            try
+            {
+                //Start the connection.
+                if (!Connected) Connect();
+
+                //Create the Command
+                var dbCommand = _connection.CreateCommand();
+                dbCommand.CommandText = sql;
+
+                //Send Command, Start Reading:
+                var reader = dbCommand.ExecuteReader();
+
+                //Read out each row:
+                while (reader.Read())
+                {
+                    //Initialise One Row:
+                    var row = new Dictionary<string, string>();
+                    //Get each column of the row.
+                    for (var field = 0; field < reader.FieldCount; field++)
+                    {
+                        row.Add(reader.GetName(field), reader[reader.GetName(field)].ToString());
+                    }
+                    //Add the row to results:
+                    results.Add(row);
+                }
+
+                // Clean Up variables:
+                reader.Close();
+                dbCommand.Dispose();
+
+                //Clean up memory usage for garbage collector:
+                reader = null;
+                dbCommand = null;
+            }
+            catch (Exception err)
+            {
+                Log.Error("DB.Command(): " + err.Message + " SQL Length:" + sql.Length + " SQL: " + sql.Substring(0, 100));
+            }
+            return results;
+        }
+
+    }
+}
--- Engine/DataFeeds/FileSystemDataFeed.cs
+++ Engine/DataFeeds/FileSystemDataFeed.cs
@@ -24,7 +24,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using QuantConnect.Data;
-using QuantConnect.Data.Market;
 using QuantConnect.Interfaces;
 using QuantConnect.Logging;
 using QuantConnect.Packets;
