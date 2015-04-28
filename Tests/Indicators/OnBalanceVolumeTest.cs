67620b4d1329c481f30534d2c98762f2fd8ccdc7
David Hsieh <dcchsieh@gmail.com>
2015-04-28 21:13:17 +1000

    Added On Balance Volume Indicator
    Added On Balance Volume Indicator
    
    Added On Balance Volume Indicator related tests.

---
+++ Tests/Indicators/OnBalanceVolumeTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using QuantConnect.Indicators;
+
+namespace QuantConnect.Tests.Indicators
+{
+    [TestFixture]
+    public class OnBalanceVolumeTests
+    {
+        [Test]
+        public void ComparesAgainstExternalData()
+        {
+            var onBalanceVolumeIndicator = new OnBalanceVolume("OBV");
+
+            onBalanceVolumeIndicator.OnBalanceVoulumeValues.Update(
+                new DateTime(2013, 4, 30), Decimal.Parse("1.156486E+08", System.Globalization.NumberStyles.Float));
+
+            TestHelper.TestIndicator(onBalanceVolumeIndicator, "spy_with_obv.txt", "OBV",
+                (ind, expected) => Assert.AreEqual(
+                    expected.ToString("0.##E-00"),
+                    (onBalanceVolumeIndicator.OnBalanceVoulumeValues.Current.Value).ToString("0.##E-00")
+                    )
+
+                );
+          
+        }
+
+        [Test]
+        public void ResetsProperly()
+        {
+            var onBalanceVolumeIndicator = new OnBalanceVolume("OBV");
+            foreach (var data in TestHelper.GetTradeBarStream("spy_with_obv.txt", false))
+            {
+                onBalanceVolumeIndicator.Update(data);
+            }
+
+            Assert.IsTrue(onBalanceVolumeIndicator.IsReady);
+
+            onBalanceVolumeIndicator.Reset();
+        }
+    }
+}
