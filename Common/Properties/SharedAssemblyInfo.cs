8976f07e9ceb6c2703c2dca0636da06782ff4909
QuantConnect <contact@quantconnect.com>
2015-05-31 22:34:30 -0400

    v2.1.0.30

--- Common/Properties/SharedAssemblyInfo.cs
+++ Common/Properties/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyCompany("QuantConnect")]
 [assembly: AssemblyTrademark("QuantConnect")]
-[assembly: AssemblyVersion("2.1.0.27")]
+[assembly: AssemblyVersion("2.1.0.30")]
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
 #else
