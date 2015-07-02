5adeee62fa2c6323decb7aec2a80444433699e24
Dmitri Pavlenkov <dmitripavlenkov@gmail.com>
2015-07-02 16:13:55 -0400

    Put dates to what they used to be

--- Algorithm.CSharp/MarginCallEventsAlgorithm.cs
+++ Algorithm.CSharp/MarginCallEventsAlgorithm.cs
@@ -32,8 +32,8 @@ namespace QuantConnect.Algorithm.Examples
         /// </summary>
         public override void Initialize()
         {
-            SetStartDate(2013, 10, 07);  //Set Start Date
-            SetEndDate(2013, 10, 11);    //Set End Date
+            SetStartDate(2013, 10, 01);  //Set Start Date
+            SetEndDate(2013, 12, 11);    //Set End Date
             SetCash(100000);             //Set Strategy Cash
             // Find more symbols here: http://quantconnect.com/data
             AddSecurity(SecurityType.Equity, "SPY", Resolution.Second);
