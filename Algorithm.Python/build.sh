a22b64344372b7f87093a746f7e5c4bd850778a1
QuantConnect <contact@quantconnect.com>
2015-07-05 21:50:15 -0400

    Committing missing build script for linux-python algorithms

---
+++ Algorithm.Python/build.sh
@@ -0,0 +1,11 @@
+﻿# QuantConnect Lean Algorithmic Trading Engine: Python Builder Script
+
+# Clean output directory
+rm QuantConnect.Algorithm.Python.dll
+
+# Set the script variables: assuming installing the ./compiler/library/ and the caller is in ./compiler/bin/Debug/build.sh
+ipy="../../IronPython-2.7.5/ipy.exe"
+pyc="../../IronPython-2.7.5/Tools/Scripts/pyc.py"
+
+# Call the compiler:
+mono $ipy $pyc /target:dll /out:QuantConnect.Algorithm.Python main.py
