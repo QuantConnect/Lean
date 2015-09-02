cce257e678a7029ab00d0b3aaf50c552394468ff
snugs <mhandschuh@gmail.com>
2015-09-01 22:08:36 -0400

    Fixes div by zero bug in Stochastics.FastStoch

--- Tests/Indicators/StochasticTests.cs
+++ Tests/Indicators/StochasticTests.cs
@@ -13,8 +13,11 @@
  * limitations under the License.
 */
 
+using System;
+using System.Linq;
 using QuantConnect.Indicators;
 using NUnit.Framework;
+using QuantConnect.Data.Market;
 
 namespace QuantConnect.Tests.Indicators
 {
@@ -74,6 +77,20 @@ namespace QuantConnect.Tests.Indicators
             TestHelper.AssertIndicatorIsInDefaultState(stochastics.FastStoch);
             TestHelper.AssertIndicatorIsInDefaultState(stochastics.StochK);
             TestHelper.AssertIndicatorIsInDefaultState(stochastics.StochD);
+        }
+
+        [Test]
+        public void HandlesEqualMinAndMax()
+        {
+            var reference = new DateTime(2015, 09, 01);
+            var stochastics = new Stochastic("sto", 2, 2, 2);
+            for (int i = 0; i < 4; i++)
+            {
+                var bar = new TradeBar{Time = reference.AddSeconds(i)};
+                bar.Open = bar.Close = bar.High = bar.Low = bar.Volume = 1;
+                stochastics.Update(bar);
+                Assert.AreEqual(0m, stochastics.Current.Value);
+            }
         }
     }
 }
