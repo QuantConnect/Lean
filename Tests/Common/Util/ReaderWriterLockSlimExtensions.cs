9f07e71bcd73cdf8038308868aa1916afdb9ac8f
snugs <mhandschuh@gmail.com>
2015-10-14 12:19:51 -0400

    Adds ReaderWriterLockSlimExtensions to reduce verbosity
    Usage:
    	var slim = new ReaderWriterLockSlim();
    	using (slim.Read())
    	{
    		// perform read
    	}
    	using (slim.Write())
    	{
    		// perform write
    	}

---
+++ Tests/Common/Util/ReaderWriterLockSlimExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using QuantConnect.Util;
+
+namespace QuantConnect.Tests.Common.Util
+{
+    [TestFixture]
+    public class ReaderWriterLockSlimExtensions
+    {
+        [Test]
+        public void EntersReadLock()
+        {
+            var slim = new ReaderWriterLockSlim();
+
+            var token = slim.Read();
+
+            Assert.IsTrue(slim.IsReadLockHeld);
+            slim.ExitReadLock();
+
+            slim.Dispose();
+        }
+        [Test]
+        public void ExitsReadLock()
+        {
+            var slim = new ReaderWriterLockSlim();
+
+            var token = slim.Read();
+            token.Dispose();
+            Assert.IsFalse(slim.IsReadLockHeld);
+
+            slim.Dispose();
+        }
+
+        [Test]
+        public void EntersWriteLock()
+        {
+            var slim = new ReaderWriterLockSlim();
+
+            var token = slim.Write();
+            Assert.IsTrue(slim.IsWriteLockHeld);
+            slim.ExitWriteLock();
+
+            slim.Dispose();
+        }
+        [Test]
+        public void ExitsWriteLock()
+        {
+            var slim = new ReaderWriterLockSlim();
+
+            var token = slim.Read();
+            token.Dispose();
+            Assert.IsFalse(slim.IsReadLockHeld);
+
+            slim.Dispose();
+        }
+    }
+}
