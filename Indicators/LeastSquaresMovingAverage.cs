a4f44ada545b253c76b9554514cebc977033bcfd
Jay-Jay-D <jjdambrosio@gmail.com>
2015-10-23 13:17:26 -0300

    LSMA indicator and test added.
    Momersion small changes and fixes:
        - Name changed to MomersionIndicator
        - Conditional to avoid division by zero and estimations with few real observations in case of forward filled data added.
    MomersionTest update IPython notebook test address.

---
+++ Indicators/LeastSquaresMovingAverage.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Linq;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace QuantConnect.Indicators
+{
+    /// <summary>
+    /// The Least Squares Moving Average (LSMA) first calculates a least squares regression line
+    /// over the preceding time periods, and then projects it forward to the current period. In
+    /// essence, it calculates what the value would be if the regression line continued.
+    /// Source: https://rtmath.net/helpFinAnalysis/html/b3fab79c-f4b2-40fb-8709-fdba43cdb363.htm
+    /// </summary>
+    public class LeastSquaresMovingAverage : WindowIndicator<IndicatorDataPoint>
+    {
+        /// <summary>
+        /// Array representing the time.
+        /// </summary>
+        private readonly double[] t;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeastSquaresMovingAverage"/> class.
+        /// </summary>
+        /// <param name="name">The name of this indicator</param>
+        /// <param name="period">The number of data points to hold in the window</param>
+        public LeastSquaresMovingAverage(string name, int period)
+            : base(name, period)
+        {
+            t = Vector<double>.Build.Dense(period, i => i + 1).ToArray();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeastSquaresMovingAverage"/> class.
+        /// </summary>
+        /// <param name="period">The number of data points to hold in the window.</param>
+        public LeastSquaresMovingAverage(int period)
+            : this("LSMA" + period, period)
+        {
+        }
+
+        /// <summary>
+        /// Computes the next value of this indicator from the given state
+        /// </summary>
+        /// <param name="window"></param>
+        /// <param name="input">The input given to the indicator</param>
+        /// <returns>
+        /// A new value for this indicator
+        /// </returns>
+        protected override decimal ComputeNextValue(IReadOnlyWindow<IndicatorDataPoint> window, IndicatorDataPoint input)
+        {
+            // Until the windows is ready, the indicator returns the input value.
+            decimal output = input;
+            if (IsReady)
+            {
+                // Sort the windows by time, convert the observations ton double and transform it to a double array
+                double[] series = window
+                    .OrderBy(i => i.Time)
+                    .Select(i => Convert.ToDouble(i.Value))
+                    .ToArray<double>();
+                // Fit OLS
+                Tuple<double, double> ols = Fit.Line(x: t, y: series);
+                var alfa = (decimal)ols.Item1;
+                var beta = (decimal)ols.Item2;
+                // Make the projection.
+                output = alfa + beta * (Period);
+            }
+            return output;
+        }
+    }
+}
---
+++ Tests/Indicators/LeastSquaresMovingAverageTest.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using NUnit.Framework;
+using QuantConnect.Indicators;
+using System;
+
+namespace QuantConnect.Tests.Indicators
+{
+    /// <summary>
+    /// Result tested vs. Python available at: http://tinyurl.com/o7redso
+    /// </summary>
+    [TestFixture]
+    public class LeastSquaresMovingAverageTest
+    {
+        #region Array input
+
+        // Real AAPL minute data rounded to 2 decimals.
+        private decimal[] prices = new decimal[40]
+        {
+            125.99m, 125.91m, 125.75m, 125.62m, 125.54m, 125.45m, 125.47m,
+            125.4m , 125.43m, 125.45m, 125.42m, 125.36m, 125.23m, 125.32m,
+            125.26m, 125.31m, 125.41m, 125.5m , 125.51m, 125.41m, 125.54m,
+            125.51m, 125.61m, 125.43m, 125.42m, 125.42m, 125.46m, 125.43m,
+            125.4m , 125.35m, 125.3m , 125.28m, 125.21m, 125.37m, 125.32m,
+            125.34m, 125.37m, 125.26m, 125.28m, 125.16m
+        };
+
+        #endregion Array input
+
+        [Test]
+        public void LSMAComputesCorrectly()
+        {
+            int LSMAPeriod = 20;
+            LeastSquaresMovingAverage LSMA = new LeastSquaresMovingAverage(LSMAPeriod);
+            DateTime time = DateTime.Now;
+
+            #region Array input
+
+            decimal[] expected = new decimal[40]
+            {
+                125.99m  , 125.91m  , 125.75m  , 125.62m  , 125.54m  , 125.45m  , 125.47m  , 125.4m   , 125.43m  , 125.45m  ,
+                125.42m  , 125.36m  , 125.23m  , 125.32m  , 125.26m  , 125.31m  , 125.41m  , 125.5m   , 125.51m  , 125.41m  ,
+                125.328m , 125.381m , 125.4423m, 125.4591m, 125.4689m, 125.4713m, 125.4836m, 125.4834m, 125.4803m, 125.4703m,
+                125.4494m, 125.4206m, 125.3669m, 125.3521m, 125.3214m, 125.2986m, 125.2909m, 125.2723m, 125.2619m, 125.2224m,
+            };
+
+            #endregion Array input
+
+            decimal[] actual = new decimal[prices.Length];
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                LSMA.Update(new IndicatorDataPoint(time, prices[i]));
+                decimal LSMAValue = Math.Round(LSMA.Current.Value, 4);
+                actual[i] = LSMAValue;
+
+                //Console.WriteLine(string.Format("Bar : {0} | {1}, Is ready? {2}", i, LSMA.ToString(), LSMA.IsReady));
+                time = time.AddMinutes(1);
+            }
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ResetsProperly()
+        {
+            int LSMAPeriod = 10;
+            DateTime time = DateTime.Now;
+
+            LeastSquaresMovingAverage LSMA = new LeastSquaresMovingAverage(LSMAPeriod);
+
+            for (int i = 0; i < LSMAPeriod + 1; i++)
+            {
+                LSMA.Update(new IndicatorDataPoint(time, 1m));
+                time.AddMinutes(1);
+            }
+            Assert.IsTrue(LSMA.IsReady, "LSMA ready");
+            LSMA.Reset();
+            TestHelper.AssertIndicatorIsInDefaultState(LSMA);
+        }
+    }
+}
