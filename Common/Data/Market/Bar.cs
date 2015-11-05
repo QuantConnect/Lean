b2d96bdfa0f1b0cabaefd198f45499dc5b511e4b
snugs <mhandschuh@gmail.com>
2015-11-05 13:17:16 -0500

    Fixes bug in TradeBar.Update and QuoteBar.Update
    The issue here was if we initialized with a default instance (all zeros) then
    the Low would be forever zero.
    
    In it's new form we use an initialized flag to determine whether or not the bar
    has been seeded, and if it hasn't been seeded yet, seed OHLC with the current
    data

--- Common/Data/Market/Bar.cs
+++ Common/Data/Market/Bar.cs
@@ -13,6 +13,8 @@
  * limitations under the License.
 */
 
+using System.Threading;
+
 namespace QuantConnect.Data.Market
 {
     /// <summary>
@@ -20,35 +22,69 @@ namespace QuantConnect.Data.Market
     /// </summary>
     public class Bar : IBar
     {
+        private int _initialized;
+        private decimal _open;
+        private decimal _high;
+        private decimal _low;
+        private decimal _close;
+
         /// <summary>
         /// Opening price of the bar: Defined as the price at the start of the time period.
         /// </summary>
-        public decimal Open { get; set; }
+        public decimal Open
+        {
+            get { return _open; }
+            set
+            {
+                Initialize(value);
+                _open = value;
+            }
+        }
 
         /// <summary>
         /// High price of the bar during the time period.
         /// </summary>
-        public decimal High { get; set; }
+        public decimal High
+        {
+            get { return _high; }
+            set
+            {
+                Initialize(value); 
+                _high = value;
+            }
+        }
 
         /// <summary>
         /// Low price of the bar during the time period.
         /// </summary>
-        public decimal Low { get; set; }
+        public decimal Low
+        {
+            get { return _low; }
+            set
+            {
+                Initialize(value); 
+                _low = value;
+            }
+        }
 
         /// <summary>
         /// Closing price of the bar. Defined as the price at Start Time + TimeSpan.
         /// </summary>
-        public decimal Close { get; set; }
+        public decimal Close
+        {
+            get { return _close; }
+            set
+            {
+                Initialize(value);
+                _close = value;
+            }
+        }
 
         /// <summary>
         /// Default initializer to setup an empty bar.
         /// </summary>
         public Bar()
         {
-            Open = 0; 
-            High = 0;
-            Low = 0; 
-            Close = 0;
         }
 
         /// <summary>
@@ -64,6 +100,7 @@ namespace QuantConnect.Data.Market
             High = high;
             Low = low;
             Close = close;
+            _initialized = 1;
         }
 
         /// <summary>
@@ -72,7 +109,7 @@ namespace QuantConnect.Data.Market
         /// <param name="value">The new value</param>
         public void Update(decimal value)
         {
-            if (Open == 0) Open = value;
+            Initialize(value);
             if (value > High) High = value;
             if (value < Low) Low = value;
             Close = value;
@@ -84,6 +121,18 @@ namespace QuantConnect.Data.Market
         public Bar Clone()
         {
             return new Bar(Open, High, Low, Close);
+        }
+
+        /// <summary>
+        /// Initializes this bar with a first data point
+        /// </summary>
+        /// <param name="value">The seed value for this bar</param>
+        private void Initialize(decimal value)
+        {
+            if (Interlocked.CompareExchange(ref _initialized, 1, 0) == 0)
+            {
+                _open = _high = _low = _close = value;
+            }
         }
     }
 }
