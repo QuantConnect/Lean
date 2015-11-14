e3ee93fc10a3dd5ee8275530c04df19c9164fb48
Stefano Raggi <stefano.raggi67@alice.it>
2015-11-14 17:58:15 +0100

    LeanDataWriter now preserves existing hourly/daily data

--- ToolBox/LeanDataWriter.cs
+++ ToolBox/LeanDataWriter.cs
@@ -17,7 +17,9 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
+using Ionic.Zip;
 using QuantConnect.Data;
 using QuantConnect.Data.Market;
 using QuantConnect.Logging;
@@ -73,9 +75,31 @@ namespace QuantConnect.ToolBox
         /// <param name="source">IEnumerable source of the data: sorted from oldest to newest.</param>
         public void Write(IEnumerable<BaseData> source)
         {
+            switch (_resolution)
+            {
+                case Resolution.Daily:
+                case Resolution.Hour:
+                    WriteDailyOrHour(source);
+                    break;
+
+                case Resolution.Minute:
+                case Resolution.Second:
+                case Resolution.Tick:
+                    WriteMinuteOrSecondOrTick(source);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Write out the data in LEAN format (minute, second or tick resolutions)
+        /// </summary>
+        /// <param name="source">IEnumerable source of the data: sorted from oldest to newest.</param>
+        /// <remarks>This function overwrites existing data files</remarks>
+        private void WriteMinuteOrSecondOrTick(IEnumerable<BaseData> source)
+        {
             var sb = new StringBuilder();
             var lastTime = new DateTime();
-            
+
             // Determine file path
             var baseDirectory = Path.Combine(_dataDirectory, _securityType.ToString().ToLower(), _market);
 
@@ -86,11 +110,10 @@ namespace QuantConnect.ToolBox
                 if (data.Time < lastTime) throw new Exception("The data must be pre-sorted from oldest to newest");
 
                 // Based on the security type and resolution, write the data to the zip file
-                if (lastTime != DateTime.MinValue && data.Time.Date > lastTime.Date && 
-                    _resolution != Resolution.Hour && _resolution != Resolution.Daily)
+                if (lastTime != DateTime.MinValue && data.Time.Date > lastTime.Date)
                 {
                     // Write and clear the file contents
-                    var outputFile = ZipOutputFile(baseDirectory, lastTime);
+                    var outputFile = GetZipOutputFileName(baseDirectory, lastTime);
                     WriteFile(outputFile, sb.ToString(), lastTime);
                     sb.Clear();
                 }
@@ -104,28 +127,105 @@ namespace QuantConnect.ToolBox
             // Write the last file
             if (sb.Length > 0)
             {
-                var outputFile = ZipOutputFile(baseDirectory, lastTime);
+                var outputFile = GetZipOutputFileName(baseDirectory, lastTime);
                 WriteFile(outputFile, sb.ToString(), lastTime);
             }
         }
 
         /// <summary>
+        /// Write out the data in LEAN format (daily or hour resolutions)
+        /// </summary>
+        /// <param name="source">IEnumerable source of the data: sorted from oldest to newest.</param>
+        /// <remarks>This function performs a merge (insert/append/overwrite) with the existing Lean zip file</remarks>
+        private void WriteDailyOrHour(IEnumerable<BaseData> source)
+        {
+            var sb = new StringBuilder();
+            var lastTime = new DateTime();
+
+            // Determine file path
+            var baseDirectory = Path.Combine(_dataDirectory, _securityType.ToString().ToLower(), _market);
+
+            var outputFile = GetZipOutputFileName(baseDirectory, lastTime);
+
+            // Load new data rows into a SortedDictionary for easy merge/update
+            var newRows = new SortedDictionary<DateTime, string>(source.ToDictionary(x => x.Time, GenerateFileLine));
+            SortedDictionary<DateTime, string> rows;
+
+            if (File.Exists(outputFile))
+            {
+                // If file exists, we load existing data and perform merge
+                rows = LoadHourlyOrDailyFile(outputFile);
+                foreach (var kvp in newRows)
+                {
+                    rows[kvp.Key] = kvp.Value;
+                }
+            }
+            else
+            {
+                // No existing file, just use the new data
+                rows = newRows;
+            }
+
+            // Loop through the SortedDictionary and write to file contents
+            foreach (var kvp in rows)
+            {
+                // Build the line and append it to the file
+                sb.Append(kvp.Value + Environment.NewLine);
+            }
+
+            // Write the file contents
+            if (sb.Length > 0)
+            {
+                WriteFile(outputFile, sb.ToString(), lastTime);
+            }
+        }
+
+        /// <summary>
+        /// Loads an existing hourly or daily Lean zip file into a SortedDictionary
+        /// </summary>
+        private static SortedDictionary<DateTime, string> LoadHourlyOrDailyFile(string fileName)
+        {
+            var rows = new SortedDictionary<DateTime, string>();
+
+            using (var zip = ZipFile.Read(fileName))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    zip[0].Extract(stream);
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            var time = DateTime.ParseExact(line.Substring(0, DateFormat.TwelveCharacter.Length), DateFormat.TwelveCharacter, CultureInfo.InvariantCulture);
+                            rows.Add(time, line);
+                        }
+                    }
+                }
+            }
+
+            return rows;
+        }
+
+        /// <summary>
         /// Write this file to disk
         /// </summary>
-        private void WriteFile(string data, string filename, DateTime time)
+        private void WriteFile(string fileName, string data, DateTime time)
         {
-            filename = filename.TrimEnd();
-            if (File.Exists(data))
+            data = data.TrimEnd();
+            if (File.Exists(fileName))
             {
-                File.Delete(data);
-                Log.Trace("LeanDataWriter.Write(): Existing deleted: " + data);
+                File.Delete(fileName);
+                Log.Trace("LeanDataWriter.Write(): Existing deleted: " + fileName);
             }
             // Create the directory if it doesnt exist
-            Directory.CreateDirectory(Path.GetDirectoryName(data));
+            Directory.CreateDirectory(Path.GetDirectoryName(fileName));
 
             // Write out this data string to a zip file
-            Compression.Zip(filename, data, Compression.CreateZipEntryName(_symbol, _securityType, time, _resolution, _dataType));
-            Log.Trace("LeanDataWriter.Write(): Created: " + data);
+            Compression.Zip(data, fileName, Compression.CreateZipEntryName(_symbol, _securityType, time, _resolution, _dataType));
+            Log.Trace("LeanDataWriter.Write(): Created: " + fileName);
         }
 
         /// <summary>
@@ -152,6 +252,7 @@ namespace QuantConnect.ToolBox
                                 line = string.Format(format, milliseconds, Scale(tick.LastPrice), tick.Quantity, tick.Exchange, tick.SaleCondition, tick.Suspicious);
                             }
                             break;
+
                         case Resolution.Minute:
                         case Resolution.Second:
                             var bar = data as TradeBar;
@@ -160,6 +261,7 @@ namespace QuantConnect.ToolBox
                                 line = string.Format(format, milliseconds, Scale(bar.Open), Scale(bar.High), Scale(bar.Low), Scale(bar.Close), bar.Volume);   
                             }
                             break;
+
                         case Resolution.Hour:
                         case Resolution.Daily:
                             var bigBar = data as TradeBar;
@@ -182,6 +284,7 @@ namespace QuantConnect.ToolBox
                                 line = string.Format("{0},{1},{2}", milliseconds, fxTick.BidPrice, fxTick.AskPrice);
                             }
                             break;
+
                         case Resolution.Second:
                         case Resolution.Minute:
                             var fxBar = data as TradeBar;
@@ -190,6 +293,7 @@ namespace QuantConnect.ToolBox
                                 line = string.Format("{0},{1},{2},{3},{4}", milliseconds, fxBar.Open, fxBar.High, fxBar.Low, fxBar.Close);
                             }
                             break;
+
                         case Resolution.Hour:
                         case Resolution.Daily:
                             var dailyBar = data as TradeBar;
@@ -201,6 +305,7 @@ namespace QuantConnect.ToolBox
                     }
                     break;
             }
+
             return line;
         }
 
@@ -218,7 +323,7 @@ namespace QuantConnect.ToolBox
         /// <param name="baseDirectory">Base output directory for the zip file</param>
         /// <param name="time">Date/time for the data we're writing</param>
         /// <returns></returns>
-        private string ZipOutputFile(string baseDirectory, DateTime time)
+        private string GetZipOutputFileName(string baseDirectory, DateTime time)
         {
             string file;
 
@@ -229,11 +334,13 @@ namespace QuantConnect.ToolBox
                 case SecurityType.Forex:
                 case SecurityType.Cfd:
                     // Base directory includes the market
-                    file = Path.Combine(baseDirectory, _resolution.ToString().ToLower(), _symbol.ToLower(), Compression.CreateZipFileName(_symbol, _securityType, time, _resolution));
-
                     if (_resolution == Resolution.Daily || _resolution == Resolution.Hour)
                     {
                         file = Path.Combine(baseDirectory, _resolution.ToString().ToLower(), Compression.CreateZipFileName(_symbol, _securityType, time, _resolution));
+                    }
+                    else
+                    {
+                        file = Path.Combine(baseDirectory, _resolution.ToString().ToLower(), _symbol.ToLower(), Compression.CreateZipFileName(_symbol, _securityType, time, _resolution));
                     }
                     break;
 
