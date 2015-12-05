e08ed1c2133582b93f434fc1d42fbe5e8b489e84
snugs <mhandschuh@gmail.com>
2015-12-04 20:08:34 -0500

    Add Benchmark algorithm with baseline time

---
+++ Algorithm.CSharp/Benchmarks/EmptyMinute400EquityAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using QuantConnect.Data;
+
+namespace QuantConnect.Algorithm.CSharp.Benchmarks
+{
+    public class EmptyMinute400EquityAlgorithm : QCAlgorithm
+    {
+        public override void Initialize()
+        {
+            SetStartDate(2015, 09, 28);
+            SetEndDate(2015, 10, 28);
+            foreach (var symbol in Symbols.Equity.All.Take(400))
+            {
+                AddSecurity(SecurityType.Equity, symbol);
+            }
+        }
+
+        public override void OnData(Slice slice)
+        {
+        }
+    }
+}
