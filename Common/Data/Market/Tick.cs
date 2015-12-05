a58e91ea7f23569ce94a5e71ca99aeee4b05a513
snugs <mhandschuh@gmail.com>
2015-12-04 21:29:25 -0500

    Remove gen parm TradeBar.Parse/Add ToCsv extension

--- Common/Data/Market/Tick.cs
+++ Common/Data/Market/Tick.cs
@@ -218,27 +218,31 @@ namespace QuantConnect.Data.Market
         {
             try
             {
-                var csv = line.Split(',');
                 DataType = MarketDataType.Tick;
 
                 // Which security type is this data feed:
                 switch (config.SecurityType)
-                { 
+                {
                     case SecurityType.Equity:
+                    {
+                        var csv = line.ToCsv(6);
                         Symbol = config.Symbol;
                         Time = date.Date.AddMilliseconds(csv[0].ToInt64()).ConvertTo(config.DataTimeZone, config.ExchangeTimeZone);
                         Value = config.GetNormalizedPrice(csv[1].ToDecimal() / 10000m);
                         TickType = TickType.Trade;
                         Quantity = csv[2].ToInt32();
-                        if (csv.Length > 3)
+                        if (csv.Count > 3)
                         {
                             Exchange = csv[3];
                             SaleCondition = csv[4];
                             Suspicious = (csv[5] == "1");
                         }
                         break;
+                    }
 
                     case SecurityType.Forex:
+                    {
+                        var csv = line.ToCsv(3);
                         Symbol = config.Symbol;
                         TickType = TickType.Quote;
                         Time = date.Date.AddMilliseconds(csv[0].ToInt64()).ConvertTo(config.DataTimeZone, config.ExchangeTimeZone);
@@ -246,6 +250,7 @@ namespace QuantConnect.Data.Market
                         AskPrice = csv[2].ToDecimal();
                         Value = (BidPrice + AskPrice) / 2;
                         break;
+                    }
                 }
             }
             catch (Exception err)
