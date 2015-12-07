457496c6eb4be33fbb3048212f7370ead98b5c36
snugs <mhandschuh@gmail.com>
2015-12-07 12:40:28 -0500

    FastForwardEnumerator - prevent emitting past data
    When running a remote file in live mode with a high resolution (~second), it is possible
    for us to download the file, fast forward to the end, and then emit duplicate data.
    
    This change adds an extra filter in the FastForwardEnumerator to prevent it from emitting
    data in the past, that is, data the is earlier than the previous piece of data emitted
    
    -- forward only behavior --

--- Tests/Engine/DataFeeds/Enumerators/FastForwardEnumeratorTests.cs
+++ Tests/Engine/DataFeeds/Enumerators/FastForwardEnumeratorTests.cs
@@ -62,5 +62,27 @@ namespace QuantConnect.Tests.Engine.DataFeeds.Enumerators
             Assert.IsTrue(fastForward.MoveNext());
             Assert.AreEqual(start.AddSeconds(-1), fastForward.Current.Time);
         }
+        [Test]
+        public void FiltersOutPastData()
+        {
+            var start = new DateTime(2015, 10, 10, 13, 0, 0);
+            var data = new List<Tick>
+            {
+                new Tick {Time = start.AddMinutes(-1)},
+                new Tick {Time = start.AddSeconds(-1)},
+                new Tick {Time = start.AddSeconds(1)},
+                new Tick {Time = start.AddSeconds(0)},
+                new Tick {Time = start.AddSeconds(2)}
+            };
+
+            var timeProvider = new ManualTimeProvider(start, TimeZones.Utc);
+            var fastForward = new FastForwardEnumerator(data.GetEnumerator(), timeProvider, TimeZones.Utc, TimeSpan.FromSeconds(0.5));
+
+            Assert.IsTrue(fastForward.MoveNext());
+            Assert.AreEqual(start.AddSeconds(1), fastForward.Current.Time);
+
+            Assert.IsTrue(fastForward.MoveNext());
+            Assert.AreEqual(start.AddSeconds(2), fastForward.Current.Time);
+        }
     }
 }
