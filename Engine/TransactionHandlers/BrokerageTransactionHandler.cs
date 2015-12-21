344473b979d2b6b3da3aee23779efbdc5e945141
snugs <mhandschuh@gmail.com>
2015-12-21 16:56:40 -0500

    Update fill logs to include broker ids

--- Engine/TransactionHandlers/BrokerageTransactionHandler.cs
+++ Engine/TransactionHandlers/BrokerageTransactionHandler.cs
@@ -105,7 +105,13 @@ namespace QuantConnect.Lean.Engine.TransactionHandlers
             _brokerage.OrderStatusChanged += (sender, fill) =>
             {
                 // log every fill in live mode
-                if (algorithm.LiveMode) Log.Trace("BrokerageTransactionHandler.OrderStatusChanged(): " + fill);
+                if (algorithm.LiveMode)
+                {
+                    var brokerIds = string.Empty;
+                    var order = GetOrderById(fill.OrderId);
+                    if (order != null && order.BrokerId.Count > 0) brokerIds = string.Join(", ", order.BrokerId);
+                    Log.Trace("BrokerageTransactionHandler.OrderStatusChanged(): " + fill + " BrokerId: " + brokerIds);
+                }
 
                 HandleOrderEvent(fill);
             };
