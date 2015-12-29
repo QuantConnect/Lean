3dd6522bc3d52d7154282f034a1ada47db08c96a
QuantConnect <contact@quantconnect.com>
2015-12-29 15:07:32 -0500

    Version bump

--- Common/Properties/SharedAssemblyInfo.cs
+++ Common/Properties/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyCompany("QuantConnect")]
 [assembly: AssemblyTrademark("QuantConnect")]
-[assembly: AssemblyVersion("2.1.6.3")]
+[assembly: AssemblyVersion("2.1.6.4")]
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
 #else
