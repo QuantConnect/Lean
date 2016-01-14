7caafb00d85281b36f7f9a7d05894f6ce569bc87
Michael Handschuh <mhandschuh@gmail.com>
2016-01-14 13:51:14 -0500

    Set version v2.2.0.1

--- Common/Properties/SharedAssemblyInfo.cs
+++ Common/Properties/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyCompany("QuantConnect")]
 [assembly: AssemblyTrademark("QuantConnect")]
-[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.0.1")]
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
 #else
