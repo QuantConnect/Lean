a12e70c837118642903bdf1c803205db0f9b4d01
AlexCatarino <alexandre.catarino@gmail.com>
2016-02-12 18:29:33 -0200

    Update build.sh
    In order to account that since 30 Nov 2015 Lean executable is Lean.Launcher.exe QuantConnect.Algorithm.Python.dll must be copied into Lean.Launcher project.

--- Algorithm.Python/build.sh
+++ Algorithm.Python/build.sh
@@ -2,6 +2,7 @@
 
 # Clean output directory
 rm QuantConnect.Algorithm.Python.dll
+rm ../Launcher/bin/Debug/QuantConnect.Algorithm.Python.dll
 
 # Set the script variables: assuming installing the ./compiler/library/ and the caller is in ./compiler/bin/Debug/build.sh
 ipy="../../IronPython-2.7.5/ipy.exe"
@@ -9,3 +10,6 @@ pyc="../../IronPython-2.7.5/Tools/Scripts/pyc.py"
  
 # Call the compiler:
 mono $ipy $pyc /target:dll /out:QuantConnect.Algorithm.Python main.py
+
+# Copy to the Lean Algorithm Project
+cp QuantConnect.Algorithm.Python.dll ../Launcher/bin/Debug/QuantConnect.Algorithm.Python.dll
