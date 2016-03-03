eb236b2ed2e829e32c662671c860a943470d4343
jameschch@outlook.com <jameschch@outlook.com>
2016-03-03 21:49:41 +0000

    Quandl Bitfinex downloader

---
+++ ToolBox/QuandlBitfinexDownloader/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using QuantConnect.Configuration;
+using QuantConnect.Logging;
+
+namespace QuantConnect.ToolBox.QuandlBitfinexDownloader
+{
+    class Program
+    {
+        /// <summary>
+        /// Quandl Bitfinex Toolbox Project For LEAN Algorithmic Trading Engine.
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Downloader FROMDATE APIKEY");
+                Console.WriteLine("FROMDATE = yyyymmdd");
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                // Load settings from config.json
+                var dataDirectory = Config.Get("data-directory", "../../../Data");
+                bool useDivisor = bool.Parse(Config.Get("bitfinex-use-divisor", "false"));
+
+                // Create an instance of the downloader
+                const string market = Market.Bitcoin;
+                var downloader = new QuandlBitfinexDownloader(args[1], useDivisor);
+
+                // Download the data
+                var symbol = Symbol.Create("BTCUSD", SecurityType.Forex, market);
+                var data = downloader.Get(symbol, Resolution.Daily, DateTime.ParseExact(args[0], "yyyyMMdd", CultureInfo.CurrentCulture), DateTime.UtcNow);
+
+                // Save the data
+                var writer = new LeanDataWriter(Resolution.Daily, symbol, dataDirectory, TickType.Quote);
+                writer.Write(data);
+
+            }
+            catch (Exception err)
+            {
+                Log.Error(err);
+            }
+        }
+    }
+}
