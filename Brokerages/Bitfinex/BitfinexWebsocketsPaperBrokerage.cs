ca1e688ff4fd983d57b2b42db0551777b60478e7
jameschch@outlook.com <jameschch@outlook.com>
2016-03-23 21:27:12 +0000

    Adds Bitfinex paper brokerage

---
+++ Brokerages/Bitfinex/BitfinexWebsocketsPaperBrokerage.cs
@@ -0,0 +1,45 @@
+﻿using QuantConnect.Brokerages.Paper;
+using QuantConnect.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantConnect.Data;
+using QuantConnect.Packets;
+using QuantConnect.Securities;
+
+namespace QuantConnect.Brokerages.Bitfinex
+{
+
+    public class BitfinexWebsocketsPaperBrokerage : PaperBrokerage, IDataQueueHandler
+    {
+
+        BitfinexWebsocketsBrokerage brokerage;
+
+        public override List<Cash> GetCashBalance()
+        {
+            return brokerage.GetCashBalance();
+        }
+
+        public BitfinexWebsocketsPaperBrokerage(IAlgorithm algorithm, LiveNodePacket job) : base(algorithm, job)
+        {
+            brokerage = (BitfinexWebsocketsBrokerage)new BitfinexBrokerageFactory().CreateBrokerage(job, algorithm);
+        }
+
+        public IEnumerable<BaseData> GetNextTicks()
+        {
+            return brokerage.GetNextTicks();
+        }
+
+        public void Subscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            brokerage.Subscribe(job, symbols);
+        }
+
+        public void Unsubscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            brokerage.Unsubscribe(job, symbols);
+        }
+    }
+}
