0cf0f33a7c79289f6fc7321958c2fc321ad7817d
jameschch@outlook.com <jameschch@outlook.com>
2016-03-23 09:43:32 +0000

    Adds BitfinexBrokerage.Messaging

---
+++ Brokerages/Bitfinex/BitfinexBrokerage.Messaging.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using QuantConnect.Configuration;
+using QuantConnect.Data.Market;
+using QuantConnect.Interfaces;
+using QuantConnect.Logging;
+using QuantConnect.Orders;
+using QuantConnect.Securities;
+using QuantConnect.Util;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TradingApi.Bitfinex;
+using TradingApi.ModelObjects.Bitfinex.Json;
+
+namespace QuantConnect.Brokerages.Bitfinex
+{
+
+    /// <summary>
+    /// Bitfinex exchange REST integration.
+    /// </summary>
+    public partial class BitfinexBrokerage : Brokerage, IDataQueueHandler
+    {
+
+        /// <summary>
+        /// Get queued tick data
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Data.BaseData> GetNextTicks()
+        {
+            lock (Ticks)
+            {
+                var copy = Ticks.ToArray();
+                Ticks.Clear();
+                return copy;
+            }
+        }
+
+        /// <summary>
+        /// Begin ticker polling
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="symbols"></param>
+        public virtual void Subscribe(Packets.LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            var task = Task.Run(() => { this.RequestTicker(); }, _tickerToken.Token);
+        }
+
+        private async Task RequestTicker()
+        {
+            var response = _restClient.GetPublicTicker(TradingApi.ModelObjects.BtcInfo.PairTypeEnum.btcusd, TradingApi.ModelObjects.BtcInfo.BitfinexUnauthenicatedCallsEnum.pubticker);
+            lock (Ticks)
+            {
+                Ticks.Add(new Tick
+                {
+                    AskPrice = decimal.Parse(response.Ask) / ScaleFactor,
+                    BidPrice = decimal.Parse(response.Bid) / ScaleFactor,
+                    Time = Time.UnixTimeStampToDateTime(double.Parse(response.Timestamp)),
+                    Value = decimal.Parse(response.LastPrice) / ScaleFactor,
+                    TickType = TickType.Quote,
+                    Symbol = Symbol,
+                    DataType = MarketDataType.Tick,
+                    Quantity = (int)(Math.Round(decimal.Parse(response.Volume), 2) * ScaleFactor)
+                });
+            }
+            if (!_tickerToken.IsCancellationRequested)
+            {
+                await Task.Delay(8000, _tickerToken.Token);
+                await RequestTicker();
+            }
+        }
+
+        /// <summary>
+        /// End ticker polling
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="symbols"></param>
+        public virtual void Unsubscribe(Packets.LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            _tickerToken.Cancel();
+        }
+
+       
+
+
+    }
+
+}
