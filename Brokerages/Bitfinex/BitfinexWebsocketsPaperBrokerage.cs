26e8c05a951d250f096541aafbdd41da4a16d70f
jameschch@outlook.com <jameschch@outlook.com>
2016-03-25 19:36:31 +0000

    Comment paper brokerage

--- Brokerages/Bitfinex/BitfinexWebsocketsPaperBrokerage.cs
+++ Brokerages/Bitfinex/BitfinexWebsocketsPaperBrokerage.cs
@@ -12,31 +12,57 @@ using QuantConnect.Securities;
 namespace QuantConnect.Brokerages.Bitfinex
 {
 
+    /// <summary>
+    /// Bitfinex paper brokerage
+    /// </summary>
     public class BitfinexWebsocketsPaperBrokerage : PaperBrokerage, IDataQueueHandler
     {
 
         BitfinexWebsocketsBrokerage brokerage;
 
+        /// <summary>
+        /// Retreive cash balance
+        /// </summary>
+        /// <returns></returns>
         public override List<Cash> GetCashBalance()
         {
             return brokerage.GetCashBalance();
         }
 
+        /// <summary>
+        /// Creates instance of brokerage
+        /// </summary>
+        /// <param name="algorithm"></param>
+        /// <param name="job"></param>
         public BitfinexWebsocketsPaperBrokerage(IAlgorithm algorithm, LiveNodePacket job) : base(algorithm, job)
         {
             brokerage = (BitfinexWebsocketsBrokerage)new BitfinexBrokerageFactory().CreateBrokerage(job, algorithm);
         }
 
+        /// <summary>
+        /// Get ticks from brokerage
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<BaseData> GetNextTicks()
         {
             return brokerage.GetNextTicks();
         }
 
+        /// <summary>
+        /// Begin ticker messages
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="symbols"></param>
         public void Subscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
         {
             brokerage.Subscribe(job, symbols);
         }
 
+        /// <summary>
+        /// End ticker messages
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="symbols"></param>
         public void Unsubscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
         {
             brokerage.Unsubscribe(job, symbols);
