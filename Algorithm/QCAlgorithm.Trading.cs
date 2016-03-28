08181aa8c8dc6b4c45fde11cddc7c2778e41dd05
Michael Handschuh <mhandschuh@gmail.com>
2016-03-28 19:48:25 -0400

    Fixes CalculateOrderQuantity regression bug from 5301d81

--- Algorithm/QCAlgorithm.Trading.cs
+++ Algorithm/QCAlgorithm.Trading.cs
@@ -644,7 +644,7 @@ namespace QuantConnect.Algorithm
 
                 iterations++;
 
-            } while (orderQuantity > 0 && (marginRequired > marginRemaining || orderValue + orderFees > targetOrderValue));
+            } while (orderQuantity > 0 && (marginRequired > marginRemaining || orderValue > targetOrderValue));
 
             // add directionality back in
             return (direction == OrderDirection.Sell ? -1 : 1) * orderQuantity;

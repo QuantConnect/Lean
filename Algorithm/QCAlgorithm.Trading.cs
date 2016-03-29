QuantConnect/Lean

commit 5301d811492125d4e4de8f5f60be580cefc757b0

Fixes bug in QCAlgorithm.CalculateOrderQuantity

Algorithm/QCAlgorithm.Trading.cs
@@ -614,6 +614,8 @@ namespace QuantConnect.Algorithm
             // continue iterating while we do not have enough margin for the order
             decimal marginRequired;
             var orderQuantity = 0;
+            decimal orderValue;
+            decimal orderFees;
 
             do
             {
@@ -628,11 +630,15 @@ namespace QuantConnect.Algorithm
                     orderQuantity--;
                 }
 
-                // calculate the margin required for the order
+                // generate the order
                 var order = new MarketOrder(security.Symbol, orderQuantity, UtcTime);
+                orderValue = order.GetValue(security);
+                orderFees = security.FeeModel.GetOrderFee(security, order);
+
+                // calculate the margin required for the order
                 marginRequired = security.MarginModel.GetInitialMarginRequiredForOrder(security, order);
 
-            } while (marginRequired > marginRemaining || marginRequired > targetOrderValue);
+            } while (orderQuantity > 0 && (marginRequired > marginRemaining || orderValue + orderFees > targetOrderValue));
 
             // add directionality back in
             return (direction == OrderDirection.Sell ? -1 : 1) * orderQuantity;

commit 08181aa8c8dc6b4c45fde11cddc7c2778e41dd05

Fixes CalculateOrderQuantity regression bug from 5301d81

Algorithm/QCAlgorithm.Trading.cs
@@ -644,7 +644,7 @@ namespace QuantConnect.Algorithm
 
                 iterations++;
 
-            } while (orderQuantity > 0 && (marginRequired > marginRemaining || orderValue + orderFees > targetOrderValue));
+            } while (orderQuantity > 0 && (marginRequired > marginRemaining || orderValue > targetOrderValue));
 
             // add directionality back in
             return (direction == OrderDirection.Sell ? -1 : 1) * orderQuantity;