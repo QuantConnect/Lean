647c198a1fe317322dd282c5fab57ad1af552f7f
Francis Gauthier <francis.gauthier.2@gmail.com>
2016-03-28 20:41:26 -0400

    Added customCommand and notify command

---
+++ Common/Commands/CustomCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantConnect.Interfaces;
+using QuantConnect.Packets;
+
+namespace QuantConnect.Commands
+{
+    public sealed class CustomCommand : ICommand
+    {
+        public CommandResultPacket Run(IAlgorithm algorithm)
+        {
+            Console.WriteLine("Toto");
+            return new CommandResultPacket(this,true);
+        }
+    }
+}
