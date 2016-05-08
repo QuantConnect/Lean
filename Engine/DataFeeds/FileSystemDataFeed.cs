855b1214d12ea1584db5b8bc779abf8fe5e52b5b
Stefano Raggi <stefano.raggi67@alice.it>
2016-05-08 23:23:26 +0200

    FileSystemDataFeed.ScheduleEnumerator performance fix

--- Engine/DataFeeds/FileSystemDataFeed.cs
+++ Engine/DataFeeds/FileSystemDataFeed.cs
@@ -149,7 +149,7 @@ namespace QuantConnect.Lean.Engine.DataFeeds
             // schedule the work on the controller
             var firstLoop = true;
             FuncParallelRunnerWorkItem workItem = null;
-            workItem = new FuncParallelRunnerWorkItem(() => enqueueable.Count < lowerThreshold, () =>
+            workItem = new FuncParallelRunnerWorkItem(() => true, () =>
             {
                 var count = 0;
                 while (enumerator.MoveNext())
