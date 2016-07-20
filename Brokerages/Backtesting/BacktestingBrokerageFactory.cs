3cc8928a0a09631814aa17163b498520cfaaa9ed
devalkeralia <deval.93@gmail.com>
2016-07-20 19:31:14 -0400

    removed the IResultHandler from the constructor for DefaultBrokerageMessageHandler

--- Brokerages/Backtesting/BacktestingBrokerageFactory.cs
+++ Brokerages/Backtesting/BacktestingBrokerageFactory.cs
@@ -13,7 +13,6 @@
  * limitations under the License.
 */
 
-using System;
 using System.Collections.Generic;
 using QuantConnect.Interfaces;
 using QuantConnect.Packets;
--- Engine/Engine.cs
+++ Engine/Engine.cs
@@ -131,7 +131,7 @@ namespace QuantConnect.Lean.Engine
                     algorithm.HistoryProvider = _algorithmHandlers.HistoryProvider;
 
                     // initialize the default brokerage message handler
-                    algorithm.BrokerageMessageHandler = new DefaultBrokerageMessageHandler(algorithm, job, _algorithmHandlers.Results, _systemHandlers.Api);
+                    algorithm.BrokerageMessageHandler = new DefaultBrokerageMessageHandler(algorithm, job, _systemHandlers.Api);
 
                     //Initialize the internal state of algorithm and job: executes the algorithm.Initialize() method.
                     initializeComplete = _algorithmHandlers.Setup.Setup(algorithm, brokerage, job, _algorithmHandlers.Results, _algorithmHandlers.Transactions, _algorithmHandlers.RealTime);
