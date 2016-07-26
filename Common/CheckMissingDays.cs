65d37f1df5a48156b7f27ba77f87e82b67af830d
Richard Curteis <richa_000@ULTRAHP-PC>
2016-07-26 22:39:22 +0100

    Pulling

---
+++ Common/CheckMissingDays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantConnect;
+
+namespace QuantConnect
+{
+    class CheckMissingDays
+    {
+        private string _localFilePath;
+
+        public CheckMissingDays()
+        {
+            _localFilePath = @"C:\Users\richa_000\Desktop\exporter\forex\fxcm\minute";
+
+        }
+
+
+    }
+}
