7b533001d86609b61dbc8a9ab25030758488b72c
Andrew Hart <andrewthart098@gmail.com>
2016-08-31 12:35:17 -0400

    LeanWinForm.cs now implements IMessageHandler
    The WinForm application now implements the interface that allows the DesktopClient to pass messages to it. This replaces the delegate methods that were being used before.
    
    The Main method in the Views project now starts the NetMQ pull socket (server) in another thread.  This is so the server does no block the rest of the WinForms application.  The server is then shut down once the application is finished running.

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using QuantConnect.Views.WinForms;
 
@@ -14,7 +10,27 @@ namespace QuantConnect.Views
         [STAThread]
         static void Main(string[] args)
         {
-        
+            if (args.Length != 1)
+            {
+                throw new Exception(
+                    "Error: You must specify the port on which the application will open a TCP socket.");
+            }
+
+            var port = args[0];
+
+            var form = new LeanWinForm();
+
+            var desktopClient = new DesktopClient();
+
+            var thread = new Thread(() => desktopClient.Run(port, form));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+            Application.Run(form);
+
+            // The above code is blocking.
+            // Once it finishes, close the NetMQ client
+            desktopClient.StopServer();
         }
     }
 }
