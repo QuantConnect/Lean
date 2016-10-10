b58f3819ffed01e3e26ea79df7f5872a107126e2
Andrew <andrewthart098@gmail.com>
2016-10-10 09:33:18 -0400

    Dividends are now applied before splits

--- Tests/Common/Util/FactorFileGeneratorTests.cs
+++ Tests/Common/Util/FactorFileGeneratorTests.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using QuantConnect.Configuration;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
 using QuantConnect.ToolBox;
 using QuantConnect.ToolBox.YahooDownloader;
 using QuantConnect.Util;
@@ -27,7 +30,7 @@ namespace QuantConnect.Tests.Common.Util
             Assert.IsTrue(yahooEvents.Any());
         }
 
-       
+
         [Test]
         public void DailyEquityData_CanBeRead_Successfully()
         {
@@ -43,9 +46,29 @@ namespace QuantConnect.Tests.Common.Util
             var yahooDataDownloader = new YahooDataDownloader();
 
             var yahooEvents = yahooDataDownloader.DownloadSplitAndDividendData(symbol, DateTime.Parse("01/01/1980"), DateTime.MaxValue);
-            var factorFile  = factorFileGenerator.CreateFactorFileFromData(yahooEvents);
+            var factorFile = factorFileGenerator.CreateFactorFileFromData(yahooEvents);
 
             Assert.IsTrue(factorFile.Permtick == symbol.ID.Symbol);
+        }
+
+        [Test]
+        public void SameDaySplitsAndDividends_AreRearranged_Successfully()
+        {
+            var factorFileGenerator = new FactorFileGenerator(symbol, dataPath);
+            var date = DateTime.Parse("1/11/2010");
+            var marketEventsList = new List<BaseData>()
+            {
+                new Split(symbol, date, 1.0M, 1.0M),
+                new Dividend(symbol, date, 3.0M),
+            };
+
+            var marketEventsQueue = new Queue<BaseData>(marketEventsList);
+
+            var factorFile = factorFileGenerator.CreateFactorFileFromData(marketEventsQueue);
+
+            // There should be 4, the first one in the future, the two added and one for the last date of the data
+            Assert.IsTrue(factorFile.SortedFactorFileData.Count == 4);
+            //Assert.IsTrue(factorFile.SortedFactorFileData. == typeof(Split));
         }
     }
 }
--- ToolBox/FactorFileGenerator.cs
+++ ToolBox/FactorFileGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
 using System.IO.Compression;
@@ -25,7 +26,7 @@ namespace QuantConnect.ToolBox
         public List<TradeBar> DailyDataForEquity { get; private set; }
 
         private readonly DateTime _lastDateFromEquityData;
-        
+
         /// <summary>
         /// Constructor
         /// </summary>
@@ -45,6 +46,9 @@ namespace QuantConnect.ToolBox
         /// <returns>A factor file object</returns>
         public FactorFile CreateFactorFileFromData(Queue<BaseData> baseData)
         {
+            // If there are splits and dividends on the same day, apply the dividends first
+            baseData = ArrangeSameDayDividendsBeforeSplits(baseData);
+
             var factorFileRows = new List<FactorFileRow>()
             {
                 // First Factor Row is set far into the future
@@ -56,6 +60,46 @@ namespace QuantConnect.ToolBox
             };
 
             return RecursivlyGenerateFactorFile(baseData, factorFileRows);
+        }
+
+        private Queue<BaseData> ArrangeSameDayDividendsBeforeSplits(Queue<BaseData> marketEventQueue)
+        {
+            var marketEventList = new ObservableCollection<BaseData>(marketEventQueue.ToList());
+
+            foreach (var marketEvent in marketEventList)
+            {
+                if (marketEvent.GetType() != typeof(Split)) continue;
+
+                var indexOfSplit = marketEventList.IndexOf(marketEvent);
+
+                Dividend sameDayDividend = GetSameDayDividendEvent(indexOfSplit, marketEventList);
+
+                // If a dividend on the same day was found, make sure the split has a later date
+                // the FactorFileConstructor will arrange them according to their date
+                if (sameDayDividend != null)
+                {
+                    marketEventList[indexOfSplit].Time = sameDayDividend.Time.AddSeconds(1);
+                }
+            }
+
+            return new Queue<BaseData>(marketEventList);
+        }
+
+        private Dividend GetSameDayDividendEvent(int indexOfSplit, ObservableCollection<BaseData> marketEventList)
+        {
+            // if it's the last item in the collection, return null
+            if (indexOfSplit == marketEventList.Count - 1)
+                return null;
+
+            // if the next item is a dividend and it's on the same day, return it
+            var nextEvent = marketEventList[indexOfSplit + 1];
+
+            if (nextEvent.GetType() == typeof(Dividend))
+                if (nextEvent.Time.Date == marketEventList[indexOfSplit].Time.Date)
+                    return (Dividend)nextEvent;
+
+            // if we made it this far, no dividend was found on the same day
+            return null;
         }
 
         /// <summary>
@@ -72,7 +116,7 @@ namespace QuantConnect.ToolBox
                 factorFileRows.Add(CreateLastFactorFileRow(factorFileRows));
                 return new FactorFile(Symbol.ID.Symbol, factorFileRows);
             }
-                
+
             var nextEvent = BaseDatas.Dequeue();
 
             // If there is no more daily equity data to use return
@@ -91,15 +135,15 @@ namespace QuantConnect.ToolBox
         }
 
         /// <summary>
-        /// Creates the first row in the factor file. 
+        /// Creates the first row in the factor file. The is the last row generated
         /// Represents the earliest date that the daily equity data contains.
         /// </summary>
         /// <param name="factorFileRows">The list of factor file rows</param>
         /// <returns>FactorFileRow</returns>
         private FactorFileRow CreateLastFactorFileRow(List<FactorFileRow> factorFileRows)
         {
-            return new FactorFileRow(DailyDataForEquity.Last().Time, 
-                                     factorFileRows.Last().PriceFactor, 
+            return new FactorFileRow(DailyDataForEquity.Last().Time,
+                                     factorFileRows.Last().PriceFactor,
                                      factorFileRows.Last().SplitFactor);
         }
 
@@ -127,13 +171,15 @@ namespace QuantConnect.ToolBox
         /// <returns></returns>
         private FactorFileRow CalculateNextDividendFactor(BaseData nextEvent, FactorFileRow lastFactorFileRow)
         {
-            var evenDayData = DailyDataForEquity.FirstOrDefault(x => x.Time.Date == nextEvent.Time);
+            var evenDayData = DailyDataForEquity.FirstOrDefault(x => x.Time.Day == nextEvent.Time.Day 
+                                                                      && x.Time.Month == nextEvent.Time.Month
+                                                                      && x.Time.Year == nextEvent.Time.Year);
 
             // If you don't have the equity data nothing can be done
             if (evenDayData == null)
                 return null;
 
-            TradeBar previousClosingPrice = FindNextPreviousClosingPrice(evenDayData.Time);
+            TradeBar previousClosingPrice = FindPreviousClosingPrice(evenDayData.Time);
 
             var priceFactor = lastFactorFileRow.PriceFactor - (nextEvent.Value / ((previousClosingPrice.Close / 10000) * lastFactorFileRow.SplitFactor));
 
@@ -160,7 +206,7 @@ namespace QuantConnect.ToolBox
         /// </summary>
         /// <param name="date">Date that is currently being processed</param>
         /// <returns>A tradebar representing that days data</returns>
-        private TradeBar FindNextPreviousClosingPrice(DateTime date)
+        private TradeBar FindPreviousClosingPrice(DateTime date)
         {
             TradeBar previousDayData = null;
 
@@ -185,7 +231,7 @@ namespace QuantConnect.ToolBox
             string[] lines;
 
             if (path.Contains(".zip"))
-                lines = ReadZipFile(path); 
+                lines = ReadZipFile(path);
             else
                 lines = File.ReadAllLines(path);
 
