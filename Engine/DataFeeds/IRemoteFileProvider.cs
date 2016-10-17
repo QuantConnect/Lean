357243a6c32c0ea5b191b6803fd841bf7310bdba
Andrew <andrewthart098@gmail.com>
2016-10-17 11:43:33 -0400

    Added IRemoteFileProviderInterface.cs
    This interface attempts to download and save data from a remote source.

---
+++ Engine/DataFeeds/IRemoteFileProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuantConnect.Lean.Engine.DataFeeds
+{
+    /// <summary>
+    /// Fetches a remote file for a security using a Symbol, Resolution and DateTime
+    /// Used to get remote files when they are not found locally
+    /// </summary>
+    public interface IRemoteFileProvider
+    {
+        /// <summary>
+        /// Gets and downloads the remote file
+        /// </summary>
+        /// <param name="symbol">Symbol of the security</param>
+        /// <param name="resolution">Resolution of the data requested</param>
+        /// <param name="date">DateTime of the data requested</param>
+        /// <returns>Bool indicating whether the remote file was fetched correctly</returns>
+        bool Fetch(Symbol symbol, Resolution resolution, DateTime date);
+    }
+}
