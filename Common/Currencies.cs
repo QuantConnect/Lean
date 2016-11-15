efc60aaa221aaff305cf7d373f64ab8996712452
Stefano Raggi <stefano.raggi67@alice.it>
2016-11-16 00:39:43 +0100

    Added GetCurrencySymbol to handle unknown currencies
    Also added missing CNH symbol and USDCNH pair

--- Common/Currencies.cs
+++ Common/Currencies.cs
@@ -204,6 +204,7 @@ namespace QuantConnect
             "TRYJPY",
             "TWDJPY",
             "USDAUD",
+            "USDCNH",
             "USDCNY",
             "USDCZK",
             "USDDKK",
@@ -242,6 +243,7 @@ namespace QuantConnect
             {"HKD", "$"},
             {"SGD", "$"},
             {"XAG", "Ag"},
+            {"CNH", "¥"},
             {"CNY", "¥"},
             {"CZK", "Kč"},
             {"DKK", "kr"},
@@ -257,5 +259,16 @@ namespace QuantConnect
             {"TWD", "NT$"},
             {"ZAR", "R"}
         };
+
+        /// <summary>
+        /// Gets the currency symbol for the specified currency code
+        /// </summary>
+        /// <param name="currency">The currency code</param>
+        /// <returns>The currency symbol</returns>
+        public static string GetCurrencySymbol(string currency)
+        {
+            string currencySymbol;
+            return CurrencySymbols.TryGetValue(currency, out currencySymbol) ? currencySymbol : "$";
+        }
     }
 }
