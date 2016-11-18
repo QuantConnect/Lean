6397ee858aa02cdff679d1dea2f87e7c581fae02
Andrew Hart <andrewthart098@gmail.com>
2016-11-18 17:58:21 -0500

    Added tests to prove data feeds can be configured in config.json

--- Tests/Algorithm/AlgorithmAddDataTests.cs
+++ Tests/Algorithm/AlgorithmAddDataTests.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using QuantConnect.Algorithm;
 using QuantConnect.Configuration;
 using QuantConnect.Data;
 using QuantConnect.Data.Auxiliary;
 using QuantConnect.Data.Market;
+using QuantConnect.Lean.Engine.Setup;
 using QuantConnect.Securities;
 
 namespace QuantConnect.Tests.Algorithm
@@ -54,6 +56,34 @@ namespace QuantConnect.Tests.Algorithm
             var future = algo.AddSecurity(SecurityType.Future, "ES");
             Assert.IsTrue(future.Subscriptions.Count() == 2);
             Assert.IsTrue(future.Subscriptions.FirstOrDefault(x => typeof(ZipEntryName).IsAssignableFrom(x.Type)) != null);
+        }
+
+        [Test]
+        public void DefaultDataFeeds_CanBeOverwritten_Successfully()
+        {
+            Config.Set("security-data-feeds", "{ Forex: [\"Quote\"] }");
+            var algo = new QCAlgorithm();
+
+            // forex defult - should be tradebar
+            var forexTrade = algo.AddSecurity(SecurityType.Forex, "eurusd");
+            Assert.IsTrue(forexTrade.Subscriptions.Count() == 1);
+            Assert.IsTrue(GetMatchingSubscription(forexTrade, typeof(TradeBar)) != null);
+
+            // Change 
+            var dataFeedsConfigString = Config.Get("security-data-feeds");
+            Dictionary<SecurityType, List<TickType>> dataFeeds = new Dictionary<SecurityType, List<TickType>>();
+            if (dataFeedsConfigString != string.Empty)
+            {
+                dataFeeds = JsonConvert.DeserializeObject<Dictionary<SecurityType, List<TickType>>>(dataFeedsConfigString);
+            }
+
+            algo.SetAvailableDataFeeds(dataFeeds);
+
+            // new forex - should be quotebar
+            var forexQuote = algo.AddForex("eurusd");
+            Assert.IsTrue(forexQuote.Subscriptions.Count() == 1);
+            Assert.IsTrue(GetMatchingSubscription(forexQuote, typeof(QuoteBar)) != null);
+
         }
     }
 }
