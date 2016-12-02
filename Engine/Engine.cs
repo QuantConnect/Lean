c3e1b1ec8f5d53ebfcbbaf92a5ff773d87840e6d
quant1729 <arcturius@gmail.com>
2016-12-02 12:30:38 +0100

    Second debug commit. Code will be removed later.

--- Engine/Engine.cs
+++ Engine/Engine.cs
@@ -29,6 +29,8 @@ using QuantConnect.Packets;
 using QuantConnect.Securities;
 using QuantConnect.Statistics;
 using QuantConnect.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace QuantConnect.Lean.Engine 
 {
@@ -97,6 +99,15 @@ namespace QuantConnect.Lean.Engine
                 Thread threadResults = null;
                 Thread threadRealTime = null;
 
+
+                var json = JsonConvert.SerializeObject(job);
+                var jObj = JObject.Parse(json);
+                jObj.Descendants().OfType<JProperty>()
+                                  .Where(p => p.Name == "oAlgorithm")
+                                  .ToList()
+                                  .ForEach(att => att.Remove());
+                var newJson = jObj.ToString();
+                throw new Exception("DEBUG: " + newJson);
 
                 //-> Initialize messaging system
                 _systemHandlers.Notify.SetAuthentication(job);
