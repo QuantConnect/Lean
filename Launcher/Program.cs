89f7403e4def62d17140498efc6afdb2ec04dc63
jaredbroad <jaredbroad@gmail.com>
2017-02-02 10:33:14 -0500

    Temporary fix while debug Launcher failing to close down cleanly

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -129,6 +129,7 @@ namespace QuantConnect.Lean.Launcher
                 leanEngineSystemHandlers.Dispose();
                 leanEngineAlgorithmHandlers.Dispose();
                 Log.LogHandler.Dispose();
+                Environment.Exit(0);
             }
         }
     }
