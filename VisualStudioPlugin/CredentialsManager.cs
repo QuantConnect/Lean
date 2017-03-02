fa525061499aa78d27fb07f2a5152c3bd9ca53ef
Ivan Mushketyk <ivan.mushketik@gmail.com>
2017-03-02 14:35:01 -0800

    Store latest password to local credential storage

---
+++ VisualStudioPlugin/CredentialsManager.cs
@@ -0,0 +1,38 @@
+﻿using CredentialManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantConnect.VisualStudioPlugin
+{
+    class CredentialsManager
+    {
+        private const string CREDENTIAL_TARGET = "QuantConnectPlugin";
+
+        public Credentials? GetLastCredential()
+        {
+            var cm = new Credential { Target = CREDENTIAL_TARGET };
+            if (!cm.Load())
+            {
+                return null;
+            }
+
+            return new Credentials(cm.Username, cm.Password);
+        }
+
+        public void SetCredentials(Credentials credentials)
+        {
+            var credential = new Credential
+            {
+                Target = CREDENTIAL_TARGET,
+                Username = credentials.UserId,
+                Password = credentials.AccessToken,
+                PersistanceType = PersistanceType.LocalComputer
+            };
+            
+            credential.Save();
+        }
+    }
+}
