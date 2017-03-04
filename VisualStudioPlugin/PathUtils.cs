076391ed512ca3b06d5d75d468248f8f795194aa
Ivan Mushketyk <ivan.mushketik@gmail.com>
2017-03-04 07:05:06 -0800

    Add PathUtils.cs

---
+++ VisualStudioPlugin/PathUtils.cs
@@ -0,0 +1,29 @@
+﻿using EnvDTE80;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantConnect.VisualStudioPlugin
+{
+    /// <summary>
+    /// Collections of util methods to work with directories
+    /// </summary>
+    class PathUtils
+    {
+
+        private PathUtils() { }
+
+        public static string GetDataFolder(string solutionFolder)
+        {
+            return Path.Combine(solutionFolder, "Data");
+        }
+
+        public static string GetSolutionFolder(DTE2 dte2)
+        {
+            return Path.GetDirectoryName(dte2.Solution.FullName);
+        }
+    }
+}
