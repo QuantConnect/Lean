a62fab742ce6e89c4f3a894a5c7a1f282e812a02
Stefano Raggi <stefano.raggi67@alice.it>
2017-04-07 15:40:24 +0200

    Fix SetBrokerageModel when called after AddSecurity and friends
    Currently, calling SetBrokerageModel after AddSecurity, AddForex, etc. has no effect, the security initializer has already been initialized and its models have been set to their default implementations.
    
    For example, Forex backtests using OandaBrokerageModel will report fees calculated with the default fee model (Oanda fees are spread-based, so they should always be reported as zero).
    
    In this PR, SetBrokerageModel now calls SecurityInitializer.Initialize on all securities added before SetBrokerageModel is called.
    
    Fees will be calculated using the correct fee models and the order of the calls in algorithm Initialize is now irrelevant.

--- Algorithm.CSharp/CustomSecurityInitializerAlgorithm.cs
+++ Algorithm.CSharp/CustomSecurityInitializerAlgorithm.cs
@@ -62,6 +62,7 @@ namespace QuantConnect.Algorithm.CSharp
             /// with the specified normalization mode
             /// </summary>
             /// <param name="brokerageModel">The brokerage model used to get fill/fee/slippage/settlement models</param>
+            /// <param name="securitySeeder">The security seeder to be used</param>
             /// <param name="dataNormalizationMode">The desired data normalization mode</param>
             public CustomSecurityInitializer(IBrokerageModel brokerageModel, ISecuritySeeder securitySeeder, DataNormalizationMode dataNormalizationMode)
                 : base(brokerageModel, securitySeeder)
@@ -73,10 +74,11 @@ namespace QuantConnect.Algorithm.CSharp
             /// Initializes the specified security by setting up the models
             /// </summary>
             /// <param name="security">The security to be initialized</param>
-            public override void Initialize(Security security)
+            /// <param name="seedSecurity">True to seed the security, false otherwise</param>
+            public override void Initialize(Security security, bool seedSecurity)
             {
                 // first call the default implementation
-                base.Initialize(security);
+                base.Initialize(security, seedSecurity);
 
                 // now apply our data normalization mode
                 security.SetDataNormalizationMode(_dataNormalizationMode);
