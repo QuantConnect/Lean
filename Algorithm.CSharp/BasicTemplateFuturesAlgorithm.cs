b574707e23562dc96dbce14cc6785817fd19aad0
Andrew <andrewthart098@gmail.com>
2017-04-26 12:41:40 -0400

    Changed start and end dates of the BasicTemplateFuturesAlgorithm.cs

--- Algorithm.CSharp/BasicTemplateFuturesAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateFuturesAlgorithm.cs
@@ -40,8 +40,8 @@ namespace QuantConnect.Algorithm.CSharp
 
         public override void Initialize()
         {
-            SetStartDate(2016, 08, 17);
-            SetEndDate(2016, 08, 20);
+            SetStartDate(2013, 10, 07);
+            SetEndDate(2013, 10, 11);
             SetCash(1000000);
 
             var futureSP500 = AddFuture(RootSP500);
