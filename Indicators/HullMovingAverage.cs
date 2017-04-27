c37282d522bc7f2f65f5722d03cb43decdcba98c
jjd <jjdambrosio@gmail.com>
2017-04-27 20:52:27 -0300

    HullMovingAverage, tests and helper method implemented

--- Indicators/HullMovingAverage.cs
+++ Indicators/HullMovingAverage.cs
@@ -21,79 +21,66 @@ namespace QuantConnect.Indicators
     /// Produces a Hull Moving Average as explained at http://www.alanhull.com/hull-moving-average/
     /// and derived from the instructions for the Excel VBA code at http://finance4traders.blogspot.com/2009/06/how-to-calculate-hull-moving-average.html
     /// </summary>
-    public class HullMovingAverage : WindowIndicator<IndicatorDataPoint>
+    public class HullMovingAverage : IndicatorBase<IndicatorDataPoint>
     {
-        private readonly LinearWeightedMovingAverage _longWma;
-        private readonly LinearWeightedMovingAverage _shortWma;
-        private readonly RollingWindow<IndicatorDataPoint> _smooth;
-        private readonly LinearWeightedMovingAverage _result;
-        // The length of the smoothed window
-        // square root of period rounded to the nearest whole number 
+        private readonly LinearWeightedMovingAverage _fastWma;
+        private readonly LinearWeightedMovingAverage _slowWma;
+        private readonly LinearWeightedMovingAverage _hullMa;
 
         /// <summary>
         /// A Hull Moving Average 
         /// </summary>
         /// <param name="name">string - a name for the indicator</param>
-        /// <param name="period">int - the number of periods over which to calculate the HMA - the length of the longWMA</param>
+        /// <param name="period">int - the number of periods to calculate the HMA - the period of the slower LWMA</param>
         public HullMovingAverage(string name, int period)
-            : base(name, period)
+            : base(name)
         {
-            // Creates the long LWMA for the number of periods specified in the constuctor
-            _longWma = new LinearWeightedMovingAverage("Long", period);
-
-            // Creates the short LWMA for half the period rounded to the nearest whole number
-            _shortWma = new LinearWeightedMovingAverage("Short", period / 2);
-
-            // Creates the smoother data set to which the resulting wma is applied
-            _smooth = new RollingWindow<IndicatorDataPoint>(period);
-
-            // number of historical periods to look at in the resulting WMA
-            int k = System.Convert.ToInt32(Math.Sqrt(period));
-
-            // Creates the LWMA for the output. This step probably could have been skipped
-            
-            _result = new LinearWeightedMovingAverage("Result", k);
+            if (period < 2) throw new ArgumentException("The Hull Moving Average period should be greater or equal to 2", "period");
+            _slowWma = new LinearWeightedMovingAverage(period);
+            _fastWma = new LinearWeightedMovingAverage((int) Math.Round(period * 1d / 2));
+            var k = (int)Math.Round(Math.Sqrt(period));
+            _hullMa = new LinearWeightedMovingAverage(k);
 
         }
         /// <summary>
-        /// A Hull Moving Average with the default name
+        /// A Hull Moving Average.
         /// </summary>
-        /// <param name="period">int - the number of periods over which to calculate the HMA - the length of the longWMA</param>
+        /// <param name="period">int - the number of periods over which to calculate the HMA - the length of the slower LWMA</param>
         public HullMovingAverage(int period)
             : this("HMA" + period, period)
         {
         }
 
-        /// <summary>
-        ///     Gets a flag indicating when this indicator is ready and fully initialized
-        /// </summary>
-        public override bool IsReady
+
+        public override void Reset()
         {
-            get { return _smooth.IsReady; }
+            base.Reset();
+            _slowWma.Reset();
+            _fastWma.Reset();
+            _hullMa.Reset();
         }
 
         /// <summary>
-        ///     Computes the next value for this indicator from the given state.
+        /// Gets a flag indicating when this indicator is ready and fully initialized
         /// </summary>
-        /// <param name="window">The window of data held in this indicator</param>
-        /// <param name="input">The input value to this indicator on this time step</param>        /// <returns></returns>
-        /// <remarks>
-        /// The Hull moving average is a series of nested weighted moving averages. 
-        /// Using the LWMA custom function for calculating weighted moving averages, 
-        /// the Hull moving average can be calculated following the steps. 
-        ///
-        ///1.Calculate the n periodweighted moving average of a series "=WMA(price for n periods)"
-        ///2.Calculate the n/2 period weighted moving average of a series"=WMA(price for n/2 periods)". Round n/2 to the nearest whole number
-        ///3.Create a time series with 2*WMA from Step 2 - WMA from Step 1
-        ///4.The HMA is the WMA of the series in Step 3. "=WMA(Step 3 outputs fo k period)"
-        /// </remarks>
-        protected override decimal ComputeNextValue(IReadOnlyWindow<IndicatorDataPoint> window, IndicatorDataPoint input)
+        public override bool IsReady
         {
-            _longWma.Update(input);
-            _shortWma.Update(input);
-            _smooth.Add(new IndicatorDataPoint(input.Time, 2 * _shortWma.Current.Value - _longWma.Current.Value));
-            _result.Update(new IndicatorDataPoint(input.Time, _smooth[0].Value));
-            return _result.Current.Value;
+            get { return _hullMa.IsReady; }
+        }
+
+        /// <summary>
+        /// Computes the next value of this indicator from the given state
+        /// </summary>
+        /// <param name="input">The input given to the indicator</param>
+        /// <returns>
+        /// A new value for this indicator
+        /// </returns>
+        protected override decimal ComputeNextValue(IndicatorDataPoint input)
+        {
+            _fastWma.Update(input);
+            _slowWma.Update(input);
+            _hullMa.Update(new IndicatorDataPoint(input.Time, 2 * _fastWma.Current.Value - _slowWma.Current.Value));
+            return _hullMa.Current.Value;
         }
     }
 }
