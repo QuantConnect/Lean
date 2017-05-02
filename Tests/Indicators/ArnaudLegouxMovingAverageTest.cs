68ddbbc84f7504d353082c26d9ecdd10259fe3ce
jjd <jjdambrosio@gmail.com>
2017-05-02 10:42:01 -0300

    ALMA and test implemented.

---
+++ Tests/Indicators/ArnaudLegouxMovingAverageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using QuantConnect.Indicators;
+
+namespace QuantConnect.Tests.Indicators
+{
+    [TestFixture]
+    public class ArnaudLegouxMovingAverageTest : CommonIndicatorTests<IndicatorDataPoint>
+    {
+        protected override IndicatorBase<IndicatorDataPoint> CreateIndicator()
+        {
+            return new ArnaudLegouxMovingAverage(9, 6);
+        }
+
+        protected override string TestFileName
+        {
+            get { return "spy_alma.txt"; }
+        }
+
+        protected override string TestColumnName
+        {
+            get { return "ALMA"; }
+        }
+    }
+}
