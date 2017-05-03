d2a10ef69a2d3482979e514c3856cab22dfafa25
Anshul <Anshul@DESKTOP-LA0HV4K>
2017-05-03 19:05:22 +0530

    Code refactor, create test for individual symbols

--- Tests/Common/Securities/Futures/FuturesExpiryFunctionsTests.cs
+++ Tests/Common/Securities/Futures/FuturesExpiryFunctionsTests.cs
@@ -42,6 +42,7 @@ namespace QuantConnect.Tests.Common.Securities.Futures
             }
         }
 
+        /*
         /// <summary>
         /// Symbol and list of dates for testing Futures
         /// </summary>
@@ -50,25 +51,103 @@ namespace QuantConnect.Tests.Common.Securities.Futures
             public String symbol;
             public List<Dates> dateList;
             public SymbolData() { }
-            public SymbolData(String symbol, List<Dates> list)
+            public SymbolData(String symbol, List<Dates> dateList)
             {
                 this.symbol = symbol;
-                this.dateList = list;
+                this.dateList = dateList;
+            }
+        } */
+
+        /// <summary>
+        /// Class to convert Array into Dictionary using XmlSerializer
+        /// </summary>
+        public class Item
+        {
+            [XmlAttribute]
+            public String symbol;
+            public List<Dates> symbolDates;
+        }
+
+        private IDictionary<String, List<Dates>> data = new Dictionary<String, List<Dates>>();
+        private const string zero = "00:00:00";
+        private const string nineSixteenCentralTime = "14:16:00";
+        private const string nineThirtyEasternTime = "13:30:00";
+        private const string twelveOclock = "12:00:00";
+        private const string twelveOne = "12:01:00";
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            var testsDirectory = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString());
+            var path = Path.Combine("TestData", "FuturesExpiryFunctionsTestData.xml");
+            path = Path.Combine(testsDirectory.ToString(), path);
+            using (var reader = XmlReader.Create(path))
+            {
+                var serializer = new XmlSerializer(typeof(Item[]));
+                data = ((Item[])serializer.Deserialize(reader)).ToDictionary(i=>i.symbol,i=>i.symbolDates);
+            }
+        }
+
+        /// <summary>
+        /// Utility method to do the common work for all the symbols i.e. asserting the 
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <param name="dayTime"></param>
+        /// <param name="market"></param>
+        [TestCase(QuantConnect.Securities.Futures.Grains.Wheat, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Grains.Corn, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Grains.Soybeans, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Grains.SoybeanMeal, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Grains.SoybeanOil, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Grains.Oats, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Currencies.GBP, nineSixteenCentralTime)]
+        [TestCase(QuantConnect.Securities.Futures.Currencies.CAD, nineSixteenCentralTime)]
+        [TestCase(QuantConnect.Securities.Futures.Currencies.JPY, nineSixteenCentralTime)]
+        [TestCase(QuantConnect.Securities.Futures.Currencies.CHF, nineSixteenCentralTime)]
+        [TestCase(QuantConnect.Securities.Futures.Currencies.EUR, nineSixteenCentralTime)]
+        [TestCase(QuantConnect.Securities.Futures.Currencies.AUD, nineSixteenCentralTime)]
+        [TestCase(QuantConnect.Securities.Futures.Currencies.NZD, nineSixteenCentralTime)]
+        [TestCase(QuantConnect.Securities.Futures.Energies.CrudeOilWTI, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Energies.HeatingOil, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Energies.Gasoline, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Energies.NaturalGas, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Financials.Y30TreasuryBond, twelveOne)]
+        [TestCase(QuantConnect.Securities.Futures.Financials.Y10TreasuryNote , twelveOne)]
+        [TestCase(QuantConnect.Securities.Futures.Financials.Y5TreasuryNote, twelveOne)]
+        [TestCase(QuantConnect.Securities.Futures.Financials.Y2TreasuryNote, twelveOne)]
+        [TestCase(QuantConnect.Securities.Futures.Indices.SP500EMini,nineThirtyEasternTime)]
+        [TestCase(QuantConnect.Securities.Futures.Indices.NASDAQ100EMini, nineThirtyEasternTime)]
+        [TestCase(QuantConnect.Securities.Futures.Indices.Dow30EMini, nineThirtyEasternTime)]
+        [TestCase(QuantConnect.Securities.Futures.Meats.LiveCattle, twelveOclock)]
+        [TestCase(QuantConnect.Securities.Futures.Meats.LeanHogs, twelveOclock)]
+        [TestCase(QuantConnect.Securities.Futures.Meats.FeederCattle, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Metals.Gold, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Metals.Silver, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Metals.Platinum, zero)]
+        [TestCase(QuantConnect.Securities.Futures.Metals.Palladium, zero)]
+        public void TestExpiryDateFunction(string symbol, string dayTime)
+        {
+            Assert.IsTrue(data.ContainsKey(symbol),"Symbol "+ symbol + " not present in Test Data");
+            foreach(var dates in data[symbol])
+            {
+                var security = Symbol.CreateFuture(symbol, Market.USA, dates.contractMonth);
+                var func = FuturesExpiryFunctions.FuturesExpiryFunction(security.ID.Symbol);
+                var calculated = func(security.ID.Date);
+                var expected = dates.lastTrade + TimeSpan.Parse(dayTime);
+                Assert.AreEqual(calculated, expected, "Failed for symbol: " + symbol);
             }
         }
 
         [Test]
         public void TestAllExpiryDateFunctions()
         {
-            var _path = Directory.GetCurrentDirectory();
-            _path = _path.Substring(0,_path.Length-10) + "\\Common\\Securities\\Futures\\FuturesExpiryFunctionsTestData.xml";
-            IList<String> symbolsForNineThirtyEasternTime = new List<String>
+            var symbolsForNineThirtyEasternTime = new List<String>
             {
                 QuantConnect.Securities.Futures.Indices.NASDAQ100EMini,
                 QuantConnect.Securities.Futures.Indices.SP500EMini,
                 QuantConnect.Securities.Futures.Indices.Dow30EMini
             };
-            IList<String> symbolsForNineSixteenCentralTime = new List<String>
+            var symbolsForNineSixteenCentralTime = new List<String>
             {
                 QuantConnect.Securities.Futures.Currencies.GBP,
                 QuantConnect.Securities.Futures.Currencies.CAD,
@@ -78,50 +157,46 @@ namespace QuantConnect.Tests.Common.Securities.Futures
                 QuantConnect.Securities.Futures.Currencies.AUD,
                 QuantConnect.Securities.Futures.Currencies.NZD
             };
-            IList<String> symbolsForTwelveOne = new List<String>
+            var symbolsForTwelveOne = new List<String>
             {
                 QuantConnect.Securities.Futures.Financials.Y30TreasuryBond,
                 QuantConnect.Securities.Futures.Financials.Y10TreasuryNote,
                 QuantConnect.Securities.Futures.Financials.Y5TreasuryNote,
                 QuantConnect.Securities.Futures.Financials.Y2TreasuryNote
             };
-            IList<String> symbolsForTwelveOclock = new List<String>
+            var symbolsForTwelveOclock = new List<String>
             {
                 QuantConnect.Securities.Futures.Meats.LiveCattle,
                 QuantConnect.Securities.Futures.Meats.LeanHogs
             };
-            using (XmlReader reader = XmlReader.Create(_path))
+
+            foreach(var symbol in data.Keys)
             {
-                List<SymbolData> data = new List<SymbolData>();
-                XmlSerializer serializer = new XmlSerializer(typeof(List<SymbolData>));
-                data = (List<SymbolData>)serializer.Deserialize(reader);
-                foreach(var _symdata in data)
+                var symbolDates = data[symbol];
+                foreach (var dates in symbolDates)
                 {
-                    var symbol = _symdata.symbol;
-                    foreach (var dates in _symdata.dateList)
+                    var security = Symbol.CreateFuture(symbol, Market.USA, dates.contractMonth);
+                    var func = FuturesExpiryFunctions.FuturesExpiryFunction(security.ID.Symbol);
+                    var calculated = func(security.ID.Date);
+                    var expected = dates.lastTrade;
+                    if (symbolsForNineThirtyEasternTime.Contains(security.ID.Symbol))
                     {
-                        var _security = Symbol.CreateFuture(symbol, Market.USA, dates.contractMonth);
-                        var func = FuturesExpiryFunctions.FuturesExpiryFunction(_security.ID.Symbol);
-                        var calculated = func(_security.ID.Date);
-                        var expected = dates.lastTrade;
-                        if (symbolsForNineThirtyEasternTime.Contains(_security.ID.Symbol))
-                        {
-                            expected = expected + new TimeSpan(13,30,0);
-                        }else if(symbolsForNineSixteenCentralTime.Contains(_security.ID.Symbol))
-                        {
-                            expected = expected + new TimeSpan(14, 16, 0);
-                        }else if(symbolsForTwelveOclock.Contains(_security.ID.Symbol))
-                        {
-                            expected = expected + new TimeSpan(12, 0, 0);
-                        }else if (symbolsForTwelveOne.Contains(_security.ID.Symbol))
-                        {
-                            expected = expected + new TimeSpan(12, 1, 0);
-                        }
-                        Assert.AreEqual(calculated,expected);
-                        
+                        expected = expected + new TimeSpan(13,30,0);
+                    }else if(symbolsForNineSixteenCentralTime.Contains(security.ID.Symbol))
+                    {
+                        expected = expected + new TimeSpan(14, 16, 0);
+                    }else if(symbolsForTwelveOclock.Contains(security.ID.Symbol))
+                    {
+                        expected = expected + new TimeSpan(12, 0, 0);
+                    }else if (symbolsForTwelveOne.Contains(security.ID.Symbol))
+                    {
+                        expected = expected + new TimeSpan(12, 1, 0);
                     }
+                    Assert.AreEqual(calculated,expected,"Failed for symbol " + symbol);
+                        
                 }
             }
+            
         }
 
         [Test]
@@ -178,23 +253,23 @@ namespace QuantConnect.Tests.Common.Securities.Futures
         {
             var june2017 = Symbol.CreateFuture(QuantConnect.Securities.Futures.Indices.SP500EMini, Market.USA, new DateTime(2017, 6, 15));
             var june2017Func = FuturesExpiryFunctions.FuturesExpiryFunction(june2017.ID.Symbol);
-            Assert.AreEqual(june2017Func(june2017.ID.Date), new DateTime(2017, 6, 16, 9, 30, 0));
+            Assert.AreEqual(june2017Func(june2017.ID.Date), new DateTime(2017, 6, 16, 13, 30, 0));
 
             var september2017 = Symbol.CreateFuture(QuantConnect.Securities.Futures.Indices.SP500EMini, Market.USA, new DateTime(2017, 9, 1));
             var september2017Func = FuturesExpiryFunctions.FuturesExpiryFunction(september2017.ID.Symbol);
-            Assert.AreEqual(september2017Func(september2017.ID.Date), new DateTime(2017, 9, 15, 9, 30, 0));
+            Assert.AreEqual(september2017Func(september2017.ID.Date), new DateTime(2017, 9, 15, 13, 30, 0));
 
             var december2017 = Symbol.CreateFuture(QuantConnect.Securities.Futures.Indices.SP500EMini, Market.USA, new DateTime(2017, 12, 31));
             var december2017Func = FuturesExpiryFunctions.FuturesExpiryFunction(december2017.ID.Symbol);
-            Assert.AreEqual(december2017Func(december2017.ID.Date), new DateTime(2017, 12, 15, 9, 30, 0));
+            Assert.AreEqual(december2017Func(december2017.ID.Date), new DateTime(2017, 12, 15, 13, 30, 0));
 
             var march2018 = Symbol.CreateFuture(QuantConnect.Securities.Futures.Indices.SP500EMini, Market.USA, new DateTime(2018, 3, 31));
             var march2018Func = FuturesExpiryFunctions.FuturesExpiryFunction(march2018.ID.Symbol);
-            Assert.AreEqual(march2018Func(march2018.ID.Date), new DateTime(2018, 3, 16, 9, 30, 0));
+            Assert.AreEqual(march2018Func(march2018.ID.Date), new DateTime(2018, 3, 16, 13, 30, 0));
 
             var june2018 = Symbol.CreateFuture(QuantConnect.Securities.Futures.Indices.SP500EMini, Market.USA, new DateTime(2018, 3, 31));
             var june2018Func = FuturesExpiryFunctions.FuturesExpiryFunction(june2018.ID.Symbol);
-            Assert.AreEqual(june2018Func(june2018.ID.Date), new DateTime(2018, 3, 16, 9, 30, 0));
+            Assert.AreEqual(june2018Func(june2018.ID.Date), new DateTime(2018, 3, 16, 13, 30, 0));
         }
 
         [Test]
