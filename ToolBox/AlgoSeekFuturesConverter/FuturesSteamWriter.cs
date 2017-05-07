5f787068b973b55e17535f151767b2e821155557
Andrew <andrewthart098@gmail.com>
2017-05-07 12:15:38 -0400

    Use FuturesStreamWriter instead of StreamWriter to write futures files
    Using a StreamWriter resulted in many empty files when futures processing was run.  This is because the StreamWritier ctor creates an empty file.  If there was no data, this resulted in an empty file. The FuturesStreamWriter wraps a StreamWriter an only creates a new file when the WriteLine method is created.

---
+++ ToolBox/AlgoSeekFuturesConverter/FuturesSteamWriter.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+
+namespace QuantConnect.ToolBox.AlgoSeekFuturesConverter
+{
+    /// <summary>
+    /// This class wraps a <see cref="StreamWriter"/> so that the StreamWriter is only
+    /// instantiated until WriteLine() is called.  This ensures that the file the StreamWriter is
+    /// writing to is only created if something is written to it. A StreamWriter will create a empty file
+    /// as soon as it is instantiated.
+    /// </summary>
+    public class FuturesStreamWriter
+    {
+        private StreamWriter _streamWriter;
+        private readonly string _path;
+
+        /// <summary>
+        /// Constructor for the <see cref="FuturesStreamWriter"/>
+        /// </summary>
+        /// <param name="path">Path to the file that should be created</param>
+        public FuturesStreamWriter(string path)
+        {
+            _path = path;
+        }
+
+        /// <summary>
+        /// Wraps the WriteLine method of the StreamWriter.
+        /// </summary>
+        /// <param name="line">The line to write</param>
+        /// <remarks>Will instantiate the StreamWriter if this is the first time this method is called</remarks>
+        public void WriteLine(string line)
+        {
+            PrepareStreamWriter();
+
+            _streamWriter.WriteLine(line);
+        }
+
+        /// <summary>
+        /// Wraps the <see cref="StreamWriter.Flush()"/> method
+        /// </summary>
+        public void Flush()
+        {
+            if (_streamWriter != null)
+            {
+                _streamWriter.Flush();
+            }
+        }
+
+        /// <summary>
+        /// Wraps the <see cref="StreamWriter.Close()"/> method
+        /// </summary>
+        public void Close()
+        {
+            if (_streamWriter != null)
+            {
+                _streamWriter.Close();
+            }
+        }
+
+        /// <summary>
+        /// Checks if the StreamWriter is instantiated. If not, it will instantiate the StreamWriter
+        /// </summary>
+        private void PrepareStreamWriter()
+        {
+            if (_streamWriter == null)
+            {
+                _streamWriter = new StreamWriter(_path);
+            }
+        }
+    }
+}
