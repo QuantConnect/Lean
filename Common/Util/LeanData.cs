74a0163b73f00f62f0f1e5a0fe2652f484e4aa3d
smartquant <joachim.loebb@loebb.ch>
2017-06-19 19:30:29 +0200

    Fix for OpenInterest daily/hourly file format in LeandData.GenerateLine to be yyyyMMdd HH:mm

--- Common/Util/LeanData.cs
+++ Common/Util/LeanData.cs
@@ -230,7 +230,7 @@ namespace QuantConnect.Util
                             var bigOpenInterest = data as OpenInterest;
                             if (bigOpenInterest != null)
                             {
-                                return ToCsv(milliseconds, bigOpenInterest.Value);
+                                return ToCsv(longTime, bigOpenInterest.Value);
                             }
                             break;
 
