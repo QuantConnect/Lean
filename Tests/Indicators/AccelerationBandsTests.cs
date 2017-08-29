c8fa4bbebe6b32c31a5cec49a18a9e1e03fe3e34
jjd <jjdambrosio@gmail.com>
2017-08-29 15:27:15 -0300

    Tests passed

--- Tests/Indicators/AccelerationBandsTests.cs
+++ Tests/Indicators/AccelerationBandsTests.cs
@@ -14,17 +14,39 @@ namespace QuantConnect.Tests.Indicators
     {
         protected override IndicatorBase<TradeBar> CreateIndicator()
         {
-            return new AccelerationBands(20, 2m);
+            return new AccelerationBands(20, 4m);
         }
 
         [Test]
-        public void ComparesWithExternalDataUpperBand() { }
+        public void ComparesWithExternalDataUpperBand()
+        {
+            var abands = CreateIndicator();
+            TestHelper.TestIndicator(
+                abands,
+                "spy_acceleration_bands_20_4.txt",
+                "UpperBand",
+                (ind, expected) => Assert.AreEqual(expected, (double)((AccelerationBands)ind).UpperBand.Current.Value, 1e-4, "Upper band test fail.")
+            );
+        }
 
         [Test]
-        public void ComparesWithExternalDataLowerBand() { }
+        public void ComparesWithExternalDataLowerBand()
+        {
+            var abands = CreateIndicator();
+            TestHelper.TestIndicator(
+                abands,
+                "spy_acceleration_bands_20_4.txt",
+                "LowerBand",
+                (ind, expected) => Assert.AreEqual(expected, (double)((AccelerationBands)ind).LowerBand.Current.Value, 1e-4, "Lower band test fail.")
+            );
+        }
 
-        protected override string TestFileName { get; }
-        protected override string TestColumnName { get; }
+        protected override string TestFileName
+        {
+            get { return "spy_acceleration_bands_20_4.txt"; }
+        }
+
+        protected override string TestColumnName { get { return "MiddleBand"; } }
     }
 
     /// <summary>
@@ -33,7 +55,7 @@ namespace QuantConnect.Tests.Indicators
     /// <seealso cref="QuantConnect.Indicators.IndicatorBase{QuantConnect.Data.Market.TradeBar}" />
     public class AccelerationBands : IndicatorBase<TradeBar>
     {
-        private decimal _with;
+        private decimal _width;
 
         /// <summary>
         /// Gets the type of moving average
@@ -59,15 +81,15 @@ namespace QuantConnect.Tests.Indicators
         /// <summary>
         /// Initializes a new instance of the <see cref="AccelerationBands"/> class.
         /// </summary>
-        /// <param name="name">The name.</param>
-        /// <param name="period">The period.</param>
-        /// <param name="width">The width.</param>
-        /// <param name="movingAverageType">Average type of the moving.</param>
+        /// <param name="name">The name of this indicator.</param>
+        /// <param name="period">The period of the three moving average (middle, upper and lower band).</param>
+        /// <param name="width">A coefficient specifying the distance between the middle band and upper or lower bands.</param>
+        /// <param name="movingAverageType">Type of the moving average .</param>
         public AccelerationBands(string name, int period, decimal width,
             MovingAverageType movingAverageType = MovingAverageType.Simple)
             : base(name)
         {
-            _with = width;
+            _width = width;
             MovingAverageType = movingAverageType;
             MiddleBand = movingAverageType.AsIndicator(name + "_MiddleBand", period);
             LowerBand = movingAverageType.AsIndicator(name + "_LowerBand", period);
@@ -75,7 +97,28 @@ namespace QuantConnect.Tests.Indicators
 
         }
 
-        public override bool IsReady { get; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccelerationBands"/> class.
+        /// </summary>
+        /// <param name="period">The period of the three moving average (middle, upper and lower band).</param>
+        /// <param name="width">A coefficient specifying the distance between the middle band and upper or lower bands.</param>
+        public AccelerationBands(int period, decimal width)
+            : this(string.Format("ABANDS({0},{1})", period, width), period, width)
+        { }
+
+        public AccelerationBands(int period)
+            : this(string.Format("ABANDS({0},{1})", period, 2), period, 2)
+        { }
+
+        public override bool IsReady => MiddleBand.IsReady && LowerBand.IsReady && UpperBand.IsReady;
+
+        public override void Reset()
+        {
+            base.Reset();
+            MiddleBand.Reset();
+            LowerBand.Reset();
+            UpperBand.Reset();
+        }
 
         protected override decimal ComputeNextValue(TradeBar input)
         {
@@ -88,13 +131,13 @@ namespace QuantConnect.Tests.Indicators
             LowerBand.Update(new IndicatorDataPoint
             {
                 Time = input.Time,
-                Value = input.Low * (1 - _with * (input.High - input.Low) / (input.High + input.Low))
+                Value = input.Low * (1 - _width * (input.High - input.Low) / (input.High + input.Low))
             });
 
             UpperBand.Update(new IndicatorDataPoint
             {
                 Time = input.Time,
-                Value = input.Low * (1 + _with * (input.High - input.Low) / (input.High + input.Low))
+                Value = input.High * (1 + _width * (input.High - input.Low) / (input.High + input.Low))
             });
 
             return MiddleBand;
