57d364d5c068432b760ac16e64aeda17196a61f8
Stefano Raggi <stefano.raggi67@alice.it>
2017-09-06 23:33:05 +0200

    Fix crypto subscription tests

--- Tests/Algorithm/AlgorithmAddDataTests.cs
+++ Tests/Algorithm/AlgorithmAddDataTests.cs
@@ -28,7 +28,7 @@ namespace QuantConnect.Tests.Algorithm
             Assert.IsTrue(forexTrade.Subscriptions.Count() == 1);
             Assert.IsTrue(GetMatchingSubscription(forexTrade, typeof(QuoteBar)) != null);
 
-            // Change 
+            // Change
             var dataFeedsConfigString = Config.Get("security-data-feeds");
             Dictionary<SecurityType, List<TickType>> dataFeeds = new Dictionary<SecurityType, List<TickType>>();
             if (dataFeedsConfigString != string.Empty)
@@ -65,20 +65,21 @@ namespace QuantConnect.Tests.Algorithm
             Assert.IsTrue(option.Subscriptions.Count() == 1);
             Assert.IsTrue(GetMatchingSubscription(option, typeof(ZipEntryName)) != null);
 
-            // cfd 
+            // cfd
             var cfd = algo.AddSecurity(SecurityType.Cfd, "abc");
             Assert.IsTrue(cfd.Subscriptions.Count() == 1);
             Assert.IsTrue(GetMatchingSubscription(cfd, typeof(QuoteBar)) != null);
 
-            // future 
+            // future
             var future = algo.AddSecurity(SecurityType.Future, "ES");
             Assert.IsTrue(future.Subscriptions.Count() == 1);
             Assert.IsTrue(future.Subscriptions.FirstOrDefault(x => typeof(ZipEntryName).IsAssignableFrom(x.Type)) != null);
 
             // Crypto
             var crypto = algo.AddSecurity(SecurityType.Crypto, "btcusd", Resolution.Daily);
-            Assert.IsTrue(crypto.Subscriptions.Count() == 1);
+            Assert.IsTrue(crypto.Subscriptions.Count() == 2);
             Assert.IsTrue(GetMatchingSubscription(crypto, typeof(QuoteBar)) != null);
+            Assert.IsTrue(GetMatchingSubscription(crypto, typeof(TradeBar)) != null);
         }
 
 
