f0d2af89a1baf90580279ef40d1762c48e9f3670
Jing Wu <wujingwj@outlook.com>
2017-09-14 17:47:22 -0400

    Added basic forex example

--- Algorithm.Python/BasicTemplateForexAlgorithm.py
+++ Algorithm.Python/BasicTemplateForexAlgorithm.py
@@ -1,10 +1,33 @@
-﻿using System;
-namespace QuantConnect.Algorithm.Python
-{
-	public class BasicTemplateForexAlgorithm
-	{
-		public BasicTemplateForexAlgorithm()
-		{
-		}
-	}
-}
+﻿from datetime import timedelta
+import numpy as np
+
+class BasicTemplateAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        # Set the cash we'd like to use for our backtest
+        self.SetCash(100000)
+        
+        # Start and end dates for the backtest.
+        self.SetStartDate(2013, 10, 07)
+        self.SetEndDate(2013, 10, 11)
+        
+        # Add FOREX contract you want to trade
+        # find available contracts here https://www.quantconnect.com/data#forex/oanda/cfd
+        self.AddForex("EURUSD", Resolution.Minute)
+        self.AddForex("GBPUSD", Resolution.Minute)
+        self.AddForex("EURGBP", Resolution.Minute)
+        
+        self.History(5, Resolution.Daily)
+        self.History(5, Resolution.Hour)
+        self.History(5, Resolution.Minute)
+
+        history = self.History(TimeSpan.FromSeconds(5), Resolution.Second)
+         
+        for data in sorted(history, key=lambda x: x.Time):
+            for key in data.Keys:
+                self.Log(str(key.Value) + ": " + str(data.Time) + " > " + str(data[key].Value))
+        
+    def OnData(self, data):
+        # Print to console to verify that data is coming in
+        for key in data.Keys:
+            self.Log(str(key.Value) + ": " + str(data.Time) + " > " + str(i[key].Value))
