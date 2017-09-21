0bc564fac6326e28a02a034536f37a357a0f94cc
Jared Broad <jaredbroad@gmail.com>
2017-09-21 08:58:53 -0400

    Fix regression tests

--- Tests/Engine/RealTimePriceUpdateTests.cs
+++ Tests/Engine/RealTimePriceUpdateTests.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using QuantConnect.Algorithm;
-using QuantConnect.Algorithm.CSharp;
-using QuantConnect.Algorithm.Examples;
 using QuantConnect.Brokerages;
 using QuantConnect.Data.Auxiliary;
 using QuantConnect.Lean.Engine.DataFeeds;
