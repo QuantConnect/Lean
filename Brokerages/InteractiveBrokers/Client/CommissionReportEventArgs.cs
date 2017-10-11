eb55752ba03b1b68180f54fb5927b742bf50c7bc
Stefano Raggi <stefano.raggi67@alice.it>
2017-10-11 13:15:33 +0200

    Update handling of IB fills to have actual IB fees in order events
    Previously, the IB brokerage was calling the InteractiveBrokerageFeeModel to calculate the fee for the full order quantity, ignoring the actual executions. This solution had a couple of issues:
    - it didn't include fees for orders partially filled, then cancelled
    - it couldn't calculate fees for orders sent to financial advisor groups/profiles
    
    Now every OrderEvent fired (for total or partial fills) will only include the actual IB fee paid for each single execution.
    
    Order fill handling logic has also been simplified by only processing fills in the IB ExecutionDetails and CommissionReport events, instead of OrderStatusChanged (which can fire multiple times or not at all in some cases, such as market orders executed immediately).

--- Brokerages/InteractiveBrokers/Client/CommissionReportEventArgs.cs
+++ Brokerages/InteractiveBrokers/Client/CommissionReportEventArgs.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,7 +26,7 @@ namespace QuantConnect.Brokerages.InteractiveBrokers.Client
         /// <summary>
         /// The structure that contains commission details.
         /// </summary>
-        public CommissionReport CommissionReport { get; private set; }
+        public CommissionReport CommissionReport { get; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CommissionReportEventArgs"/> class
@@ -34,6 +34,14 @@ namespace QuantConnect.Brokerages.InteractiveBrokers.Client
         public CommissionReportEventArgs(CommissionReport commissionReport)
         {
             CommissionReport = commissionReport;
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"ExecId: {CommissionReport.ExecId}, Commission: {CommissionReport.Commission}, Currency: {CommissionReport.Currency}";
         }
     }
 }
