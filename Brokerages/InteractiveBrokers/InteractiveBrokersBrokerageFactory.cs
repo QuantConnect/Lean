d8722d80997c67c8922fed5ab78f99e4c2d8a7c9
Jared Broad <jaredbroad@gmail.com>
2017-10-22 18:20:48 -0400

    More logging

--- Brokerages/InteractiveBrokers/InteractiveBrokersBrokerageFactory.cs
+++ Brokerages/InteractiveBrokers/InteractiveBrokersBrokerageFactory.cs
@@ -1,11 +1,11 @@
 ﻿/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,8 @@ using QuantConnect.Configuration;
 using QuantConnect.Interfaces;
 using QuantConnect.Packets;
 using QuantConnect.Util;
+using QuantConnect.Logging;
+using Newtonsoft.Json;
 
 namespace QuantConnect.Brokerages.InteractiveBrokers
 {
@@ -86,6 +88,8 @@ namespace QuantConnect.Brokerages.InteractiveBrokers
             var password = Read<string>(job.BrokerageData, "ib-password", errors);
             var tradingMode = Read<string>(job.BrokerageData, "ib-trading-mode", errors);
             var agentDescription = Read<string>(job.BrokerageData, "ib-agent-description", errors);
+
+            Log.Trace("InteractiveBrokersBrokerageFactory.CreateBrokerage(): Full brokerage data: " + JsonConvert.SerializeObject(job.BrokerageData));
 
             if (errors.Count != 0)
             {
