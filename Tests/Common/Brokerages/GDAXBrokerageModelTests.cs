6aaba01b0ba5ac6ea9166dda6478f911fda87ee4
Andrew <andrewthart098@gmail.com>
2017-10-23 11:00:57 -0400

    Updated GDAXBrokerage.CanSubmitOrderTests
    The new test not only test BrokerageId, but also quantity of GDAX orders. These represent the current two checks of the order in the GDAX brokerage

--- Tests/Common/Brokerages/GDAXBrokerageModelTests.cs
+++ Tests/Common/Brokerages/GDAXBrokerageModelTests.cs
@@ -34,22 +34,38 @@ namespace QuantConnect.Tests.Common.Brokerages
         public void CanUpdateOrderTest()
         {
             BrokerageMessageEvent message;
-            Assert.AreEqual(false, _unit.CanUpdateOrder(GDAXTestsHelpers.GetSecurity(), Mock.Of<QuantConnect.Orders.Order>(), 
+            Assert.AreEqual(false, _unit.CanUpdateOrder(GDAXTestsHelpers.GetSecurity(), Mock.Of<QuantConnect.Orders.Order>(),
                 new QuantConnect.Orders.UpdateOrderRequest(DateTime.UtcNow, 1, new QuantConnect.Orders.UpdateOrderFields()), out message));
         }
 
         [TestCase(true)]
         [TestCase(false)]
-        public void CanSubmitOrderTest(bool isUpdate)
+        public void CanSubmitOrder_WhenBrokerageIdIsCorrect(bool isUpdate)
         {
             BrokerageMessageEvent message;
             var order = new Mock<QuantConnect.Orders.Order>();
+            // Order quantity must be greater than 0.01
+            // Order brokerageId is under test
+            order.Object.Quantity = 0.01m;
+
             if (isUpdate)
             {
-                order.Object.BrokerId = new List<string>() { "abc123" };
+                order.Object.BrokerId = new List<string>() {"abc123"};
             }
 
             Assert.AreEqual(!isUpdate, _unit.CanSubmitOrder(GDAXTestsHelpers.GetSecurity(), order.Object, out message));
+        }
+
+        [TestCase(0.01, true)]
+        [TestCase(0.009, false)]
+        public void CanSubmitOrder_WhenQuantityIsLargeEnough(decimal orderQuantity, bool isValidOrderQuantity)
+        {
+            BrokerageMessageEvent message;
+            var order = new Mock<QuantConnect.Orders.Order>();
+
+            order.Object.Quantity = orderQuantity;
+
+            Assert.AreEqual(isValidOrderQuantity, _unit.CanSubmitOrder(GDAXTestsHelpers.GetSecurity(), order.Object, out message));
         }
     }
 }
