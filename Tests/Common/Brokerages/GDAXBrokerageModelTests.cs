ded5ddad1f3b523d11c8dfbb9af52ee93bd4d783
Stefano Raggi <stefano.raggi67@gmail.com>
2017-10-25 01:26:18 +0200

    Fix fee percentage in GDAXFeeModel

--- Tests/Common/Brokerages/GDAXBrokerageModelTests.cs
+++ Tests/Common/Brokerages/GDAXBrokerageModelTests.cs
@@ -1,15 +1,27 @@
-﻿using Moq;
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using Moq;
 using NUnit.Framework;
 using QuantConnect.Brokerages;
-using QuantConnect.Orders.Fees;
-using QuantConnect.Securities;
 using QuantConnect.Tests.Brokerages.GDAX;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using QuantConnect.Data.Market;
 using QuantConnect.Orders;
+using QuantConnect.Orders.Fees;
 
 namespace QuantConnect.Tests.Common.Brokerages
 {
@@ -95,6 +107,28 @@ namespace QuantConnect.Tests.Common.Brokerages
             var order = Order.CreateOrder(new SubmitOrderRequest(orderType, SecurityType.Crypto, security.Symbol, 10.0m, 1.0m, 10.0m, DateTime.Now, "Test Order"));
 
             Assert.AreEqual(isValidOrderType, _unit.CanSubmitOrder(security, order, out message));
+        }
+
+        [Test]
+        public void FeeModelReturnsCorrectOrderFeeForTakerOrder()
+        {
+            var security = GDAXTestsHelpers.GetSecurity();
+            security.FeeModel = new GDAXFeeModel();
+            security.SetMarketPrice(new TradeBar { Symbol = security.Symbol, Close = 5000m });
+            var orderFee = security.FeeModel.GetOrderFee(security, new MarketOrder(security.Symbol, 1, DateTime.MinValue));
+
+            Assert.AreEqual(12.5m, orderFee);
+        }
+
+        [Test]
+        public void FeeModelReturnsCorrectOrderFeeForMakerOrder()
+        {
+            var security = GDAXTestsHelpers.GetSecurity();
+            security.FeeModel = new GDAXFeeModel();
+            security.SetMarketPrice(new TradeBar { Symbol = security.Symbol, Close = 5000m });
+            var orderFee = security.FeeModel.GetOrderFee(security, new LimitOrder(security.Symbol, 1, 5000m, DateTime.MinValue));
+
+            Assert.AreEqual(0, orderFee);
         }
     }
 }
