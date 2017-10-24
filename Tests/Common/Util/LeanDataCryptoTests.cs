406eef196b6fac297cfcdc76849e01c6cd7c01f5
Andrew <andrewthart098@gmail.com>
2017-10-24 09:43:27 -0400

    Specify crypto only switch statement in LeanData.GenerateLine
    Because cryptocurrencies can be either Trades or Quotes, the Crypto security type needs it's own switch statement in LeanData.GenerateLine. This produces non-scaled TradeBars or QuoteBarrs.

---
+++ Tests/Common/Util/LeanDataCryptoTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace QuantConnect.Tests.Common.Util
+{
+    [TestFixture]
+    public class LeanDataCryptoTests
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void CryotTradeBarFileNamesAreCreatedCorrectly()
+        {
+
+        }
+    }
+}
