d9036d62557c17faf7b0911f63d8e672c0a96b58
thonmakerformvp <32805462+thonmakerformvp@users.noreply.github.com>
2017-10-28 20:24:36 -0700

    Truncated the EnsureCurrencyDataFeed
    this method was trying to load currency conversion rates for every currency any of my IB assets are denominated in. I had some USD in my IB account, despite my IB account being based in AUD so to avoid an attempt to load a conversion rate for those usd assets i had to remove a large part of the EnsureCurrencyDataFeed method.

--- Common/Securities/Cash.cs
+++ Common/Securities/Cash.cs
@@ -139,97 +139,12 @@ namespace QuantConnect.Securities
                 _isBaseCurrency = true;
                 ConversionRate = 1.0m;
                 return null;
-            }
-
-            if (subscriptions.Count == 0)
+            } else 
             {
-                throw new InvalidOperationException("Unable to add cash when no subscriptions are present. Please add subscriptions in the Initialize() method.");
+                return null;   
             }
-
-            // we require a subscription that converts this into the base currency
-            string normal = Symbol + CashBook.AccountCurrency;
-            string invert = CashBook.AccountCurrency + Symbol;
-            foreach (var config in subscriptions.Subscriptions.Where(config => config.SecurityType == SecurityType.Forex || config.SecurityType == SecurityType.Cfd ||
-            config.SecurityType == SecurityType.Crypto))
-            {
-                if (config.Symbol.Value == normal)
-                {
-                    SecuritySymbol = config.Symbol;
-                    return null;
-                }
-                if (config.Symbol.Value == invert)
-                {
-                    SecuritySymbol = config.Symbol;
-                    _invertRealTimePrice = true;
-                    return null;
-                }
-            }
-            // if we've made it here we didn't find a subscription, so we'll need to add one
-
-            // Create a SecurityType to Market mapping with the markets from SecurityManager members
-            var markets = securities.Keys.GroupBy(x => x.SecurityType).ToDictionary(x => x.Key, y => y.First().ID.Market);
-            if (markets.ContainsKey(SecurityType.Cfd) && !markets.ContainsKey(SecurityType.Forex))
-            {
-                markets.Add(SecurityType.Forex, markets[SecurityType.Cfd]);
-            }
-            if (markets.ContainsKey(SecurityType.Forex) && !markets.ContainsKey(SecurityType.Cfd))
-            {
-                markets.Add(SecurityType.Cfd, markets[SecurityType.Forex]);
-            }
-
-            var currencyPairs = Currencies.CurrencyPairs.Select(x =>
-            {
-                // allow XAU or XAG to be used as quote currencies, but pairs including them are CFDs
-                var securityType = Symbol.StartsWith("XAU") || Symbol.StartsWith("XAG") ? SecurityType.Cfd : SecurityType.Forex;
-                var market = string.Empty;
-                if (!markets.TryGetValue(securityType, out market))
-                {
-                    market = marketMap[securityType];
-                }
-                return QuantConnect.Symbol.Create(x, securityType, market);
-            });
-            var minimumResolution = subscriptions.Subscriptions.Select(x => x.Resolution).DefaultIfEmpty(Resolution.Minute).Min();
-            var objectType = minimumResolution == Resolution.Tick ? typeof (Tick) : typeof (QuoteBar);
-            foreach (var symbol in currencyPairs)
-            {
-                if (symbol.Value == normal || symbol.Value == invert)
-                {
-                    _invertRealTimePrice = symbol.Value == invert;
-                    var securityType = symbol.ID.SecurityType;
-                    var symbolProperties = symbolPropertiesDatabase.GetSymbolProperties(symbol.ID.Market, symbol.Value, securityType, Symbol);
-                    Cash quoteCash;
-                    if (!cashBook.TryGetValue(symbolProperties.QuoteCurrency, out quoteCash))
-                    {
-                        throw new Exception("Unable to resolve quote cash: " + symbolProperties.QuoteCurrency + ". This is required to add conversion feed: " + symbol.ToString());
-                    }
-                    var marketHoursDbEntry = marketHoursDatabase.GetEntry(symbol.ID.Market, symbol.Value, symbol.ID.SecurityType);
-                    var exchangeHours = marketHoursDbEntry.ExchangeHours;
-                    // set this as an internal feed so that the data doesn't get sent into the algorithm's OnData events
-                    var config = subscriptions.Add(objectType, TickType.Quote, symbol, minimumResolution, marketHoursDbEntry.DataTimeZone, exchangeHours.TimeZone, false, true, false, true);
-                    SecuritySymbol = config.Symbol;
-
-                    Security security;
-                    if (securityType == SecurityType.Cfd)
-                    {
-                        security = new Cfd.Cfd(exchangeHours, quoteCash, config, symbolProperties);
-                    }
-                    else if (securityType == SecurityType.Crypto)
-                    {
-                        security = new Crypto.Crypto(exchangeHours, quoteCash, config, symbolProperties);
-                    }
-                    else
-                    {
-                        security = new Forex.Forex(exchangeHours, quoteCash, config, symbolProperties);
-                    }
-                    securities.Add(config.Symbol, security);
-                    Log.Trace("Cash.EnsureCurrencyDataFeed(): Adding " + symbol.Value + " for cash " + Symbol + " currency feed");
-                    return security;
-                }
-            }
-
-            // if this still hasn't been set then it's an error condition
-            throw new ArgumentException(string.Format("In order to maintain cash in {0} you are required to add a subscription for Forex pair {0}{1} or {1}{0}", Symbol, CashBook.AccountCurrency));
-        }
+            
+           }
 
         /// <summary>
         /// Returns a <see cref="System.String"/> that represents the current <see cref="QuantConnect.Securities.Cash"/>.
