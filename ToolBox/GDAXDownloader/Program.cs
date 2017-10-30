62ac6d9032f6be3803b578684781141646d993ac
Marco Carola <marcocarola@gmail.com>
2017-10-30 19:27:51 +0000

    License added

--- ToolBox/GDAXDownloader/Program.cs
+++ ToolBox/GDAXDownloader/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using System;
 using System.Globalization;
 using QuantConnect.Configuration;
 using QuantConnect.Logging;
@@ -46,16 +60,14 @@ namespace QuantConnect.ToolBox.GDAXDownloader
                 var writer = new LeanDataWriter(Resolution.Hour, symbolObject, dataDirectory, TickType.Quote);
                 writer.Write(data);
                 
-                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("Finish data download");
                 
             }
             catch (Exception err)
             {
                 Log.Error(err);
-                Console.WriteLine(err.Message);
-                Console.WriteLine(err.InnerException);
-                Console.WriteLine(err.StackTrace);
+                Log.Trace(err.Message);
+                Log.Trace(err.StackTrace);
             }
             Console.ReadLine();
         }
