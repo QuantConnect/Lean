8e4054acde31a729d73760331946c6b7be20acb5
Andrew <andrewthart098@gmail.com>
2017-11-10 16:45:13 -0500

    Fixes 1261
    This issue is related to the AlgoSeek Options and Futures reader skipping every other line. The streamReader.ReadLine() method was called twice every time Reader.MoveNext() is called.

--- ToolBox/AlgoSeekOptionsConverter/AlgoSeekOptionsReader.cs
+++ ToolBox/AlgoSeekOptionsConverter/AlgoSeekOptionsReader.cs
@@ -1,11 +1,11 @@
 ﻿/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -99,8 +99,8 @@ namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
         {
             string line;
             Tick tick = null;
-            while ((line = _streamReader.ReadLine()) != null && tick == null)
-            {
+            while (tick == null && (line = _streamReader.ReadLine()) != null)
+                {
                 // If line is invalid continue looping to find next valid line.
                 tick = Parse(line);
             }
@@ -113,8 +113,8 @@ namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
         /// </summary>
         public Tick Current
         {
-            get; private set; 
-            
+            get; private set;
+
         }
 
         /// <summary>
@@ -146,7 +146,7 @@ namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
             _streamReader.Close();
             _streamReader.Dispose();
         }
-        
+
         /// <summary>
         /// Parse a string line into a option tick.
         /// </summary>
