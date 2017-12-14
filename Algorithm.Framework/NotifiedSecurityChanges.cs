e09a4404b9f311333908e71488e753e3a675b2e4
Michael Handschuh <mhandschuh@gmail.com>
2017-12-14 13:45:35 -0500

    Add more convenience methods to NotifiedSecurityChanges

--- Algorithm.Framework/NotifiedSecurityChanges.cs
+++ Algorithm.Framework/NotifiedSecurityChanges.cs
@@ -13,6 +13,7 @@
  * limitations under the License.
 */
 
+using System;
 using System.Collections.Generic;
 using QuantConnect.Data.UniverseSelection;
 using QuantConnect.Securities;
@@ -20,7 +21,7 @@ using QuantConnect.Securities;
 namespace QuantConnect.Algorithm.Framework
 {
     /// <summary>
-    /// Provides convenience methods for handling security changes
+    /// Provides convenience methods for updating collections in responses to securities changed events
     /// </summary>
     public static class NotifiedSecurityChanges
     {
@@ -31,13 +32,81 @@ namespace QuantConnect.Algorithm.Framework
         /// <param name="changes">The changes to be applied to the securities collection</param>
         public static void UpdateCollection(ICollection<Security> securities, SecurityChanges changes)
         {
+            Update(changes, securities.Add, removed => securities.Remove(removed));
+        }
+
+        /// <summary>
+        /// Adds and removes the security changes to/from the collection
+        /// </summary>
+        /// <param name="securities">The securities collection to be updated with the changes</param>
+        /// <param name="changes">The changes to be applied to the securities collection</param>
+        /// <param name="valueFactory">Delegate used to create instances of <see cref="TValue"/> from a <see cref="Security"/> objec</param>
+        public static void UpdateCollection<TValue>(ICollection<TValue> securities, SecurityChanges changes, Func<Security, TValue> valueFactory)
+        {
+            Update(changes, added => securities.Add(valueFactory(added)), removed => securities.Remove(valueFactory(removed)));
+        }
+
+        /// <summary>
+        /// Adds and removes the security changes to/from the collection
+        /// </summary>
+        /// <param name="dictionary">The securities collection to be updated with the changes</param>
+        /// <param name="changes">The changes to be applied to the securities collection</param>
+        /// <param name="valueFactory">Factory for creating dictonary values for a key</param>
+        public static void UpdateDictionary<TValue>(
+            IDictionary<Security, TValue> dictionary,
+            SecurityChanges changes,
+            Func<Security, TValue> valueFactory
+            )
+        {
+            UpdateDictionary(dictionary, changes, security => security, valueFactory);
+        }
+
+        /// <summary>
+        /// Adds and removes the security changes to/from the collection
+        /// </summary>
+        /// <param name="dictionary">The securities collection to be updated with the changes</param>
+        /// <param name="changes">The changes to be applied to the securities collection</param>
+        /// <param name="valueFactory">Factory for creating dictonary values for a key</param>
+        public static void UpdateDictionary<TValue>(
+            IDictionary<Symbol, TValue> dictionary,
+            SecurityChanges changes,
+            Func<Security, TValue> valueFactory
+            )
+        {
+            UpdateDictionary(dictionary, changes, security => security.Symbol, valueFactory);
+        }
+
+        /// <summary>
+        /// Most generic form of <see cref="UpdateCollection"/>
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="changes"></param>
+        /// <param name="keyFactory"></param>
+        /// <param name="valueFactory"></param>
+        public static void UpdateDictionary<TKey, TValue>(
+            IDictionary<TKey, TValue> dictionary,
+            SecurityChanges changes,
+            Func<Security, TKey> keyFactory,
+            Func<Security, TValue> valueFactory
+            )
+        {
+            Update(changes,
+                added => dictionary.Add(keyFactory(added), valueFactory(added)),
+                removed => dictionary.Remove(keyFactory(removed))
+            );
+        }
+
+        private static void Update(SecurityChanges changes, Action<Security> add, Action<Security> remove)
+        {
             foreach (var added in changes.AddedSecurities)
             {
-                securities.Add(added);
+                add(added);
             }
             foreach (var removed in changes.RemovedSecurities)
             {
-                securities.Remove(removed);
+                remove(removed);
             }
         }
     }
