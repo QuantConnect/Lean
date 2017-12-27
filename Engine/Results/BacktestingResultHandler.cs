a02e9a378ccdd96e59d524315be793b20ebf311a
Jared Broad <jaredbroad@gmail.com>
2017-12-27 17:28:29 -0500

    null ref exception

--- Engine/Results/BacktestingResultHandler.cs
+++ Engine/Results/BacktestingResultHandler.cs
@@ -78,6 +78,8 @@ namespace QuantConnect.Lean.Engine.Results
         private IMessagingHandler _messagingHandler;
         private ITransactionHandler _transactionHandler;
         private ISetupHandler _setupHandler;
+        private string _algorithmId;
+        private int _projectId;
 
         private const double _samples = 4000;
         private const double _minimumSamplePeriod = 4;
@@ -204,6 +206,8 @@ namespace QuantConnect.Lean.Engine.Results
         /// <param name="transactionHandler"></param>
         public virtual void Initialize(AlgorithmNodePacket job, IMessagingHandler messagingHandler, IApi api, IDataFeed dataFeed, ISetupHandler setupHandler, ITransactionHandler transactionHandler)
         {
+            _algorithmId = job.AlgorithmId;
+            _projectId = job.ProjectId;
             _messagingHandler = messagingHandler;
             _transactionHandler = transactionHandler;
             _setupHandler = setupHandler;
@@ -538,7 +542,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// <param name="message">Message we'd like shown in console.</param>
         public void DebugMessage(string message)
         {
-            Messages.Enqueue(new DebugPacket(_job.ProjectId, _backtestId, _compileId, message));
+            Messages.Enqueue(new DebugPacket(_projectId, _backtestId, _compileId, message));
 
             //Save last message sent:
             if (_algorithm != null)
@@ -554,7 +558,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// <param name="message">Message we'd like shown in console.</param>
         public void SystemDebugMessage(string message)
         {
-            Messages.Enqueue(new SystemDebugPacket(_job.ProjectId, _backtestId, _compileId, message));
+            Messages.Enqueue(new SystemDebugPacket(_projectId, _backtestId, _compileId, message));
 
             //Save last message sent:
             if (_algorithm != null)
@@ -748,7 +752,7 @@ namespace QuantConnect.Lean.Engine.Results
             if (!_exitTriggered)
             {
                 ProcessSynchronousEvents(true);
-                var logLocation = SaveLogs(_algorithm.AlgorithmId, _log);
+                var logLocation = SaveLogs(_algorithmId, _log);
                 SystemDebugMessage("Your log was successfully created and can be retrieved from: " + logLocation);
             }
 
@@ -774,7 +778,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// <param name="message">Additional optional status message.</param>
         public virtual void SendStatusUpdate(AlgorithmStatus status, string message = "")
         {
-            var statusPacket = new AlgorithmStatusPacket(_algorithm.AlgorithmId, _job.ProjectId, status, message);
+            var statusPacket = new AlgorithmStatusPacket(_algorithmId, _projectId, status, message);
             _messagingHandler.Send(statusPacket);
         }
 
