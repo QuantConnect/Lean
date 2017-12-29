068e837238eb612e280ee13a121a15bb03b6f2ea
Stefano Raggi <stefano.raggi67@gmail.com>
2017-12-29 01:12:58 +0100

    Remove usage of Lazy<DateTime> in LocalTimeKeeper
    According to the VS profiler, in a long backtest about 2% of the time was spent in the UpdateTime method. The new Lazy object was immediately evaluated anyway, so it was removed.

--- Common/LocalTimeKeeper.cs
+++ Common/LocalTimeKeeper.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,9 +24,6 @@ namespace QuantConnect
     /// </summary>
     public class LocalTimeKeeper
     {
-        private Lazy<DateTime> _localTime;
-        private readonly DateTimeZone _timeZone;
-
         /// <summary>
         /// Event fired each time <see cref="UpdateTime"/> is called
         /// </summary>
@@ -35,18 +32,12 @@ namespace QuantConnect
         /// <summary>
         /// Gets the time zone of this <see cref="LocalTimeKeeper"/>
         /// </summary>
-        public DateTimeZone TimeZone
-        {
-            get { return _timeZone; }
-        }
+        public DateTimeZone TimeZone { get; }
 
         /// <summary>
         /// Gets the current time in terms of the <see cref="TimeZone"/>
         /// </summary>
-        public DateTime LocalTime
-        {
-            get { return _localTime.Value; }
-        }
+        public DateTime LocalTime { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="LocalTimeKeeper"/> class
@@ -55,8 +46,8 @@ namespace QuantConnect
         /// <param name="timeZone">The time zone</param>
         internal LocalTimeKeeper(DateTime utcDateTime, DateTimeZone timeZone)
         {
-            _timeZone = timeZone;
-            _localTime = new Lazy<DateTime>(() => utcDateTime.ConvertTo(DateTimeZone.Utc, _timeZone));
+            TimeZone = timeZone;
+            LocalTime = utcDateTime.ConvertTo(DateTimeZone.Utc, TimeZone);
         }
 
         /// <summary>
@@ -65,12 +56,8 @@ namespace QuantConnect
         /// <param name="utcDateTime">The current time in UTC</param>
         internal void UpdateTime(DateTime utcDateTime)
         {
-            // redefine the lazy conversion each time this is set
-            _localTime = new Lazy<DateTime>(() => utcDateTime.ConvertTo(DateTimeZone.Utc, _timeZone));
-            if (TimeUpdated != null)
-            {
-                TimeUpdated(this, new TimeUpdatedEventArgs(_localTime.Value, TimeZone));
-            }
+            LocalTime = utcDateTime.ConvertTo(DateTimeZone.Utc, TimeZone);
+            TimeUpdated?.Invoke(this, new TimeUpdatedEventArgs(LocalTime, TimeZone));
         }
     }
 }
