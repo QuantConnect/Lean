a2c535e1d3bb4f1b5180c7244f2203ce3d4a4a51
jameschch@outlook.com <jameschch@outlook.com>
2017-12-30 14:18:46 +0000

    Adds bitfinex test algorithm

---
+++ Algorithm.CSharp/BitfinexAlgorithm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using QuantConnect.Data.Consolidators;
+using QuantConnect.Data.Market;
+using System;
+using QuantConnect.Brokerages;
+using QuantConnect.Data;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+
+    public class BitfinexAlgorithm : QCAlgorithm
+    {
+
+        public override void Initialize()
+        {
+            SetStartDate(2015, 11, 12);
+            SetEndDate(2016, 04, 01);
+
+            //Set the cash for the strategy:
+            SetCash(100000);
+            SetBrokerageModel(BrokerageName.Bitfinex, AccountType.Cash);
+
+            SetTimeZone(NodaTime.DateTimeZone.Utc);
+            var security = AddSecurity(SecurityType.Crypto, "BTCUSD", Resolution.Tick, Market.Bitfinex, false, 3.3m, true);
+            AddCrypto("IOTUSD", Resolution.Tick, Market.Bitfinex, false, 3.3m);
+            SetBenchmark(security.Symbol);
+
+            var con = new QuoteBarConsolidator(new TimeSpan(0, 1, 0));
+            SubscriptionManager.AddConsolidator("BTCUSD", con);
+            con.DataConsolidated += DataConsolidated;
+        }
+
+        public override void OnData(Slice slice)
+        {
+
+        }
+
+        private void DataConsolidated(object sender, QuoteBar e)
+        {
+        }
+    }
+}
