0cc6a53e9ce5459a52f34365b83bf4d892d331c5
Michael Handschuh <mhandschuh@gmail.com>
2018-01-04 12:24:31 -0500

    Set correct time zone for custom data in MarketHoursDatabase
    Add MarketHoursDatabase.SetEntry and SetEntryAlwaysOpen. This allows runtime modification of the
    market hours database which is necessary for correct custom data time zone handling.
    
    Extracts complicate ternary logic into its own method and make it human readable.
    Set the market hours entry for custom data universe subscriptions defaulting to the security's time zone.

--- Common/Securities/Crypto/CryptoExchange.cs
+++ Common/Securities/Crypto/CryptoExchange.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,7 +13,7 @@
  * limitations under the License.
 */
 
-namespace QuantConnect.Securities.Crypto 
+namespace QuantConnect.Securities.Crypto
 {
     /// <summary>
     /// Crypto exchange class - information and helper tools for Crypto exchange properties
@@ -27,7 +27,7 @@ namespace QuantConnect.Securities.Crypto
         /// derived from the market-hours-database for the Crypto market
         /// </summary>
         public CryptoExchange(string market)
-            : base(MarketHoursDatabase.FromDataFolder().GetExchangeHours(market, null, SecurityType.Crypto, TimeZones.Utc))
+            : base(MarketHoursDatabase.FromDataFolder().GetExchangeHours(market, null, SecurityType.Crypto))
         {
         }
 
--- Common/Securities/Forex/ForexExchange.cs
+++ Common/Securities/Forex/ForexExchange.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,7 +13,7 @@
  * limitations under the License.
 */
 
-namespace QuantConnect.Securities.Forex 
+namespace QuantConnect.Securities.Forex
 {
     /// <summary>
     /// Forex exchange class - information and helper tools for forex exchange properties
@@ -35,7 +35,7 @@ namespace QuantConnect.Securities.Forex
         /// derived from the market-hours-database for the FXCM Forex market
         /// </summary>
         public ForexExchange()
-            : base(MarketHoursDatabase.FromDataFolder().GetExchangeHours(Market.FXCM, null, SecurityType.Forex, TimeZones.NewYork))
+            : base(MarketHoursDatabase.FromDataFolder().GetExchangeHours(Market.FXCM, null, SecurityType.Forex))
         {
         }
 
