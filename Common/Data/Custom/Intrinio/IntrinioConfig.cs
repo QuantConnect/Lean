5d57ff25eb2f75d275868afaee95707faf13f8f4
Juan José D'Ambrosio <jjdambrosio@gmail.com>
2018-01-05 22:05:00 -0300

    IntrinioConfig implemented

--- Common/Data/Custom/Intrinio/IntrinioConfig.cs
+++ Common/Data/Custom/Intrinio/IntrinioConfig.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace QuantConnect.Data.Custom.Intrinio
 {
-    class IntrinioConfig
+    /// <summary>
+    ///     Auxiliary class to access all Intrinio API data.
+    /// </summary>
+    public static class IntrinioConfig
     {
+        /// <summary>
+        ///     Intrinio API user
+        /// </summary>
+        public static string User { get; set; }
+        /// <summary>
+        ///     Intrinio API password
+        /// </summary>
+        public static string Password { get; set; }
+
+        /// <summary>
+        ///     Check if Intrinio API user and password are not empty or null.
+        /// </summary>
+        public static bool AreUserAndPasswordSet => !(string.IsNullOrWhiteSpace(User) && string.IsNullOrWhiteSpace(Password));
     }
 }
