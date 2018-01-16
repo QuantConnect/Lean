facf8139f865c9e3e58419def1b3ac40724b35c3
Stefano Raggi <stefano.raggi67@gmail.com>
2018-01-17 00:01:37 +0100

    Fix CashBook serialization error
    The following error was being thrown in BaseResultsHandler.SaveResults:
    Token PropertyName in state Property would result in an invalid JSON object. Path 'Cash.USD'.

--- Common/SymbolJsonConverter.cs
+++ Common/SymbolJsonConverter.cs
@@ -1,11 +1,11 @@
 ﻿/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,12 +17,11 @@
 using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using QuantConnect.Securities;
 
 namespace QuantConnect
 {
     /// <summary>
-    /// Defines a <see cref="JsonConverter"/> to be used when deserializing to 
+    /// Defines a <see cref="JsonConverter"/> to be used when deserializing to
     /// the <see cref="Symbol"/> class.
     /// </summary>
     public class SymbolJsonConverter : JsonConverter
@@ -34,7 +33,7 @@ namespace QuantConnect
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var symbol = value as Symbol;
-            if (symbol == null) return;
+            if (ReferenceEquals(symbol, null)) return;
 
             writer.WriteStartObject();
             writer.WritePropertyName("$type");
