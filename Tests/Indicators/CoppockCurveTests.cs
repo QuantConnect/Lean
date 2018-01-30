e7223da4a71180e566a27838a98f7b154964229b
Juan José D'Ambrosio <jjdambrosio@gmail.com>
2018-01-30 12:05:13 -0300

    Coppock Curve indicator implementation

---
+++ Tests/Indicators/CoppockCurveTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using QuantConnect.Indicators;
+
+namespace QuantConnect.Tests.Indicators
+{
+    [TestFixture]
+    public class CoppockCurveTests : CommonIndicatorTests<IndicatorDataPoint>
+    {
+        protected override IndicatorBase<IndicatorDataPoint> CreateIndicator() { return new CoppockCurve(); }
+
+        protected override string TestFileName => "spy_coppock_curve.csv";
+        protected override string TestColumnName => "CoppockCurve";
+    }
+}
