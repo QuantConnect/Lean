07fb42f82329d5eb29835564d191e0d6956fadd0
Ian Worthington <worthy.vii@gmail.com>
2018-02-14 16:17:23 +0900

    Wrote test for TradeBarConsolidation to make sure EndTime works as expected in the count case. Minor adjustment to the QuoteBar test.

--- Tests/Common/Data/BaseDataConsolidatorTests.cs
+++ Tests/Common/Data/BaseDataConsolidatorTests.cs
@@ -25,7 +25,7 @@ namespace QuantConnect.Tests.Common.Data
     public class BaseDataConsolidatorTests
     {
         [Test]
-        public void AggregatesNewTradeBarProperly()
+        public void AggregatesTickToNewTradeBarProperly()
         {
             TradeBar newTradeBar = null;
             var creator = new BaseDataConsolidator(4);
@@ -82,6 +82,82 @@ namespace QuantConnect.Tests.Common.Data
 
             // base data can't aggregate volume
             Assert.AreEqual(0, newTradeBar.Volume);
+        }
+
+        [Test]
+        public void AggregatesTradeBarsProperly()
+        {
+            TradeBar newTradeBar = null;
+            var creator = new TradeBarConsolidator(4);
+            creator.DataConsolidated += (sender, args) =>
+            {
+                newTradeBar = args;
+            };
+
+            var time = DateTime.Today;
+            var period = TimeSpan.FromMinutes(1);
+            var bar1 = new TradeBar
+            {
+                Time = time,
+                Symbol = Symbols.SPY,
+                Open = 1,
+                High = 2,
+                Low = 0.75m,
+                Close = 1.25m,
+                Period = period
+            };
+            creator.Update(bar1);
+            Assert.IsNull(newTradeBar);
+
+            var bar2 = new TradeBar
+            {
+                Time = time + TimeSpan.FromMinutes(1),
+                Symbol = Symbols.SPY,
+                Open = 1.1m,
+                High = 2.2m,
+                Low = 0.9m,
+                Close = 2.1m,
+                Period = period
+            };
+            creator.Update(bar2);
+            Assert.IsNull(newTradeBar);
+
+            var bar3 = new TradeBar
+            {
+                Time = time + TimeSpan.FromMinutes(2),
+                Symbol = Symbols.SPY,
+                Open = 1,
+                High = 2,
+                Low = 0.1m,
+                Close = 1.75m,
+                Period = period
+            };
+            creator.Update(bar3);
+            Assert.IsNull(newTradeBar);
+
+            var bar4 = new TradeBar
+            {
+                Time = time + TimeSpan.FromMinutes(3),
+                Symbol = Symbols.SPY,
+                Open = 1,
+                High = 7,
+                Low = 0.5m,
+                Close = 4.4m,
+                Period = period
+            };
+            creator.Update(bar4);
+            Assert.IsNotNull(newTradeBar);
+            Assert.AreEqual(bar1.Symbol, newTradeBar.Symbol);
+            Assert.AreEqual(1, newTradeBar.Open);
+            Assert.AreEqual(7, newTradeBar.High);
+            Assert.AreEqual(0.1m, newTradeBar.Low);
+            Assert.AreEqual(4.4m, newTradeBar.Close);
+            Assert.AreEqual(newTradeBar.Close, newTradeBar.Value);
+            Assert.AreEqual(bar4.EndTime, newTradeBar.EndTime);
+            Assert.AreEqual(TimeSpan.FromMinutes(4), newTradeBar.Period);
+            
+            Assert.AreEqual(bar1.Volume + bar2.Volume + bar3.Volume + bar4.Volume, newTradeBar.Volume);
+
         }
 
 
