30263b2a516bc176957de2399a079758887c5e9e
AlexCatarino <alexandre.catarino@gmail.com>
2018-03-12 22:32:26 +0000

    Implements catch-all Python Exception Interpreter
    Implements `PythonExceptionInterpreter` that works as a "catch-all" exception intrepreter for `PythonException`. Since it can interpret more specific python exceptions that are already covered by other interpreters, its `Order` is set to `int.MaxValue` to be the last to be tested.

---
+++ Common/Exceptions/PythonExceptionInterpreter.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using Python.Runtime;
+using QuantConnect.Util;
+
+namespace QuantConnect.Exceptions
+{
+    /// <summary>
+    /// Interprets <see cref="PythonExceptionInterpreter"/> instances
+    /// </summary>
+    public class PythonExceptionInterpreter : IExceptionInterpreter
+    {
+        /// <summary>
+        /// Determines the order that an instance of this class should be called
+        /// </summary>
+        public int Order => int.MaxValue;
+
+        /// <summary>
+        /// Determines if this interpreter should be applied to the specified exception. f
+        /// </summary>
+        /// <param name="exception">The exception to check</param>
+        /// <returns>True if the exception can be interpreted, false otherwise</returns>
+        public bool CanInterpret(Exception exception) => exception?.GetType() == typeof(PythonException);
+
+        /// <summary>
+        /// Interprets the specified exception into a new exception
+        /// </summary>
+        /// <param name="exception">The exception to be interpreted</param>
+        /// <param name="innerInterpreter">An interpreter that should be applied to the inner exception.
+        /// <returns>The interpreted exception</returns>
+        public Exception Interpret(Exception exception, IExceptionInterpreter innerInterpreter)
+        {
+            var pe = (PythonException)exception;
+
+            var message = pe.Message + PythonUtil.PythonExceptionStackParser(pe.StackTrace);
+            return new Exception(message, pe);
+        }
+    }
+}
---
+++ Tests/RegressionAlgorithms/Test_PythonExceptionInterpreter.py
@@ -0,0 +1,37 @@
+﻿# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from clr import AddReference
+AddReference("System")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+from System import *
+from QuantConnect import *
+from QuantConnect.Algorithm import QCAlgorithm
+import decimal 
+
+class Test_PythonExceptionInterpreter(QCAlgorithm):
+    def Initialize(self):
+        pass
+
+    def key_error(self):
+        x = dict()['SPY']
+
+    def no_method_match(self):
+        self.Log(1)
+
+    def unsupported_operand(self):
+        x = decimal.Decimal(1) * 1.1
+
+    def zero_division_error(self):
+        x = 1 / 0
