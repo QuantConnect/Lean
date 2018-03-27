ecf6e59c88a2e4a38bfc9b6c5441ff6e361adf5e
Michael Handschuh <mhandschuh@gmail.com>
2018-03-27 14:05:53 -0400

    Add EmptySingleSecuritySecondEquityAlgorithm

---
+++ Algorithm.CSharp/Benchmarks/EmptySingleSecuritySecondEquityAlgorithm.cs
@@ -0,0 +1,19 @@
+﻿using QuantConnect.Data;
+
+namespace QuantConnect.Algorithm.CSharp.Benchmarks
+{
+    public class EmptySingleSecuritySecondEquityAlgorithm : QCAlgorithm
+    {
+        public override void Initialize()
+        {
+            SetStartDate(2008, 01, 01);
+            SetEndDate(2009, 01, 01);
+            SetBenchmark(dt => 1m);
+            AddEquity("SPY", Resolution.Second);
+        }
+
+        public override void OnData(Slice data)
+        {
+        }
+    }
+}
