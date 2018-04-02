ac6fa2902c3b509b8c8690a2c321aa37a93c8797
Stefano Raggi <stefano.raggi67@gmail.com>
2018-04-02 21:41:03 +0200

    Update TickConsolidator to process only TickType.Trade ticks

--- Common/Data/Consolidators/TickConsolidator.cs
+++ Common/Data/Consolidators/TickConsolidator.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@ using System;
 using QuantConnect.Data.Market;
 
 namespace QuantConnect.Data.Consolidators
-{    
+{
     /// <summary>
     /// A data consolidator that can make bigger bars from ticks over a given
     /// time span or a count of pieces of data.
@@ -50,6 +50,16 @@ namespace QuantConnect.Data.Consolidators
         public TickConsolidator(int maxCount, TimeSpan period)
             : base(maxCount, period)
         {
+        }
+
+        /// <summary>
+        /// Determines whether or not the specified data should be processd
+        /// </summary>
+        /// <param name="data">The data to check</param>
+        /// <returns>True if the consolidator should process this data, false otherwise</returns>
+        protected override bool ShouldProcess(Tick data)
+        {
+            return data.TickType == TickType.Trade;
         }
 
         /// <summary>
