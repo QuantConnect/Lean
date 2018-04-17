4e6d43f5c7414c99cf8b613469ecf1a6987e3d3f
AlexCatarino <alexandre.catarino@gmail.com>
2018-04-17 13:11:17 +0100

    Adds more locations to PYTHONPATH
    Before loading python algorithms, set `PYTHONPATH` with `CurrentDirectory`,
    algorithm location and current/environment `PYTHONPATH`.

--- AlgorithmFactory/Loader.cs
+++ AlgorithmFactory/Loader.cs
@@ -96,9 +96,6 @@ namespace QuantConnect.AlgorithmFactory
 
             _loaderTimeLimit = loaderTimeLimit;
             _multipleTypeNameResolverFunction = multipleTypeNameResolverFunction;
-
-            //Set the python path for loading python algorithms.
-            Environment.SetEnvironmentVariable("PYTHONPATH", Environment.CurrentDirectory);
         }
 
 
@@ -158,8 +155,11 @@ namespace QuantConnect.AlgorithmFactory
             var pythonFile = new FileInfo(assemblyPath);
             var moduleName = pythonFile.Name.Replace(".pyc", "").Replace(".py", "");
 
-            //Help python find the module
-            Environment.SetEnvironmentVariable("PYTHONPATH", pythonFile.DirectoryName);
+            // Set the python path for loading python algorithms.
+            // For Unix systems (PlatformID 4 and 6), separator is ":" while it is ";" for Windows
+            var separator = (int)Environment.OSVersion.Platform > 3 ? ":" : ";";
+            var pythonPath = new[] { Environment.CurrentDirectory, pythonFile.DirectoryName, Environment.GetEnvironmentVariable("PYTHONPATH") };
+            Environment.SetEnvironmentVariable("PYTHONPATH", string.Join(separator, pythonPath));
 
             try
             {
