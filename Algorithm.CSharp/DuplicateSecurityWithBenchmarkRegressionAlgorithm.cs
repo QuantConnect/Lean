5893a81cbdb9fe843148cda84c366dae8b30660b
Michael Handschuh <mhandschuh@gmail.com>
2018-06-05 16:42:57 -0400

    Fix regression algorithms incorrectly marked as supporting python

--- Algorithm.CSharp/DuplicateSecurityWithBenchmarkRegressionAlgorithm.cs
+++ Algorithm.CSharp/DuplicateSecurityWithBenchmarkRegressionAlgorithm.cs
@@ -77,7 +77,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// This is used by the regression test system to indicate which languages this algorithm is written in.
         /// </summary>
-        public Language[] Languages { get; } = { Language.CSharp, Language.Python };
+        public Language[] Languages { get; } = { Language.CSharp };
 
         /// <summary>
         /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
