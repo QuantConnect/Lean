6f55392149205c59575b8893beaf5b8f36a361c8
Art Wild <wildart@gmail.com>
2018-07-22 01:23:43 -0400

    MV & BL portfolio optimization implementation
    Closes #1998. Closes #2219.

---
+++ Algorithm.CSharp/MeanVarianceOptimizationFrameworkAlgorithm.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System.Collections.Generic;
+using QuantConnect.Algorithm.Framework;
+using QuantConnect.Algorithm.Framework.Alphas;
+using QuantConnect.Algorithm.Framework.Execution;
+using QuantConnect.Algorithm.Framework.Portfolio;
+using QuantConnect.Algorithm.Framework.Risk;
+using QuantConnect.Algorithm.Framework.Selection;
+using QuantConnect.Orders;
+using QuantConnect.Interfaces;
+using System.Linq;
+using QuantConnect.Data.UniverseSelection;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class MeanVarianceOptimizationFrameworkAlgorithm : QCAlgorithmFramework, IRegressionAlgorithmDefinition
+    {
+        private IEnumerable<Symbol> _symbols = (new string[] { "AIG", "BAC", "IBM", "SPY" }).Select(s => QuantConnect.Symbol.Create(s, SecurityType.Equity, Market.USA));
+
+        /// <summary>
+        /// Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
+        /// </summary>
+        public override void Initialize()
+        {
+            // Set requested data resolution
+            UniverseSettings.Resolution = Resolution.Minute;
+
+            SetStartDate(2013, 10, 07);  //Set Start Date
+            SetEndDate(2013, 10, 11);    //Set End Date
+            SetCash(100000);             //Set Strategy Cash
+
+            // Find more symbols here: http://quantconnect.com/data
+            // Forex, CFD, Equities Resolutions: Tick, Second, Minute, Hour, Daily.
+            // Futures Resolution: Tick, Second, Minute
+            // Options Resolution: Minute Only.
+            
+            // set algorithm framework models
+            SetUniverseSelection(new CoarseFundamentalUniverseSelectionModel(CoarseSelector));
+            SetAlpha(new HistoricalReturnsAlphaModel(resolution: Resolution.Daily));
+            SetPortfolioConstruction(new MeanVarianceOptimizationPortfolioConstructionModel());
+            SetExecution(new ImmediateExecutionModel());
+            SetRiskManagement(new NullRiskManagementModel());
+        }
+
+        public IEnumerable<Symbol> CoarseSelector(IEnumerable<CoarseFundamental> coarse)
+        {
+            int last = Time.Day > 8 ? 3 : _symbols.Count();
+            return _symbols.Take(last);
+        }
+
+        public override void OnOrderEvent(OrderEvent orderEvent)
+        {
+            if (orderEvent.Status.IsFill())
+            {
+                Debug(orderEvent.ToString());
+            }
+        }
+
+        public bool CanRunLocally => true;
+
+        /// <summary>
+        /// This is used by the regression test system to indicate which languages this algorithm is written in.
+        /// </summary>
+        public Language[] Languages { get; } = { Language.CSharp, Language.Python };
+
+        /// <summary>
+        /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
+        /// </summary>
+        public Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
+        {
+            {"Total Trades", "14"},
+            {"Average Win", "0.42%"},
+            {"Average Loss", "-0.08%"},
+            {"Compounding Annual Return", "83.733%"},
+            {"Drawdown", "1.700%"},
+            {"Expectancy", "1.671"},
+            {"Net Profit", "0.837%"},
+            {"Sharpe Ratio", "2.239"},
+            {"Loss Rate", "57%"},
+            {"Win Rate", "43%"},
+            {"Profit-Loss Ratio", "5.23"},
+            {"Alpha", "0"},
+            {"Beta", "31.394"},
+            {"Annual Standard Deviation", "0.161"},
+            {"Annual Variance", "0.026"},
+            {"Information Ratio", "2.168"},
+            {"Tracking Error", "0.161"},
+            {"Treynor Ratio", "0.011"},
+            {"Total Fees", "$55.58"},
+            {"Total Insights Generated", "14"},
+            {"Total Insights Closed", "4"},
+            {"Total Insights Analysis Completed", "0"},
+            {"Long Insight Count", "6"},
+            {"Short Insight Count", "4"},
+            {"Long/Short Ratio", "150%"},
+            {"Estimated Monthly Alpha Value", "$0"},
+            {"Total Accumulated Estimated Alpha Value", "$0"},
+            {"Mean Population Estimated Insight Value", "$0"},
+            {"Mean Population Direction", "0%"},
+            {"Mean Population Magnitude", "0%"},
+            {"Rolling Averaged Population Direction", "0%"},
+            {"Rolling Averaged Population Magnitude", "0%"}
+        };
+    }
+}
