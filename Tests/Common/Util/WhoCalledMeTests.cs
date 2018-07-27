6929b6f632466e9cd254754c1e131651d0350536
Martin Molinero <martin.molinero1@gmail.com>
2018-07-27 17:36:36 -0300

    Suppressing inlining on methods that call WhoCalledMe

--- Tests/Common/Util/WhoCalledMeTests.cs
+++ Tests/Common/Util/WhoCalledMeTests.cs
@@ -1,11 +1,11 @@
 ﻿/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,6 +13,7 @@
  * limitations under the License.
 */
 
+using System.Runtime.CompilerServices;
 using NUnit.Framework;
 using QuantConnect.Logging;
 
@@ -22,6 +23,7 @@ namespace QuantConnect.Tests.Common.Util
     public class WhoCalledMeTests
     {
         [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public void GetMethodNameTest()
         {
             string expected = "WhoCalledMeTests.GetMethodNameTest";
