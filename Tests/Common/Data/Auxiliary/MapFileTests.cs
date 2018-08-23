8861f251f8eb343c217a04108df426ac6e023ded
Michael Handschuh <mhandschuh@gmail.com>
2018-08-23 13:11:27 -0400

    Adds MapFile.FirstTicker
    The first ticker and first date are most commonly used to properly create
    security identifiers. Currently resolution of the first ticker is strewn
    about in various places and often times it doesn't take into account the
    case of an empty map file, where the first (and last) ticker are assumed
    to be the permtick after removing the '.<number>' specifier at the end.
    It's important to also not remove '.<letter>' -- as some securities are
    named as such.

---
+++ Tests/Common/Data/Auxiliary/MapFileTests.cs
@@ -0,0 +1,69 @@
+﻿/*
+* QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+* Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using QuantConnect.Data.Auxiliary;
+
+namespace QuantConnect.Tests.Common.Data.Auxiliary
+{
+    [TestFixture]
+    public class MapFileTests
+    {
+        [Test]
+        public void ResolvesFirstTicker()
+        {
+            var mapFile = new MapFile("goog", new List<MapFileRow>
+            {
+                new MapFileRow(new DateTime(2014, 03, 27), "goocv"),
+                new MapFileRow(new DateTime(2014, 04, 02), "goocv"),
+                new MapFileRow(new DateTime(2050, 12, 31), "goog")
+            });
+
+            Assert.AreEqual("goocv", mapFile.FirstTicker);
+        }
+
+        [Test]
+        [TestCase("abc", "ABC")]
+        [TestCase("abc.1", "ABC")]
+        [TestCase("brk.a", "BRK.A")]
+        [TestCase("brk.a.1", "BRK.A")]
+        public void ResolvesFirstTickerFromPermtickIfEmptyMapFile(string permtick, string expectedFirstTicker)
+        {
+            var mapFile = new MapFile(permtick, new List<MapFileRow>());
+            Assert.AreEqual(expectedFirstTicker, mapFile.FirstTicker);
+        }
+
+        [Test]
+        public void ResolvesFirstDate()
+        {
+            var mapFile = new MapFile("goog", new List<MapFileRow>
+            {
+                new MapFileRow(new DateTime(2014, 03, 27), "goocv"),
+                new MapFileRow(new DateTime(2014, 04, 02), "goocv"),
+                new MapFileRow(new DateTime(2050, 12, 31), "goog")
+            });
+
+            Assert.AreEqual(new DateTime(2014, 03, 27), mapFile.FirstDate);
+        }
+
+
+    }
+}
