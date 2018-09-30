2f88dbc7dfc34d303bb72c7c4791e27b44a7894f
Adalyat Nazirov <aenazirov@gmail.com>
2018-09-30 20:41:38 +0300

    Tests: base brokerage tests

--- Tests/Brokerages/BrokerageTests.cs
+++ Tests/Brokerages/BrokerageTests.cs
@@ -464,7 +464,7 @@ namespace QuantConnect.Tests.Brokerages
         }
 
         [Test]
-        public void GetAccountHoldings()
+        public virtual void GetAccountHoldings()
         {
             Log.Trace("");
             Log.Trace("GET ACCOUNT HOLDINGS");
