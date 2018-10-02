af36da329ed8fad46bd481be46d4ee578addb1cf
smartchris84 <smart.chris@tutanota.com>
2018-10-03 01:14:52 +0800

    Licensing, other requested changes

--- Brokerages/Alpaca/Markets/Interfaces/IAggHistoricalItems.cs
+++ Brokerages/Alpaca/Markets/Interfaces/IAggHistoricalItems.cs
@@ -1,4 +1,9 @@
-﻿namespace QuantConnect.Brokerages.Alpaca.Markets
+﻿/*
+ * The official C# API client for alpaca brokerage
+ * Sourced from: https://github.com/alpacahq/alpaca-trade-api-csharp/commit/161b114b4b40d852a14a903bd6e69d26fe637922
+*/
+
+namespace QuantConnect.Brokerages.Alpaca.Markets
 {
     /// <summary>
     /// Encapsulates list of historical aggregates (bars) from Polygon REST API.
--- Brokerages/Alpaca/Markets/RestClient.cs
+++ Brokerages/Alpaca/Markets/RestClient.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * The official C# API client for alpaca brokerage
- * https://github.com/alpacahq/alpaca-trade-api-csharp
+ * Sourced from: https://github.com/alpacahq/alpaca-trade-api-csharp/commit/161b114b4b40d852a14a903bd6e69d26fe637922
 */
 
 using System;
