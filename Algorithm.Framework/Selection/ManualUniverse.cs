eaba3ab24e3c9c225796eab8a4cc1e3f65e1a6b8
Martin Molinero <martin.molinero1@gmail.com>
2018-10-10 11:51:47 -0300

    Fix ManualUniverse duplicate effort
    - Removed usages of algorithm.Securities.key as a parameter for the
    `ManualUniverseSelectionModel()` since those securities, added through
    `AddXXXX` calls will be managed by the `UserDefinedUniverse`. This was
    causing for Universes to try to add the same subscription requests
    - Adding new empty constructor for ManualUniverseSelectionModel,
    required for Python
    - ManualUniverse will return any existing SDC for the
    symbol. This is for maintaining existing behavior and
    preventing breaking changes: Specifically motivated by usages of
    Algorithm.Securities.Keys as constructor parameter of the
    ManualUniverseSelectionModel, since those Symbols added by Addxxx()
    calls will already be managed by the UserDefinedUniverse
    - Making some format modifications to aling with used Lean formatting

--- Algorithm.Framework/Selection/ManualUniverse.cs
+++ Algorithm.Framework/Selection/ManualUniverse.cs
@@ -27,6 +27,8 @@ namespace QuantConnect.Algorithm.Framework.Selection
     /// Defines a universe as a set of manually set symbols. This differs from <see cref="UserDefinedUniverse"/>
     /// in that these securities were not added via AddSecurity.
     /// </summary>
+    /// <remarks>Incompatible with multiple <see cref="Universe"/> selecting the same <see cref="Symbol"/>.
+    /// with different <see cref="SubscriptionDataConfig"/>. More information <see cref="GetSubscriptionRequests"/></remarks>
     public class ManualUniverse : UserDefinedUniverse
     {
         /// <summary>
@@ -60,18 +62,26 @@ namespace QuantConnect.Algorithm.Framework.Selection
         /// <param name="subscriptionService">Instance which implements <see cref="ISubscriptionDataConfigService"/> interface</param>
         /// <returns>All subscriptions required by this security</returns>
         public override IEnumerable<SubscriptionRequest> GetSubscriptionRequests(Security security, DateTime currentTimeUtc, DateTime maximumEndTimeUtc,
-                                                                                ISubscriptionDataConfigService subscriptionService)
+            ISubscriptionDataConfigService subscriptionService)
         {
-            return security.Subscriptions.Select(config =>
-                new SubscriptionRequest(
-                    isUniverseSubscription: false,
-                    universe: this,
-                    security: security,
-                    configuration: new SubscriptionDataConfig(config, isInternalFeed: false),
-                    startTimeUtc: currentTimeUtc,
-                    endTimeUtc: maximumEndTimeUtc
-                )
-            );
+            // ManualUniverse will return any existing SDC for the symbol, else will create new, using universe settings.
+            // This is for maintaining existing behavior and preventing breaking changes: Specifically motivated
+            // by usages of Algorithm.Securities.Keys as constructor parameter of the ManualUniverseSelectionModel.
+            // Symbols at Algorithm.Securities.Keys added by Addxxx() calls will already be added by the UserDefinedUniverse.
+
+            var existingSubscriptionDataConfigs = subscriptionService.GetSubscriptionDataConfigs(security.Symbol);
+
+            if (existingSubscriptionDataConfigs.Any())
+            {
+                return existingSubscriptionDataConfigs.Select(
+                    config => new SubscriptionRequest(isUniverseSubscription: false,
+                        universe: this,
+                        security: security,
+                        configuration: config,
+                        startTimeUtc: currentTimeUtc,
+                        endTimeUtc: maximumEndTimeUtc));
+            }
+            return base.GetSubscriptionRequests(security, currentTimeUtc, maximumEndTimeUtc, subscriptionService);
         }
     }
 }
