ea55b8ba09cffefa238ccb4bea05063925b45a57
Stefano Raggi <stefano.raggi67@gmail.com>
2018-10-22 14:48:16 +0200

    Fix failing CustomDataRegressionAlgorithm

--- Algorithm.Python/CustomDataRegressionAlgorithm.py
+++ Algorithm.Python/CustomDataRegressionAlgorithm.py
@@ -60,7 +60,7 @@ class Bitcoin(PythonData):
 
         #return "http://my-ftp-server.com/futures-data-" + date.ToString("Ymd") + ".zip"
         # OR simply return a fixed small data file. Large files will slow down your backtest
-        return SubscriptionDataSource("https://www.quandl.com/api/v3/datasets/BCHARTS/BITSTAMPUSD.csv?order=asc&api_key=WyAazVXnq7ATy_fefTqm", SubscriptionTransportMedium.RemoteFile)
+        return SubscriptionDataSource("https://www.quantconnect.com/api/v2/proxy/quandl/api/v3/datasets/BCHARTS/BITSTAMPUSD.csv?order=asc&api_key=WyAazVXnq7ATy_fefTqm", SubscriptionTransportMedium.RemoteFile)
 
 
     def Reader(self, config, line, date, isLiveMode):
