8c853543c0157617c15101b0f4bb7f5c4b097021
AlexCatarino <alexandre.catarino@gmail.com>
2018-11-08 15:03:36 +0000

    Adds ContainsKey check in MacdAlphaModel
    In `MacdAlphaModel.OnSecuritiesChanged`, a missing `ContainsKey` is not preventing a second key addition to a dictionary.
    
    Closes #2656

--- Algorithm.Framework/Alphas/MacdAlphaModel.cs
+++ Algorithm.Framework/Alphas/MacdAlphaModel.cs
@@ -111,6 +111,10 @@ namespace QuantConnect.Algorithm.Framework.Alphas
         {
             foreach (var added in changes.AddedSecurities)
             {
+                if (_symbolData.ContainsKey(added.Symbol))
+                {
+                    continue;
+                }
                 _symbolData.Add(added.Symbol, new SymbolData(algorithm, added, _fastPeriod, _slowPeriod, _signalPeriod, _movingAverageType, _resolution));
             }
 
@@ -121,6 +125,7 @@ namespace QuantConnect.Algorithm.Framework.Alphas
                 {
                     // clean up our consolidator
                     algorithm.SubscriptionManager.RemoveConsolidator(data.Security.Symbol, data.Consolidator);
+                    _symbolData.Remove(removed.Symbol);
                 }
             }
         }
