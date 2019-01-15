22ba5d431f8994b2214755e0e6031d4ddf7536be
Michael Handschuh <mhandschuh@gmail.com>
2019-01-15 14:33:10 -0500

    #2824: Add RandomValueGenerator
     The random value generator aims to abstract away the generation of the
     key bits of data from the toolbox project. This provides a baseline
     implementation for anyone who wishes to customize their data randomizaton.
     Simply subclass and override the desired methods. A full test suite is
     included to ensure the data generated meets specifications.

---
+++ ToolBox/RandomDataGenerator/RandomValueGeneratorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QuantConnect.ToolBox.RandomDataGenerator
+{
+    /// <summary>
+    /// Provides a base class for exceptions thrown by implementations of <see cref="IRandomValueGenerator"/>
+    /// </summary>
+    public class RandomValueGeneratorException : ApplicationException
+    {
+        public RandomValueGeneratorException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ ToolBox/RandomDataGenerator/TooManyFailedAttemptsException.cs
@@ -0,0 +1,13 @@
+﻿namespace QuantConnect.ToolBox.RandomDataGenerator
+{
+    /// <summary>
+    /// Exception thrown when multiple attempts to generate a valid random value end in failure
+    /// </summary>
+    public class TooManyFailedAttemptsException : RandomValueGeneratorException
+    {
+        public TooManyFailedAttemptsException(string method, int attempts)
+            : base($"Failed to generate a valid value for '{method}' after {attempts} attempts.")
+        {
+        }
+    }
+}
