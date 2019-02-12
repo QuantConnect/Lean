8309b57839fb891eb60c78e1d111a4ab08021504
Juan José D'Ambrosio <jjdambrosio@gmail.com>
2019-02-12 15:21:07 +0000

    Add Update method to TickAggregator abstract class

--- ToolBox/TickAggregator.cs
+++ ToolBox/TickAggregator.cs
@@ -39,6 +39,13 @@ namespace QuantConnect.ToolBox
         public Resolution Resolution { get; }
 
         /// <summary>
+        /// Updates the consolidator with the specified bar.
+        /// </summary>
+        /// <param name="data">The latest data observation.</param>
+        public virtual void Update(BaseData data)
+        { }
+
+        /// <summary>
         /// Return all the consolidated data as well as the
         /// bar the consolidator is currently working on
         /// </summary>
