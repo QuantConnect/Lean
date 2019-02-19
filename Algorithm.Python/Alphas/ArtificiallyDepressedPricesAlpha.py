0973038f2a286361e7b92277d5b229ef15881fa7
Jack Simonson <jack.simonson@quantconnect.com>
2019-02-19 14:52:46 -0800

    Alpha Models
    New AlphaModels for Algorithm.Python/Alphas

---
+++ Algorithm.Python/Alphas/ArtificiallyDepressedPricesAlpha.py
@@ -0,0 +1,200 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+''' 
+    Assets whose current trading volume is below its average volume and
+    whose price is below its recent average are sometiems viewed as being
+    priced artificially low, and as such will rebound in value once trading
+    volume picks up again. Using Simple Moving Average indicators, we check
+    for these conditions and generate insights acoordingly for the top 50
+    assets by Dollar Volume using a Coarse Universe selection function.
+'''
+
+from System import *
+from QuantConnect import *
+from QuantConnect.Indicators import *
+from QuantConnect.Algorithm import *
+from QuantConnect.Algorithm.Framework import *
+from QuantConnect.Algorithm.Framework.Risk import *
+from QuantConnect.Algorithm.Framework.Alphas import *
+from QuantConnect.Algorithm.Framework.Selection import * 
+from QuantConnect.Algorithm.Framework.Execution import *
+from QuantConnect.Algorithm.Framework.Portfolio import PortfolioTarget, EqualWeightingPortfolioConstructionModel
+
+import numpy as np
+from datetime import timedelta, datetime
+
+
+class VolumeValueDirectionAlgorithm(QCAlgorithmFramework):
+
+    def Initialize(self):
+
+        self.SetStartDate(2018, 2, 1)   #Set Start Date
+        self.SetCash(100000)            #Set Strategy Cash
+
+        ## To be used later in Universe Selection
+        self.symbols = None
+        self.month = None
+
+        ## Coarse/Fine Universe Selection
+        self.UniverseSettings.Resolution = Resolution.Hour
+        self.SetUniverseSelection(CoarseFundamentalUniverseSelectionModel(self.CoarseSelectionFunction) )
+
+        ## Set our custom Alpha Model
+        self.SetAlpha(VolumeValueAnalysisAlphaModel())
+    
+        ## Set equal weighting portfolio contruction
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
+    
+        ## Set immediate execution
+        self.SetExecution(ImmediateExecutionModel())
+    
+        ## Set null risk management
+        self.SetRiskManagement(NullRiskManagementModel())
+    
+
+    def CoarseSelectionFunction(self, coarse):
+    
+        ## This control ensures that we only update our universe once per month
+        if self.Time.month == self.month:
+            return self.symbols
+        
+        self.month = self.Time.month
+    
+        ## Sort by dollar volume
+        sortedByDollarVolume = sorted(coarse, key=lambda x: x.DollarVolume, reverse=True)
+
+        filtered = [ x.Symbol for x in sortedByDollarVolume ]
+        
+        ## Take top 25 companies
+        self.symbols = filtered[:25]
+
+        return self.symbols
+
+
+class VolumeValueAnalysisAlphaModel:
+    
+    def __init__(self, *args, **kwargs):
+        '''
+            Initialize variables and Symbol Data dictionary to use in our Alpha Model
+        '''
+        self.lookback = kwargs['lookback'] if 'lookback' in kwargs else 10          ## This will set the SMA period, so the default is a 5-day SMA
+        self.resolution = kwargs['resolution'] if 'resolution' in kwargs else Resolution.Daily
+        self.prediction_interval = Time.Multiply(Extensions.ToTimeSpan(self.resolution), 3) ## Arbitrary
+        self.symbolDataBySymbol = {}
+        self.close = {}
+        self.volume = {}
+        
+        
+    def Update(self, algorithm, data):
+        insights = []
+        
+        ## Loop through all of the Symbol Data objects in the dictionary
+        for symbol, symbolData in self.symbolDataBySymbol.items():
+
+            # Update the symbolData with new data 
+            if not symbolData.Update(data): return insights
+
+            ## Check to see if both current asset price and volume are above their respective SMA values.
+            ## If so, emit insights accordingly
+            if symbolData.IsUpTrend():
+                insights.append(Insight(symbolData.Symbol, self.prediction_interval, InsightType.Price, InsightDirection.Up, symbolData.CloseSMA.Current.Value, None))
+
+        return insights
+        
+    def OnSecuritiesChanged(self, algorithm, changes):
+        '''Event fired each time the we add/remove securities from the data feed
+        Args:
+            algorithm: The algorithm instance that experienced the change in securities
+            changes: The security additions and removals from the algorithm'''
+        for removed in changes.RemovedSecurities:
+            algorithm.Log('Removed: ' + str(removed.Symbol))
+            symbolData = self.symbolDataBySymbol.pop(removed.Symbol, None)
+            if symbolData is not None:
+                symbolData.RemoveConsolidators(algorithm)
+
+
+        # initialize data for added securities
+        symbols = [ x.Symbol for x in changes.AddedSecurities ]
+        
+        ## Get historical data to warm-up indicators
+        history = algorithm.History(symbols, self.lookback, self.resolution)
+        if history.empty: return
+
+        tickers = history.index.levels[0]
+        for ticker in tickers:
+            symbol = SymbolCache.GetSymbol(ticker)
+
+            if symbol not in self.symbolDataBySymbol:
+                
+                ## Create SymbolData objects for any new assets
+                symbolData = SymbolData(symbol, self.lookback)
+                self.symbolDataBySymbol[symbol] = symbolData
+                symbolData.RegisterIndicators(algorithm, self.resolution)
+                symbolData.WarmUpIndicators(history.loc[ticker])
+
+
+class SymbolData:
+    def __init__(self, symbol, lookback):
+        self.Symbol = symbol
+        self.Close = 0
+        self.Volume = 0
+        self.CloseSMA = SimpleMovingAverage(f'{symbol}.CloseSMA({lookback})', lookback)
+        self.VolumeSMA = SimpleMovingAverage(f'{symbol}.VolumeSMA({lookback})', lookback)
+        self.Consolidator = None
+        
+    def Update(self, data):
+        ## Check to see if symbol is in the data dictionary
+        if self.Symbol not in data.Keys:
+            return False
+        
+        ## Check to see if data contains a special event, which
+        ## will result in no TradeBar being passed in OnData()
+        ## and so it will skip creating any insights for that slice
+        if data.Splits.ContainsKey(self.Symbol) or \
+           data.Dividends.ContainsKey(self.Symbol) or \
+           data.Delistings.ContainsKey(self.Symbol) or \
+           data.SymbolChangedEvents.ContainsKey(self.Symbol):
+            return False
+        
+        ## Update close, volume, and SMAs
+        bar = data[self.Symbol]
+        self.Close = bar.Close
+        self.Volume = bar.Volume
+        self.CloseSMA.Update(bar.EndTime, bar.Close)
+        self.VolumeSMA.Update(bar.EndTime, bar.Volume)
+        return True
+
+    def RegisterIndicators(self, algorithm, resolution):
+        self.Consolidator = algorithm.ResolveConsolidator(self.Symbol, resolution)
+        algorithm.RegisterIndicator(self.Symbol, self.CloseSMA, self.Consolidator)
+        algorithm.RegisterIndicator(self.Symbol, self.VolumeSMA, self.Consolidator)        
+
+    def RemoveConsolidators(self, algorithm):
+        if self.Consolidator is not None:
+            algorithm.SubscriptionManager.RemoveConsolidator(self.Symbol, self.Consolidator)
+
+    def WarmUpIndicators(self, history):
+        for tuple in history.itertuples():
+            self.Close = tuple.close
+            self.Volume = tuple.volume
+            self.CloseSMA.Update(tuple.Index, self.Close)
+            self.VolumeSMA.Update(tuple.Index, self.Volume)
+            
+    def IsUpTrend(self):
+        ## UpTrend is when the close and volume are below their respective means,
+        ## and so the projection is that the price will rebound
+        return self.Close < self.CloseSMA.Current.Value and \
+               self.Volume < self.VolumeSMA.Current.Value
+
+<br><br>This alpha is part of the Benchmark Alpha Series created by QuantConnect which are open sourced so the community and client funds can see an example of an alpha. You can read the source code for this alpha on Github in  <a href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.CSharp/Alphas/ArtificiallyDepressedPricesAlpha.cs">C#</a> or  <a href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Python/Alphas/ArtificiallyDepressedPricesAlpha.py">Python</a>.
---
+++ Algorithm.Python/Alphas/GasAndCrudeOilEnergyCorrelationAlpha.py
@@ -0,0 +1,256 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+    Energy prices, especially Oil and Natural Gas, are in general fairly correlated,
+    meaning they typically move in the same direction as an overall trend. This Alpha
+    uses this idea and implements an Alpha Model that takes Natural Gas ETF price
+    movements as a leading indicator for Crude Oil ETF price movements. We take the
+    Natural Gas/Crude Oil ETF pair with the highest historical price correlation and
+    then create insights for Crude Oil depending on whether or not the Natural Gas ETF price change
+    is above/below a certain threshold that we set (arbitrarily).
+'''
+
+from clr import AddReference
+AddReference("System")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+AddReference("QuantConnect.Indicators")
+
+from System import *
+from QuantConnect import *
+from QuantConnect.Algorithm import *
+from QuantConnect.Indicators import *
+from QuantConnect.Algorithm.Framework import *
+from QuantConnect.Algorithm.Framework.Risk import *
+from QuantConnect.Algorithm.Framework.Alphas import *
+from QuantConnect.Algorithm.Framework.Selection import *
+from QuantConnect.Algorithm.Framework.Execution import *
+from QuantConnect.Algorithm.Framework.Portfolio import PortfolioTarget, EqualWeightingPortfolioConstructionModel
+
+import numpy as np
+from scipy import stats
+from scipy.stats import kendalltau 
+from datetime import timedelta, datetime
+
+class EnergyETFPairsTradingAlgorithm(QCAlgorithmFramework):
+
+    def Initialize(self):
+        
+        ## Pick 3 natural gas ETFs and 3 crude oil ETFs and use all 9 possible combinations
+        ## One combination of natural gas ETF/crude oil ETF will be used
+        self.tickers = [["UNG","UNG","UNG",'BOIL','BOIL','BOIL','UNL','UNL','UNL'],   ## Natural gas ETFs
+                        ["USO",'UCO','DBO',"USO",'UCO','DBO',"USO",'UCO','DBO']]      ## Crude oil ETFs
+        tickers = ['UNG','BOIL','UNL','USO','UCO','DBO']
+        self.SetStartDate(2019, 1, 1)   #Set Start Date
+        self.SetCash(100000)           #Set Strategy Cash
+        
+        ## Set Universe resolution and subscribed to data
+        self.Universe.Resolution = Resolution.Minute
+        symbols = [ Symbol.Create(ticker, SecurityType.Equity, Market.USA) for ticker in tickers ]
+
+        ## Manual Universe Selection
+        self.SetUniverseSelection( ManualUniverseSelectionModel(symbols) )
+        
+        ## Custom Alpha Model
+        self.SetAlpha(PairsAlphaModel(pairs_tickers = self.tickers, history_days = 90))
+        
+        ## Equal-weight our positions, in this case 100% in USO
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel(resolution = Resolution.Minute))
+
+        ## Immediate Execution Fill Model        
+        self.SetExecution(CustomExecutionModel())
+        
+        ## Null Risk-Management Model
+        self.SetRiskManagement(NullRiskManagementModel())
+        
+    def OnOrderEvent(self, orderEvent):
+        if orderEvent.Status == OrderStatus.Filled:
+            self.Debug("Purchased Stock: {0}".format(orderEvent.Symbol))
+            
+    def OnEndOfAlgorithm(self):
+        for kvp in self.Portfolio:
+            if self.Portfolio[kvp.Key].Invested:
+                self.Log('Invested in: ' + str(kvp.Key))
+
+
+class PairsAlphaModel:
+    '''This Alpha model assumes that the ETF for natural gas is a good leading-indicator
+        of the price of the crude oil ETF. The model will take in arguments for a threshold
+        at which the model triggers an insight, the length of the look-back period for evaluating
+        rate-of-change of UNG prices, and the duration of the insight'''
+    
+    def __init__(self, *args, **kwargs):
+        
+        self.difference_trigger = kwargs['difference_trigger'] if 'difference_trigger' in kwargs else 2.5
+        self.lookback = kwargs['lookback'] if 'lookback' in kwargs else 5 ## In hours
+        self.history_days = kwargs['history_days'] if 'history_days' in kwargs else 90 ## In days
+        self.resolution = kwargs['resolution'] if 'resolution' in kwargs else Resolution.Hour
+        self.prediction_interval = Time.Multiply(Extensions.ToTimeSpan(self.resolution), 5) ## Arbitrary
+        self.symbolDataBySymbol = {}
+        self.pairs_tickers = kwargs['pairs_tickers'] if 'pairs_tickers' in kwargs else None
+        self.next_update = None
+        
+    def Update(self, algorithm, data):
+        
+        if (self.next_update is None) or (algorithm.Time > self.next_update):
+            self.pairs = self.CorrelationPairsSelection(algorithm)
+            self.next_update = algorithm.Time + (timedelta(days = 30))
+        
+        '''Updates this alpha model with the latest data from the algorithm.
+        This is called each time the algorithm receives data for subscribed securities
+        Args:
+            algorithm: The algorithm instance
+            data: The new data available
+        Returns:
+            The new insights generated'''
+            
+        ## Build a list to hold our insights
+        insights = []
+        
+        ## These lists hold the Symbol for the following ETF and data from the leading ETF that we need to pass into the Insight() constructor
+        leading_data = []       
+        following_symbol = []    
+        
+        for symbol, symbolData in self.symbolDataBySymbol.items():
+            if symbol.Value == self.pairs[0]:
+                leading_data.append(symbolData)             ## Add symbol data if it's Natural Gas
+            elif symbol.Value == self.pairs[1]:
+                following_symbol.append(symbol)             ## Stash the Symbol object if it's Crude Oil
+        
+        for i in range(len(following_symbol)):
+            symbolData = leading_data[i]
+            if symbolData.Return > self.difference_trigger:      ## Check if Natural Gas returns are greater than the threshold we've set
+                ## If so, create and Insight with this information for Crude Oil
+                insights.append(Insight(following_symbol[i], timedelta(minutes=5), InsightType.Price, InsightDirection.Up, symbolData.Return, None))
+
+            elif symbolData.Return < -self.difference_trigger:   ## Check if UNG returns are greater than the threshold we've set
+                insights.append(Insight(following_symbol[i], timedelta(minutes=5), InsightType.Price, InsightDirection.Down, symbolData.Return, None))
+
+        return insights
+    
+    def CorrelationPairsSelection(self, algorithm):
+        tick_syl = self.pairs_tickers
+        logreturn={}
+        ## Get log returns for each natural gas/oil ETF pair
+        for i in range(2):
+            syl = tick_syl[i]
+            history = algorithm.History(syl, self.history_days,Resolution.Daily)
+            # generate the log return series of paired stocks
+            close = history['close'].unstack(level=0)
+            df_logreturn = (np.log(close) - np.log(close.shift(1))).dropna()
+            for j in tick_syl[i]:
+                logreturn[j] = df_logreturn[j]
+        
+        ## Estimate coefficients of different correlation measures 
+        tau_coef = []
+        for i in range(len(tick_syl[i])):
+            tik_x, tik_y= logreturn[tick_syl[0][i]], logreturn[tick_syl[1][i]]
+            tau_coef.append(kendalltau(tik_x, tik_y)[0]) 
+        index_max = tau_coef.index(max(tau_coef))    
+        pair = [tick_syl[0][index_max],tick_syl[1][index_max]]
+        
+        ## Return the pair with highest historical correlation
+        return pair
+        
+    def OnSecuritiesChanged(self, algorithm, changes):
+        '''Event fired each time the we add/remove securities from the data feed
+        Args:
+            algorithm: The algorithm instance that experienced the change in securities
+            changes: The security additions and removals from the algorithm'''
+        for removed in changes.RemovedSecurities:
+            algorithm.Log('Removed: ' + str(removed.Symbol))
+            symbolData = self.symbolDataBySymbol.pop(removed.Symbol, None)
+            if symbolData is not None:
+                symbolData.RemoveConsolidators(algorithm)
+
+        # initialize data for added securities
+        symbols = [ x.Symbol for x in changes.AddedSecurities ]
+        history = algorithm.History(symbols, self.lookback, self.resolution)
+        if history.empty: return
+
+        tickers = history.index.levels[0]
+        for ticker in tickers:
+            symbol = SymbolCache.GetSymbol(ticker)
+
+            if symbol not in self.symbolDataBySymbol:
+                symbolData = SymbolData(symbol, self.lookback)
+                self.symbolDataBySymbol[symbol] = symbolData
+                symbolData.RegisterIndicators(algorithm, self.resolution)
+                symbolData.WarmUpIndicators(history.loc[ticker])
+        
+class SymbolData:
+    '''Contains data specific to a symbol required by this model'''
+    def __init__(self, symbol, lookback):
+        self.Symbol = symbol
+        self.ROC = RateOfChangePercent('{}.ROC({})'.format(symbol, lookback), lookback)
+        self.Consolidator = None
+        self.previous = 0
+
+    def RegisterIndicators(self, algorithm, resolution):
+        self.Consolidator = algorithm.ResolveConsolidator(self.Symbol, resolution)
+        algorithm.RegisterIndicator(self.Symbol, self.ROC, self.Consolidator)
+
+    def RemoveConsolidators(self, algorithm):
+        if self.Consolidator is not None:
+            algorithm.SubscriptionManager.RemoveConsolidator(self.Symbol, self.Consolidator)
+
+    def WarmUpIndicators(self, history):
+        for tuple in history.itertuples():
+            self.ROC.Update(tuple.Index, tuple.close)
+
+    @property
+    def Return(self):
+        return float(self.ROC.Current.Value)
+
+    @property
+    def CanEmit(self):
+        if self.previous == self.ROC.Samples:
+            return False
+
+        self.previous = self.ROC.Samples
+        return self.ROC.IsReady
+
+    def __str__(self, **kwargs):
+        return '{}: {:.2%}'.format(self.ROC.Name, (1 + self.Return)**252 - 1)
+        
+
+class CustomExecutionModel(ExecutionModel):
+    '''Provides an implementation of IExecutionModel that immediately submits market orders to achieve the desired portfolio targets'''
+
+    def __init__(self):
+        '''Initializes a new instance of the ImmediateExecutionModel class'''
+        self.targetsCollection = PortfolioTargetCollection()
+        self.previous_symbol = None
+
+    def Execute(self, algorithm, targets):
+        '''Immediately submits orders for the specified portfolio targets.
+        Args:
+            algorithm: The algorithm instance
+            targets: The portfolio targets to be ordered'''
+
+        self.targetsCollection.AddRange(targets)
+
+        for target in self.targetsCollection.OrderByMarginImpact(algorithm):
+            open_quantity = sum([x.Quantity for x in algorithm.Transactions.GetOpenOrders(target.Symbol)])
+            existing = algorithm.Securities[target.Symbol].Holdings.Quantity + open_quantity
+            quantity = target.Quantity - existing
+            ## Liquidate positions in Crude Oil ETF that is no longer part of the highest-correlation pair
+            if (str(target.Symbol) != str(self.previous_symbol)) and (self.previous_symbol is not None):
+                algorithm.Liquidate(self.previous_symbol)
+            if quantity != 0:
+                algorithm.MarketOrder(target.Symbol, quantity)
+                self.previous_symbol = target.Symbol
+        self.targetsCollection.ClearFulfilled(algorithm)
+
+<br><br>This alpha is part of the Benchmark Alpha Series created by QuantConnect which are open sourced so the community and client funds can see an example of an alpha. You can read the source code for this alpha on Github in  <a href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.CSharp/Alphas/GasAndCrudeOilEnergyCorrelationAlpha.cs">C#</a> or  <a href="https://github.com/QuantConnect/Lean/blob/master/Algorithm.Python/Alphas/GasAndCrudeOilEnergyCorrelationAlpha.py">Python</a>.
