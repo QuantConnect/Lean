f0cacfaf585156ae525b2edd4cb7aa0f240342ed
Jack Simonson <jack.simonson@quantconnect.com>
2019-02-27 13:56:26 -0800

    Added QC header info

--- Algorithm.Python/Alphas/ContingentClaimsAnalysisDefaultPredictionAlpha.py
+++ Algorithm.Python/Alphas/ContingentClaimsAnalysisDefaultPredictionAlpha.py
@@ -1,3 +1,16 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ''' Contingent Claim Analysis is put forth by Robert Merton, recepient of the Noble Prize in Economics in 1997 for his work in contributing to
     Black-Scholes option pricing theory, which says that the equity market value of stockholders’ equity is given by the Black-Scholes solution
     for a European call option. This equation takes into account Debt, which in CCA is the equivalent to a strike price in the BS solution. The probability
