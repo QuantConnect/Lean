df6bdedd64d32c2dfb1f5debfd8e53556d43d0f7
AlexCatarino <alexandre.catarino@gmail.com>
2019-03-05 22:53:38 +0000

    Adds GreenblattMagicFormulaAlpha
    - Adds C# version
    - Minor improments in python version

--- Algorithm.Python/Alphas/GreenblattMagicFormulaAlpha.py
+++ Algorithm.Python/Alphas/GreenblattMagicFormulaAlpha.py
@@ -29,7 +29,7 @@ from datetime import timedelta, datetime
 from math import ceil
 from itertools import chain
 
-# 
+#
 # This alpha picks stocks according to Joel Greenblatt's Magic Formula.
 # First, each stock is ranked depending on the relative value of the ratio EV/EBITDA. For example, a stock
 # that has the lowest EV/EBITDA ratio in the security universe receives a score of one while a stock that has 
@@ -43,7 +43,7 @@ from itertools import chain
 #
 # This alpha is part of the Benchmark Alpha Series created by QuantConnect which are open
 # sourced so the community and client funds can see an example of an alpha.
-# 
+#
 
 class GreenblattMagicFormulaAlpha(QCAlgorithmFramework):
     ''' Alpha Streams: Benchmark Alpha: Pick stocks according to Joel Greenblatt's Magic Formula'''
@@ -52,7 +52,7 @@ class GreenblattMagicFormulaAlpha(QCAlgorithmFramework):
 
         self.SetStartDate(2018, 1, 1)
         self.SetCash(100000)
-        
+
         #Set zero transaction fees
         self.SetSecurityInitializer(lambda security: security.SetFeeModel(ConstantFeeModel(0)))
 
@@ -64,23 +64,22 @@ class GreenblattMagicFormulaAlpha(QCAlgorithmFramework):
 
         # Equally weigh securities in portfolio, based on insights
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
-        
+
         ## Set Immediate Execution Model
         self.SetExecution(ImmediateExecutionModel())
 
         ## Set Null Risk Management Model
         self.SetRiskManagement(NullRiskManagementModel())
-        
 
 class RateOfChangeAlphaModel(AlphaModel):
     '''Uses Rate of Change (ROC) to create magnitude prediction for insights.'''
 
     def __init__(self, *args, **kwargs): 
-        self.lookback = kwargs['lookback'] if 'lookback' in kwargs else 1
-        self.resolution = kwargs['resolution'] if 'resolution' in kwargs else Resolution.Daily
+        self.lookback = kwargs.get('lookback', 1)
+        self.resolution = kwargs.get('resolution', Resolution.Daily)
         self.predictionInterval = Time.Multiply(Extensions.ToTimeSpan(self.resolution), self.lookback)
         self.symbolDataBySymbol = {}
-        
+
     def Update(self, algorithm, data):
         insights = []
         for symbol, symbolData in self.symbolDataBySymbol.items():
@@ -89,7 +88,7 @@ class RateOfChangeAlphaModel(AlphaModel):
         return insights
 
     def OnSecuritiesChanged(self, algorithm, changes):
-        
+
         # clean up data for removed securities
         for removed in changes.RemovedSecurities:
             symbolData = self.symbolDataBySymbol.pop(removed.Symbol, None)
@@ -116,7 +115,7 @@ class SymbolData:
     '''Contains data specific to a symbol required by this model'''
     def __init__(self, symbol, lookback):
         self.Symbol = symbol
-        self.ROC = RateOfChange('{}.ROC({})'.format(symbol, lookback), lookback)
+        self.ROC = RateOfChange(f'{symbol}.ROC({lookback})', lookback)
         self.Consolidator = None
         self.previous = 0
 
@@ -146,7 +145,7 @@ class SymbolData:
 
     def __str__(self, **kwargs):
         return '{}: {:.2%}'.format(self.ROC.Name, (1 + self.Return)**252 - 1)
-        
+
 
 class GreenBlattMagicFormulaUniverseSelectionModel(FundamentalUniverseSelectionModel):
     '''Defines a universe according to Joel Greenblatt's Magic Formula, as a universe selection model for the framework algorithm. 
@@ -159,14 +158,14 @@ class GreenBlattMagicFormulaUniverseSelectionModel(FundamentalUniverseSelectionM
                  securityInitializer = None):
         '''Initializes a new default instance of the MagicFormulaUniverseSelectionModel'''
         super().__init__(filterFineData, universeSettings, securityInitializer)
-        
+
         # Number of stocks in Coarse Universe
         self.NumberOfSymbolsCoarse = 500
         # Number of sorted stocks in the fine selection subset using the valuation ratio, EV to EBITDA (EV/EBITDA)
         self.NumberOfSymbolsFine = 20
         # Final number of stocks in security list, after sorted by the valuation ratio, Return on Assets (ROA)
         self.NumberOfSymbolsInPortfolio = 10
-        
+
         self.lastMonth = -1
         self.dollarVolumeBySymbol = {}
         self.symbols = []
@@ -176,7 +175,6 @@ class GreenBlattMagicFormulaUniverseSelectionModel(FundamentalUniverseSelectionM
         The stocks must have fundamental data
         The stock must have positive previous-day close price
         The stock must have positive volume on the previous trading day'''
-        
         month = algorithm.Time.month
         if month == self.lastMonth:
             return self.symbols
@@ -186,9 +184,7 @@ class GreenBlattMagicFormulaUniverseSelectionModel(FundamentalUniverseSelectionM
         # The stocks must have fundamental data
         # The stock must have positive previous-day close price
         # The stock must have positive volume on the previous trading day
-        filtered = [x for x in coarse if x.HasFundamentalData
-                                      and x.Volume > 0
-                                      and x.Price > 0]
+        filtered = [x for x in coarse if x.HasFundamentalData]
         # sort the stocks by dollar volume and take the top 1000
         top = sorted(filtered, key=lambda x: x.DollarVolume, reverse=True)[:self.NumberOfSymbolsCoarse]
 
@@ -205,8 +201,7 @@ class GreenBlattMagicFormulaUniverseSelectionModel(FundamentalUniverseSelectionM
         The stock must be traded on either the NYSE or NASDAQ
         At least half a year since its initial public offering
         The stock's market cap must be greater than 500 million
-        
-        
+
         Magic Formula: Rank stocks by Enterprise Value to EBITDA (EV/EBITDA)
         Rank subset of previously ranked stocks (EV/EBITDA), using the valuation ratio Return on Assets (ROA)'''
 
@@ -231,24 +226,20 @@ class GreenBlattMagicFormulaUniverseSelectionModel(FundamentalUniverseSelectionM
             value = sorted(value, key=lambda x: self.dollarVolumeBySymbol[x.Symbol], reverse = True)
             myDict[key] = value[:ceil(len(value) * percent)]
 
-
         # stocks in QC500 universe 
-        topFine = list(chain.from_iterable(myDict.values()))[:self.NumberOfSymbolsCoarse]
-
+        topFine = chain.from_iterable(myDict.values())
 
         #  Magic Formula: 
         ## Rank stocks by Enterprise Value to EBITDA (EV/EBITDA)
         ## Rank subset of previously ranked stocks (EV/EBITDA), using the valuation ratio Return on Assets (ROA)
 
-        
         # sort stocks in the security universe of QC500 based on Enterprise Value to EBITDA valuation ratio
         sortedByEVToEBITDA = sorted(topFine, key=lambda x: x.ValuationRatios.EVToEBITDA , reverse=True)
 
         # sort subset of stocks that have been sorted by Enterprise Value to EBITDA, based on the valuation ratio Return on Assets (ROA)
         sortedByROA = sorted(sortedByEVToEBITDA[:self.NumberOfSymbolsFine], key=lambda x: x.ValuationRatios.ForwardROA, reverse=False)
-        
+
         # retrieve list of securites in portfolio
-        top = sortedByROA[:self.NumberOfSymbolsInPortfolio]
-        self.symbols = [f.Symbol for f in top]
-        
+        self.symbols = [f.Symbol for f in sortedByROA[:self.NumberOfSymbolsInPortfolio]]
+
         return self.symbols
