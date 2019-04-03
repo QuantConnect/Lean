dfe1d3f092548964a8bffffe947e14167bbf0d3f
AlexCatarino <alexandre.catarino@gmail.com>
2019-04-03 20:52:07 +0100

    Replaces Exception for Error Logging when OnData for Custom Data is not Defined

--- Engine/AlgorithmManager.cs
+++ Engine/AlgorithmManager.cs
@@ -190,11 +190,9 @@ namespace QuantConnect.Lean.Engine
                     //If we couldnt find the event handler, let the user know we can't fire that event.
                     if (genericMethod == null && !hasOnDataSlice)
                     {
-                        algorithm.RunTimeError = new Exception("Data event handler not found, please create a function matching this template: public void OnData(" + config.Type.Name + " data) {  }");
-                        _algorithm.Status = AlgorithmStatus.RuntimeError;
-                        return;
+                        algorithm.Error("Data event handler not found, please create a function matching this template: public void OnData(" + config.Type.Name + " data) {  }");
                     }
-                    if (genericMethod != null)
+                    else if (genericMethod != null)
                     {
                         methodInvokers.Add(config.Type, genericMethod.DelegateForCallMethod());
                     }
