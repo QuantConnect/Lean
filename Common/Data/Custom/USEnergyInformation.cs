4f97f2c719143243aacf446e7d47dbbd58665238
AlexCatarino <alexandre.catarino@gmail.com>
2019-04-26 11:20:39 +0100

    Fixes Bugs and Refactors USEnergyInformation class
    The `USEnergyInformation.Reader` was not taking into account the data time zone and the date of the data collection should be the end time of the series.
    
    - Adds missings auth code logic in `SubscriptionDataReader`.

--- Common/Data/Custom/USEnergyInformation.cs
+++ Common/Data/Custom/USEnergyInformation.cs
@@ -13,13 +13,11 @@
  * limitations under the License.
 */
 
-using System;
-using System.Linq;
-using System.Data;
-using System.Globalization;
 using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
 using QuantConnect.Data.UniverseSelection;
+using System;
+using System.Globalization;
+using System.Linq;
 
 namespace QuantConnect.Data.Custom
 {
@@ -28,48 +26,34 @@ namespace QuantConnect.Data.Custom
     /// and forecasting across all US energy sectors.
     /// https://www.eia.gov/opendata/
     /// </summary>
-
     public class USEnergyInformation : BaseData
     {
-        /// <summary>
-        /// Declare string representations of different periods and the variable
-        /// to hold our chosen date format
-        /// </summary>
-        private const string _hourly = "yyyyMMdd'T'HH'Z'";
-        private const string _daily = "yyyyMMdd";
-        private const string _monthly = "yyyyMM";
-        private const string _quarterly = "yyyyMMdd";
-        private const string _annual = "yyyy";
+        private string _previousContent = string.Empty;
 
         /// <summary>
-        /// The end time of this data. Some data covers spans (trade bars) and as such we want
-        /// to know the entire time span covered
+        /// The end time of this data.
         /// </summary>
         public override DateTime EndTime => Time + Period;
 
         /// <summary>
         /// The period of this data (hour, month, quarter, or annual)
         /// </summary>
-        public TimeSpan Period
-        {
-            get;
-            private set;
-        }
+        public TimeSpan Period { get; private set; }
 
         /// <summary>
-        /// Gets the Eia API token.
+        /// Gets the EIA API token.
         /// </summary>
         public static string AuthCode { get; private set; } = string.Empty;
 
         /// <summary>
-        /// Returns true if the Tiingo API token has been set.
+        /// Returns true if the EIA API token has been set.
         /// </summary>
         public static bool IsAuthCodeSet { get; private set; }
 
         /// <summary>
-        /// Sets the Tiingo API token.
+        /// Sets the EIA API token.
         /// </summary>
-        /// <param name="authCode">The Tiingo API token</param>
+        /// <param name="authCode">The EIA API token</param>
         public static void SetAuthCode(string authCode)
         {
             if (string.IsNullOrWhiteSpace(authCode)) return;
@@ -87,8 +71,55 @@ namespace QuantConnect.Data.Custom
         /// <returns>Subscription Data Source.</returns>
         public override SubscriptionDataSource GetSource(SubscriptionDataConfig config, DateTime date, bool isLiveMode)
         {
-            var source = $"https://api.eia.gov/series/?api_key={USEnergyInformation.AuthCode}&series_id={config.Symbol}";
-            return new SubscriptionDataSource(source, SubscriptionTransportMedium.Rest, FileFormat.Collection);
+            return new SubscriptionDataSource(
+                $"https://api.eia.gov/series/?api_key={AuthCode}&series_id={config.Symbol}",
+                SubscriptionTransportMedium.Rest,
+                FileFormat.Collection);
+        }
+
+        /// <summary>
+        /// Reader converts each line of the data source into BaseData objects.
+        /// </summary>
+        /// <param name="config">Subscription data config setup object</param>
+        /// <param name="content">Content of the source document</param>
+        /// <param name="date">Date of the requested data</param>
+        /// <param name="isLiveMode">true if we're in live mode, false for backtesting mode</param>
+        /// <returns>
+        /// Collection of USEnergyInformation objects
+        /// </returns>
+        public override BaseData Reader(SubscriptionDataConfig config, string content, DateTime date, bool isLiveMode)
+        {
+            if (_previousContent == content)
+            {
+                return null;
+            }
+            _previousContent = content;
+
+            try
+            {
+                var format = GetFormat(config.Symbol.Value);
+                var series = JObject.Parse(content)["series"][0];
+                date = DateTimeConverter(series["end"], format);
+
+                var objectList = (
+                    from jToken in series["data"]
+                    where jToken[1].Type != JTokenType.Null
+                    select new USEnergyInformation
+                    {
+                        Symbol = config.Symbol,
+                        Period = Period,
+                        Value = (decimal)jToken[1],
+                        Time = DateTimeConverter(jToken[0], format)
+                            .ConvertFromUtc(config.DataTimeZone) - Period
+                    })
+                    .OrderBy(x => x.EndTime);
+
+                return new BaseDataCollection(date, config.Symbol, objectList);
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         /// <summary>
@@ -104,98 +135,62 @@ namespace QuantConnect.Data.Custom
                 // Annual data has Period ~ 365 days
                 case 'A':
                     Period = TimeSpan.FromDays(365);
-                    return _annual;
+                    return "yyyy";
                 // Quarterly data has Period ~ 90 days
                 case 'Q':
                     Period = TimeSpan.FromDays(90);
-                    return _quarterly;
+                    return DateFormat.EightCharacter;
                 // Monthly data has Period ~ 30 days
                 case 'M':
                     Period = TimeSpan.FromDays(30);
-                    return _monthly;
+                    return DateFormat.YearMonth;
                 // Daily has Period = 1 day
                 case 'D':
                     Period = TimeSpan.FromDays(1);
-                    return _daily;
+                    return DateFormat.EightCharacter;
                 // Hourly has period = 1 Hour
                 case 'H':
                     Period = TimeSpan.FromHours(1);
-                    return _hourly;
+                    return "yyyyMMdd'T'HHZ";
                 default:
                     throw new Exception("Unsupported Period");
             }
         }
 
         /// <summary>
-        /// Special case handler for quarterly data (Series ID ends with Q1, Q2, etc)
+        /// Converts a <see cref="JToken"/> object containing raw date into a <see cref="DateTime"/> object.
+        /// Handles special case for quarterly data (Series ID ends with Q1, Q2, etc)
         /// </summary>
-        /// <param name="dateData">String containing raw date format</param>
+        /// <param name="jToken">The raw date</param>
+        /// <param name="format">The date format</param>
         /// <returns>
-        ///     Properly formatted datestring
+        /// <see cref="DateTime"/> corresponding to the <see cref="JToken"/>.
         /// </returns>
-        private string QuarterDateHandler(string dateData)
+        private DateTime DateTimeConverter(JToken jToken, string format)
         {
+            var dateData = jToken.ToString();
+
             if (dateData.Contains("Q"))
             {
                 switch (dateData.Last())
                 {
                     case '1':
-                        return dateData.Substring(0, 4) + "0101";
+                        dateData = dateData.Substring(0, 4) + "0101";
+                        break;
                     case '2':
-                        return dateData.Substring(0, 4) + "0401";
+                        dateData = dateData.Substring(0, 4) + "0401";
+                        break;
                     case '3':
-                        return dateData.Substring(0, 4) + "0701";
+                        dateData = dateData.Substring(0, 4) + "0701";
+                        break;
                     case '4':
-                        return dateData.Substring(0, 4) + "1001";
+                        dateData = dateData.Substring(0, 4) + "1001";
+                        break;
                     default:
                         throw (new Exception("invalid quarter input"));
                 }
             }
-            return dateData;
-        }
-
-        /// <summary>
-        ///     Reader converts each line of the data source into BaseData objects.
-        /// </summary>
-        /// <param name="config">Subscription data config setup object</param>
-        /// <param name="content">Content of the source document</param>
-        /// <param name="date">Date of the requested data</param>
-        /// <param name="isLiveMode">true if we're in live mode, false for backtesting mode</param>
-        /// <returns>
-        ///     Collection of BaseData objects
-        /// </returns>
-        public override BaseData Reader(SubscriptionDataConfig config, string content, DateTime date, bool isLiveMode)
-        {
-            try
-            {
-                var rawData = JObject.Parse(content)["series"][0]["data"];                
-                var format = GetFormat(config.Symbol.Value);
-                var objectList = new List<USEnergyInformation>();
-
-                foreach (var element in (JArray)rawData)
-                {
-                    decimal value;
-                    if (!Decimal.TryParse(element[1].ToString(), out value))
-                    {
-                        continue;
-                    }
-                    objectList.Add(new USEnergyInformation
-                    {
-                        Time = DateTime.ParseExact(QuarterDateHandler(element[0].ToString()), format, CultureInfo.InvariantCulture),
-                        Value = value,
-                        Period = Period,
-                        Symbol = config.Symbol
-                    });
-                }
-                objectList = objectList.OrderBy(element => element.Time).ToList();
-
-                return new BaseDataCollection(date, config.Symbol, objectList);
-            }
-            catch(Exception err)
-            {
-                Logging.Log.Error($"Exception: {err}");
-                return null;
-            }
+            return DateTime.ParseExact(dateData, format, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
         }
     }
 }
