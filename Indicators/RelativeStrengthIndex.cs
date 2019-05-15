30bc74984ec3ce516263e9e728965bc63d530303
AlexCatarino <alexandre.catarino@gmail.com>
2019-05-15 15:10:19 +0100

    Extends RelativeStrengthIndex, Stochastic and WilliamsPercentR Indicators With IIndicatorWarmUpPeriodProvider

--- Indicators/RelativeStrengthIndex.cs
+++ Indicators/RelativeStrengthIndex.cs
@@ -1,41 +1,42 @@
 ﻿/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
+
 namespace QuantConnect.Indicators
 {
     /// <summary>
     /// Represents the  Relative Strength Index (RSI) developed by K. Welles Wilder.
     /// You can optionally specified a different moving average type to be used in the computation
     /// </summary>
-    public class RelativeStrengthIndex : Indicator
+    public class RelativeStrengthIndex : Indicator, IIndicatorWarmUpPeriodProvider
     {
-        private IndicatorDataPoint previousInput;
+        private IndicatorDataPoint _previousInput;
 
         /// <summary>
         /// Gets the type of indicator used to compute AverageGain and AverageLoss
         /// </summary>
-        public MovingAverageType MovingAverageType { get; private set; }
+        public MovingAverageType MovingAverageType { get; }
 
         /// <summary>
         /// Gets the EMA for the down days
         /// </summary>
-        public IndicatorBase<IndicatorDataPoint> AverageLoss { get; private set; }
+        public IndicatorBase<IndicatorDataPoint> AverageLoss { get; }
 
         /// <summary>
         /// Gets the indicator for average gain
         /// </summary>
-        public IndicatorBase<IndicatorDataPoint> AverageGain { get; private set; }
+        public IndicatorBase<IndicatorDataPoint> AverageGain { get; }
 
         /// <summary>
         /// Initializes a new instance of the RelativeStrengthIndex class with the specified name and period
@@ -43,7 +44,7 @@ namespace QuantConnect.Indicators
         /// <param name="period">The period used for up and down days</param>
         /// <param name="movingAverageType">The type of moving average to be used for computing the average gain/loss values</param>
         public RelativeStrengthIndex(int period, MovingAverageType movingAverageType = MovingAverageType.Wilders)
-            : this("RSI" + period, period, movingAverageType)
+            : this($"RSI({period})", period, movingAverageType)
         {
         }
 
@@ -59,15 +60,18 @@ namespace QuantConnect.Indicators
             MovingAverageType = movingAverageType;
             AverageGain = movingAverageType.AsIndicator(name + "Up", period);
             AverageLoss = movingAverageType.AsIndicator(name + "Down", period);
+            WarmUpPeriod = period + 1;
         }
 
         /// <summary>
         /// Gets a flag indicating when this indicator is ready and fully initialized
         /// </summary>
-        public override bool IsReady
-        {
-            get { return AverageGain.IsReady && AverageLoss.IsReady; }
-        }
+        public override bool IsReady => AverageGain.IsReady && AverageLoss.IsReady;
+
+        /// <summary>
+        /// Required period, in data points, for the indicator to be ready and fully initialized.
+        /// </summary>
+        public int WarmUpPeriod { get; }
 
         /// <summary>
         /// Computes the next value of this indicator from the given state
@@ -76,18 +80,18 @@ namespace QuantConnect.Indicators
         /// <returns>A new value for this indicator</returns>
         protected override decimal ComputeNextValue(IndicatorDataPoint input)
         {
-            if (previousInput != null && input.Value >= previousInput.Value)
+            if (_previousInput != null && input.Value >= _previousInput.Value)
             {
-                AverageGain.Update(input.Time, input.Value - previousInput.Value);
+                AverageGain.Update(input.Time, input.Value - _previousInput.Value);
                 AverageLoss.Update(input.Time, 0m);
             }
-            else if (previousInput != null && input.Value < previousInput.Value)
+            else if (_previousInput != null && input.Value < _previousInput.Value)
             {
                 AverageGain.Update(input.Time, 0m);
-                AverageLoss.Update(input.Time, previousInput.Value - input.Value);
+                AverageLoss.Update(input.Time, _previousInput.Value - input.Value);
             }
 
-            previousInput = input;
+            _previousInput = input;
             if (AverageLoss == 0m)
             {
                 // all up days is 100
@@ -103,6 +107,7 @@ namespace QuantConnect.Indicators
         /// </summary>
         public override void Reset()
         {
+            _previousInput = null;
             AverageGain.Reset();
             AverageLoss.Reset();
             base.Reset();
