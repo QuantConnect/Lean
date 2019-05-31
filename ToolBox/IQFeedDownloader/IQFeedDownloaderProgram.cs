d6c5bc56355c1b758f2b25def4c44909e29f83ab
Aytaç Aşan <aytacasan@hotmail.com>
2019-05-31 11:36:35 -0400

    IQFeed Downloader
    IQFeed data downloader program that download history data for all resolutions

---
+++ ToolBox/IQFeedDownloader/IQFeedDownloaderProgram.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using System;
+using System.Globalization;
+using System.Linq;
+using QuantConnect.Configuration;
+using QuantConnect.Data.Market;
+using QuantConnect.Logging;
+
+namespace QuantConnect.ToolBox.IQFeedDownloader
+{
+    /// <summary>
+    /// IQFeed Downloader Toolbox Project For LEAN Algorithmic Trading Engine.
+    /// </summary>
+    public static class IQFeedDownloaderProgram
+    {
+        /// <summary>
+        /// Primary entry point to the program. This program only supports EQUITY for now.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Usage: IQFeedDownloader SYMBOLS RESOLUTION FROMDATE TODATE");
+                Console.WriteLine("SYMBOLS      = eg SPY,AAPL");
+                Console.WriteLine("RESOLUTION   = Tick/Second/Minute/Hour/Daily/All");
+                Console.WriteLine("FROMDATE     = yyyymmdd");
+                Console.WriteLine("TODATE       = yyyymmdd");
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                // Load settings from command line
+                var tickers = args[0].Split(',');
+                var allResolutions = args[1].ToLower() == "all";
+                var resolution = allResolutions ? Resolution.Tick : (Resolution)Enum.Parse(typeof(Resolution), args[1]);
+                var startDate = DateTime.ParseExact(args[2], "yyyyMMdd", CultureInfo.InvariantCulture);
+                var endDate = DateTime.ParseExact(args[3], "yyyyMMdd", CultureInfo.InvariantCulture);
+                endDate = endDate.AddDays(1).AddMilliseconds(-1);
+
+                // Load settings from config.json
+                var dataDirectory = Config.Get("data-folder", "../../../Data");
+                var userName = Config.Get("iqfeed-username", "username");
+                var password = Config.Get("iqfeed-password", "password");
+                var productName = Config.Get("iqfeed-productName", "productname");
+                var productVersion = Config.Get("iqfeed-version", "productversion");
+
+                // Create an instance of the downloader
+                const string market = Market.USA;
+                var downloader = new IQFeedDataDownloader(userName, password, productName, productVersion);
+
+                foreach (var ticker in tickers)
+                {
+                    // Download the data
+                    var symbol = Symbol.Create(ticker, SecurityType.Equity, market);
+                    var data = downloader.Get(symbol, resolution, startDate, endDate);
+
+                    if (allResolutions)
+                    {
+                        var ticks = data.Cast<Tick>().ToList();
+
+                        // Save the data (tick resolution)
+                        var writer = new LeanDataWriter(resolution, symbol, dataDirectory);
+                        writer.Write(ticks);
+
+                        // Save the data (other resolutions)
+                        foreach (var res in new[] { Resolution.Second, Resolution.Minute, Resolution.Hour, Resolution.Daily })
+                        {
+                            var resData = IQFeedDataDownloader.AggregateTicks(symbol, ticks, res.ToTimeSpan());
+
+                            writer = new LeanDataWriter(res, symbol, dataDirectory);
+                            writer.Write(resData);
+                        }
+                    }
+                    else
+                    {
+                        // Save the data (single resolution)
+                        var writer = new LeanDataWriter(resolution, symbol, dataDirectory);
+                        writer.Write(data);
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Log.Error(err);
+            }
+        }
+    }
+}
