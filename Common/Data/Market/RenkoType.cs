141eaa1fee9887b8867342fb180419e6a16bf341
Michael Handschuh <mhandschuh@gmail.com>
2019-08-06 23:58:39 -0400

    Add missing license banner

--- Common/Data/Market/RenkoType.cs
+++ Common/Data/Market/RenkoType.cs
@@ -1,4 +1,19 @@
-﻿namespace QuantConnect.Data.Market
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+namespace QuantConnect.Data.Market
 {
     /// <summary>
     /// The type of the RenkoBar
@@ -6,10 +21,10 @@
     public enum RenkoType
     {
         /// <summary>
-        /// Indicates that the RenkoConsolidator works in a 
-        /// "Classic" manner (ie. that it only returns a single 
-        /// bar, at most, irrespective of tick movement).  
-        /// NOTE: the Classic mode has only been retained for 
+        /// Indicates that the RenkoConsolidator works in a
+        /// "Classic" manner (ie. that it only returns a single
+        /// bar, at most, irrespective of tick movement).
+        /// NOTE: the Classic mode has only been retained for
         /// backwards compatability with existing code.
         /// </summary>
         Classic,
--- Common/Securities/Interfaces/IContinuousContractModel.cs
+++ Common/Securities/Interfaces/IContinuousContractModel.cs
@@ -1,28 +1,40 @@
-﻿using QuantConnect.Data;
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using QuantConnect.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuantConnect.Securities.Interfaces
 {
     /// <summary>
-    /// Enum defines types of possible price adjustments in continuous contract modeling. 
+    /// Enum defines types of possible price adjustments in continuous contract modeling.
     /// </summary>
     public enum AdjustmentType
     {
         /// ForwardAdjusted - new quotes are adjusted as new data comes
         ForwardAdjusted,
-        
+
         /// BackAdjusted - old quotes are retrospectively adjusted as new data comes
         BackAdjusted
     };
 
     /// <summary>
-    /// Continuous contract model interface. Interfaces is implemented by different classes 
-    /// realizing various methods for modeling continuous security series. Primarily, modeling of continuous futures. 
-    /// Continuous contracts are used in backtesting of otherwise expiring derivative contracts. 
+    /// Continuous contract model interface. Interfaces is implemented by different classes
+    /// realizing various methods for modeling continuous security series. Primarily, modeling of continuous futures.
+    /// Continuous contracts are used in backtesting of otherwise expiring derivative contracts.
     /// Continuous contracts are not traded, and are not products traded on exchanges.
     /// </summary>
     public interface IContinuousContractModel
@@ -33,13 +45,13 @@ namespace QuantConnect.Securities.Interfaces
         AdjustmentType AdjustmentType { get; set; }
 
         /// <summary>
-        /// List of current and historical data series for one root symbol. 
+        /// List of current and historical data series for one root symbol.
         /// e.g. 6BH16, 6BM16, 6BU16, 6BZ16
         /// </summary>
         IEnumerator<BaseData> InputSeries { get; set; }
 
         /// <summary>
-        /// Method returns continuous prices from the list of current and historical data series for one root symbol. 
+        /// Method returns continuous prices from the list of current and historical data series for one root symbol.
         /// It returns enumerator of stitched continuous quotes, produced by the model.
         /// e.g. 6BH15, 6BM15, 6BU15, 6BZ15 will result in one 6B continuous historical series for 2015
         /// </summary>
@@ -47,13 +59,13 @@ namespace QuantConnect.Securities.Interfaces
         IEnumerator<BaseData> GetContinuousData(DateTime dateTime);
 
         /// <summary>
-        /// Returns the list of roll dates for the contract. 
+        /// Returns the list of roll dates for the contract.
         /// </summary>
         /// <returns>The list of roll dates</returns>
         IEnumerator<DateTime> GetRollDates();
 
         /// <summary>
-        /// Returns current symbol name that corresponds to the current continuous model, 
+        /// Returns current symbol name that corresponds to the current continuous model,
         /// or null if none.
         /// </summary>
         /// <returns>Current symbol name</returns>
