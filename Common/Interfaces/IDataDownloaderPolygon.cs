27758685cd0894b92f4e25cb301c2faed58f8d41
mulili <mulili.nzuki@gmail.com>
2019-08-27 10:29:26 +0300

    initial scaffolding

---
+++ Common/Interfaces/IDataDownloaderPolygon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace QuantConnect.Interfaces
+{
+    /// <summary>
+    /// Wrapper on the API for getting data from polygon.io
+    /// </summary>
+    public interface IDataDownloaderPolygon
+    {
+        /// <summary>
+        /// Authenticate the api
+        /// </summary>
+        void Authenticate(String ApiKey);
+    }
+}
