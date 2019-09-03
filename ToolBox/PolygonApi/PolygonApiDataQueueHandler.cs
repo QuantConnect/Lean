980c5697b5f4598554c34c46efc3bd19f434c04b
mulili <mulili.nzuki@gmail.com>
2019-09-03 16:25:50 +0300

    added rest api url

--- ToolBox/PolygonApi/PolygonApiDataQueueHandler.cs
+++ ToolBox/PolygonApi/PolygonApiDataQueueHandler.cs
@@ -10,12 +10,13 @@ using QuantConnect.Packets;
 namespace QuantConnect.ToolBox.PolygonApi
 {
     /// <summary>
-    /// An implementation of <see cref="IDataQueueHandler"/> for CoinAPI
+    /// An implementation of <see cref="IDataQueueHandler"/> for PolygonAPI
     /// </summary>
     public class PolygonApiDataQueueHandler : IDataQueueHandler, IDisposable
     {
 
         private const string WebSocketUrl = "wss://socket.polygon.io/stocks";
+        private const string restUrl = "https://api.polygon.io/";
 
         private readonly string _apiKey = Config.Get("polygon-api-key");
         private readonly WebSocketWrapper _webSocket = new WebSocketWrapper();
@@ -23,9 +24,53 @@ namespace QuantConnect.ToolBox.PolygonApi
         private readonly List<Tick> _ticks = new List<Tick>();
         private readonly DefaultConnectionHandler _connectionHandler = new DefaultConnectionHandler();
 
-        public void Dispose()
+        public PolygonApiDataQueueHandler()
+        {
+            _connectionHandler.ConnectionLost += OnConnectionLost;
+            _connectionHandler.ConnectionRestored += OnConnectionRestored;
+            _connectionHandler.ReconnectRequested += OnReconnectRequested;
+
+            _connectionHandler.Initialize(string.Empty);
+
+            _webSocket.Initialize(WebSocketUrl);
+
+            _webSocket.Message += (s, m) => EnqueueMessage(m);
+
+            _webSocket.Connect();
+
+            //new Thread(new ThreadStart(MessagesProcessorThread)).Start();
+        }
+
+        private void EnqueueMessage(WebSocketMessage m)
         {
             throw new NotImplementedException();
+        }
+
+        private void OnReconnectRequested(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnConnectionLost(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnConnectionRestored(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+
+            if (_webSocket.IsOpen)
+            {
+                _webSocket.Close();
+            }
         }
 
         public IEnumerable<BaseData> GetNextTicks()
