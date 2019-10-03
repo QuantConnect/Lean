7f2d9fa4110da22512010a0c8687b2cd03939ce4
Gerardo Salazar <gsalaz9800@gmail.com>
2019-10-03 10:55:16 -0700

    Cleanup Trading Economics Calendar Downloader code
    * Check for invalid values in ParseDecimal to prevent crashes

--- ToolBox/TradingEconomicsDataDownloader/TradingEconomicsCalendarDownloader.cs
+++ ToolBox/TradingEconomicsDataDownloader/TradingEconomicsCalendarDownloader.cs
@@ -264,28 +264,36 @@ namespace QuantConnect.ToolBox.TradingEconomicsDataDownloader
                 return null;
             }
 
-            if (inPercent)
+            // Return null If we can't parse the result as is
+            decimal finalFigure;
+            if (!decimal.TryParse(newFigure, NumberStyles.Any, CultureInfo.InvariantCulture, out finalFigure))
             {
-                return Convert.ToDecimal(newFigure, CultureInfo.InvariantCulture) / 100m;
-            }
-            else if (inTrillions)
-            {
-                return Convert.ToDecimal(newFigure, CultureInfo.InvariantCulture) * 1000000000000m;
-            }
-            else if (inBillions)
-            {
-                return Convert.ToDecimal(newFigure, CultureInfo.InvariantCulture) * 1000000000m;
-            }
-            else if (inMillions)
-            {
-                return Convert.ToDecimal(newFigure, CultureInfo.InvariantCulture) * 1000000m;
-            }
-            else if (inThousands)
-            {
-                return Convert.ToDecimal(newFigure, CultureInfo.InvariantCulture) * 1000m;
+                Log.Error($"TradingEconomicsCalendarDownloader.ParseDecimal(): Failed to parse the figure {value}. Final form before parsing: {newFigure}");
+                return null;
             }
 
-            return Convert.ToDecimal(newFigure, CultureInfo.InvariantCulture);
+            if (inPercent)
+            {
+                return finalFigure / 100m;
+            }
+            if (inTrillions)
+            {
+                return finalFigure * 1000000000000m;
+            }
+            if (inBillions)
+            {
+                return finalFigure * 1000000000m;
+            }
+            if (inMillions)
+            {
+                return finalFigure * 1000000m;
+            }
+            if (inThousands)
+            {
+                return finalFigure * 1000m;
+            }
+
+            return finalFigure;
         }
     }
 }
