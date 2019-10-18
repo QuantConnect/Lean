e3ff35b5d486d089d1a4169b6f6501f18d78989f
Juan José D'Ambrosio <jjdambrosio@gmail.com>
2019-10-18 15:17:36 +0000

    Implement Central Bank and LIBOR helper class

--- Common/Data/Custom/FRED/CommercialPaper.cs
+++ Common/Data/Custom/FRED/CommercialPaper.cs
@@ -14,12 +14,6 @@
  *
 */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace QuantConnect.Data.Custom.FRED
 {
     public static partial class FredDataSeries
