fb7600304878270fc3ba189634c091cf3f8b3a5a
AlexCatarino <alexandre.catarino@gmail.com>
2019-11-07 17:01:15 +0000

    Adds New Static Members to LiquidETFUniverse
    New members represent the different ETF categories. Each one has a `List<Symbol>` for Long and Inverse ETFs.

--- Algorithm.Framework/Selection/LiquidETFUniverse.cs
+++ Algorithm.Framework/Selection/LiquidETFUniverse.cs
@@ -13,7 +13,7 @@
  * limitations under the License.
 */
 
-using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace QuantConnect.Algorithm.Framework.Selection
@@ -21,103 +21,135 @@ namespace QuantConnect.Algorithm.Framework.Selection
     /// <summary>
     /// Universe Selection Model that adds the following ETFs at their inception date
     /// </summary>
-	public class LiquidETFUniverse : InceptionDateUniverseSelectionModel
+    public class LiquidETFUniverse : InceptionDateUniverseSelectionModel
     {
         /// <summary>
-        /// Initializes a new instance of the EnergyETFUniverse class
+        /// Represents the Energy ETF Category which can be used to access the list of Long and Inverse symbols
+        /// </summary>
+        public static Grouping Energy = new Grouping(
+            new[]
+            {
+                "VDE", "USO", "XES", "XOP", "UNG", "ICLN", "ERX",
+                "UCO", "AMJ", "BNO", "AMLP", "DGAZ", "TAN"
+            },
+            new[] {"ERY", "SCO", "UGAZ"}
+        );
+
+        /// <summary>
+        /// Represents the Metals ETF Category which can be used to access the list of Long and Inverse symbols
+        /// </summary>
+        public static Grouping Metals = new Grouping(
+            new[] {"GLD", "IAU", "SLV", "GDX", "AGQ", "PPLT", "NUGT", "USLV", "UGLD", "JNUG"},
+            new[] {"DUST", "JDST"}
+        );
+
+        /// <summary>
+        /// Represents the Technology ETF Category which can be used to access the list of Long and Inverse symbols
+        /// </summary>
+        public static Grouping Technology = new Grouping(
+            new[] {"QQQ", "IGV", "QTEC", "FDN", "FXL", "TECL", "SOXL", "SKYY", "KWEB"},
+            new[] {"TECS", "SOXS"}
+        );
+
+        /// <summary>
+        /// Represents the Treasuries ETF Category which can be used to access the list of Long and Inverse symbols
+        /// </summary>
+        public static Grouping Treasuries = new Grouping(
+            new[]
+            {
+                "IEF", "SHY", "TLT", "IEI", "SHV", "TLH", "BIL", "SPTL",
+                "TBT", "TMF", "TBF", "SCHO", "SCHR", "SPTS", "GOVT"
+            },
+            new[] { "TMV" }
+        );
+
+        /// <summary>
+        /// Represents the Volatility ETF Category which can be used to access the list of Long and Inverse symbols
+        /// </summary>
+        public static Grouping Volatility = new Grouping(
+            new[] {"VIXY", "SPLV", "UVXY", "EEMV", "EFAV", "USMV"},
+            new[] {"TVIX", "SVXY"}
+        );
+
+        /// <summary>
+        /// Represents the SP500 Sectors ETF Category which can be used to access the list of Long and Inverse symbols
+        /// </summary>
+        public static Grouping SP500Sectors = new Grouping(
+            new[] {"XLB", "XLE", "XLF", "XLI", "XLK", "XLP", "XLU", "XLV", "XLY"},
+            new string[0]
+        );
+
+        /// <summary>
+        /// Initializes a new instance of the LiquidETFUniverse class
         /// </summary>
         public LiquidETFUniverse() :
             base(
                 "qc-liquid-etf-basket",
-                new Dictionary<string, DateTime>()
-                {
-                    // Energy
-                    {"VDE", new DateTime(2004, 9, 29)},
-                    {"USO", new DateTime(2006, 4, 10)},
-                    {"XES", new DateTime(2006, 6, 22)},
-                    {"XOP", new DateTime(2006, 6, 22)},
-                    {"UNG", new DateTime(2007, 4, 18)},
-                    {"ICLN", new DateTime(2008, 6, 25)},
-                    {"ERX", new DateTime(2008, 11, 6)},
-                    {"ERY", new DateTime(2008, 11, 6)},
-                    {"SCO", new DateTime(2008, 11, 25)},
-                    {"UCO", new DateTime(2008, 11, 25)},
-                    {"AMJ", new DateTime(2009, 6, 2)},
-                    {"BNO", new DateTime(2010, 6, 2)},
-                    {"AMLP", new DateTime(2010, 8, 25)},
-                    {"DGAZ", new DateTime(2012, 2, 8)},
-                    {"UGAZ", new DateTime(2012, 2, 8)},
-                    {"TAN", new DateTime(2012, 2, 15)},
-
-                    // Metals
-                    {"GLD", new DateTime(2004, 11, 18)},
-                    {"IAU", new DateTime(2005, 1, 28)},
-                    {"SLV", new DateTime(2006, 4, 28)},
-                    {"GDX", new DateTime(2006, 5, 22)},
-                    {"AGQ", new DateTime(2008, 12, 4)},
-                    {"PPLT", new DateTime(2010, 1, 8)},
-                    {"NUGT", new DateTime(2010, 12, 8)},
-                    {"DUST", new DateTime(2010, 12, 8)},
-                    {"USLV", new DateTime(2011, 10, 17)},
-                    {"UGLD", new DateTime(2011, 10, 17)},
-                    {"JNUG", new DateTime(2013, 10, 3)},
-                    {"JDST", new DateTime(2013, 10, 3)},
-
-                    // Technology
-                    {"QQQ", new DateTime(1999, 3, 10)},
-                    {"IGV", new DateTime(2001, 7, 13)},
-                    {"QTEC", new DateTime(2006, 4, 25)},
-                    {"FDN", new DateTime(2006, 6, 23)},
-                    {"FXL", new DateTime(2007, 5, 10)},
-                    {"TECL", new DateTime(2008, 12, 17)},
-                    {"TECS", new DateTime(2008, 12, 17)},
-                    {"SOXL", new DateTime(2010, 3, 11)},
-                    {"SOXS", new DateTime(2010, 3, 11)},
-                    {"SKYY", new DateTime(2011, 7, 6)},
-                    {"KWEB", new DateTime(2013, 8, 1)},
-
-                    // US Treasuries
-                    {"IEF", new DateTime(2002, 7, 26)},
-                    {"SHY", new DateTime(2002, 7, 26)},
-                    {"TLT", new DateTime(2002, 7, 26)},
-                    {"IEI", new DateTime(2007, 1, 11)},
-                    {"SHV", new DateTime(2007, 1, 11)},
-                    {"TLH", new DateTime(2007, 1, 11)},
-                    {"BIL", new DateTime(2007, 5, 30)},
-                    {"SPTL", new DateTime(2007, 5, 30)},
-                    {"TBT", new DateTime(2008, 5, 1)},
-                    {"TMF", new DateTime(2009, 4, 16)},
-                    {"TMV", new DateTime(2009, 4, 16)},
-                    {"TBF", new DateTime(2009, 8, 20)},
-                    {"SCHO", new DateTime(2010, 8, 6)},
-                    {"SCHR", new DateTime(2010, 8, 6)},
-                    {"SPTS", new DateTime(2011, 12, 1)},
-                    {"GOVT", new DateTime(2012, 2, 24)},
-
-                    // Volatility
-                    {"TVIX", new DateTime(2010, 11, 30)},
-                    {"VIXY", new DateTime(2011, 1, 4)},
-                    {"SPLV", new DateTime(2011, 5, 5)},
-                    {"SVXY", new DateTime(2011, 10, 4)},
-                    {"UVXY", new DateTime(2011, 10, 4)},
-                    {"EEMV", new DateTime(2011, 10, 20)},
-                    {"EFAV", new DateTime(2011, 10, 20)},
-                    {"USMV", new DateTime(2011, 10, 20)},
-		    
-		    //Sp500 Sectors:
-		    {"XLB", new DateTime(1998, 12, 22)},
-                    {"XLE", new DateTime(1998, 12, 22)},
-                    {"XLF", new DateTime(1998, 12, 22)},
-                    {"XLI", new DateTime(1998, 12, 22)},
-                    {"XLK", new DateTime(1998, 12, 22)},
-                    {"XLP", new DateTime(1998, 12, 22)},
-                    {"XLU", new DateTime(1998, 12, 22)},
-                    {"XLV", new DateTime(1998, 12, 22)},
-                    {"XLY", new DateTime(1998, 12, 22)}
-                }
+                SP500Sectors
+                    .Concat(Energy)
+                    .Concat(Metals)
+                    .Concat(Technology)
+                    .Concat(Treasuries)
+                    .Concat(Volatility)
+                    // Convert the concatenated list of Symbol into a Dictionary of DateTime keyed by Symbol
+                    // For equities, Symbol.ID is the first date the security is traded.
+                    .ToDictionary(x => x.Value, x => x.ID.Date)
             )
         {
 
+        }
+
+        /// <summary>
+        /// Represent a collection of ETF symbols that is grouped according to a given criteria
+        /// </summary>
+        public class Grouping : List<Symbol>
+        {
+            /// <summary>
+            /// List of Symbols that follow the components direction
+            /// </summary>
+            public readonly List<Symbol> Long;
+
+            /// <summary>
+            /// List of Symbols that follow the components inverse direction
+            /// </summary>
+            public readonly List<Symbol> Inverse;
+
+            /// <summary>
+            /// Creates a new instance of <see cref="Grouping"/>.
+            /// </summary>
+            /// <param name="longTickers">List of tickers of ETFs that follows the components direction</param>
+            /// <param name="inverseTickers">List of tickers of ETFs that follows the components inverse direction</param>
+            public Grouping(IEnumerable<string> longTickers, IEnumerable<string> inverseTickers)
+            {
+                Long = longTickers.Select(x => Symbol.Create(x, SecurityType.Equity, Market.USA)).ToList();
+                Inverse = inverseTickers.Select(x => Symbol.Create(x, SecurityType.Equity, Market.USA)).ToList();
+                AddRange(Long);
+                AddRange(Inverse);
+            }
+
+            /// <summary>
+            /// Returns a string that represents the current object.
+            /// </summary>
+            /// <returns>
+            /// A string that represents the current object.
+            /// </returns>
+            public override string ToString()
+            {
+                if (Count == 0)
+                {
+                    return "No Symbols";
+                }
+
+                var longSymbols = Long.Count == 0
+                    ? string.Empty
+                    : $" Long: {string.Join(",", Long.Select(x => x.Value))}";
+
+                var inverseSymbols = Inverse.Count == 0
+                    ? string.Empty
+                    : $" Inverse: {string.Join(",", Inverse.Select(x => x.Value))}";
+
+                return $"{Count} symbols:{longSymbols}{inverseSymbols}";
+            }
         }
     }
 }
