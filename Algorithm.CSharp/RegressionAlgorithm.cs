21f636feccb84a8210654e50edb24af823255c4d
Martin Molinero <martin.molinero1@gmail.com>
2020-02-06 13:45:17 -0300

    Fix undeterministic unit test
    - Fix underterministic python indicator unit test
    - Refactor `QCAlgorithm.Indicators.GetSubscriptions`

--- Algorithm.CSharp/RegressionAlgorithm.cs
+++ Algorithm.CSharp/RegressionAlgorithm.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Collections.Generic;
 using QuantConnect.Data;
-using QuantConnect.Data.Market;
 using QuantConnect.Interfaces;
 
 namespace QuantConnect.Algorithm.CSharp
