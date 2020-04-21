7462934efa77a571b23b4f56bfc28658512e2212
Stefano Raggi <stefano.raggi67@gmail.com>
2020-04-21 17:23:40 +0200

    Update Coinbase brokerage unit tests
    - should have been included in PR #4317

--- Tests/Brokerages/GDAX/GDAXBrokerageTests.cs
+++ Tests/Brokerages/GDAX/GDAXBrokerageTests.cs
@@ -27,6 +27,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
+using QuantConnect.Brokerages.GDAX.Messages;
+using Order = QuantConnect.Orders.Order;
 
 namespace QuantConnect.Tests.Brokerages.GDAX
 {
@@ -134,10 +136,13 @@ namespace QuantConnect.Tests.Brokerages.GDAX
         {
             string json = _matchData;
             string id = "132fb6ae-456b-4654-b4e0-d681ac05cea1";
+
             //not our order
             if (expectedQuantity == 99)
             {
-                json = json.Replace(id, Guid.NewGuid().ToString());
+                var message = JsonConvert.DeserializeObject<Matched>(json);
+                message.UserId = null;
+                json = JsonConvert.SerializeObject(message);
             }
 
             decimal orderQuantity = 6.1m;
@@ -155,7 +160,7 @@ namespace QuantConnect.Tests.Brokerages.GDAX
                 actualQuantity += e.AbsoluteFillQuantity;
 
                 Assert.IsTrue(actualQuantity != orderQuantity);
-                Assert.AreEqual(OrderStatus.Filled, e.Status);
+                Assert.AreEqual(OrderStatus.PartiallyFilled, e.Status);
                 Assert.AreEqual(expectedQuantity, e.FillQuantity);
                 // fill quantity = 5.23512
                 // fill price = 400.23
@@ -167,7 +172,9 @@ namespace QuantConnect.Tests.Brokerages.GDAX
             _unit.OnMessage(_unit, GDAXTestsHelpers.GetArgs(json));
 
             // not our order, market order is completed even if not totally filled
-            json = json.Replace(id, Guid.NewGuid().ToString());
+            var msg = JsonConvert.DeserializeObject<Matched>(json);
+            msg.UserId = null;
+            json = JsonConvert.SerializeObject(msg);
             _unit.OnMessage(_unit, GDAXTestsHelpers.GetArgs(json));
 
             //if not our order should get no event
@@ -351,7 +358,7 @@ namespace QuantConnect.Tests.Brokerages.GDAX
             _unit.Unsubscribe(new List<Symbol> { Symbol.Create("BTCUSD", SecurityType.Crypto, Market.GDAX) });
             StringAssert.Contains("user", actual);
             StringAssert.Contains("heartbeat", actual);
-            StringAssert.Contains("matches", actual);
+            StringAssert.DoesNotContain("matches", actual);
         }
 
         [Test, Ignore("This test is obsolete, the 'ticker' channel is no longer used.")]
@@ -403,7 +410,7 @@ namespace QuantConnect.Tests.Brokerages.GDAX
             const string json = "{\"type\":\"error\",\"message\":\"Failed to subscribe\",\"reason\":\"Invalid product ID provided\"}";
             _unit.OnMessage(_unit, GDAXTestsHelpers.GetArgs(json));
 
-            Assert.AreEqual(BrokerageMessageType.Warning, messageType);
+            Assert.AreEqual(BrokerageMessageType.Error, messageType);
         }
     }
 }
