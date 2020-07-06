220ccbae7c6298dcee2712c543d9dc87fe6676f3
Adalyat Nazirov <aenazirov@gmail.com>
2020-07-06 11:50:08 +0300

    A/D Ratio : wip

--- Tests/Indicators/AdvanceDeclineRatioTests.cs
+++ Tests/Indicators/AdvanceDeclineRatioTests.cs
@@ -24,7 +24,11 @@ namespace QuantConnect.Tests.Indicators
     {
         protected override IndicatorBase<TradeBar> CreateIndicator()
         {
-            return new AdvanceDeclineRatio("test_name");
+            var adr = new AdvanceDeclineRatio("test_name");
+            adr.AddStock(Symbol.Create("AAPL", SecurityType.Equity, Market.USA));
+            adr.AddStock(Symbol.Create("IBM", SecurityType.Equity, Market.USA));
+            adr.AddStock(Symbol.Create("GOOG", SecurityType.Equity, Market.USA));
+            return adr;
         }
 
         protected override string TestFileName => "arms_data.txt";
