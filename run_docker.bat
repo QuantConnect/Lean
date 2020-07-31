8cb0b4aba33c507442f10e01d6eada13f5eb47b5
Colton Sellers <Colton.R.Sellers@gmail.com>
2020-07-30 22:56:10 -0700

    rework of launch scripts; also allows inline args

--- run_docker.bat
+++ run_docker.bat
@@ -22,14 +22,28 @@ set python_location=%current_dir%Algorithm.Python\
 set csharp_dll=%current_dir%Launcher\bin\Debug\QuantConnect.Algorithm.CSharp.dll
 set csharp_pdb=%current_dir%Launcher\bin\Debug\QuantConnect.Algorithm.CSharp.pdb
 
+REM If the arg is a file load in the params from the file (run_docker.cfg)
 if exist "%~1" (
     for /f "eol=- delims=" %%a in (%~1) do set "%%a"
+    goto build_command
+)
+
+REM If the args are just inline args load them in, if not ask questions
+:arg_loop
+if not "%*"=="" (
+    for /F "tokens=1*" %%a in ("%*") do (
+        set %%a
+        if NOT x%%b==x call :arg_loop %%b
+    )
 ) else (
     set /p image="Enter docker image [default: %default_image%]: "
     set /p config_file="Enter absolute path to Lean config file [default: %default_config_file%]: "
     set /p data_dir="Enter absolute path to Data folder [default: %default_data_dir%]: "
     set /p results_dir="Enter absolute path to store results [default: %default_results_dir%]: "
+    set /p debugging="Would you like to debug C#? (Requires mono debugger attachment) [default: N]: "	
 )
+
+:build_command
 
 if "%image%" == "" (
     set image=%default_image%
@@ -62,24 +76,42 @@ if not exist "%results_dir%" (
     goto script_exit
 )
 
-if not exist "%csharp_dll%" (
-    echo Csharp dll at '%csharp_dll%' does not exist; be sure to build the project first. 
-    goto script_exit
-)
-
-echo Docker container starting, attach to Mono process at localhost:55555 to begin
-
-docker run --rm --mount type=bind,source=%config_file%,target=/Lean/Launcher/config.json,readonly^
+REM First part of the docker command that is static, then we build the rest
+set command=docker run --rm --mount type=bind,source=%config_file%,target=/Lean/Launcher/config.json,readonly^
     --mount type=bind,source=%data_dir%,target=/Data,readonly^
     --mount type=bind,source=%results_dir%,target=/Results^
-    --mount type=bind,source=%csharp_dll%,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.dll^
-    --mount type=bind,source=%csharp_pdb%,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.pdb^
-    --mount type=bind,source=%python_location%,target=/Lean/Algorithm.Python^
-    -p 55555:55555 -p 5678:5678^
     --name LeanEngine^
+    -p 5678:5678
+
+REM If the csharp dll and pdb are present, mount them
+if not exist "%csharp_dll%" (
+    echo Csharp file at '%csharp_dll%' does not exist; no CSharp files will be mounted
+) else (
+    set command=%command% --mount type=bind,source=%csharp_dll%,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.dll^
+     --mount type=bind,source=%csharp_pdb%,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.pdb
+)
+
+REM If python algorithms are present, mount them
+if not exist "%python_location%" (
+    echo No Python Algorithm location found at '%python_location%'; no Python files will be mounted
+) else (
+    set command=%command% --mount type=bind,source=%python_location%,target=/Lean/Algorithm.Python
+)
+
+REM If debugging is set then set the entrypoint to run mono with a debugger server
+if /I "%debugging%" == "Y" (
+    set command=%command% -p 55555:55555^
     --entrypoint mono^
     %image% --debug --debugger-agent=transport=dt_socket,server=y,address=0.0.0.0:55555,suspend=y^
     QuantConnect.Lean.Launcher.exe --data-folder /Data --results-destination-folder /Results --config /Lean/Launcher/config.json
+
+    echo Docker container starting, attach to Mono process at localhost:55555 to begin
+) else (
+    set command=%command% %image% --data-folder /Data --results-destination-folder /Results --config /Lean/Launcher/config.json
+)
+
+REM Run built docker command
+%command%
 
 :script_exit
 
@@ -89,3 +121,5 @@ set results_dir=
 set config_file=
 set python_location=
 set csharp_dll=
+
+exit
--- run_docker.sh
+++ run_docker.sh
@@ -23,16 +23,24 @@ python_location=$current_dir/Algorithm.Python/
 csharp_dll=$current_dir/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.dll
 csharp_pdb=$current_dir/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.pdb
 
+#If arg is a file process the key values
 if [ -f "$1" ]; then
     IFS="="
     while read -r key value; do
         eval "$key='$value'"
     done < $1
+#If there are in line args, process them
+elif [ ! -z "$*" ]; then
+    for arg in "$@"; do
+        eval "$arg"
+    done
+#Else query user for settings
 else
     read -p "Enter docker image [default: $default_image]: " image
     read -p "Enter absolute path to Lean config file [default: $default_config_file]: " config_file
     read -p "Enter absolute path to Data folder [default: $default_data_dir]: " data_dir
     read -p "Enter absolute path to store results [default: $default_results_dir]: " results_dir
+    read -p "Would you like to debug C#? (Requires mono debugger attachment) [default: N]: " debugging
 fi
 
 if [ -z "$image" ]; then
@@ -66,21 +74,39 @@ if [ ! -d "$results_dir" ]; then
     exit 1
 fi
 
-if [ ! -f "$csharp_dll" ]; then
-    echo "Csharp dll at $csharp_dll does not exist; be sure to build the project first"
-    exit 1
-fi
-
-echo "Docker container starting, attach to Mono process at localhost:55555 to begin"
-
-docker run --rm --mount type=bind,source=$config_file,target=/Lean/Launcher/config.json,readonly \
+#First part of the docker command that is static, then we build the rest
+command="docker run --rm --mount type=bind,source=$config_file,target=/Lean/Launcher/config.json,readonly \
     --mount type=bind,source=$data_dir,target=/Data,readonly \
     --mount type=bind,source=$results_dir,target=/Results \
-    --mount type=bind,source=$csharp_dll,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.dll \
-    --mount type=bind,source=$csharp_pdb,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.pdb \
-    --mount type=bind,source=$python_location,target=/Lean/Algorithm.Python \
-    -p 55555:55555 -p 5678:5678 \
     --name LeanEngine \
+    -p 5678:5678 "
+
+#If the csharp dll and pdb are present, mount them
+if [ ! -f "$csharp_dll" ]; then
+    echo "Csharp file at '$csharp_dll' does not exist; no CSharp files will be mounted"
+else 
+    command+="--mount type=bind,source=$csharp_dll,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.dll \
+    --mount type=bind,source=$csharp_pdb,target=/Lean/Launcher/bin/Debug/QuantConnect.Algorithm.CSharp.pdb "
+fi
+
+#If python algorithms are present, mount them
+if [ ! -d "$python_location" ]; then
+    echo "No Python Algorithm location found at '$python_location'; no Python files will be mounted"
+else
+    command+="--mount type=bind,source=$python_location,target=/Lean/Algorithm.Python "
+fi
+
+#If debugging is set then set the entrypoint to run mono with a debugger server
+if [ "${debugging,,}" == "y" ]; then
+    command+="-p 55555:55555 \
     --entrypoint mono \
-    %image% --debug --debugger-agent=transport=dt_socket,server=y,address=0.0.0.0:55555,suspend=y \
-    QuantConnect.Lean.Launcher.exe --data-folder /Data --results-destination-folder /Results --config /Lean/Launcher/config.json
+    $image --debug --debugger-agent=transport=dt_socket,server=y,address=0.0.0.0:55555,suspend=y \
+    QuantConnect.Lean.Launcher.exe --data-folder /Data --results-destination-folder /Results --config /Lean/Launcher/config.json"
+
+    echo "Docker container starting, attach to Mono process at localhost:55555 to begin"
+else 
+    command+="$image --data-folder /Data --results-destination-folder /Results --config /Lean/Launcher/config.json"
+fi
+
+#Run built docker command; docker requires sudo privledges by default
+eval sudo $command
