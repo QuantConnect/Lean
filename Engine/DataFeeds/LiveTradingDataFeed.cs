f75f33f6a479fd49fcf376ef2f5a548d0f58ffb3
Martin Molinero <martin.molinero1@gmail.com>
2020-08-10 21:29:50 -0300

    Override data-queue-handler with config

--- Engine/DataFeeds/LiveTradingDataFeed.cs
+++ Engine/DataFeeds/LiveTradingDataFeed.cs
@@ -169,6 +169,7 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// <returns>The loaded <see cref="IDataQueueHandler"/></returns>
         protected virtual IDataQueueHandler GetDataQueueHandler()
         {
+            _job.DataQueueHandler = Config.Get("data-queue-handler", _job.DataQueueHandler);
             Log.Trace($"LiveTradingDataFeed.GetDataQueueHandler(): will use {_job.DataQueueHandler}");
             return Composer.Instance.GetExportedValueByTypeName<IDataQueueHandler>(_job.DataQueueHandler);
         }
