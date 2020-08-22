12f5bfcd6e670a070cfc4e9503f7a89261948cfb
Colton Sellers <Colton.R.Sellers@gmail.com>
2020-08-21 17:50:45 -0700

    Update regressions to reflect fixes

--- Algorithm.CSharp/OrderTicketDemoAlgorithm.cs
+++ Algorithm.CSharp/OrderTicketDemoAlgorithm.cs
@@ -527,7 +527,7 @@ namespace QuantConnect.Algorithm.CSharp
             {"Mean Population Magnitude", "0%"},
             {"Rolling Averaged Population Direction", "0%"},
             {"Rolling Averaged Population Magnitude", "0%"},
-            {"OrderListHash", "1237222672"}
+            {"OrderListHash", "-1594146186"}
         };
     }
 }
