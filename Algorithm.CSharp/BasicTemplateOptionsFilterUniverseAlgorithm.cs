b3061649ed6000511c3cee88f635e4b9ffa4473b
Colton Sellers <Colton.R.Sellers@gmail.com>
2020-09-09 14:59:34 -0700

    Update regressions with new orderhash

--- Algorithm.CSharp/BasicTemplateOptionsFilterUniverseAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateOptionsFilterUniverseAlgorithm.cs
@@ -141,7 +141,7 @@ namespace QuantConnect.Algorithm.CSharp
             {"Mean Population Magnitude", "0%"},
             {"Rolling Averaged Population Direction", "0%"},
             {"Rolling Averaged Population Magnitude", "0%"},
-            {"OrderListHash", "-1214175458"}
+            {"OrderListHash", "1935621950"}
         };
     }
 }
