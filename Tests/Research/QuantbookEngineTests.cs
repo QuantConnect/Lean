5a03464b87c8d698dfaf34af4bd4cb92ff58088d
Colton Sellers <Colton.R.Sellers@gmail.com>
2020-09-23 17:55:02 -0700

    Expand testing

--- Tests/Research/QuantbookEngineTests.cs
+++ Tests/Research/QuantbookEngineTests.cs
@@ -1,45 +1,96 @@
 ﻿using NUnit.Framework;
 using Python.Runtime;
 using System;
-using System.Collections.Generic;
-using QuantConnect.Data.Market;
+using System.Linq;
 using QuantConnect.Research;
-using QuantConnect.Securities;
+using QuantConnect.Data.Consolidators;
+using QuantConnect.Data.UniverseSelection;
+
 
 namespace QuantConnect.Tests.Research
 {
     [TestFixture]
     public class QuantbookEngineTests
     {
+        private QuantBook qb;
+        private readonly DateTime startDate = new DateTime(2013, 10, 7);
+        private readonly DateTime endDate = new DateTime(2013, 10, 11);
+
+        [SetUp]
+        public void Setup()
+        {
+            qb = new QuantBook();
+            qb.SetStartDate(startDate);
+            qb.SetEndDate(endDate);
+        }
 
         [TestCaseSource(nameof(TimeTestCases))]
         public void AlgorithmTimeTest(dynamic input)
         {
-            var qb = new QuantBook();
-            var startDate = new DateTime(2014, 10, 7);
-            var endDate = new DateTime(2014, 10, 11);
-
-            qb.SetStartDate(startDate);
-            qb.SetEndDate(endDate);
-
             Assert.AreEqual(qb.Time, startDate);
 
             qb.AddEquity("SPY", input[0]);
-
             qb.Step(input[1]);
 
-            //Assert.AreEqual(qb.Time, input[2]);
-            var test = qb.Time;
+            Assert.AreEqual(input[2], qb.Time);
         }
 
-        // Different requests and their expected values
+        [Test]
+        public void ConsolidatorTest()
+        {
+            var spy = qb.AddEquity("SPY", Resolution.Minute).Symbol;
+            var consolidator = new QuoteBarConsolidator(10);
+            qb.SubscriptionManager.AddConsolidator(spy, consolidator);
+
+            Assert.IsTrue(consolidator.Consolidated == null);
+            qb.Step(10);
+            Assert.IsTrue(consolidator.Consolidated != null);
+            Assert.AreEqual(153.0223, (double)consolidator.Consolidated.Value, 0.005);
+        }
+
+        [Test]
+        public void IndicatorTest()
+        {
+            var spy = qb.AddEquity("SPY", Resolution.Minute).Symbol;
+            var indicator = qb.EMA(spy, 10);
+
+            Assert.AreEqual(0, indicator.Current.Value);
+            qb.Step(10);
+            Assert.AreEqual(153.0310, (double)indicator.Current.Value, 0.005);
+        }
+
+        [Test]
+        public void UniverseTest()
+        {
+            Assert.IsTrue(qb.UniverseManager.Count == 0);
+
+            // subscriptions added via universe selection will have this resolution
+            qb.UniverseSettings.Resolution = Resolution.Hour;
+
+            //Go select spy
+            qb.AddUniverse(coarse =>
+            {
+                return from c in coarse
+                    let sym = c.Symbol.Value
+                    where sym == "SPY"
+                    select c.Symbol;
+            });
+
+            // Take one step to see the changes reflected
+            qb.Step();
+            Assert.IsTrue(qb.UniverseManager.Count == 1);
+
+            //TODO: Test subscription? Values?
+        }
+
+        // Time test cases: resolution, steps, and expected end time
+        // QB Starts at 9:30.00
         private static readonly object[] TimeTestCases =
         {
-            //new object[] {Resolution.Tick, 1000, new DateTime(2014, 10, 7, 1, 0, 0)},
-            new object[] {Resolution.Second, 500,new DateTime(2014, 10, 7, 9, 8, 20)},
-            new object[] {Resolution.Minute, 80, new DateTime(2014, 10, 7, 10, 20, 0)},
-            new object[] {Resolution.Hour, 5, new DateTime(2014, 10, 7, 1, 0, 0)},
-            new object[] {Resolution.Daily, 2, new DateTime(2014, 10, 9)},
+            new object[] {Resolution.Tick, 1000, new DateTime(2013, 10, 7, 9, 30, 13).AddMilliseconds(3)},
+            new object[] {Resolution.Second, 600,new DateTime(2013, 10, 7, 9, 40, 00)},
+            new object[] {Resolution.Minute, 80, new DateTime(2013, 10, 7, 10, 50, 0)},
+            new object[] {Resolution.Hour, 5, new DateTime(2013, 10, 7, 14, 0, 0)},
         };
     }
 }
