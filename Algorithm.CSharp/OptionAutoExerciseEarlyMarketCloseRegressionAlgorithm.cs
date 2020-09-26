afe5bcdb47083e5e847b65c5c9ec85531f80ce1b
Michael Handschuh <mhandschuh@gmail.com>
2020-09-26 04:56:00 -0400

    Add OptionAutoExerciseEarlyMarketCloseRegressionAlgorithm
    Ensures that option auto-exercise orders submitted at the end of the day
    are properly filled. In some cases they are canceled. This appears to be
    when adding the contract manually via AddOptionContract but not when adding
    the entire option chain via AddOption in Initialize

--- Algorithm.CSharp/OptionAutoExerciseEarlyMarketCloseRegressionAlgorithm.cs
+++ Algorithm.CSharp/OptionAutoExerciseEarlyMarketCloseRegressionAlgorithm.cs
@@ -1,86 +1,172 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using QuantConnect.Data;
-using QuantConnect.Data.Market;
 using QuantConnect.Interfaces;
 using QuantConnect.Orders;
-using QuantConnect.Securities.Equity;
-using QuantConnect.Securities.Option;
 
 namespace QuantConnect.Algorithm.CSharp
 {
+    /// <summary>
+    /// This is a regression algorithm ensuring that auto-exercised option orders are filled even when the market closes early.
+    /// </summary>
+    /// <meta name="tag" content="regression test" />
+    /// <meta name="tag" content="options" />
     public class OptionAutoExerciseEarlyMarketCloseRegressionAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
     {
-        private bool purchasedEquity;
-        private bool purchasedOption;
+    	private int optionQuantity = 1;
+    	private int equityQuantity = 100;
+    	private OptionRight? right = OptionRight.Put;
 
-        private Equity GOOG;
-        private Option GOOGOptionChain;
-
-        private Option GOOGPutContract;
-        private Option GOOGCallContract;
+    	private bool logAvailableContracts = false;
+    	private DateTime lastTime;
+    	private bool purchasedOption;
+    	private bool purchasedUnderlying;
+        private DateTime? forceOptionExercise = null;// new DateTime(2020, 09, 04, 15, 59, 0);
+        private bool loggedContracts;
+        private Symbol symbol;
+        private Symbol optionContract;
+        private readonly HashSet<Symbol> contractsAdded = new HashSet<Symbol>();
 
         public override void Initialize()
         {
             SetStartDate(2015, 12, 24);
             SetEndDate(2015, 12, 28);
-            SetCash(100000);
-            GOOG = AddEquity("GOOG", Resolution.Minute);
-            GOOGOptionChain = AddOption("GOOG");
-            GOOGOptionChain.SetFilter(u => u.IncludeWeeklys()
-                .Strikes(-20, +20)
-                .Expiration(TimeSpan.Zero, TimeSpan.FromDays(1)));
+            SetCash(1000000);
+            var equity = AddEquity("GOOG", Resolution.Minute);
+            equity.SetDataNormalizationMode(DataNormalizationMode.Raw);
+            symbol = equity.Symbol;
+
+            //If we explicitly add the option then this test passes
+            //If we instead add the option via AddOptionContract, it fails
+            //AddOption("GOOG");
+
+            Debug($"SETTINGS: EquityQuantity: {equityQuantity} | Right: {right} OptionQuantity: {optionQuantity} ForceExercise: {forceOptionExercise}");
         }
 
         public override void OnData(Slice data)
         {
-            if (!purchasedEquity)
+        	lastTime = Time;
+            if (!purchasedUnderlying)
             {
-                purchasedEquity = true;
-                MarketOrder(GOOG.Symbol, 100);
+                Debug($"Submitting market order for {equityQuantity} {GetSymbolString(symbol)}");
+                MarketOrder(symbol, equityQuantity);
+                purchasedUnderlying = true;
             }
 
-            if (GOOGCallContract == null)
+            if (contractsAdded.Count == 0)
             {
-                // find a DITM call and a DITM put
-                OptionChain chain;
-                if (!data.OptionChains.TryGetValue(GOOGOptionChain.Symbol, out chain))
-                {
-                    return;
-                }
+            	AddOptionContracts(data);
+            }
+            else if (!purchasedOption && Securities[optionContract].Price != 0)
+            {
+            	purchasedOption = true;
+                Debug($"Submitting market order for {optionQuantity} {GetSymbolString(optionContract)}");
+                MarketOrder(optionContract, optionQuantity);
+            }
 
-                var deepInTheMoney = chain.Contracts.Values
-                    .OrderByDescending(c => GetIntrinsicValue(c.Symbol))
-                    .Take(10).ToList();
-
-                GOOGPutContract = (Option) Securities[deepInTheMoney.First(c => c.Right == OptionRight.Put).Symbol];
-                MarketOrder(GOOGPutContract.Symbol, 1);
-
-                GOOGCallContract = (Option) Securities[deepInTheMoney.First(c => c.Right == OptionRight.Call).Symbol];
-                MarketOrder(GOOGCallContract.Symbol, 1);
+            if (forceOptionExercise == Time && Portfolio[optionContract].Invested)
+            {
+            	Debug($"Exercising option: {optionQuantity} of {GetSymbolString(optionContract)}");
+            	ExerciseOption(optionContract, optionQuantity);
             }
         }
 
         public override void OnOrderEvent(OrderEvent fill)
         {
             var order = Transactions.GetOrderById(fill.OrderId);
-            Debug($"{Time:O}:: ORDER: {order}");
-            Debug($"{Time:O}::  FILL: {order}");
+            Debug($"CASH: {Portfolio.Cash} | GOOG: {Portfolio["GOOG"].Quantity} | {GetSymbolString(fill.Symbol)} >> ORDER:: {order} >> FILL:: {fill}");
+
+            if (order.Type == OrderType.OptionExercise && fill.Status == OrderStatus.Canceled)
+            {
+                throw new Exception("OptionExerciseOrder was canceled!");
+            }
+        }
+
+        public override void OnEndOfDay()
+        {
+        	Debug($"--------LAST TIME: {lastTime:O} CASH: {Portfolio.Cash} GOOG: {Portfolio["GOOG"].Quantity}--------");
+        }
+
+        private string GetSymbolString(Symbol symbol)
+        {
+            switch (symbol.SecurityType)
+            {
+                case SecurityType.Equity:
+                    return $"Equity: {symbol.Value}";
+
+                case SecurityType.Option:
+                    return $"Option: {symbol.Underlying.Value}: {symbol.ID.OptionRight}@{symbol.ID.StrikePrice:0.00}({GetIntrinsicValue(symbol):0.00}";
+
+                default:
+                    return symbol.ToString();
+            }
+        }
+
+        private void AddOptionContracts(Slice data)
+        {
+            var contracts = OptionChainProvider.GetOptionContractList(symbol, data.Time)
+                .Where(c => right == null || c.ID.OptionRight == right)
+                .GroupBy(c => c.ID.Date)
+                .OrderBy(grp => grp.Key)
+                .Take(1)
+                .SelectMany(grp => grp)
+                .ToList();
+
+            if (contracts.Count == 0)
+            {
+                return;
+            }
+
+            if (logAvailableContracts && !loggedContracts)
+            {
+                foreach (var c in contracts)
+                {
+                    Debug($"{c}: {c.ID.Date:yy-MM-dd}: {c.ID.StrikePrice:0.00}");
+                }
+
+                loggedContracts = true;
+            }
+
+            // find the most ITM contract
+            var contract = contracts.OrderByDescending(GetIntrinsicValue).First();
+
+            optionContract = contract;
+            if (contractsAdded.Add(optionContract))
+            {
+                // use AddOptionContract() to subscribe the data for specified contract
+                Debug($"Adding Option Contract: {GetSymbolString(optionContract)}:: Intrinsic Value: {GetIntrinsicValue(optionContract)}");
+                AddOptionContract(optionContract, Resolution.Minute);
+            }
         }
 
         private decimal GetIntrinsicValue(Symbol contract)
         {
-            var right = contract.ID.OptionRight;
-            var strike = contract.ID.StrikePrice;
-            var underlyingPrice = Securities[contract.Underlying].Price;
-            return right == OptionRight.Call
-                ? underlyingPrice - strike
-                : strike - underlyingPrice;
+        	var right = contract.ID.OptionRight;
+        	var strike = contract.ID.StrikePrice;
+        	var underlying = Securities[contract.Underlying].Price;
+        	return right == OptionRight.Call
+                    ? underlying - strike
+                    : strike - underlying;
         }
 
         public bool CanRunLocally { get; } = true;
         public Language[] Languages { get; } = {Language.CSharp};
-        public Dictionary<string, string> ExpectedStatistics { get; }
+        public Dictionary<string, string> ExpectedStatistics { get; } //TBD - requires fixing OptionExerciseOrder cancellation
     }
 }
