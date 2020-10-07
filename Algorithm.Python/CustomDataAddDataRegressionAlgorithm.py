1cb138f03942bf388f970e817a775160e5d82b0a
Colton Sellers <Colton.R.Sellers@gmail.com>
2020-10-07 16:49:30 -0700

    Fix for running regressions algorithms together

--- Algorithm.Python/CustomDataAddDataRegressionAlgorithm.py
+++ Algorithm.Python/CustomDataAddDataRegressionAlgorithm.py
@@ -23,6 +23,9 @@ from QuantConnect.Data.Custom.SEC import *
 from QuantConnect.Data.Custom.USTreasury import *
 import numpy as np
 
+from collections import namedtuple
+Market = namedtuple('_Market', 'USA')('usa')
+
 ### <summary>
 ### Regression algorithm checks that adding data via AddData
 ### works as expected
