d1e6cbb402def13783b2237209c4fe9618d753e3
IlshatGaripov <igjump@gmail.com>
2020-10-19 17:03:02 +0300

    Revert the changes in Tick.cs

--- Common/Data/Market/Tick.cs
+++ Common/Data/Market/Tick.cs
@@ -667,7 +667,7 @@ namespace QuantConnect.Data.Market
             switch (TickType)
             {
                 case TickType.Trade:
-                    return $"{Symbol}: Price: {Price} Quantity: {Quantity} Time: {Time:O}";
+                    return $"{Symbol}: Price: {Price} Quantity: {Quantity}";
 
                 case TickType.Quote:
                     return $"{Symbol}: Bid: {BidSize}@{BidPrice} Ask: {AskSize}@{AskPrice}";
