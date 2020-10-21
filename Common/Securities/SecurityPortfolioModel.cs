244b972ddb8250eee97ba0cf5ba745481d7034c0
Gerardo Salazar <gsalaz9800@gmail.com>
2020-10-21 10:57:56 -0700

    Partially fixes future option mis-calculated profit/loss

--- Common/Securities/SecurityPortfolioModel.cs
+++ Common/Securities/SecurityPortfolioModel.cs
@@ -68,8 +68,9 @@ namespace QuantConnect.Securities
                 }
 
                 // apply the funds using the current settlement model
-                // we dont adjust funds for futures and CFDs: it is zero upfront payment derivative (margin applies though)
-                if (security.Type != SecurityType.Future && security.Type != SecurityType.Cfd)
+                // we dont adjust funds for future options, futures and CFDs: it is zero upfront payment derivative (margin applies though)
+                if ((security.Type == SecurityType.Option && security.Symbol.Underlying.SecurityType != SecurityType.Future) &&
+                    (security.Type != SecurityType.Future && security.Type != SecurityType.Cfd))
                 {
                     security.SettlementModel.ApplyFunds(portfolio, security, fill.UtcTime, quoteCash.Symbol, -fill.FillQuantity * fill.FillPrice * security.SymbolProperties.ContractMultiplier);
                 }
