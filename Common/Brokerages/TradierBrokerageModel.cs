6efeee07dc4f164fc0886438b4ea1eb681b6c43a
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2020-11-04 18:51:41 +0000

    Implements Equity Fill Model (#4913)
    * Implements Equity Fill Model
    
    This commit sets the base to create a new equity fill model and the `EquityFillModel` is just a copy of `FillModel`.
    
    * Adds Summary to FillModelPythonWrapper.GetPricesInternal
    
    Adds summary to FillModelPythonWrapper.GetPricesInternal with remarks that it's a temporarily method to help the refactoring of fill models.

--- Common/Brokerages/TradierBrokerageModel.cs
+++ Common/Brokerages/TradierBrokerageModel.cs
@@ -17,7 +17,6 @@ using System.Collections.Generic;
 using QuantConnect.Data.Market;
 using QuantConnect.Orders;
 using QuantConnect.Orders.Fees;
-using QuantConnect.Orders.Fills;
 using QuantConnect.Orders.Slippage;
 using QuantConnect.Securities;
 using QuantConnect.Securities.Equity;
@@ -157,16 +156,6 @@ namespace QuantConnect.Brokerages
             {
                 base.ApplySplit(tickets, split);
             }
-        }
-
-        /// <summary>
-        /// Gets a new fill model that represents this brokerage's fill behavior
-        /// </summary>
-        /// <param name="security">The security to get fill model for</param>
-        /// <returns>The new fill model for this brokerage</returns>
-        public override IFillModel GetFillModel(Security security)
-        {
-            return new ImmediateFillModel();
         }
 
         /// <summary>
