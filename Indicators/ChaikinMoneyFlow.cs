f1726e82dd129c7632b627f74836461b160dc275
Aaron <ajstone@uwaterloo.ca>
2020-12-05 02:59:20 -0500

    Implement suggestions of @AlexCatarino

--- Indicators/ChaikinMoneyFlow.cs
+++ Indicators/ChaikinMoneyFlow.cs
@@ -30,32 +30,21 @@ namespace QuantConnect.Indicators
     /// L = low
     /// C = close
     /// Vol = volume
+    /// 
+    /// https://www.fidelity.com/learning-center/trading-investing/technical-analysis/technical-indicator-guide/cmf
     /// </summary>
-    public class ChaikinMoneyFlow : WindowIndicator<TradeBar>, IIndicatorWarmUpPeriodProvider
+    public class ChaikinMoneyFlow : TradeBarIndicator, IIndicatorWarmUpPeriodProvider
     {
         /// <summary>
         /// Holds the point-wise flow and volume terms. 
         /// </summary>
         private readonly RollingWindow<List<decimal>> _rollingFlow;
 
-
-        /// <summary>
-        /// Initializes a new instance of the ChaikinMoneyFlow class
-        /// </summary>
-        /// <param name="name">A name for the indicator</param>
-        /// <param name="period">The period over which to perform computation</param>
-        public ChaikinMoneyFlow(string name, int period)
-            : base($"CMF({name})", period)
-        {
-            WarmUpPeriod = period + 1;
-            _rollingFlow = new RollingWindow<List<decimal>>(period);
-        }
-
-
         /// <summary>
         /// Gets a flag indicating when this indicator is ready and fully initialized
         /// </summary>
-        public override bool IsReady => Samples > Period;
+        public override bool IsReady => Samples > _period;
+        private readonly int _period;
 
         /// <summary>
         /// Required period, in data points, for the indicator to be ready and fully initialized.
@@ -72,12 +61,26 @@ namespace QuantConnect.Indicators
         }
 
         /// <summary>
+        /// Initializes a new instance of the ChaikinMoneyFlow class
+        /// </summary>
+        /// <param name="name">A name for the indicator</param>
+        /// <param name="period">The period over which to perform computation</param>
+        public ChaikinMoneyFlow(string name, int period)
+            : base($"CMF({name})")
+        {
+            _period = period;
+            WarmUpPeriod = period + 1;
+            _rollingFlow = new RollingWindow<List<decimal>>(period);
+        }
+
+
+        /// <summary>
         /// Computes the next value for this indicator from the given state.
         /// </summary>
         /// <param name="window">The window of data held in this indicator</param>
         /// <param name="input">The input value to this indicator on this time step</param>
         /// <returns>A new value for this indicator</returns>
-        protected override decimal ComputeNextValue(IReadOnlyWindow<TradeBar> window, TradeBar input)
+        protected override decimal ComputeNextValue(TradeBar input)
         {
             var denominator = (input.High - input.Low);
             var flowRatio = denominator > 0
