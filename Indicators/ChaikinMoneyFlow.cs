6c6e4663bad75723fce05c3a414e7781ccf3921f
Martin Molinero <martin.molinero1@gmail.com>
2020-12-11 18:03:26 -0300

    Minor nit changes

--- Indicators/ChaikinMoneyFlow.cs
+++ Indicators/ChaikinMoneyFlow.cs
@@ -46,7 +46,6 @@ namespace QuantConnect.Indicators
         /// </summary>
         public override bool IsReady => _flowRatioSum.IsReady;
 
-
         /// <summary>
         /// Required period, in data points, for the indicator to be ready and fully initialized.
         /// </summary>
@@ -75,11 +74,9 @@ namespace QuantConnect.Indicators
             _volumeSum = new Sum(period);
         }
 
-
         /// <summary>
         /// Computes the next value for this indicator from the given state.
         /// </summary>
-        /// <param name="window">The window of data held in this indicator</param>
         /// <param name="input">The input value to this indicator on this time step</param>
         /// <returns>A new value for this indicator</returns>
         protected override decimal ComputeNextValue(TradeBar input)
