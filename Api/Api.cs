bd3ead3480aab2754e35c170912c9c1048f0790d
Colton Sellers <41277998+C-SELLERS@users.noreply.github.com>
2020-12-11 11:01:57 -0800

    ApiDataProvider Zip Download Fixes (#5020)
    * Stop non zip responses from being saved as a zip
    
    * Capture and log message
    
    * Log as Error

--- Api/Api.cs
+++ Api/Api.cs
@@ -20,6 +20,7 @@ using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using QuantConnect.Interfaces;
+using QuantConnect.Logging;
 using QuantConnect.Orders;
 using RestSharp;
 using RestSharp.Extensions;
@@ -814,8 +815,17 @@ namespace QuantConnect.Api
             var uri     = new Uri(link.DataLink);
             var client  = new RestClient(uri.Scheme + "://" + uri.Host);
             var request = new RestRequest(uri.PathAndQuery, Method.GET);
-            client.DownloadData(request).SaveAs(path);
 
+            // If the response is not a zip then it is not data, don't write it.
+            var response = client.Execute(request);
+            if (response.ContentType != "application/zip")
+            {
+                var message = JObject.Parse(response.Content)["message"].Value<string>();
+                Log.Error($"Api.DownloadData(): Failed to download zip for {symbol} {resolution} data for date {date}, Api response: {message}");
+                return false;
+            }
+            
+            response.RawBytes.SaveAs(path);
             return true;
         }
 
