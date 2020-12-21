a24c3f2c4f1b7dd9ab06ffc6fb5c7eeed1870cef
Mark Virchenko <mark.virchenko@calienteam.com>
2020-12-21 18:47:57 +0200

    added test covering minValue / maxValue issue with JsonRoundingConverter

---
+++ Tests/Common/Util/JsonRoundingConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using MachinaEngine.Statistics;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachinaEngine.Tests.Common.Util
+{
+    [TestFixture]
+    public class JsonRoundingConverterTests
+    {
+        [Test]
+        public void MinMaxValueDeserializesSuccessfuly()
+        {
+            var portfolioStatistics = new PortfolioStatistics()
+            {
+                AverageWinRate = decimal.MaxValue,
+                AverageLossRate = decimal.MinValue,
+                CompoundingAnnualReturn = decimal.MaxValue
+            };
+
+            var serializedValue = JsonConvert.SerializeObject(portfolioStatistics);
+            var deserializedValue = JsonConvert.DeserializeObject<PortfolioStatistics>(serializedValue);
+
+            Assert.AreEqual(portfolioStatistics.AverageWinRate, deserializedValue.AverageWinRate);
+            Assert.AreEqual(portfolioStatistics.AverageLossRate, deserializedValue.AverageLossRate);
+            Assert.AreEqual(portfolioStatistics.CompoundingAnnualReturn, deserializedValue.CompoundingAnnualReturn);
+
+            Assert.AreEqual(portfolioStatistics.AnnualStandardDeviation, deserializedValue.AnnualStandardDeviation);
+            Assert.AreEqual(portfolioStatistics.Expectancy, deserializedValue.Expectancy);
+        }
+    }
+}
