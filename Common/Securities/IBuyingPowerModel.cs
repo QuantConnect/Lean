ec641639ee9c1d6fd350362ee4a1f3d863988c75
Michael Handschuh <mhandschuh@gmail.com>
2020-12-21 18:19:25 -0500

    Fix typo in IBuyingPowerModel.GetBuyingPower xml docs

--- Common/Securities/IBuyingPowerModel.cs
+++ Common/Securities/IBuyingPowerModel.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -92,7 +92,7 @@ namespace QuantConnect.Securities
         /// <summary>
         /// Gets the buying power available for a trade
         /// </summary>
-        /// <param name="parameters">A parameters object containing the algorithm's potrfolio, security, and order direction</param>
+        /// <param name="parameters">A parameters object containing the algorithm's portfolio, security, and order direction</param>
         /// <returns>The buying power available for the trade</returns>
         BuyingPower GetBuyingPower(BuyingPowerParameters parameters);
     }
