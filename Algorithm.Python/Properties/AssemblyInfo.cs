284f7c838d041f1ef35655a9a2dda80d8768c33d
Colton Sellers <Colton.R.Sellers@gmail.com>
2021-01-08 12:21:02 -0800

    Remove repetitive assembly info

--- Algorithm.Python/Properties/AssemblyInfo.cs
+++ Algorithm.Python/Properties/AssemblyInfo.cs
@@ -4,12 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuantConnect.Algorithm.Python")]
-[assembly: AssemblyDescription("QuantConnect LEAN Engine: Algorithm.Python Project - A collection of Py algorithm demonstrations for how to use the API")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuantConnect.Algorithm.Python")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +25,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
