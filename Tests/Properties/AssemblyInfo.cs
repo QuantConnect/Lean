7453dcf1ee4954301877b5574336974fbceafc16
Colton Sellers <Colton.R.Sellers@gmail.com>
2021-01-08 11:43:20 -0800

    Test project fixes

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -21,6 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7b9bd814-ef00-4c83-9843-81ca75db53d1")]
 [assembly: AssemblyDescription("QuantConnect LEAN Engine: Tests Project - The test collection")]
+[assembly: InternalsVisibleTo("QuantConnect")]
 
 // Version information for an assembly consists of the following four values:
 //
