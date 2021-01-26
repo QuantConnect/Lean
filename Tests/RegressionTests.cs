c86fceac97370e2dd3aab18af52487e4113f4b94
Colton Sellers <41277998+C-SELLERS@users.noreply.github.com>
2021-01-26 10:31:57 -0800

    Fix regression typo (#5224)

--- Tests/RegressionTests.cs
+++ Tests/RegressionTests.cs
@@ -78,7 +78,7 @@ namespace QuantConnect.Tests
             {
                 {"TrainingInitializeRegressionAlgorithm", AlgorithmStatus.RuntimeError},
                 {"OnOrderEventExceptionRegression", AlgorithmStatus.RuntimeError},
-                {"WarmUpAfterIntializeRegression", AlgorithmStatus.RuntimeError }
+                {"WarmUpAfterInitializeRegression", AlgorithmStatus.RuntimeError }
             };
 
             // find all regression algorithms in Algorithm.CSharp
