df6e13383319aaadf3bbf430cea1ff6fc282d512
Martin-Molinero <martin@quantconnect.com>
2021-02-17 16:42:23 -0300

    Fixes for assembly information (#5306)
    - Update version to 2.5
    - Normalize version and assembly information
    - Remove commented and unrequired code

--- ToolBox/Properties/AssemblyInfo.cs
+++ ToolBox/Properties/AssemblyInfo.cs
@@ -5,13 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ToolBox")]
-[assembly: AssemblyDescription("QuantConnect LEAN Engine: ToolBox Project - A collection of data downloaders and converters")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ToolBox")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("QuantConnect.ToolBox")]
+[assembly: AssemblyProduct("QuantConnect.ToolBox")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,18 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5c3d0688-07ac-4cd4-8f2e-e74ebcf32a88")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
+[assembly: AssemblyDescription("QuantConnect LEAN Engine: ToolBox Project - A collection of data downloaders and converters")]
 [assembly: InternalsVisibleTo("QuantConnect.Tests")]
