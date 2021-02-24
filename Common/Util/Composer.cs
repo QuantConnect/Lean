9cde4344fc7754abb228121d1839938887f1641a
Jasper van Merle <jaspervmerle@gmail.com>
2021-02-25 00:50:56 +0100

    Re-apply changes from #4870 to fix ThreadAbortException in ToolBox (#5339)

--- Common/Util/Composer.cs
+++ Common/Util/Composer.cs
@@ -82,7 +82,11 @@ namespace QuantConnect.Util
                 }
                 catch (Exception exception)
                 {
-                    Log.Error(exception);
+                    // ThreadAbortException is triggered when we shutdown ignore the error log
+                    if (!(exception is ThreadAbortException))
+                    {
+                        Log.Error(exception);
+                    }
                 }
                 return new List<ComposablePartDefinition>();
             });
