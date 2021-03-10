2335fefbe33e6966679b645e0f677d5d7bf4eb95
Colton Sellers <Colton.R.Sellers@gmail.com>
2021-03-10 15:44:47 -0800

    Address review

--- Algorithm.Python/NamedArgumentsRegression.py
+++ Algorithm.Python/NamedArgumentsRegression.py
@@ -35,12 +35,23 @@ class NamedArgumentsRegression(QCAlgorithm):
     def Initialize(self):
         '''Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.'''
 
-        self.SetStartDate(2013,10,8)   #Set Start Date
-        self.SetEndDate(2013,10,17)    #Set End Date
-        self.SetCash(100000)           #Set Strategy Cash
+        #Use named args for setting up our algorithm
+        self.SetStartDate(month=10,day=8,year=2013)   #Set Start Date
+        self.SetEndDate(month=10,day=17,year=2013)    #Set End Date
+        self.SetCash(startingCash=100000)           #Set Strategy Cash
+
+        #Check our values
+        if self.StartDate.year != 2013 or self.StartDate.month != 10 or self.StartDate.day != 8:
+            raise AssertionError(f"Start date was incorrect! Expected 10/8/2013 Recieved {self.StartDate}");
+
+        if self.EndDate.year != 2013 or self.EndDate.month != 10 or self.EndDate.day != 17:
+            raise AssertionError(f"End date was incorrect! Expected 10/17/2013 Recieved {self.EndDate}");
+
+        if self.Portfolio.Cash != 100000:
+            raise AssertionError(f"Portfolio cash was incorrect! Expected 100000 Recieved {self.Portfolio.Cash}");
 
         # Use named args for addition of this security to our algorithm
-        symbol = self.AddEquity(ticker="SPY", resolution=Resolution.Daily).Symbol
+        symbol = self.AddEquity(resolution=Resolution.Daily, ticker="SPY").Symbol
 
         # Check our subscriptions for the symbol and check its resolution
         for config in self.SubscriptionManager.SubscriptionDataConfigService.GetSubscriptionDataConfigs(symbol):
