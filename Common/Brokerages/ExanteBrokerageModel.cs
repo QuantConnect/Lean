fa0d89ef80eb43ac168d297df97af646c33a1946
Alexander Myltsev <alexander@myltsev.com>
2021-04-05 23:28:29 +0300

    Step 1.3: Create (Empty) Brokerage Model

---
+++ Common/Brokerages/ExanteBrokerageModel.cs
@@ -0,0 +1,9 @@
+namespace QuantConnect.Brokerages
+{
+    /// <summary>
+    /// Exante Brokerage Model Implementation for Back Testing.
+    /// </summary>
+    public class ExanteBrokerageModel : DefaultBrokerageModel
+    {
+    }
+}
