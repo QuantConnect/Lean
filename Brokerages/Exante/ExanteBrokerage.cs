3f4610f4366d9482b471074d2ad218c04c245666
Alexander Myltsev <alexander@myltsev.com>
2021-04-05 23:23:30 +0300

    Step 1.0: add stub for Exante brokerage and factory

---
+++ Brokerages/Exante/ExanteBrokerage.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using Newtonsoft.Json;
+using QuantConnect.Orders;
+using QuantConnect.Securities;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Logging;
+using QuantConnect.Orders.Fees;
+using QuantConnect.Util;
+
+namespace QuantConnect.Brokerages.Exante
+{
+    public class ExanteBrokerage : Brokerage
+    {
+        private bool _isConnected;
+
+        public ExanteBrokerage(string name)
+            : base("Exante Brokerage")
+        {
+        }
+
+        public override bool IsConnected => _isConnected;
+
+        public override List<Order> GetOpenOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Holding> GetAccountHoldings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<CashAmount> GetCashBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool PlaceOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CancelOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Connect()
+        {
+            _isConnected = true;
+        }
+
+        public override void Disconnect()
+        {
+            _isConnected = false;
+        }
+    }
+}
