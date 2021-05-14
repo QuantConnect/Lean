e5b27023807f03c948701a98f16bd238481a4fcf
Brian Mello <Brian.Mello@brukerdetection.us>
2021-05-14 09:47:02 -0400

    remove rogue import

--- Research/start.py
+++ Research/start.py
@@ -1,7 +1,7 @@
 # QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
 # Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
 #
-# Licensed under the Apache License, Version 2.0 (the "License")  import *
+# Licensed under the Apache License, Version 2.0 (the "License")
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
