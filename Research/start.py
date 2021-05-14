c6d771bab46e9a245d044ce2351b3a71db73e1e8
Brian Mello <Brian.Mello@brukerdetection.us>
2021-05-14 09:42:05 -0400

    Fix bugs calling lean from jupyter notebooks

--- Research/start.py
+++ Research/start.py
@@ -1,7 +1,7 @@
-﻿# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
 # Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License")  import *
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -29,24 +29,65 @@ set_runtime(clr_loader.get_coreclr(os.path.join(os.path.dirname(os.path.realpath
 
 from clr import AddReference
 AddReference("System")
-AddReference("QuantConnect.Algorithm")
-AddReference("QuantConnect.Api")
-AddReference("QuantConnect.Common")
-AddReference("QuantConnect.Configuration")
-AddReference("QuantConnect.Research")
-AddReference("QuantConnect.Indicators")
 
-from System import *
-from QuantConnect import *
-from QuantConnect.Algorithm import *
-from QuantConnect.Api import *
-from QuantConnect.Configuration import *
-from QuantConnect.Data import *
-from QuantConnect.Research import *
-from QuantConnect.Indicators import *
+file_absolute_path = os.path.abspath(__file__)
+for file in os.listdir(os.path.dirname(file_absolute_path)):
+    if file.endswith(".dll"):
+        AddReference(file.replace(".dll", ""))
+
+from System  import *
+from System.Collections  import *
+from System.Collections.Generic  import *
+from System.Linq  import *
+from System.Globalization  import *
+from QuantConnect  import *
+from QuantConnect.Algorithm  import *
+from QuantConnect.Algorithm.Framework  import *
+from QuantConnect.Algorithm.Framework.Selection  import *
+from QuantConnect.Algorithm.Framework.Alphas  import *
+from QuantConnect.Algorithm.Framework.Portfolio  import *
+from QuantConnect.Algorithm.Framework.Execution  import *
+from QuantConnect.Algorithm.Framework.Risk  import *
+from QuantConnect.Api  import *
+from QuantConnect.Parameters  import *
+from QuantConnect.Benchmarks  import *
+from QuantConnect.Brokerages  import *
+from QuantConnect.Util  import *
+from QuantConnect.Interfaces  import *
+from QuantConnect.Indicators  import *
+from QuantConnect.Research  import *
+from QuantConnect.Data  import *
+from QuantConnect.Data.Consolidators  import *
+from QuantConnect.Data.Custom  import *
+from QuantConnect.Data.Fundamental  import *
+from QuantConnect.Data.Market  import *
+from QuantConnect.Data.UniverseSelection  import *
+from QuantConnect.Notifications  import *
+from QuantConnect.Orders  import *
+from QuantConnect.Orders.Fees  import *
+from QuantConnect.Orders.Fills  import *
+from QuantConnect.Orders.Slippage  import *
+from QuantConnect.Scheduling  import *
+from QuantConnect.Securities  import *
+from QuantConnect.Securities.Equity  import *
+from QuantConnect.Securities.Forex  import *
+from QuantConnect.Securities.Interfaces  import *
+from QuantConnect.Configuration  import *
 
 # Start an instance of an API class
 api = Api()
 api.Initialize(Config.GetInt("job-user-id", 1), 
     Config.Get("api-access-token", "default"),
     Config.Get("data-folder"))
+
+# Loads composer so that we do not get an exception in the jupyter notebook
+Composer.Instance
+
+#Site packages path needs to be added back for jupyter
+# otherwise Lean engine cannot import pandas, etc.
+import sys
+import site
+site_packages_path = site.getsitepackages()
+for site_package_path in site_packages_path:
+    if site_package_path not in sys.path:
+        sys.path.append(site_package_path)
