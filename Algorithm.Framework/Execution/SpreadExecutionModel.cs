12683fc077027ffbadacb594609f8a3d12a22f78
Louis Szeto <56447733+31620000@users.noreply.github.com>
2021-06-07 09:12:32 +0800

    Create SpreadExecutionModel.cs
    Creating a spread execution model to execute when spread is desirably small.

---
+++ Algorithm.Framework/Execution/SpreadExecutionModel.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuantConnect.Algorithm.Framework.Portfolio;
+using QuantConnect.Data;
+using QuantConnect.Data.Consolidators;
+using QuantConnect.Data.UniverseSelection;
+using QuantConnect.Indicators;
+using QuantConnect.Securities;
+using QuantConnect.Orders;
+
+namespace QuantConnect.Algorithm.Framework.Execution
+{
+    /// <summary>
+    /// Execution model that submits orders while the current spread is in desirably tight extent.
+    /// </summary>
+    public class SpreadExecutionModel : ExecutionModel
+    {
+    	private readonly decimal _acceptingSpreadPercent;
+        private readonly PortfolioTargetCollection _targetsCollection;
+        
+		/// <summary>
+        /// Initializes a new instance of the <see cref="SpreadExecutionModel"/> class
+        /// </summary>
+        /// <param name="acceptingSpreadPercent">Maximum spread accepted comparing to current price in percentage.</param>
+        public SpreadExecutionModel(
+            decimal acceptingSpreadPercent = 0.005m
+            )
+        {
+            _acceptingSpreadPercent = acceptingSpreadPercent;
+            _targetsCollection = new PortfolioTargetCollection();
+        }
+
+        /// <summary>
+        /// Submit orders for the specified portolio targets if the spread is tighter/equal to preset level
+        /// </summary>
+        /// <param name="algorithm">The algorithm instance</param>
+        /// <param name="targets">The portfolio targets to be ordered</param>
+        public override void Execute(QCAlgorithm algorithm, IPortfolioTarget[] targets)
+        {
+            // update the complete set of portfolio targets with the new targets
+            _targetsCollection.AddRange(targets);
+
+            // for performance we check count value, OrderByMarginImpact and ClearFulfilled are expensive to call
+            if (_targetsCollection.Count > 0)
+            {
+                foreach (var target in _targetsCollection.OrderByMarginImpact(algorithm))
+                {
+                    var symbol = target.Symbol;
+
+                    // calculate remaining quantity to be ordered
+                    var unorderedQuantity = OrderSizing.GetUnorderedQuantity(algorithm, target);
+
+                    // check order entry conditions
+                    if ((PriceIsFavorable(algorithm, symbol)) && (unorderedQuantity != 0))
+                        {
+                            algorithm.MarketOrder(symbol, unorderedQuantity);
+                        }
+                }
+
+                _targetsCollection.ClearFulfilled(algorithm);
+            }
+        }
+
+        /// <summary>
+        /// Determines if the current spread is equal or tighter than preset level
+        /// </summary>
+        protected virtual bool PriceIsFavorable(QCAlgorithm algorithm, Symbol symbol)
+        {   
+            // Price has to be larger than zero to avoid zero division error, or negative price causing the spread percentage lower than preset value by accident
+            // Has to be in opening hours of exchange to avoid extreme spread in OTC period
+            if ((algorithm.Securities[symbol].Price > 0) && (algorithm.Securities[symbol].Exchange.ExchangeOpen) && ((algorithm.Securities[symbol].AskPrice - algorithm.Securities[symbol].BidPrice)/algorithm.Securities[symbol].Price <= _acceptingSpreadPercent))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
