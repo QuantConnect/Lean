6c532e49d1b165ca84d71993c2d23709e63ae700
Martin-Molinero <martin@quantconnect.com>
2021-06-10 19:28:09 -0300

    Binance live holdings loading

--- Tests/Brokerages/Binance/BinanceBrokerageTests.cs
+++ Tests/Brokerages/Binance/BinanceBrokerageTests.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -69,7 +69,8 @@ namespace QuantConnect.Tests.Brokerages.Binance
                     apiKey,
                     apiSecret,
                     algorithm.Object,
-                    new AggregationManager()
+                    new AggregationManager(),
+                    null
                 );
         }
 
