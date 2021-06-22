53c8fd73b785c205a035bc3243e38f2d919ae4c4
Alexander Myltsev <alexander@myltsev.com>
2021-06-22 21:45:40 +0300

    Add `ExanteBrokerageFactoryTests`

---
+++ Tests/Brokerages/Exante/ExanteBrokerageFactoryTests.cs
@@ -0,0 +1,54 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using NUnit.Framework;
+using QuantConnect.Algorithm;
+using QuantConnect.Brokerages.Exante;
+using QuantConnect.Interfaces;
+using QuantConnect.Packets;
+using QuantConnect.Util;
+
+namespace QuantConnect.Tests.Brokerages.Exante
+{
+    public class ExanteBrokerageFactoryTests
+    {
+        static readonly IAlgorithm AlgorithmDependency = new ExanteBrokerageFactoryAlgorithmDependency();
+
+        [Test]
+        public void InitializesInstanceFromComposer()
+        {
+            var composer = Composer.Instance;
+            using (var factory =
+                composer.Single<IBrokerageFactory>(instance => instance.BrokerageType == typeof(ExanteBrokerage)))
+            {
+                Assert.IsNotNull(factory);
+
+                var job = new LiveNodePacket {BrokerageData = factory.BrokerageData};
+                using (var brokerage = factory.CreateBrokerage(job, AlgorithmDependency))
+                {
+                    Assert.IsNotNull(brokerage);
+                    Assert.IsInstanceOf<ExanteBrokerage>(brokerage);
+
+                    brokerage.Connect();
+                    Assert.IsTrue(brokerage.IsConnected);
+                }
+            }
+        }
+
+        class ExanteBrokerageFactoryAlgorithmDependency : QCAlgorithm
+        {
+        }
+    }
+}
