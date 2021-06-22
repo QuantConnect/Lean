637d1919f21946f2a2211fe8e8667b9c39531f55
Alexander Myltsev <alexander@myltsev.com>
2021-06-22 10:56:12 +0300

    Add XML comments to Exante data downloader

--- ToolBox/ExanteDownloader/ExanteDataDownloader.cs
+++ ToolBox/ExanteDownloader/ExanteDataDownloader.cs
@@ -2,10 +2,16 @@ namespace QuantConnect.ToolBox.ExanteDownloader
 {
     public class ExanteDataDownloader
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExanteDataDownloader"/>
+        /// </summary>
         public ExanteDataDownloader()
         {
         }
 
+        /// <summary>
+        /// Downloads data from Exante data source
+        /// </summary>
         public void Download()
         {
         }
--- ToolBox/ExanteDownloader/ExanteDownloaderProgram.cs
+++ ToolBox/ExanteDownloader/ExanteDownloaderProgram.cs
@@ -5,6 +5,9 @@ namespace QuantConnect.ToolBox.ExanteDownloader
 {
     public class ExanteDownloaderProgram
     {
+        /// <summary>
+        /// Primary entry point to the program
+        /// </summary>
         public static void ExanteDownloader(IList<string> tickers, string resolution, DateTime fromDate, DateTime toDate)
         {
             var downloader = new ExanteDataDownloader();
