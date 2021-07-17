852e15229068d4d50d72b55a1137669f34acd933
bmello4688 <brian.mello@outlook.com>
2021-07-17 13:47:28 -0400

    Undo

--- Algorithm/QCAlgorithm.cs
+++ Algorithm/QCAlgorithm.cs
@@ -1885,9 +1885,6 @@ namespace QuantConnect.Algorithm
             // Clear cache
             security.Cache.Reset();
 
-            //Remove from data queue
-            SubscriptionManager.SubscriptionDataConfigService.Remove(symbol);
-
             // Mark security as not tradable
             security.IsTradable = false;
             if (symbol.IsCanonical())
