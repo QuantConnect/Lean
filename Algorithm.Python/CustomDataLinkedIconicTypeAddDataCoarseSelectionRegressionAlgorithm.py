3839bd15d618fc2ef46280abb4933c439b228854
Gerardo Salazar <gsalaz9800@gmail.com>
2021-07-20 14:00:37 -0700

    Regression algos updated and python algorithms moved to DataSource repos

--- Algorithm.Python/CustomDataLinkedIconicTypeAddDataCoarseSelectionRegressionAlgorithm.py
+++ Algorithm.Python/CustomDataLinkedIconicTypeAddDataCoarseSelectionRegressionAlgorithm.py
@@ -12,9 +12,9 @@
 # limitations under the License.
 
 from AlgorithmImports import *
-from QuantConnect.Data.Custom.SEC import *
+from QuantConnect.Data.Custom.IconicTypes import *
 
-class CustomDataAddDataOnSecuritiesChangedRegressionAlgorithm(QCAlgorithm):
+class CustomDataLinkedIconicTypeAddDataCoarseSelectionRegressionAlgorithm(QCAlgorithm):
 
     def Initialize(self):
         self.SetStartDate(2014, 3, 24)
@@ -26,7 +26,7 @@ class CustomDataAddDataOnSecuritiesChangedRegressionAlgorithm(QCAlgorithm):
         self.AddUniverseSelection(CoarseFundamentalUniverseSelectionModel(self.CoarseSelector))
 
     def CoarseSelector(self, coarse):
-        return [
+        symbols = [
             Symbol.Create("AAPL", SecurityType.Equity, Market.USA),
             Symbol.Create("BAC", SecurityType.Equity, Market.USA),
             Symbol.Create("FB", SecurityType.Equity, Market.USA),
@@ -34,6 +34,13 @@ class CustomDataAddDataOnSecuritiesChangedRegressionAlgorithm(QCAlgorithm):
             Symbol.Create("GOOG", SecurityType.Equity, Market.USA),
             Symbol.Create("IBM", SecurityType.Equity, Market.USA),
         ]
+
+        self.customSymbols = []
+
+        for symbol in symbols:
+            self.customSymbols.append(self.AddData(LinkedData, symbol, Resolution.Daily).Symbol)
+
+        return symbols
 
     def OnData(self, data):
         if not self.Portfolio.Invested and len(self.Transactions.GetOpenOrders()) == 0:
@@ -43,13 +50,3 @@ class CustomDataAddDataOnSecuritiesChangedRegressionAlgorithm(QCAlgorithm):
         for customSymbol in self.customSymbols:
             if not self.ActiveSecurities.ContainsKey(customSymbol.Underlying):
                 raise Exception(f"Custom data undelrying ({customSymbol.Underlying}) Symbol was not found in active securities")
-
-    def OnSecuritiesChanged(self, changes):
-        iterated = False
-
-        for added in changes.AddedSecurities:
-            if not iterated:
-                self.customSymbols = []
-                iterated = True
-
-            self.customSymbols.append(self.AddData(SECReport8K, added.Symbol, Resolution.Daily).Symbol)
