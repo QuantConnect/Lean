2e5ad6158f8508c78d51007f9792b2d748b503bc
rjra2611 <rjra2611@gmail.com>
2021-07-27 22:20:31 +0530

    revert as per comments by martin in PR #5760

--- Brokerages/Samco/SamcoBrokerageFactory.cs
+++ Brokerages/Samco/SamcoBrokerageFactory.cs
@@ -86,7 +86,6 @@ namespace QuantConnect.Brokerages.Samco
                 Composer.Instance.GetExportedValueByTypeName<IDataAggregator>(Config.Get("data-aggregator", "QuantConnect.Lean.Engine.DataFeeds.AggregationManager")));
             //Add the brokerage to the composer to ensure its accessible to the live data feed.
             Composer.Instance.AddPart<IDataQueueHandler>(brokerage);
-            Composer.Instance.AddPart<IDataQueueUniverseProvider>(brokerage);
             return brokerage;
         }
     }
