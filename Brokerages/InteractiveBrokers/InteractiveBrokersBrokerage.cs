cef76458d4a325545e45a8e0b5cc9aeffe417f59
Stefano Raggi <stefano.raggi67@gmail.com>
2021-07-30 17:40:39 +0200

    Trigger build

--- Brokerages/InteractiveBrokers/InteractiveBrokersBrokerage.cs
+++ Brokerages/InteractiveBrokers/InteractiveBrokersBrokerage.cs
@@ -3432,5 +3432,6 @@ namespace QuantConnect.Brokerages.InteractiveBrokers
         {
             1100, 1101, 1102, 2103, 2104, 2105, 2106, 2107, 2108, 2119, 2157, 2158, 10197
         };
+
     }
 }
