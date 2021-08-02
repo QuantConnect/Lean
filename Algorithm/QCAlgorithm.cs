90b7df41c176ba1fb6e32e7058c621fa856661e1
rjra2611 <rjra2611@gmail.com>
2021-08-02 19:51:39 +0530

    samco options and template correctoin

--- Algorithm/QCAlgorithm.cs
+++ Algorithm/QCAlgorithm.cs
@@ -1561,7 +1561,7 @@ namespace QuantConnect.Algorithm
         /// <param name="leverage">The requested leverage for the </param>
         /// <returns></returns>
         /// <exception cref="KeyNotFoundException"></exception>
-        public Option AddOption(Symbol underlying, Resolution? resolution = null, string market = null, bool fillDataForward = true, decimal leverage = Security.NullLeverage, OptionStyle optionStyle= OptionStyle.American)
+        public Option AddOption(Symbol underlying, Resolution? resolution = null, string market = null, bool fillDataForward = true, decimal leverage = Security.NullLeverage)
         {
             var optionType = QuantConnect.Symbol.GetOptionTypeFromUnderlying(underlying);
 
