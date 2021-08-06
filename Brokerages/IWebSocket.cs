2252da68b8cbc43859c1fc7f7a9666443ac248bd
rjra2611 <rjra2611@gmail.com>
2021-08-06 23:27:04 +0530

    samco-websocketclient rebase
    (cherry picked from commit 555a68dd284054312f1a9ed819c9bddd3369a376)

--- Brokerages/IWebSocket.cs
+++ Brokerages/IWebSocket.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -27,7 +27,7 @@ namespace QuantConnect.Brokerages
         /// Wraps constructor
         /// </summary>
         /// <param name="url"></param>
-        void Initialize(string url);
+        void Initialize(string url, string sessionToken = null);
 
         /// <summary>
         /// Wraps send method
