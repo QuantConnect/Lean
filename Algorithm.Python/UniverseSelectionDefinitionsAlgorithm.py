343be33e6c64900c61304e26227334076618fefc
Gerardo Salazar <gsalaz9800@gmail.com>
2021-08-23 16:47:40 -0700

    Address review: restore QC500 and DollarVolume.Top(...)
      * Restores algorithms related to both helper universe
        definition methods

---
+++ Algorithm.Python/UniverseSelectionDefinitionsAlgorithm.py
@@ -0,0 +1,57 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from AlgorithmImports import *
+
+### <summary>
+### This algorithm shows some of the various helper methods available when defining universes
+### </summary>
+### <meta name="tag" content="using data" />
+### <meta name="tag" content="universes" />
+### <meta name="tag" content="coarse universes" />
+class UniverseSelectionDefinitionsAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        # subscriptions added via universe selection will have this resolution
+        self.UniverseSettings.Resolution = Resolution.Hour
+        # force securities to remain in the universe for a minimm of 30 minutes
+        self.UniverseSettings.MinimumTimeInUniverse = timedelta(minutes=30)
+
+        self.SetStartDate(2013,10,7)    # Set Start Date
+        self.SetEndDate(2013,10,11)     # Set End Date
+        self.SetCash(100000)            # Set Strategy Cash
+
+        # add universe for the top 50 stocks by dollar volume
+        self.AddUniverse(self.Universe.Top(50))
+
+        self.changes = None
+
+    def OnData(self, data):
+        if self.changes is None: return
+
+        # liquidate securities that fell out of our universe
+        for security in self.changes.RemovedSecurities:
+            if security.Invested:
+                self.Liquidate(security.Symbol)
+
+        # invest in securities just added to our universe
+        for security in self.changes.AddedSecurities:
+            if not security.Invested:
+                self.MarketOrder(security.Symbol, 10)
+
+        self.changes = None
+
+
+    # this event fires whenever we have changes to our universe
+    def OnSecuritiesChanged(self, changes):
+        self.changes = changes
---
+++ Algorithm/DollarVolumeUniverseDefinitions.cs
@@ -0,0 +1,56 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics.Statistics;
+using QuantConnect.Data;
+using QuantConnect.Data.UniverseSelection;
+
+namespace QuantConnect.Algorithm
+{
+    /// <summary>
+    /// Provides helpers for defining universes based on the daily dollar volume
+    /// </summary>
+    public class DollarVolumeUniverseDefinitions
+    {
+        private readonly QCAlgorithm _algorithm;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DollarVolumeUniverseDefinitions"/> class
+        /// </summary>
+        /// <param name="algorithm">The algorithm instance, used for obtaining the default <see cref="UniverseSettings"/></param>
+        public DollarVolumeUniverseDefinitions(QCAlgorithm algorithm)
+        {
+            _algorithm = algorithm;
+        }
+
+        /// <summary>
+        /// Creates a new coarse universe that contains the top count of stocks
+        /// by daily dollar volume
+        /// </summary>
+        /// <param name="count">The number of stock to select</param>
+        /// <param name="universeSettings">The settings for stocks added by this universe.
+        /// Defaults to <see cref="QCAlgorithm.UniverseSettings"/></param>
+        /// <returns>A new coarse universe for the top count of stocks by dollar volume</returns>
+        [Obsolete("This method is deprecated. Use method `Universe.DollarVolume.Top(...)` instead")]
+        public Universe Top(int count, UniverseSettings universeSettings = null)
+        {
+            return _algorithm.Universe.Top(count, universeSettings);
+        }
+    }
+}
