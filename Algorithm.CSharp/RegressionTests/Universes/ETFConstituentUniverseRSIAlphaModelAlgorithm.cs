df29c691c4e9deea1eba8599ee6a39c711a3314a
Gerardo Salazar <gsalaz9800@gmail.com>
2021-08-24 14:48:07 -0700

    Address review: make ETF Constituent RSI Alpha Model algo a regression test

--- Algorithm.CSharp/RegressionTests/Universes/ETFConstituentUniverseRSIAlphaModelAlgorithm.cs
+++ Algorithm.CSharp/RegressionTests/Universes/ETFConstituentUniverseRSIAlphaModelAlgorithm.cs
@@ -22,6 +22,7 @@ using QuantConnect.Algorithm.Framework.Portfolio;
 using QuantConnect.Data;
 using QuantConnect.Data.UniverseSelection;
 using QuantConnect.Indicators;
+using QuantConnect.Interfaces;
 
 namespace QuantConnect.Algorithm.CSharp
 {
@@ -30,7 +31,7 @@ namespace QuantConnect.Algorithm.CSharp
     /// in combination with ETF constituents data to replicate the weighting
     /// of the ETF's assets in our own account.
     /// </summary>
-    public class ETFConstituentUniverseRSIAlphaModelAlgorithm : QCAlgorithm
+    public class ETFConstituentUniverseRSIAlphaModelAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
     {
         /// <summary>
         /// Initialize the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
@@ -201,5 +202,64 @@ namespace QuantConnect.Algorithm.CSharp
                 Rsi = algorithm.RSI(symbol, period, MovingAverageType.Exponential, Resolution.Hour);
             }
         }
+        
+        /// <summary>
+        /// This is used by the regression test system to indicate if the open source Lean repository has the required data to run this algorithm.
+        /// </summary>
+        public bool CanRunLocally { get; } = true;
+
+        /// <summary>
+        /// This is used by the regression test system to indicate which languages this algorithm is written in.
+        /// </summary>
+        public Language[] Languages { get; } = { Language.CSharp, Language.Python };
+
+        /// <summary>
+        /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
+        /// </summary>
+        public Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
+        {
+            {"Total Trades", "9"},
+            {"Average Win", "0.00%"},
+            {"Average Loss", "-0.01%"},
+            {"Compounding Annual Return", "-0.064%"},
+            {"Drawdown", "0.100%"},
+            {"Expectancy", "-0.331"},
+            {"Net Profit", "-0.010%"},
+            {"Sharpe Ratio", "-1.204"},
+            {"Probabilistic Sharpe Ratio", "15.336%"},
+            {"Loss Rate", "50%"},
+            {"Win Rate", "50%"},
+            {"Profit-Loss Ratio", "0.34"},
+            {"Alpha", "-0.001"},
+            {"Beta", "-0.001"},
+            {"Annual Standard Deviation", "0.001"},
+            {"Annual Variance", "0"},
+            {"Information Ratio", "-0.86"},
+            {"Tracking Error", "0.129"},
+            {"Treynor Ratio", "1.008"},
+            {"Total Fees", "$9.00"},
+            {"Estimated Strategy Capacity", "$120000000.00"},
+            {"Lowest Capacity Asset", "AIG R735QTJ8XC9X"},
+            {"Fitness Score", "0.002"},
+            {"Kelly Criterion Estimate", "54.21"},
+            {"Kelly Criterion Probability Value", "0.251"},
+            {"Sortino Ratio", "79228162514264337593543950335"},
+            {"Return Over Maximum Drawdown", "-2.425"},
+            {"Portfolio Turnover", "0.003"},
+            {"Total Insights Generated", "4"},
+            {"Total Insights Closed", "4"},
+            {"Total Insights Analysis Completed", "4"},
+            {"Long Insight Count", "4"},
+            {"Short Insight Count", "0"},
+            {"Long/Short Ratio", "100%"},
+            {"Estimated Monthly Alpha Value", "$-37463.63"},
+            {"Total Accumulated Estimated Alpha Value", "$-74771.15"},
+            {"Mean Population Estimated Insight Value", "$-18692.79"},
+            {"Mean Population Direction", "75%"},
+            {"Mean Population Magnitude", "75%"},
+            {"Rolling Averaged Population Direction", "75%"},
+            {"Rolling Averaged Population Magnitude", "75%"},
+            {"OrderListHash", "719522d3df1987e19b715af1fe65e1f6"}
+        };
     }
 }
