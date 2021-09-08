03905c89dae4c422c39d7ca8ecb37e2da7698e6c
C-SELLERS <Colton.R.Sellers@gmail.com>
2021-09-08 23:30:26 +0000

    Implement options support for history requests

--- Brokerages/Tradier/TradierBrokerage.HistoryProvider.cs
+++ Brokerages/Tradier/TradierBrokerage.HistoryProvider.cs
@@ -81,7 +81,7 @@ namespace QuantConnect.Brokerages.Tradier
         {
             foreach (var request in requests)
             {
-                if (request.Symbol.ID.SecurityType != SecurityType.Equity)
+                if (request.Symbol.ID.SecurityType != SecurityType.Equity && request.Symbol.ID.SecurityType != SecurityType.Option)
                 {
                     throw new ArgumentException("Invalid security type: " + request.Symbol.ID.SecurityType);
                 }
@@ -89,6 +89,11 @@ namespace QuantConnect.Brokerages.Tradier
                 if (request.StartTimeUtc >= request.EndTimeUtc)
                 {
                     throw new ArgumentException("Invalid date range specified");
+                }
+
+                if (request.Symbol.IsCanonical())
+                {
+                    throw new ArgumentException("Invalid symbol, cannot use canonical symbols for history request");
                 }
 
                 var start = request.StartTimeUtc.ConvertTo(DateTimeZone.Utc, TimeZones.NewYork);
@@ -164,7 +169,7 @@ namespace QuantConnect.Brokerages.Tradier
 
         private IEnumerable<Slice> GetHistoryTick(Symbol symbol, DateTime start, DateTime end)
         {
-            var history = GetTimeSeries(symbol.Value, start, end, TradierTimeSeriesIntervals.Tick);
+            var history = GetTimeSeries(symbol, start, end, TradierTimeSeriesIntervals.Tick);
 
             if (history == null)
                 return Enumerable.Empty<Slice>();
@@ -185,7 +190,7 @@ namespace QuantConnect.Brokerages.Tradier
 
         private IEnumerable<Slice> GetHistorySecond(Symbol symbol, DateTime start, DateTime end)
         {
-            var history = GetTimeSeries(symbol.Value, start, end, TradierTimeSeriesIntervals.Tick);
+            var history = GetTimeSeries(symbol, start, end, TradierTimeSeriesIntervals.Tick);
 
             if (history == null)
                 return Enumerable.Empty<Slice>();
@@ -220,7 +225,7 @@ namespace QuantConnect.Brokerages.Tradier
 
         private IEnumerable<Slice> GetHistoryMinute(Symbol symbol, DateTime start, DateTime end)
         {
-            var history = GetTimeSeries(symbol.Value, start, end, TradierTimeSeriesIntervals.OneMinute);
+            var history = GetTimeSeries(symbol, start, end, TradierTimeSeriesIntervals.OneMinute);
 
             if (history == null)
                 return Enumerable.Empty<Slice>();
@@ -234,7 +239,7 @@ namespace QuantConnect.Brokerages.Tradier
 
         private IEnumerable<Slice> GetHistoryHour(Symbol symbol, DateTime start, DateTime end)
         {
-            var history = GetTimeSeries(symbol.Value, start, end, TradierTimeSeriesIntervals.FifteenMinutes);
+            var history = GetTimeSeries(symbol, start, end, TradierTimeSeriesIntervals.FifteenMinutes);
 
             if (history == null)
                 return Enumerable.Empty<Slice>();
@@ -262,7 +267,7 @@ namespace QuantConnect.Brokerages.Tradier
 
         private IEnumerable<Slice> GetHistoryDaily(Symbol symbol, DateTime start, DateTime end)
         {
-            var history = GetHistoricalData(symbol.Value, start, end);
+            var history = GetHistoricalData(symbol, start, end);
 
             DataPointCount += history.Count;
 
