2ccc10f719b4c5f389acb27a6ffa1ecdd5b75f6c
bmello4688 <brian.mello@outlook.com>
2021-10-19 06:44:36 -0400

    change client to static so we only initialize once

--- ToolBox/TDAmeritradeDownloader/TDAmeritradeDataDownloader.cs
+++ ToolBox/TDAmeritradeDownloader/TDAmeritradeDataDownloader.cs
@@ -10,15 +10,18 @@ namespace QuantConnect.ToolBox.TDAmeritradeDownloader
 {
     public class TDAmeritradeDataDownloader : IDataDownloader
     {
-        private readonly TDAmeritradeClient tdClient;
+        private static TDAmeritradeClient tdClient;
 
         public TDAmeritradeDataDownloader()
         {
             var clientID = Config.Get("td-client-id", "");
             var redirectUri = Config.Get("td-redirect-uri", "");
             var tdCredentials = Composer.Instance.GetExportedValueByTypeName<ICredentials>(Config.Get("td-credentials-provider", "QuantConnect.Brokerages.TDAmeritrade.TDCliCredentialProvider"));
-            tdClient = new TDAmeritradeClient(clientID, redirectUri);
-            tdClient.LogIn(tdCredentials).Wait();
+            if (tdClient == null)
+            {
+                tdClient = new TDAmeritradeClient(clientID, redirectUri);
+                tdClient.LogIn(tdCredentials).Wait();
+            }
         }
 
         public IEnumerable<BaseData> Get(Symbol symbol, Resolution resolution, DateTime startUtc, DateTime endUtc)
