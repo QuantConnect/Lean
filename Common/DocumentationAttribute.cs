e3a562d3c9a80b5dc55367a4162660374119194a
Martin-Molinero <martin@quantconnect.com>
2021-12-20 22:30:04 -0300

    Add file and line number to documentation attribute (#6122)

--- Common/DocumentationAttribute.cs
+++ Common/DocumentationAttribute.cs
@@ -14,7 +14,9 @@
 */
 
 using System;
+using System.Linq;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace QuantConnect
 {
@@ -22,8 +24,14 @@ namespace QuantConnect
     /// Custom attribute used for documentation
     /// </summary>
     [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    [DocumentationAttribute("Reference")]
     public sealed class DocumentationAttribute : Attribute
     {
+        private static readonly DocumentationAttribute Attribute =
+            typeof(DocumentationAttribute).GetCustomAttributes<DocumentationAttribute>().Single();
+        private static readonly string BasePath =
+            Attribute.FileName.Substring(0, Attribute.FileName.LastIndexOf("Common", StringComparison.Ordinal));
+
         /// <summary>
         /// The documentation tag
         /// </summary>
@@ -37,6 +45,18 @@ namespace QuantConnect
         public int Weight { get; }
 
         /// <summary>
+        ///  The associated line of this attribute
+        /// </summary>
+        [JsonProperty(PropertyName = "line")]
+        public int Line { get; }
+
+        /// <summary>
+        ///  The associated file name of this attribute
+        /// </summary>
+        [JsonProperty(PropertyName = "fileName")]
+        public string FileName { get; }
+
+        /// <summary>
         /// The attributes type id, we override it to ignore it when serializing
         /// </summary>
         [JsonIgnore]
@@ -45,10 +65,22 @@ namespace QuantConnect
         /// <summary>
         /// Creates a new instance
         /// </summary>
-        public DocumentationAttribute(string tag, int weight = 0)
+        public DocumentationAttribute(string tag, int weight = 0,
+            [System.Runtime.CompilerServices.CallerLineNumber] int line = 0,
+            [System.Runtime.CompilerServices.CallerFilePath] string fileName = "")
         {
             Tag = tag;
+            Line = line;
             Weight = weight;
+            // will be null for the attribute of DocumentationAttribute itself
+            if (BasePath != null)
+            {
+                FileName = fileName.Replace(BasePath, string.Empty, StringComparison.InvariantCultureIgnoreCase);
+            }
+            else
+            {
+                FileName = fileName;
+            }
         }
     }
 }
