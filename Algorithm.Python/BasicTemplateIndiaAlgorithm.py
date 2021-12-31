4700b58ccc49b55366c71683a02562cd79969d4b
rjra2611 <rjra2611@gmail.com>
2021-12-31 22:06:05 +0530

    add python algo for BasicTemplateIndiaAlgorithm

---
+++ Algorithm.Python/BasicTemplateIndiaAlgorithm.py
@@ -0,0 +1,85 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from AlgorithmImports import *
+from QuantConnect import *
+from QuantConnect.Parameters import *
+from QuantConnect.Benchmarks import *
+from QuantConnect.Brokerages import *
+from QuantConnect.Util import *
+from QuantConnect.Interfaces import *
+from QuantConnect.Algorithm import *
+from QuantConnect.Algorithm.Framework import *
+from QuantConnect.Algorithm.Framework.Selection import *
+from QuantConnect.Algorithm.Framework.Alphas import *
+from QuantConnect.Algorithm.Framework.Portfolio import *
+from QuantConnect.Algorithm.Framework.Execution import *
+from QuantConnect.Algorithm.Framework.Risk import *
+from QuantConnect.Indicators import *
+from QuantConnect.Data import *
+from QuantConnect.Data.Consolidators import *
+from QuantConnect.Data.Custom import *
+from QuantConnect.Data.Fundamental import *
+from QuantConnect.Data.Market import *
+from QuantConnect.Data.UniverseSelection import *
+from QuantConnect.Notifications import *
+from QuantConnect.Orders import *
+from QuantConnect.Orders.Fees import *
+from QuantConnect.Orders.Fills import *
+from QuantConnect.Orders.Slippage import *
+from QuantConnect.Scheduling import *
+from QuantConnect.Securities import *
+from QuantConnect.Securities.Equity import *
+from QuantConnect.Securities.Forex import *
+from QuantConnect.Securities.Interfaces import *
+from datetime import date, datetime, timedelta
+from QuantConnect.Python import *
+from QuantConnect.Storage import *
+QCAlgorithmFramework = QCAlgorithm
+QCAlgorithmFrameworkBridge = QCAlgorithm
+
+### <summary>
+### Basic template framework algorithm uses framework components to define the algorithm.
+### </summary>
+### <meta name="tag" content="using data" />
+### <meta name="tag" content="using quantconnect" />
+### <meta name="tag" content="trading and orders" />
+class BasicTemplateIndiaAlgorithm(QCAlgorithm):
+    '''Basic template framework algorithm uses framework components to define the algorithm.'''
+
+    def Initialize(self):
+        ''' Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.'''
+
+        self.SetStartDate(2013,10,7)   #Set Start Date
+        self.SetEndDate(2013,10,11)    #Set End Date
+        self.SetCash(100000)           #Set Strategy Cash
+
+        # Find more symbols here: http://quantconnect.com/data
+        # Forex, CFD, Equities Resolutions: Tick, Second, Minute, Hour, Daily.
+        # Futures Resolution: Tick, Second, Minute
+        # Options Resolution: Minute Only.
+        symbols = [ Symbol.Create("AARTIDRUGS", SecurityType.Equity, Resolution.Minute, Market.India) ]
+
+        # Set Order Prperties as per the requirements for order placement
+        DefaultOrderProperties = IndiaOrderProperties(Exchange.NSE)
+
+        self.Debug("numpy test >>> print numpy.pi: " + str(np.pi))
+
+
+    def OnData(self, data):
+        if Portfolio.Invested is False:
+            marketTicket = MarketOrder("AARTIDRUGS", 1)
+
+    def OnOrderEvent(self, orderEvent):
+        if orderEvent.Status == OrderStatus.Filled:
+            self.Debug("Purchased Stock: {0}".format(orderEvent.Symbol))
