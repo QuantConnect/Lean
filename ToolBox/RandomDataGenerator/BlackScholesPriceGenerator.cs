ca63c00a4a7ed0b2b412de53d7b7020a66f64539
Adalyat Nazirov <aenazirov@gmail.com>
2022-01-03 23:59:37 +0300

    more comments and unit tests

--- ToolBox/RandomDataGenerator/BlackScholesPriceGenerator.cs
+++ ToolBox/RandomDataGenerator/BlackScholesPriceGenerator.cs
@@ -6,12 +6,17 @@ using QuantConnect.Data.Market;
 namespace QuantConnect.ToolBox.RandomDataGenerator
 {
     /// <summary>
-    /// Pricing model used to determine the fair price or theoretical value for a call or a put option based
+    /// Pricing model used to determine the fair price or theoretical value for a call or a put option price
+    /// based on Black-Scholes-Merton model
     /// </summary>
     public class BlackScholesPriceGenerator : IPriceGenerator
     {
         private readonly Securities.Option.Option _option;
 
+        /// <summary>
+        /// Creates instance of <see cref="BlackScholesPriceGenerator"/>
+        /// </summary>
+        ///<param name="security"><see cref="Security"/> object for which to generate price data</param>
         public BlackScholesPriceGenerator(Security security)
         {
             if (security == null)
@@ -27,12 +32,25 @@ namespace QuantConnect.ToolBox.RandomDataGenerator
             _option = security as Securities.Option.Option;
         }
 
+        /// <summary>
+        /// For Black-Scholes-Merton model the price of underlying security is used as reference
+        /// </summary>
+        /// <param name="referencePrice">previous reference price</param>
+        /// <param name="maximumPercentDeviation">The maximum percent deviation. This value is in percent space,
+        ///     so a value of 1m is equal to 1%.</param>
+        /// <returns>A new decimal suitable for usage as reference price</returns>
         public decimal NextReferencePrice(
             decimal referencePrice,
             decimal maximumPercentDeviation
             )
             => _option.Underlying.Price;
 
+        /// <summary>
+        /// For Black-Scholes-Merton model price calculation relies <see cref="IOptionPriceModel"/> of the security
+        /// </summary>
+        /// <param name="referencePrice">current reference price</param>
+        /// /// <param name="referenceDate">current reference date</param>
+        /// <returns>A new decimal suitable for usage as new security price</returns>
         public decimal NextValue(decimal referencePrice, DateTime referenceDate)
         {
             return _option.PriceModel
--- ToolBox/RandomDataGenerator/RandomPriceGenerator.cs
+++ ToolBox/RandomDataGenerator/RandomPriceGenerator.cs
@@ -2,25 +2,41 @@ using System;
 
 namespace QuantConnect.ToolBox.RandomDataGenerator
 {
+    /// <summary>
+    /// Random pricing model used to determine the fair price or theoretical value for a call or a put option
+    /// </summary>
     public class RandomPriceGenerator : IPriceGenerator
     {
         private readonly Symbol _symbol;
         private readonly IRandomValueGenerator _random;
 
         /// <summary>
-        /// 
+        /// Creates instance of <see cref="RandomPriceGenerator"/>
         /// </summary>
-        /// <param name="symbol"></param>
-        /// <param name="random"></param>
+        /// <param name="symbol">The symbol of the security</param>
+        /// <param name="random"><see cref="IRandomValueGenerator"/> type capable of producing random values</param>
         public RandomPriceGenerator(Symbol symbol, IRandomValueGenerator random)
         {
             _symbol = symbol;
             _random = random;
         }
 
+        /// <summary>
+        /// Generates a random price used in further price calculation
+        /// </summary>
+        /// <param name="referencePrice">previous reference price</param>
+        /// <param name="maximumPercentDeviation">The maximum percent deviation. This value is in percent space,
+        ///     so a value of 1m is equal to 1%.</param>
+        /// <returns>A new decimal suitable for usage as reference price</returns>
         public decimal NextReferencePrice(decimal referencePrice, decimal maximumPercentDeviation)
             => _random.NextPrice(_symbol.SecurityType, _symbol.ID.Market, referencePrice, maximumPercentDeviation);
-        
+
+        /// <summary>
+        /// Generates an asset price
+        /// </summary>
+        /// <param name="referencePrice">reference price used in price calculation</param>
+        /// <param name="referenceDate">date used in price calculation</param>
+        /// <returns>Returns a new decimal as price</returns>
         public decimal NextValue(decimal referencePrice, DateTime referenceDate)
             => referencePrice;
     }
