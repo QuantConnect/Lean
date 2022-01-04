914d85611c485ff2c62475695dd75b5dd32f014d
Adalyat Nazirov <aenazirov@gmail.com>
2022-01-04 17:05:31 +0300

    add disclaimer

--- ToolBox/RandomDataGenerator/IRandomDataGenerator.cs
+++
@@ -1,9 +0,0 @@
-namespace QuantConnect.ToolBox.RandomDataGenerator
-{
-    public interface IRandomDataGenerator
-    {
-        BaseSymbolGenerator CreateSymbolGenerator();
-
-        ITickGenerator CreateTickGenerator();
-    }
-}
--- ToolBox/RandomDataGenerator/ITickGenerator.cs
+++ ToolBox/RandomDataGenerator/ITickGenerator.cs
@@ -1,3 +1,18 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 using System;
 using QuantConnect.Data.Market;
 using System.Collections.Generic;
@@ -42,7 +57,5 @@ namespace QuantConnect.ToolBox.RandomDataGenerator
         /// <returns>A new <see cref="DateTime"/> that is after <paramref name="previous"/> according to the specified <paramref name="resolution"/>
         /// and <paramref name="density"/> specified</returns>
         DateTime NextTickTime( DateTime previous, Resolution resolution, DataDensity density);
-
-
     }
 }
