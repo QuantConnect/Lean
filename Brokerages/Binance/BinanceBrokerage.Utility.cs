ea7715f1e6419f53b4c4df4bf4a409dd97022466
Adalyat Nazirov <aenazirov@gmail.com>
2022-01-29 00:10:11 +0300

    "NEW" means that the order has been accepted by the Binance engine.

--- Brokerages/Binance/BinanceBrokerage.Utility.cs
+++ Brokerages/Binance/BinanceBrokerage.Utility.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -27,7 +27,7 @@ namespace QuantConnect.Brokerages.Binance
             switch (raw.LazyToUpper())
             {
                 case "NEW":
-                    return OrderStatus.New;
+                    return OrderStatus.Submitted;
 
                 case "PARTIALLY_FILLED":
                     return OrderStatus.PartiallyFilled;
