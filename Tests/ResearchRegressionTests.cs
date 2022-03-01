fa32201933977ad64c879e01d49c602a8c482da0
Ronit Jain <rjra2611@gmail.com>
2022-03-02 01:26:14 +0530

    automatically update expected output

--- Tests/ResearchRegressionTests.cs
+++ Tests/ResearchRegressionTests.cs
@@ -15,9 +15,12 @@
 
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
-using QuantConnect.Algorithm.CSharp;
+using QuantConnect.Configuration;
 using QuantConnect.Interfaces;
+using QuantConnect.Tests.Research.RegressionDefinitions;
+using QuantConnect.Util;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -28,10 +31,118 @@ namespace QuantConnect.Tests
     [TestFixture, Category("ResearchRegressionTests")]
     public class ResearchRegressionTests
     {
+        // Update in config.json when template expected result needs to be updated
+        private static readonly bool UpdateResearchRegressionOutput = Config.GetBool("research-regression-update-output", false);
+
         [Test, TestCaseSource(nameof(GetResearchRegressionTestParameters))]
         public void ResearchRegression(ResearchRegressionTestParameters parameters)
         {
-            var args = $"\"{parameters.NotebookPath}\" \"{parameters.NotebookOutputPath}\" --log-output --cwd {Directory.GetCurrentDirectory()}";
+            var actualOutput = RunResearchNotebookAndGetOutput(parameters.NotebookPath, parameters.NotebookOutputPath, Directory.GetCurrentDirectory());
+            var actualCells = JToken.Parse(actualOutput)["cells"];
+            var expeectedCells = JToken.Parse(parameters.ExpectedOutput)["cells"];
+            var expectedAndActualCells = expeectedCells.Zip(actualCells, (e, a) => new { Expected = e, Actual = a });
+            foreach (var cell in expectedAndActualCells)
+            {
+                // Assert Notebook Cell Input
+                Assert.AreEqual(cell.Expected["source"], cell.Actual["source"]);
+
+                // Assert Notebook Cell Output
+                var expectedCellOutputs = cell.Expected["outputs"];
+                var actualCellOutputs = cell.Actual["outputs"];
+                if (expectedCellOutputs != null)
+                {
+                    if (actualCellOutputs == null)
+                    {
+                        Assert.Fail("Shouldn't be null when expected is not null");
+                    }
+                    // Iterate over all outputs for the given notebook cell
+                    var expectedCellOutputsAndActualCellOutputs = expectedCellOutputs.Zip(actualCellOutputs, (e, a) => new { Expected = e, Actual = a });
+                    foreach (var cellOutputsItem in expectedCellOutputsAndActualCellOutputs)
+                    {
+                        if (cellOutputsItem.Expected["data"] != null && cellOutputsItem.Expected["data"]["text/html"] != null)
+                        {
+                            continue;
+                        }
+                        else if (cellOutputsItem.Expected["name"]?.ToString() == "stdout" && cellOutputsItem.Expected["text"] != null)
+                        {
+                            if (!IsDeterministic(cellOutputsItem.Expected["text"].ToString()))
+                            {
+                                continue;
+                            }
+                        }
+                        Assert.AreEqual(cellOutputsItem.Expected, cellOutputsItem.Actual);
+                    }
+                }
+            }
+        }
+
+        public class ResearchRegressionTestParameters
+        {
+            public readonly string NotebookName;
+            public readonly string NotebookPath;
+            public readonly string ExpectedOutput;
+            public readonly string NotebookOutputPath;
+
+            public ResearchRegressionTestParameters(string notebookName, string notebookPath, string expectedOutput)
+            {
+                NotebookName = notebookName;
+                NotebookPath = notebookPath;
+                ExpectedOutput = expectedOutput;
+                NotebookOutputPath = notebookPath.Split(".")[0] + "-output" + ".ipynb";
+            }
+        }
+
+        private static void UpdateResearchRegressionOutputInSourceFile(string templateName, string notebookPath, string notebookoutputPath, string workingDirectoryForNotebook)
+        {
+            var templatePath = Directory.EnumerateFiles("../../Research/RegressionTemplates", $"*{templateName}.cs", SearchOption.AllDirectories).Single();
+            var file = File.ReadAllLines(templatePath).ToList().GetEnumerator();
+            var lines = new List<string>();
+            while (file.MoveNext())
+            {
+                var line = file.Current;
+                if (line == null)
+                {
+                    continue;
+                }
+
+                if (line.Contains("public string ExpectedOutput =>"))
+                {
+                    lines.Add(line);
+                    var expectedOutput = RunResearchNotebookAndGetOutput(notebookPath, notebookoutputPath, workingDirectoryForNotebook);
+                    expectedOutput = expectedOutput
+                        .Replace("\\\\", "\\\\\\\\")
+                        .Replace("\\\"", "\\\\\"")
+                        .Replace("\"", "\\\"")
+                        .Replace("\\n", "\\\n")
+                        .Replace("\n", "\\n")
+                        .Replace("\t", "\\t")
+                        .Replace("\r", "\\r");
+                    lines.Add($"            \"{expectedOutput}\";");
+
+                    // now we skip existing expected statistics in file
+                    while (file.MoveNext())
+                    {
+                        line = file.Current;
+                        if (line != null && line.StartsWith("    }"))
+                        {
+                            lines.Add(line);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    lines.Add(line);
+                }
+            }
+
+            file.DisposeSafely();
+            File.WriteAllLines(templatePath, lines);
+        }
+
+        private static string RunResearchNotebookAndGetOutput(string notebookPath, string notebookoutputPath, string workingDirectoryForNotebook)
+        {
+            var args = $"\"{notebookPath}\" \"{notebookoutputPath}\" --log-output --cwd {workingDirectoryForNotebook}";
 
             // Use ProcessStartInfo class
             var startInfo = new ProcessStartInfo("papermill", args)
@@ -53,44 +164,36 @@ namespace QuantConnect.Tests
             process.BeginOutputReadLine();
             process.WaitForExit();
             process.Dispose();
-            var actualOutput = File.ReadAllText(parameters.NotebookOutputPath);
-            var actualCells = JToken.Parse(actualOutput)["cells"];
-            var expeectedCells = JToken.Parse(parameters.ExpectedOutput)["cells"];
-            var expectedAndActual = expeectedCells.Zip(actualCells, (e, a) => new { Expected = e, Actual = a });
-            foreach (var item in expectedAndActual)
-            {
-                Assert.AreEqual(item.Expected["source"], item.Actual["source"]);
-                Assert.AreEqual(item.Expected["outputs"], item.Actual["outputs"]);
-            }
+            return File.ReadAllText(notebookoutputPath);
         }
 
-        public class ResearchRegressionTestParameters
+        private static bool IsDeterministic(string input)
         {
-            public readonly string NotebookName;
-            public readonly string NotebookPath;
-            public readonly string ExpectedOutput;
-            public readonly string NotebookOutputPath;
-
-            public ResearchRegressionTestParameters(string notebookName, string notebookPath, string expectedOutput)
+            Regex rgxDateTime = new(@"(\d{4})(\d{2})(\d{2}) (\d{2}):(\d{2}):(\d{2}).(\d{3}) TRACE::");
+            if (input.Contains("Initialize.csx"))
             {
-                NotebookName = notebookName;
-                NotebookPath = notebookPath;
-                ExpectedOutput = expectedOutput;
-                NotebookOutputPath = notebookPath.Split(".")[0] + "-output" + ".ipynb";
+                return false;
             }
+            else if (input.Contains("Runtime.Initialize():"))
+            {
+                return false;
+            }
+            else if (input.Contains("PythonEngine.Initialize():"))
+            {
+                return false;
+            }
+            else if (rgxDateTime.IsMatch(input))
+            {
+                return false;
+            }
+            return true;
         }
 
-        private static TestCaseData[] GetResearchRegressionTestParameters()
+        private static ResearchRegressionTestParameters[] GetResearchTemplates()
         {
-            TestGlobals.Initialize();
-
-            // since these are static test cases, they are executed before test setup
-            AssemblyInitialize.AdjustCurrentDirectory();
-
-            // find all research regression algorithms in Algorithm.CSharp
             var result =
             (
-                from type in typeof(BasicTemplateResearch).Assembly.GetTypes()
+                from type in typeof(BasicTemplateResearchPython).Assembly.GetTypes()
                 where typeof(IRegressionResearchDefinition).IsAssignableFrom(type)
                 where !type.IsAbstract                          // non-abstract
                 where type.GetConstructor(Array.Empty<Type>()) != null  // has default ctor
@@ -98,11 +201,32 @@ namespace QuantConnect.Tests
                 let path = Path.GetFullPath(Path.Combine(Path.Combine(type.Assembly.Location, @"../"), type.Name + ".ipynb"))
                 select new ResearchRegressionTestParameters(type.Name, path, instance.ExpectedOutput)
             )
-            .OrderBy(x => x.NotebookName)
-            // generate test cases from test parameters
-            .Select(x => new TestCaseData(x).SetName(x.NotebookName))
+            .Select(x => x)
             .ToArray();
             return result;
+        }
+
+        private static TestCaseData[] GetResearchRegressionTestParameters()
+        {
+            if (UpdateResearchRegressionOutput)
+            {
+                var templates = GetResearchTemplates();
+                foreach (var template in templates)
+                {
+                    UpdateResearchRegressionOutputInSourceFile(template.NotebookName, template.NotebookPath, template.NotebookOutputPath, Directory.GetCurrentDirectory());
+                }
+            }
+
+            TestGlobals.Initialize();
+
+            // since these are static test cases, they are executed before test setup
+            AssemblyInitialize.AdjustCurrentDirectory();
+            var result = GetResearchTemplates()
+                .OrderBy(x => x.NotebookName)
+                // generate test cases from test parameters
+                .Select(x => new TestCaseData(x).SetName(x.NotebookName))
+                .ToArray();
+            return result; ;
         }
     }
 }
