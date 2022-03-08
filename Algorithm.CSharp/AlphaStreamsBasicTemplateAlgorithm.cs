49a73aa15c16b9854525040f2d1fcc106215dcf4
Ronit Jain <rjra2611@gmail.com>
2022-03-09 00:16:54 +0530

    add data count properties

--- Algorithm.CSharp/AlphaStreamsBasicTemplateAlgorithm.cs
+++ Algorithm.CSharp/AlphaStreamsBasicTemplateAlgorithm.cs
@@ -66,6 +66,16 @@ namespace QuantConnect.Algorithm.CSharp
         public Language[] Languages { get; } = { Language.CSharp };
 
         /// <summary>
+        /// Data Points count of all timeslices of algorithm
+        /// </summary>
+        public virtual long DataPoints => 890;
+
+        /// </summary>
+        /// Data Points count of the algorithm history
+        /// </summary>
+        public virtual int AlgorithmHistoryDataPoints => 12;
+
+        /// <summary>
         /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
         /// </summary>
         public virtual Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
