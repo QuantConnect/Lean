48c33f65a707e31dc0129df1a8be436f842c4b86
Ronit Jain <rjra2611@gmail.com>
2022-03-14 23:34:29 +0530

    change data count

--- Algorithm.CSharp/HistoryAlgorithm.cs
+++ Algorithm.CSharp/HistoryAlgorithm.cs
@@ -293,12 +293,12 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 47;
+        public long DataPoints => -1;
 
         /// </summary>
         /// Data Points count of the algorithm history
         /// </summary>
-        public int AlgorithmHistoryDataPoints => 3526;
+        public int AlgorithmHistoryDataPoints => -1;
 
         /// <summary>
         /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
