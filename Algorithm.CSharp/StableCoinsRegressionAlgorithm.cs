7d0dd2c2bffaee76e2f71571bd33796372aebaf2
Ricardo Andrés Marino Rojas <rmarino@unal.edu.co>
2022-04-08 12:04:08 -0500

    Solve bugs

--- Algorithm.CSharp/StableCoinsRegressionAlgorithm.cs
+++ Algorithm.CSharp/StableCoinsRegressionAlgorithm.cs
@@ -51,7 +51,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 102;
+        public long DataPoints => 50;
 
         /// </summary>
         /// Data Points count of the algorithm history
