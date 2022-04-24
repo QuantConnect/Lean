02b621083988dc8bbefc1924d51a0c2e722f74c5
Martin-Molinero <martin@quantconnect.com>
2022-04-24 17:59:47 -0300

    Update to net6

--- Report/ReportElements/MarketsReportElement.cs
+++ Report/ReportElements/MarketsReportElement.cs
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-using MoreLinq;
+
 using System.Linq;
 using QuantConnect.Orders;
 using QuantConnect.Packets;
