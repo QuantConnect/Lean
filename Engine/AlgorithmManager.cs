a2ec0c9c42a17325595695c1455fcd6604fa4ac2
Martin-Molinero <martin@quantconnect.com>
2022-06-02 17:20:47 -0300

    Add console message during warmup period

--- Engine/AlgorithmManager.cs
+++ Engine/AlgorithmManager.cs
@@ -750,6 +750,7 @@ namespace QuantConnect.Lean.Engine
                         if (newPercent != warmingUpPercent)
                         {
                             warmingUpPercent = newPercent;
+                            algorithm.Debug($"Processing algorithm warm-up request {warmingUpPercent}%...");
                             results.SendStatusUpdate(AlgorithmStatus.History, $"{warmingUpPercent}");
                         }
                     }
