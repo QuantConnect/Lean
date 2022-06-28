bb4bf46ff2bc4ebe7d0fc9b64ed4a54c8239bfb2
Martin-Molinero <martin@quantconnect.com>
2022-06-28 12:21:29 -0300

    Fix custom fine universe selection model
    - For for custom fine universe selection model so it can return
      Universe.Unchanged. Adding regression tests

---
+++ Algorithm.Python/CustomUniverseSelectionModelRegressionAlgorithm.py
@@ -0,0 +1,52 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from AlgorithmImports import *
+from Selection.FundamentalUniverseSelectionModel import FundamentalUniverseSelectionModel
+
+### <summary>
+### Regression algorithm showing how to implement a custom universe selection model and asserting it's behavior
+### </summary>
+class CustomUniverseSelectionModelRegressionAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2014,3,24)
+        self.SetEndDate(2014,4,7)
+
+        self.UniverseSettings.Resolution = Resolution.Daily
+        self.SetUniverseSelection(CustomUniverseSelectionModel())
+
+    def OnData(self, data):
+        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+
+        Arguments:
+            data: Slice object keyed by symbol containing the stock data
+        '''
+        if not self.Portfolio.Invested:
+            for kvp in self.ActiveSecurities:
+                self.SetHoldings(kvp.Key, 0.1)
+
+class CustomUniverseSelectionModel(FundamentalUniverseSelectionModel):
+
+    def __init__(self, filterFineData = True, universeSettings = None):
+        super().__init__(filterFineData, universeSettings)
+        self._selected = False
+
+    def SelectCoarse(self, algorithm, coarse):
+        return [Symbol.Create('AAPL', SecurityType.Equity, Market.USA)]
+
+    def SelectFine(self, algorithm, fine):
+        if not self._selected:
+            self._selected = True
+            return [ x.Symbol for x in fine ]
+        return Universe.Unchanged
