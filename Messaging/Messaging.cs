826f2cbbf94ba2ddcdb4234d7212b9771d8654c5
Martin-Molinero <martin@quantconnect.com>
2022-07-11 16:24:26 -0300

    Minor refactor messaging handler (#6472)

--- Messaging/Messaging.cs
+++ Messaging/Messaging.cs
@@ -42,7 +42,8 @@ namespace QuantConnect.Messaging
         /// <summary>
         /// Initialize the messaging system
         /// </summary>
-        public void Initialize()
+        /// <param name="initializeParameters">The parameters required for initialization</param>
+        public void Initialize(MessagingHandlerInitializeParameters initializeParameters)
         {
             //
         }
