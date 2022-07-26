1b57a3e53aef7f7ac48a927b0d04ee5b53156e30
Jhonathan Abreu <jdabreu25@gmail.com>
2022-07-26 09:26:11 -0400

    Fixed DelistingFutureOptionRegressionAlgorithm to use extended market hours

--- Algorithm.CSharp/DelistingFutureOptionRegressionAlgorithm.cs
+++ Algorithm.CSharp/DelistingFutureOptionRegressionAlgorithm.cs
@@ -17,9 +17,9 @@ using System;
 using QuantConnect.Data;
 using QuantConnect.Interfaces;
 using QuantConnect.Securities;
+using QuantConnect.Securities.Future;
 using System.Collections.Generic;
 using System.Linq;
-using QuantConnect.Orders;
 
 namespace QuantConnect.Algorithm.CSharp
 {
@@ -31,6 +31,7 @@ namespace QuantConnect.Algorithm.CSharp
         protected virtual Resolution Resolution => Resolution.Minute;
         private bool _traded;
         private int _lastMonth;
+        private Future _dcFuture;
 
         public override void Initialize()
         {
@@ -38,15 +39,17 @@ namespace QuantConnect.Algorithm.CSharp
             SetEndDate(2013, 1, 1);
             SetCash(10000000);
 
-            var dc = AddFuture(Futures.Dairy.ClassIIIMilk, Resolution, Market.CME);
-            dc.SetFilter(1, 120);
+            _dcFuture = AddFuture(Futures.Dairy.ClassIIIMilk, Resolution, Market.CME, extendedMarketHours: true);
+            _dcFuture.SetFilter(1, 120);
 
-            AddFutureOption(dc.Symbol, universe => universe.Strikes(-2, 2));
+            AddFutureOption(_dcFuture.Symbol, universe => universe.Strikes(-2, 2));
             _lastMonth = -1;
 
             // This is required to prevent the algorithm from automatically delisting the underlying. Without this, future options will be subscribed
-            // with resolution default to Minute insted of this.Resolution. This could be replaced after GH issue #6491 is implemented.
+            // with resolution default to Minute insted of this.Resolution Same applies for ExtendedMarketHours.
+            // This could be replaced after GH issue #6491 is implemented.
             UniverseSettings.Resolution = Resolution;
+            UniverseSettings.ExtendedMarketHours = true;
         }
 
         public override void OnData(Slice data)
@@ -72,11 +75,21 @@ namespace QuantConnect.Algorithm.CSharp
                 return;
             }
 
+            var exchangeOpen = _dcFuture.Exchange.ExchangeOpen;
+
             foreach (var chain in data.OptionChains.Values)
             {
                 foreach (var contractsValue in chain.Contracts.Values)
                 {
-                    MarketOrder(contractsValue.Symbol, 1);
+                    if (exchangeOpen)
+                    {
+                        MarketOrder(contractsValue.Symbol, 1);
+                    }
+                    else
+                    {
+                        // High limit price so the order gets filled on the bar
+                        LimitOrder(contractsValue.Symbol, 1, contractsValue.AskPrice * 2m);
+                    }
                     _traded = true;
                 }
             }
@@ -107,7 +120,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>0
-        public virtual long DataPoints => 15228955;
+        public virtual long DataPoints => 14091830;
 
         /// <summary>
         /// Data Points count of the algorithm history
@@ -126,7 +139,7 @@ namespace QuantConnect.Algorithm.CSharp
             {"Drawdown", "0.100%"},
             {"Expectancy", "-0.678"},
             {"Net Profit", "-0.111%"},
-            {"Sharpe Ratio", "-0.967"},
+            {"Sharpe Ratio", "-0.939"},
             {"Probabilistic Sharpe Ratio", "0.000%"},
             {"Loss Rate", "80%"},
             {"Win Rate", "20%"},
@@ -137,14 +150,14 @@ namespace QuantConnect.Algorithm.CSharp
             {"Annual Variance", "0"},
             {"Information Ratio", "-1.075"},
             {"Tracking Error", "0.107"},
-            {"Treynor Ratio", "1.353"},
+            {"Treynor Ratio", "1.395"},
             {"Total Fees", "$14.80"},
             {"Estimated Strategy Capacity", "$860000000.00"},
             {"Lowest Capacity Asset", "DC V5E8P9SH0U0X"},
             {"Fitness Score", "0"},
             {"Kelly Criterion Estimate", "0"},
             {"Kelly Criterion Probability Value", "0"},
-            {"Sortino Ratio", "-0.128"},
+            {"Sortino Ratio", "-0.118"},
             {"Return Over Maximum Drawdown", "-0.995"},
             {"Portfolio Turnover", "0"},
             {"Total Insights Generated", "0"},
