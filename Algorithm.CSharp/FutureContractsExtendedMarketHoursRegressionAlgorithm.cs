f1ba61709d45e7e8eab11e46969aa093dc32fdd2
Jhonathan Abreu <jdabreu25@gmail.com>
2022-07-26 09:38:15 -0400

    Fixed FutureContractsExtendedMarketHoursRegressionAlgorithm to use extended market hours

--- Algorithm.CSharp/FutureContractsExtendedMarketHoursRegressionAlgorithm.cs
+++ Algorithm.CSharp/FutureContractsExtendedMarketHoursRegressionAlgorithm.cs
@@ -103,7 +103,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 497;
+        public long DataPoints => 525;
 
         /// <summary>
         /// Data Points count of the algorithm history
