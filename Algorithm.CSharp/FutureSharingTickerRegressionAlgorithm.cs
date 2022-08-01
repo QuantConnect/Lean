418a0f63802411fc1b3c08360ef85e786c6cb213
Jhonathan Abreu <jdabreu25@gmail.com>
2022-08-01 14:51:43 -0400

    Revert FutureSharingTickerRegressionAlgorithm

--- Algorithm.CSharp/FutureSharingTickerRegressionAlgorithm.cs
+++ Algorithm.CSharp/FutureSharingTickerRegressionAlgorithm.cs
@@ -37,11 +37,11 @@ namespace QuantConnect.Algorithm.CSharp
             SetStartDate(2013, 10, 08);
             SetEndDate(2013, 10, 10);
 
-            _goldFuture = AddFuture(Futures.Metals.Gold, market: Market.COMEX, extendedMarketHours: true);
+            _goldFuture = AddFuture(Futures.Metals.Gold, market: Market.COMEX);
             _goldFuture.SetFilter(0, 182);
 
             // this future does not exist just added as an example
-            var gold2 = AddFuture(Futures.Metals.Gold, market: Market.NYMEX, extendedMarketHours: true);
+            var gold2 = AddFuture(Futures.Metals.Gold, market: Market.NYMEX);
             gold2.SetFilter(0, 182);
         }
 
@@ -51,7 +51,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <param name="data">Slice object keyed by symbol containing the stock data</param>
         public override void OnData(Slice data)
         {
-            if (!Portfolio.Invested && _goldFuture.Exchange.ExchangeOpen)
+            if (!Portfolio.Invested)
             {
                 foreach (var chain in data.FutureChains)
                 {
@@ -83,7 +83,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 135885;
+        public long DataPoints => 44671;
 
         /// <summary>
         /// Data Points count of the algorithm history
@@ -98,31 +98,31 @@ namespace QuantConnect.Algorithm.CSharp
             {"Total Trades", "1"},
             {"Average Win", "0%"},
             {"Average Loss", "0%"},
-            {"Compounding Annual Return", "-98.097%"},
-            {"Drawdown", "5.400%"},
+            {"Compounding Annual Return", "-99.355%"},
+            {"Drawdown", "4.500%"},
             {"Expectancy", "0"},
-            {"Net Profit", "-3.204%"},
-            {"Sharpe Ratio", "-69.092"},
+            {"Net Profit", "-3.674%"},
+            {"Sharpe Ratio", "-15.427"},
             {"Probabilistic Sharpe Ratio", "0%"},
             {"Loss Rate", "0%"},
             {"Win Rate", "0%"},
             {"Profit-Loss Ratio", "0"},
-            {"Alpha", "-1.893"},
-            {"Beta", "0.057"},
-            {"Annual Standard Deviation", "0.014"},
-            {"Annual Variance", "0"},
-            {"Information Ratio", "-75.05"},
-            {"Tracking Error", "0.229"},
-            {"Treynor Ratio", "-16.769"},
+            {"Alpha", "3.273"},
+            {"Beta", "-0.263"},
+            {"Annual Standard Deviation", "0.064"},
+            {"Annual Variance", "0.004"},
+            {"Information Ratio", "-56.095"},
+            {"Tracking Error", "0.306"},
+            {"Treynor Ratio", "3.744"},
             {"Total Fees", "$1.85"},
-            {"Estimated Strategy Capacity", "$20000000.00"},
+            {"Estimated Strategy Capacity", "$19000000.00"},
             {"Lowest Capacity Asset", "GC VOFJUCDY9XNH"},
             {"Fitness Score", "0.007"},
             {"Kelly Criterion Estimate", "0"},
             {"Kelly Criterion Probability Value", "0"},
-            {"Sortino Ratio", "-8.629"},
-            {"Return Over Maximum Drawdown", "-27.358"},
-            {"Portfolio Turnover", "0.443"},
+            {"Sortino Ratio", "-10.317"},
+            {"Return Over Maximum Drawdown", "-53.182"},
+            {"Portfolio Turnover", "0.665"},
             {"Total Insights Generated", "0"},
             {"Total Insights Closed", "0"},
             {"Total Insights Analysis Completed", "0"},
@@ -136,7 +136,7 @@ namespace QuantConnect.Algorithm.CSharp
             {"Mean Population Magnitude", "0%"},
             {"Rolling Averaged Population Direction", "0%"},
             {"Rolling Averaged Population Magnitude", "0%"},
-            {"OrderListHash", "68322d096a39dbc611f73b83dc513b47"}
+            {"OrderListHash", "fea761e839af44276dd9c4bdca1ef7bb"}
         };
     }
 }
