a62aeecf2d500081ed8223686f0293ba666b0daf
Ryan Russell <git@ryanrussell.org>
2022-08-17 08:49:44 -0500

    docs: `algortihm` -> `algorithm`
    Signed-off-by: Ryan Russell <git@ryanrussell.org>

--- Engine/Results/BacktestingResultHandler.cs
+++ Engine/Results/BacktestingResultHandler.cs
@@ -468,7 +468,7 @@ namespace QuantConnect.Lean.Engine.Results
         }
 
         /// <summary>
-        /// Send list of security asset types the algortihm uses to browser.
+        /// Send list of security asset types the algorithm uses to browser.
         /// </summary>
         public virtual void SecurityType(List<SecurityType> types)
         {
