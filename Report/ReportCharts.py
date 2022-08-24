e83fdcdf14e20f285a9f4524e47bcb052a0aa4d5
Jhonathan Abreu <jdabreu25@gmail.com>
2022-08-24 14:45:27 -0400

    Use correct colors in report's exposure plot

--- Report/ReportCharts.py
+++ Report/ReportCharts.py
@@ -979,8 +979,10 @@ class ReportCharts:
         for k, v in list(color_map.items()):
             color_map[k + ' - Short'] = '#' + hex(int(v[1:], 16) ^ 0xffffff)[2:].zfill(6)
 
-        labels = long_securities + short_securities
-        live_labels = live_long_securities + live_short_securities
+        long_colors = [color_map[security] for security in long_securities]
+        long_live_colors = [color_map[security] for security in live_long_securities]
+        short_colors = [color_map[security + ' - Short'] for security in short_securities]
+        short_live_colors = [color_map[security + ' - Short'] for security in live_short_securities]
 
         ax = plt.gca()
 
@@ -1064,26 +1066,25 @@ class ReportCharts:
 
         # No need to check if live is empty or not, this will handle it, just needs to plot whichever has the longer time index first
         if max([len(x) for x in long_data_copy]) > max([len(x) for x in short_data_copy]):
-            ax.stackplot(time_copy[:max([len(x) for x in long_data_copy])], np.vstack(long_data_copy),
-                         color=[color_map[security] for security in long_securities], alpha = 0.75)
-            ax.stackplot(time_copy[:max([len(x) for x in short_data_copy])], np.vstack(short_data_copy),
-                         color=[color_map[security + ' - Short'] for security in short_securities], alpha=0.75)
+            ax.stackplot(time_copy[:max([len(x) for x in long_data_copy])], np.vstack(long_data_copy), colors=long_colors, alpha = 0.75)
+            ax.stackplot(time_copy[:max([len(x) for x in short_data_copy])], np.vstack(short_data_copy),color=short_colors, alpha=0.75)
         else:
-            ax.stackplot(time_copy[:max([len(x) for x in short_data_copy])], np.vstack(short_data_copy),
-                         color=[color_map[security + ' - Short'] for security in short_securities], alpha=0.75)
-            ax.stackplot(time_copy[:max([len(x) for x in long_data_copy])], np.vstack(long_data_copy),
-                         color=[color_map[security] for security in long_securities], alpha=0.75)
+            ax.stackplot(time_copy[:max([len(x) for x in short_data_copy])], np.vstack(short_data_copy), color=short_colors, alpha=0.75)
+            ax.stackplot(time_copy[:max([len(x) for x in long_data_copy])], np.vstack(long_data_copy), colors=long_colors, alpha=0.75)
 
         if max([len(x) for x in live_long_data_copy]) > max([len(x) for x in live_short_data_copy]):
             ax.stackplot(live_time_copy[:max([len(x) for x in live_long_data_copy])], np.vstack(live_long_data_copy),
-                         color=[color_map[security] for security in live_long_securities], alpha = 0.75)
+                         color=long_live_colors, alpha = 0.75)
             ax.stackplot(live_time_copy[:max([len(x) for x in live_short_data_copy])], np.vstack(live_short_data_copy),
-                         color=[color_map[security + ' - Short'] for security in live_short_securities], alpha = 0.75)
+                         color=short_live_colors, alpha = 0.75)
         else:
             ax.stackplot(live_time_copy[:max([len(x) for x in live_short_data_copy])], np.vstack(live_short_data_copy),
-                         color=[color_map[security + ' - Short'] for security in live_short_securities], alpha=0.75)
+                         color=short_live_colors, alpha=0.75)
             ax.stackplot(live_time_copy[:max([len(x) for x in live_long_data_copy])], np.vstack(live_long_data_copy),
-                         color=[color_map[security] for security in live_long_securities], alpha=0.75)
+                         color=long_live_colors, alpha=0.75)
+
+        labels = long_securities
+        live_labels = live_long_securities
 
         for security in short_securities:
             if not all([all([abs(y) == 0.0 for y in x]) for x in short_data]):
@@ -1093,8 +1094,8 @@ class ReportCharts:
             if not all([all([abs(y) == 0.0 for y in x]) for x in live_short_data]):
                 live_labels.append(security + ' - Short')
 
-        labels = list(set(labels))
-        live_labels = list(set(live_labels))
+        labels = list(dict.fromkeys(labels))
+        live_labels = list(dict.fromkeys(live_labels))
         rectangles = [plt.Rectangle((0, 0), 1, 1, fc=color_map[lab]) for lab in labels]
         live_rectangles = [plt.Rectangle((0, 0), 1, 1, fc=color_map[lab]) for lab in live_labels]
         ax.legend(rectangles + live_rectangles, labels + [f'{lab} - Live' for lab in live_labels], handlelength=0.8,
