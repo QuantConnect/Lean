d34ef95cd73c9125e2f0cc0ca92edf3e38c5d2ef
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2022-10-19 20:14:57 +0100

    Simplify Jupyter Notebook Deployment

--- .vscode/launch_research.sh
+++ .vscode/launch_research.sh
@@ -11,31 +11,5 @@ BUILD_DIR=$(absolute_path "${BUILD_DIR}")
 #Add our build directory to python path for python kernel
 export PYTHONPATH="${PYTHONPATH}:${BUILD_DIR}"
 
-# Get data directory from args position 2, or use default
-DEFAULT_DATA_DIR=../Data
-DATA_DIR=${2:-$DEFAULT_DATA_DIR}
-DATA_DIR=$(absolute_path "${DATA_DIR}")
-
-#Get Notebook location from args position 3, or use default
-DEFAULT_NOTEBOOK_DIR=$BUILD_DIR/Notebooks
-NOTEBOOK_DIR=${3:-$DEFAULT_NOTEBOOK_DIR}
-NOTEBOOK_DIR=$(absolute_path "${NOTEBOOK_DIR}")
-
-#If the folder doesn't exist create it.
-if [ ! -d "$NOTEBOOK_DIR" ]; then
-    echo "Creating Notebook Directory at $NOTEBOOK_DIR"
-    mkdir $NOTEBOOK_DIR
-fi
-
-# Copy config.json to notebook directory
-cp -n $BUILD_DIR/config.json $NOTEBOOK_DIR/config.json
-
-# Copy over any notebooks from build directory to notebook directory
-RESEARCH_FILES=$BUILD_DIR/*.ipynb
-if [ -d "$BUILD_DIR" ]; then
-    echo "Copying research files from $RESEARCH_FILES to $NOTEBOOK_DIR; will not overwrite existing files"
-    cp -n $RESEARCH_FILES $NOTEBOOK_DIR
-fi
-
 # Launch jupyter-lab
-jupyter-lab --allow-root --no-browser --notebook-dir=$NOTEBOOK_DIR --LabApp.token=''
+jupyter-lab --allow-root --no-browser --notebook-dir=$BUILD_DIR --LabApp.token=''
