845d8c14a1319c90aecb5500dac1b19e5f0ab711
Jhonathan Abreu <jdabreu25@gmail.com>
2022-11-15 16:14:16 -0400

    Use selector on indicator warm up

---
+++ Tests/Algorithm/AlgorithmIndicatorsTests.cs
@@ -0,0 +1,71 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+
+using Moq;
+using NUnit.Framework;
+
+using QuantConnect.Algorithm;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Indicators;
+using QuantConnect.Interfaces;
+using QuantConnect.Lean.Engine.DataFeeds;
+using QuantConnect.Lean.Engine.HistoricalData;
+using QuantConnect.Tests.Engine.DataFeeds;
+
+namespace QuantConnect.Tests.Algorithm
+{
+    [TestFixture]
+    public class AlgorithmIndicatorsTests
+    {
+        private QCAlgorithm _algorithm;
+
+        [SetUp]
+        public void Setup()
+        {
+            _algorithm = new QCAlgorithm();
+            _algorithm.SubscriptionManager.SetDataManager(new DataManagerStub(_algorithm));
+
+            var historyProvider = new SubscriptionDataReaderHistoryProvider();
+            using var cacheProvider = new ZipDataCacheProvider(TestGlobals.DataProvider);
+            historyProvider.Initialize(new HistoryProviderInitializeParameters(null, null,
+                TestGlobals.DataProvider, cacheProvider, TestGlobals.MapFileProvider, TestGlobals.FactorFileProvider,
+                null, true, new DataPermissionManager()));
+            _algorithm.SetHistoryProvider(historyProvider);
+
+            _algorithm.SetDateTime(new DateTime(2013, 10, 11, 15, 0, 0));
+            _algorithm.AddEquity("SPY");
+            _algorithm.EnableAutomaticIndicatorWarmUp = true;
+        }
+
+        [Test]
+        public void IndicatorsPassSelectorToWarmUp()
+        {
+            var mockSelector = new Mock<Func<IBaseData, TradeBar>>();
+            mockSelector.Setup(_ => _(It.IsAny<IBaseData>())).Returns<TradeBar>(_ => (TradeBar)_);
+
+            var indicator = _algorithm.ABANDS(Symbols.SPY, 20, selector: mockSelector.Object);
+
+            Assert.IsTrue(indicator.IsReady);
+            mockSelector.Verify(_ => _(It.IsAny<IBaseData>()), Times.Exactly(indicator.WarmUpPeriod));
+        }
+    }
+}
