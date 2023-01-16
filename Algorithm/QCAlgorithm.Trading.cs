19e307363066c994be8e5e4e464786fa366aaf7b
Jhonathan Abreu <jdabreu25@gmail.com>
2023-01-16 12:57:39 -0400

    Combo order legs ratio validation

--- Algorithm/QCAlgorithm.Trading.cs
+++ Algorithm/QCAlgorithm.Trading.cs
@@ -748,8 +748,27 @@ namespace QuantConnect.Algorithm
             return SubmitComboOrder(legs, strategyQuantity, 0, asynchronous, tag, orderProperties);
         }
 
+        private void CheckComboOrderSizing(List<Leg> legs, decimal quantity)
+        {
+            // check that the order quantity is a multiple of the GCD of the leg quantities
+            var legQuantities = legs.Select(leg => Math.Abs(leg.Quantity)).ToArray();
+            var gcd = GreatestCommonDivisor(legQuantities);
+
+            if (gcd != 1)
+            {
+                throw new ArgumentException(
+                    "The global combo quantity should be used to increase or reduce the size of the order, " +
+                    "while the leg quantities should be used to specify the ratio of the order. " +
+                    "The combo order legs ratios should be reduced " +
+                    $"from {quantity}x({string.Join(", ", legs.Select(leg => $"{leg.Quantity} {leg.Symbol}"))}) " +
+                    $"to {quantity * gcd}x({string.Join(", ", legs.Select(leg => $"{leg.Quantity / gcd} {leg.Symbol}"))}).");
+            }
+        }
+
         private List<OrderTicket> SubmitComboOrder(List<Leg> legs, decimal quantity, decimal limitPrice, bool asynchronous, string tag, IOrderProperties orderProperties)
         {
+            CheckComboOrderSizing(legs, quantity);
+
             var orderType = OrderType.ComboMarket;
             if(limitPrice != 0)
             {
@@ -1355,6 +1374,28 @@ namespace QuantConnect.Algorithm
             decimal stopPrice = 0m, decimal limitPrice = 0m,  decimal triggerPrice = 0m, GroupOrderManager groupOrderManager = null)
         {
             return new SubmitOrderRequest(orderType, security.Type, security.Symbol, quantity, stopPrice, limitPrice, triggerPrice, UtcTime, tag, properties, groupOrderManager);
+        }
+
+        private static int GreatestCommonDivisor(int a, int b)
+        {
+            int remainder;
+            while (b != 0)
+            {
+                remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+            return a;
+        }
+
+        private static int GreatestCommonDivisor(int[] numbers)
+        {
+            var result = numbers[0];
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                result = GreatestCommonDivisor(result, numbers[i]);
+            }
+            return result;
         }
     }
 }
