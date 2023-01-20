b55202ed21fe26ce9338a02ef212f0592d846bb0
Jhonathan Abreu <jdabreu25@gmail.com>
2023-01-20 11:24:56 -0400

    Checking both min and max forex order size limits in IB brokerage model
    Also updated the limits table

--- Common/Brokerages/InteractiveBrokersBrokerageModel.cs
+++ Common/Brokerages/InteractiveBrokersBrokerageModel.cs
@@ -196,72 +196,77 @@ namespace QuantConnect.Brokerages
         /// </summary>
         private bool IsForexWithinOrderSizeLimits(string currencyPair, decimal quantity, out BrokerageMessageEvent message)
         {
-            /* https://www.interactivebrokers.com/en/?f=%2Fen%2Ftrading%2FforexOrderSize.php
+            /* https://www.interactivebrokers.com/en/trading/forexOrderSize.php
             Currency    Currency Description	    Minimum Order Size	Maximum Order Size
-            USD	        US Dollar	        	    25,000              7,000,000
-            AUD	        Australian Dollar	        25,000              6,000,000
-            CAD	        Canadian Dollar	            25,000              6,000,000
+            USD	        US Dollar	                25,000	            7,000,000
+            AUD	        Australian Dollar	        25,000	            6,000,000
+            CAD	        Canadian Dollar	            25,000	            6,000,000
             CHF	        Swiss Franc	                25,000	            6,000,000
-            CNH	        China Renminbi (offshore)	160,000	            40,000,000
-            CZK	        Czech Koruna	            USD 25,000(1)	    USD 7,000,000(1)
+            CNH	        China Renminbi (offshore)	150,000	            40,000,000
+            CZK	        Czech Koruna	            USD 25,000(1)       USD 7,000,000(1)
             DKK	        Danish Krone	            150,000	            35,000,000
-            EUR	        Euro	                    20,000	            5,000,000
-            GBP	        British Pound Sterling	    17,000	            4,000,000
+            EUR	        Euro	                    20,000	            6,000,000
+            GBP	        British Pound Sterling	    20,000	            5,000,000
             HKD	        Hong Kong Dollar	        200,000	            50,000,000
-            HUF	        Hungarian Forint	        USD 25,000(1)	    USD 7,000,000(1)
-            ILS	        Israeli Shekel	            USD 25,000(1)	    USD 7,000,000(1)
-            KRW	        Korean Won	                50,000,000	        750,000,000
+            HUF	        Hungarian Forint	        USD 25,000(1)   	USD 7,000,000(1)
+            ILS	        Israeli Shekel	            USD 25,000(1)   	USD 7,000,000(1)
+            KRW	        Korean Won	                0	                200,000,000
             JPY	        Japanese Yen	            2,500,000	        550,000,000
             MXN	        Mexican Peso	            300,000	            70,000,000
             NOK	        Norwegian Krone	            150,000	            35,000,000
             NZD	        New Zealand Dollar	        35,000	            8,000,000
+            PLN	        Polish Zloty	            USD 25,000(1)       USD 7,000,000(1)
             RUB	        Russian Ruble	            750,000	            30,000,000
             SEK	        Swedish Krona	            175,000	            40,000,000
             SGD	        Singapore Dollar	        35,000	            8,000,000
+            ZAR	        South African Rand	        350,000	            100,000,000
              */
 
             message = null;
 
             // switch on the currency being bought
-            string baseCurrency, quoteCurrency;
-            Forex.DecomposeCurrencyPair(currencyPair, out baseCurrency, out quoteCurrency);
+            Forex.DecomposeCurrencyPair(currencyPair, out var baseCurrency, out _);
 
-            decimal max;
-            ForexCurrencyLimits.TryGetValue(baseCurrency, out max);
+            ForexCurrencyLimits.TryGetValue(baseCurrency, out var limits);
+            var min = limits?.Item1 ?? 0m;
+            var max = limits?.Item2 ?? 0m;
 
-            var orderIsWithinForexSizeLimits = quantity < max;
+            var orderIsWithinForexSizeLimits = quantity > min && quantity < max;
             if (!orderIsWithinForexSizeLimits)
             {
                 message = new BrokerageMessageEvent(BrokerageMessageType.Warning, "OrderSizeLimit",
-                    Invariant($"The maximum allowable order size is {max}{baseCurrency}.")
+                    Invariant($"The minimum and maximum limits for the allowable order size are ({min}, {max}){baseCurrency}.")
                 );
             }
             return orderIsWithinForexSizeLimits;
         }
 
-
-        private static readonly IReadOnlyDictionary<string, decimal> ForexCurrencyLimits = new Dictionary<string, decimal>()
-        {
-            {"USD", 7000000m},
-            {"AUD", 6000000m},
-            {"CAD", 6000000m},
-            {"CHF", 6000000m},
-            {"CNH", 40000000m},
-            {"CZK", 0m}, // need market price in USD or EUR -- do later when we support
-            {"DKK", 35000000m},
-            {"EUR", 5000000m},
-            {"GBP", 4000000m},
-            {"HKD", 50000000m},
-            {"HUF", 0m}, // need market price in USD or EUR -- do later when we support
-            {"ILS", 0m}, // need market price in USD or EUR -- do later when we support
-            {"KRW", 750000000m},
-            {"JPY", 550000000m},
-            {"MXN", 70000000m},
-            {"NOK", 35000000m},
-            {"NZD", 8000000m},
-            {"RUB", 30000000m},
-            {"SEK", 40000000m},
-            {"SGD", 8000000m}
-        };
+        // currency -> (min, max)
+        private static readonly IReadOnlyDictionary<string, Tuple<decimal, decimal>> ForexCurrencyLimits =
+            new Dictionary<string, Tuple<decimal, decimal>>()
+            {
+                {"USD", Tuple.Create(25000m, 7000000m)},
+                {"AUD", Tuple.Create(25000m, 6000000m)},
+                {"CAD", Tuple.Create(25000m, 6000000m)},
+                {"CHF", Tuple.Create(25000m, 6000000m)},
+                {"CNH", Tuple.Create(150000m, 40000000m)},
+                {"CZK", Tuple.Create(0m, 0m)}, // need market price in USD or EUR -- do later when we support
+                {"DKK", Tuple.Create(150000m, 35000000m)},
+                {"EUR", Tuple.Create(20000m, 6000000m)},
+                {"GBP", Tuple.Create(20000m, 5000000m)},
+                {"HKD", Tuple.Create(200000m, 50000000m)},
+                {"HUF", Tuple.Create(0m, 0m)}, // need market price in USD or EUR -- do later when we support
+                {"ILS", Tuple.Create(0m, 0m)}, // need market price in USD or EUR -- do later when we support
+                {"KRW", Tuple.Create(0m, 200000000m)},
+                {"JPY", Tuple.Create(2500000m, 550000000m)},
+                {"MXN", Tuple.Create(300000m, 70000000m)},
+                {"NOK", Tuple.Create(150000m, 35000000m)},
+                {"NZD", Tuple.Create(35000m, 8000000m)},
+                {"PLN", Tuple.Create(0m, 0m)}, // need market price in USD or EUR -- do later when we support
+                {"RUB", Tuple.Create(750000m, 30000000m)},
+                {"SEK", Tuple.Create(175000m, 40000000m)},
+                {"SGD", Tuple.Create(35000m, 8000000m)},
+                {"ZAR", Tuple.Create(350000m, 100000000m)}
+            };
     }
 }
