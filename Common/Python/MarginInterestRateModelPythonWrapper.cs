a7a4af6b709d30ece2429459cf2f0a74fb58cc34
Jhonathan Abreu <jdabreu25@gmail.com>
2023-02-06 11:57:55 -0400

    Add Security.SetMarginInterestRateModel methods and its python wrapper (#6879)

---
+++ Common/Python/MarginInterestRateModelPythonWrapper.cs
@@ -0,0 +1,49 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using Python.Runtime;
+using QuantConnect.Securities;
+
+namespace QuantConnect.Python
+{
+    /// <summary>
+    /// Wraps a <see cref="PyObject"/> object that represents a security's margin interest rate model
+    /// </summary>
+    public class MarginInterestRateModelPythonWrapper : IMarginInterestRateModel
+    {
+        private readonly dynamic _model;
+
+        /// <summary>
+        /// Constructor for initializing the <see cref="MarginInterestRateModelPythonWrapper"/> class with wrapped <see cref="PyObject"/> object
+        /// </summary>
+        /// <param name="model">Represents a security's model of buying power</param>
+        public MarginInterestRateModelPythonWrapper(PyObject model)
+        {
+            _model = model.ValidateImplementationOf<IMarginInterestRateModel>();
+        }
+
+        /// <summary>
+        /// Apply margin interest rates to the portfolio
+        /// </summary>
+        /// <param name="parameters">The parameters to use</param>
+        public void ApplyMarginInterestRate(MarginInterestRateParameters parameters)
+        {
+            using (Py.GIL())
+            {
+                _model.ApplyMarginInterestRate(parameters);
+            }
+        }
+    }
+}
