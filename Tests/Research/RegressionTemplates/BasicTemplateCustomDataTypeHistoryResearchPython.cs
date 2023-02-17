441f5d9358dfb83fba6e047d1f4c7d9a16462971
Jhonathan Abreu <jdabreu25@gmail.com>
2023-02-17 17:37:13 -0400

    Minor changes

--- Tests/Research/RegressionTemplates/BasicTemplateCustomDataTypeHistoryResearchPython.cs
+++ Tests/Research/RegressionTemplates/BasicTemplateCustomDataTypeHistoryResearchPython.cs
@@ -28,28 +28,28 @@ namespace QuantConnect.Tests.Research.RegressionTemplates
         /// <remarks>Requires to be implemented last in the file <see cref="ResearchRegressionTests.UpdateResearchRegressionOutputInSourceFile"/>
         /// get should start from next line</remarks>
         public string ExpectedOutput =>
-            "{ \"cells\": [  {   \"cell_type\": \"markdown\",   \"id\": \"cffa5698\",   \"metadata\": {    \"papermill\": {     \"duration\": 0.016987,     \"end_t" +
-            "ime\": \"2023-02-17T19:22:20.995895\",     \"exception\": false,     \"start_time\": \"2023-02-17T19:22:20.978908\",     \"status\": \"completed\"    " +
+            "{ \"cells\": [  {   \"cell_type\": \"markdown\",   \"id\": \"d0ea3064\",   \"metadata\": {    \"papermill\": {     \"duration\": 0.003996,     \"end_t" +
+            "ime\": \"2023-02-17T21:33:10.586532\",     \"exception\": false,     \"start_time\": \"2023-02-17T21:33:10.582536\",     \"status\": \"completed\"    " +
             "},    \"tags\": []   },   \"source\": [    \"![QuantConnect Logo](https://cdn.quantconnect.com/web/i/qc_notebook_logo_rev0.png)\",    \"## Welcome to " +
             "The QuantConnect Research Page\",    \"#### Refer to this page for documentation https://www.quantconnect.com/docs/research/overview#\",    \"#### Con" +
             "tribute to this template file https://github.com/QuantConnect/Lean/blob/master/Research/BasicQuantBookTemplate.ipynb\"   ]  },  {   \"cell_type\": \"m" +
-            "arkdown\",   \"id\": \"70f078aa\",   \"metadata\": {    \"papermill\": {     \"duration\": 0.025497,     \"end_time\": \"2023-02-17T19:22:21.033394\"," +
-            "     \"exception\": false,     \"start_time\": \"2023-02-17T19:22:21.007897\",     \"status\": \"completed\"    },    \"tags\": []   },   \"source\": " +
+            "arkdown\",   \"id\": \"e3e3d0c7\",   \"metadata\": {    \"papermill\": {     \"duration\": 0.003965,     \"end_time\": \"2023-02-17T21:33:10.593525\"," +
+            "     \"exception\": false,     \"start_time\": \"2023-02-17T21:33:10.589560\",     \"status\": \"completed\"    },    \"tags\": []   },   \"source\": " +
             "[    \"## QuantBook Basics\",    \"\",    \"### Start QuantBook\",    \"- Add the references and imports\",    \"- Create a QuantBook instance\"   ]  " +
-            "},  {   \"cell_type\": \"code\",   \"execution_count\": 1,   \"id\": \"0ca0fe27\",   \"metadata\": {    \"execution\": {     \"iopub.execute_input\": " +
-            "\"2023-02-17T19:22:21.066897Z\",     \"iopub.status.busy\": \"2023-02-17T19:22:21.065898Z\",     \"iopub.status.idle\": \"2023-02-17T19:22:21.086395Z\"" +
-            ",     \"shell.execute_reply\": \"2023-02-17T19:22:21.084896Z\"    },    \"papermill\": {     \"duration\": 0.046997,     \"end_time\": \"2023-02-17T19" +
-            ":22:21.091894\",     \"exception\": false,     \"start_time\": \"2023-02-17T19:22:21.044897\",     \"status\": \"completed\"    },    \"tags\": []   }" +
+            "},  {   \"cell_type\": \"code\",   \"execution_count\": 1,   \"id\": \"d0e8fcfc\",   \"metadata\": {    \"execution\": {     \"iopub.execute_input\": " +
+            "\"2023-02-17T21:33:10.602549Z\",     \"iopub.status.busy\": \"2023-02-17T21:33:10.601530Z\",     \"iopub.status.idle\": \"2023-02-17T21:33:10.616522Z\"" +
+            ",     \"shell.execute_reply\": \"2023-02-17T21:33:10.614535Z\"    },    \"papermill\": {     \"duration\": 0.021984,     \"end_time\": \"2023-02-17T21" +
+            ":33:10.618527\",     \"exception\": false,     \"start_time\": \"2023-02-17T21:33:10.596543\",     \"status\": \"completed\"    },    \"tags\": []   }" +
             ",   \"outputs\": [],   \"source\": [    \"import warnings\",    \"warnings.filterwarnings(\\\"ignore\\\")\"   ]  },  {   \"cell_type\": \"code\",   \"" +
-            "execution_count\": 2,   \"id\": \"82ab27ab\",   \"metadata\": {    \"execution\": {     \"iopub.execute_input\": \"2023-02-17T19:22:21.125400Z\",     " +
-            "\"iopub.status.busy\": \"2023-02-17T19:22:21.123900Z\",     \"iopub.status.idle\": \"2023-02-17T19:22:25.488798Z\",     \"shell.execute_reply\": \"202" +
-            "3-02-17T19:22:25.487818Z\"    },    \"papermill\": {     \"duration\": 4.388906,     \"end_time\": \"2023-02-17T19:22:25.491302\",     \"exception\": " +
-            "false,     \"start_time\": \"2023-02-17T19:22:21.102396\",     \"status\": \"completed\"    },    \"tags\": []   },   \"outputs\": [],   \"source\": [" +
+            "execution_count\": 2,   \"id\": \"a845a7ca\",   \"metadata\": {    \"execution\": {     \"iopub.execute_input\": \"2023-02-17T21:33:10.627549Z\",     " +
+            "\"iopub.status.busy\": \"2023-02-17T21:33:10.627549Z\",     \"iopub.status.idle\": \"2023-02-17T21:33:15.142098Z\",     \"shell.execute_reply\": \"202" +
+            "3-02-17T21:33:15.140599Z\"    },    \"papermill\": {     \"duration\": 4.526574,     \"end_time\": \"2023-02-17T21:33:15.149104\",     \"exception\": " +
+            "false,     \"start_time\": \"2023-02-17T21:33:10.622530\",     \"status\": \"completed\"    },    \"tags\": []   },   \"outputs\": [],   \"source\": [" +
             "    \"# Load in our startup script, required to set runtime for PythonNet\",    \"%run ./start.py\"   ]  },  {   \"cell_type\": \"code\",   \"executio" +
-            "n_count\": 3,   \"id\": \"297d8757\",   \"metadata\": {    \"execution\": {     \"iopub.execute_input\": \"2023-02-17T19:22:25.521298Z\",     \"iopub." +
-            "status.busy\": \"2023-02-17T19:22:25.520297Z\",     \"iopub.status.idle\": \"2023-02-17T19:22:25.596300Z\",     \"shell.execute_reply\": \"2023-02-17T" +
-            "19:22:25.594798Z\"    },    \"papermill\": {     \"duration\": 0.097502,     \"end_time\": \"2023-02-17T19:22:25.598301\",     \"exception\": false,  " +
-            "   \"start_time\": \"2023-02-17T19:22:25.500799\",     \"status\": \"completed\"    },    \"tags\": []   },   \"outputs\": [],   \"source\": [    \"cl" +
+            "n_count\": 3,   \"id\": \"9b0cd81c\",   \"metadata\": {    \"execution\": {     \"iopub.execute_input\": \"2023-02-17T21:33:15.162102Z\",     \"iopub." +
+            "status.busy\": \"2023-02-17T21:33:15.160599Z\",     \"iopub.status.idle\": \"2023-02-17T21:33:15.266621Z\",     \"shell.execute_reply\": \"2023-02-17T" +
+            "21:33:15.263102Z\"    },    \"papermill\": {     \"duration\": 0.117533,     \"end_time\": \"2023-02-17T21:33:15.270138\",     \"exception\": false,  " +
+            "   \"start_time\": \"2023-02-17T21:33:15.152605\",     \"status\": \"completed\"    },    \"tags\": []   },   \"outputs\": [],   \"source\": [    \"cl" +
             "ass CustomDataType(PythonData):\",    \"\",    \"    def GetSource(self, config: SubscriptionDataConfig, date: datetime, isLive: bool) -> Subscription" +
             "DataSource:\",    \"        source = \\\"https://www.dl.dropboxusercontent.com/s/d83xvd7mm9fzpk0/path_to_my_csv_data.csv?dl=0\\\"\",    \"        retu" +
             "rn SubscriptionDataSource(source, SubscriptionTransportMedium.RemoteFile)\",    \"\",    \"    def Reader(self, config: SubscriptionDataConfig, line: " +
@@ -61,32 +61,21 @@ namespace QuantConnect.Tests.Research.RegressionTemplates
             "the correct data is fetched\",    \"            obj_data[\\\"some_property\\\"] = \\\"some property value\\\"\",    \"        except ValueError:\",   " +
             " \"            return None\",    \"\",    \"        return obj_data\",    \"\",    \"    def __str__ (self):\",    \"        return f\\\"Time: {self.T" +
             "ime}, Value: {self.Value}, SomeProperty: {self['some_property']}, Open: {self['open']}, High: {self['high']}, Low: {self['low']}, Close: {self['close'" +
-            "]}\\\"\"   ]  },  {   \"cell_type\": \"code\",   \"execution_count\": 4,   \"id\": \"c2ca9e65\",   \"metadata\": {    \"execution\": {     \"iopub.exe" +
-            "cute_input\": \"2023-02-17T19:22:25.631624Z\",     \"iopub.status.busy\": \"2023-02-17T19:22:25.631125Z\",     \"iopub.status.idle\": \"2023-02-17T19:" +
-            "22:27.883290Z\",     \"shell.execute_reply\": \"2023-02-17T19:22:27.880788Z\"    },    \"papermill\": {     \"duration\": 2.281479,     \"end_time\": " +
-            "\"2023-02-17T19:22:27.887289\",     \"exception\": false,     \"start_time\": \"2023-02-17T19:22:25.605810\",     \"status\": \"completed\"    },    \"" +
-            "tags\": []   },   \"outputs\": [    {     \"name\": \"stdout\",     \"output_type\": \"stream\",     \"text\": [      \"23\",      \"CUSTOMDATATYPE.Cu" +
-            "stomDataType: ¤5,813.60\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,822.25\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,655.90\",      \"CUSTOMDATATYPE" +
-            ".CustomDataType: ¤5,667.65\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,590.25\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,609.10\",      \"CUSTOMDATAT" +
-            "YPE.CustomDataType: ¤5,588.70\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,682.35\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,842.20\",      \"CUSTOMDA" +
-            "TATYPE.CustomDataType: ¤5,898.85\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,857.55\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,770.90\",      \"CUSTO" +
-            "MDATATYPE.CustomDataType: ¤5,836.95\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,867.90\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,811.55\",      \"CU" +
-            "STOMDATATYPE.CustomDataType: ¤5,859.00\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,816.70\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,935.10\",      \"" +
-            "CUSTOMDATATYPE.CustomDataType: ¤6,009.00\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,842.20\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,898.85\",     " +
-            " \"CUSTOMDATATYPE.CustomDataType: ¤5,857.55\",      \"CUSTOMDATATYPE.CustomDataType: ¤5,770.90\"     ]    }   ],   \"source\": [    \"# Create an inst" +
-            "ance\",    \"qb = QuantBook()\",    \"\",    \"symbol = qb.AddData(CustomDataType, \\\"CustomDataType\\\", Resolution.Hour).Symbol\",    \"\",    \"st" +
-            "artDate = datetime(2017, 8, 20)\",    \"endDate = startDate + timedelta(hours=48)\",    \"history = list(qb.History[CustomDataType](symbol, startDate," +
-            " endDate, Resolution.Hour))\",    \"\",    \"print(len(history))\",    \"\",    \"for data in history:\",    \"    print(str(data))\"   ]  },  {   \"c" +
-            "ell_type\": \"code\",   \"execution_count\": null,   \"id\": \"efc5a8c1\",   \"metadata\": {    \"papermill\": {     \"duration\": 0.021002,     \"end" +
-            "_time\": \"2023-02-17T19:22:27.942291\",     \"exception\": false,     \"start_time\": \"2023-02-17T19:22:27.921289\",     \"status\": \"completed\"  " +
-            "  },    \"tags\": []   },   \"outputs\": [],   \"source\": []  } ], \"metadata\": {  \"kernelspec\": {   \"display_name\": \"Python 3 (ipykernel)\",  " +
-            " \"language\": \"python\",   \"name\": \"python3\"  },  \"language_info\": {   \"codemirror_mode\": {    \"name\": \"ipython\",    \"version\": 3   }," +
-            "   \"file_extension\": \".py\",   \"mimetype\": \"text/x-python\",   \"name\": \"python\",   \"nbconvert_exporter\": \"python\",   \"pygments_lexer\":" +
-            " \"ipython3\",   \"version\": \"3.8.10\"  },  \"papermill\": {   \"default_parameters\": {},   \"duration\": 14.999576,   \"end_time\": \"2023-02-17T1" +
-            "9:22:30.728687\",   \"environment_variables\": {},   \"exception\": null,   \"input_path\": \"C:\\\\Users\\\\jhona\\\\QuantConnect\\\\Lean\\\\Tests\\\\b" +
-            "in\\\\Debug\\\\Research\\\\RegressionTemplates\\\\BasicTemplateCustomDataTypeHistoryResearchPython.ipynb\",   \"output_path\": \"C:\\\\Users\\\\jhona\\\\Q" +
-            "uantConnect\\\\Lean\\\\Tests\\\\bin\\\\Debug\\\\Research\\\\RegressionTemplates\\\\BasicTemplateCustomDataTypeHistoryResearchPython-output.ipynb\",   " +
-            "\"parameters\": {},   \"start_time\": \"2023-02-17T19:22:15.729111\",   \"version\": \"2.4.0\"  },  \"vscode\": {   \"interpreter\": {    \"hash\": \"" +
-            "9650cb4e16cdd4a8e8e2d128bf38d875813998db22a3c986335f89e0cb4d7bb2\"   }  } }, \"nbformat\": 4, \"nbformat_minor\": 5}";
+            "]}\\\"\"   ]  },  {   \"cell_type\": \"code\",   \"execution_count\": 4,   \"id\": \"e21f2b04\",   \"metadata\": {    \"execution\": {     \"iopub.exe" +
+            "cute_input\": \"2023-02-17T21:33:15.282600Z\",     \"iopub.status.busy\": \"2023-02-17T21:33:15.281102Z\",     \"iopub.status.idle\": \"2023-02-17T21:" +
+            "33:17.198103Z\",     \"shell.execute_reply\": \"2023-02-17T21:33:17.196601Z\"    },    \"papermill\": {     \"duration\": 1.926991,     \"end_time\": " +
+            "\"2023-02-17T21:33:17.200100\",     \"exception\": false,     \"start_time\": \"2023-02-17T21:33:15.273109\",     \"status\": \"completed\"    },    \"" +
+            "tags\": []   },   \"outputs\": [],   \"source\": [    \"# Create an instance\",    \"qb = QuantBook()\",    \"symbol = qb.AddData(CustomDataType, \\\"" +
+            "CustomDataType\\\", Resolution.Hour).Symbol\",    \"\",    \"startDate = datetime(2017, 8, 20)\",    \"endDate = startDate + timedelta(hours=48)\",   " +
+            " \"history = list(qb.History[CustomDataType](symbol, startDate, endDate, Resolution.Hour))\",    \"\",    \"if len(history) == 0:\",    \"    raise Ex" +
+            "ception(\\\"No history data returned\\\")\"   ]  } ], \"metadata\": {  \"kernelspec\": {   \"display_name\": \"Python 3 (ipykernel)\",   \"language\":" +
+            " \"python\",   \"name\": \"python3\"  },  \"language_info\": {   \"codemirror_mode\": {    \"name\": \"ipython\",    \"version\": 3   },   \"file_exte" +
+            "nsion\": \".py\",   \"mimetype\": \"text/x-python\",   \"name\": \"python\",   \"nbconvert_exporter\": \"python\",   \"pygments_lexer\": \"ipython3\"," +
+            "   \"version\": \"3.8.10\"  },  \"papermill\": {   \"default_parameters\": {},   \"duration\": 13.405899,   \"end_time\": \"2023-02-17T21:33:20.058754" +
+            "\",   \"environment_variables\": {},   \"exception\": null,   \"input_path\": \"C:\\\\Users\\\\jhona\\\\QuantConnect\\\\Lean\\\\Tests\\\\bin\\\\Debug\\\\R" +
+            "esearch\\\\RegressionTemplates\\\\BasicTemplateCustomDataTypeHistoryResearchPython.ipynb\",   \"output_path\": \"C:\\\\Users\\\\jhona\\\\QuantConnect\\\\L" +
+            "ean\\\\Tests\\\\bin\\\\Debug\\\\Research\\\\RegressionTemplates\\\\BasicTemplateCustomDataTypeHistoryResearchPython-output.ipynb\",   \"parameters\": " +
+            "{},   \"start_time\": \"2023-02-17T21:33:06.652855\",   \"version\": \"2.4.0\"  },  \"vscode\": {   \"interpreter\": {    \"hash\": \"9650cb4e16cdd4a8" +
+            "e8e2d128bf38d875813998db22a3c986335f89e0cb4d7bb2\"   }  } }, \"nbformat\": 4, \"nbformat_minor\": 5}";
     }
 }
