0af3981a44be6921433410bd98a6c790818c733b
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-02-22 23:56:55 +0000

    Updates the PearsonCorrelationPairsTradingAlphaModelFrameworkAlgorithm
    Change the `PearsonCorrelationPairsTradingAlphaModelFrameworkAlgorithm` logic to show that it doesn't remove the consolidators used in the Alpha Model's indicators.

--- Algorithm.Python/PearsonCorrelationPairsTradingAlphaModelFrameworkAlgorithm.py
+++ Algorithm.Python/PearsonCorrelationPairsTradingAlphaModelFrameworkAlgorithm.py
@@ -28,13 +28,22 @@ class PearsonCorrelationPairsTradingAlphaModelFrameworkAlgorithm(QCAlgorithm):
         self.SetStartDate(2013,10,7)
         self.SetEndDate(2013,10,11)
 
-        self.SetUniverseSelection(ManualUniverseSelectionModel(
-            Symbol.Create('AIG', SecurityType.Equity, Market.USA),
-            Symbol.Create('BAC', SecurityType.Equity, Market.USA),
-            Symbol.Create('IBM', SecurityType.Equity, Market.USA),
-            Symbol.Create('SPY', SecurityType.Equity, Market.USA)))
+        symbols = [Symbol.Create(ticker, SecurityType.Equity, Market.USA)
+            for ticker in ["SPY", "AIG", "BAC", "IBM"]]
+
+        self.SetUniverseSelection(ManualUniverseSelectionModel(symbols[:2]))
+
+        self.AddUniverseSelection(ScheduledUniverseSelectionModel(
+            self.DateRules.EveryDay(), self.TimeRules.Midnight,
+            lambda dt: symbols if dt.day <= (self.EndDate - timedelta(1)).day else []))
 
         self.SetAlpha(PearsonCorrelationPairsTradingAlphaModel(252, Resolution.Daily))
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
         self.SetExecution(ImmediateExecutionModel())
         self.SetRiskManagement(NullRiskManagementModel())
+
+    def OnEndOfAlgorithm(self) -> None:
+        # We have removed all securities from the universe. The Alpha Model should remove the consolidator
+        consolidatorCount = sum(s.Consolidators.Count for s in self.SubscriptionManager.Subscriptions)
+        if consolidatorCount > 0:
+            raise Exception(f"The number of consolidator should be zero. Actual: {consolidatorCount}")
