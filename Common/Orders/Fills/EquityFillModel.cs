8bcd588602d14fce9c2d73a576f737212b56d240
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-02-24 13:35:34 +0000

    Fixes EquityFillModel.FillModel (#7005)
    * Adds Unit Tests
    
    The `LimitFill` method should not fill using `QuoteBar` or `Tick` with `TickType.Quote` type.
    Adds missing test for tick data (`TickType.Trade`).
    
    * Fixes EquityFillModel.FillModel
    
    Use `Tick` with `TickType.Trade` or `TradeBar` information to fill limit orders.
    
    * Update Regression Test Expected Statistics
    
    The regression tests changed because of different fills.
    The `ExtendedMarketTradingRegressionAlgorithm` has different number of trades because of an extra fill on the 4th order generated by TradeBar with a Low lower than than the QuoteBar.Ask Low:
    
    > 20230222 13:56:24.251 TRACE:: Log: Time: 10/10/2013 12:01:00 OrderID: 4 EventID: 2 Symbol: SPY Status: Filled Quantity: 10 FillQuantity: 10 FillPrice: 143.8998 USD
    
    > asset.Cache.GetData<QuoteBar>().ToString()
    "SPY: Bid: O: 144.2457 Bid: H: 144.2629 Bid: **L: 144.2457** Bid: C: 144.2629 Ask: O: 144.2543 Ask: H: 144.2889 Ask: **L: 144.2543** Ask: C: 144.2889 "
    
    > asset.Cache.GetData<TradeBar>().ToString()
    "SPY: O: 144.2543 H: 144.4532 **L: 143.4156** C: 144.2716 V: 75423"
    
    * Improves Tick Resolution Unit Test
    
    * Fixes Tick Resolution Case Handling
    
    `master` only considers the latest trade, missing possible fills in the batch of trades.
    
    * Adds Unit Test for Gap
    
    See https://github.com/QuantConnect/Lean/issues/963
    
    * Addresses Fill Optimistic Assumption
    
    If we have a bar that gaps in our favor, we accept the limit price to avoid optimitic fills.
    
    * Fixes Regression Tests
    
    All regression tests with limit orders have worst performance after we remove the optimitic assumption, and use the limit price instead.

--- Common/Orders/Fills/EquityFillModel.cs
+++ Common/Orders/Fills/EquityFillModel.cs
@@ -350,39 +350,65 @@ namespace QuantConnect.Orders.Fills
             {
                 return fill;
             }
-            //Get the range of prices in the last bar:
-            var prices = GetPricesCheckingPythonWrapper(asset, order.Direction);
-            var pricesEndTime = prices.EndTime.ConvertToUtc(asset.Exchange.TimeZone);
+
+            // Get the range of prices in the last bar:
+            var tradeHigh = 0m;
+            var tradeLow = 0m;
+            var endTimeUtc = DateTime.MinValue;
+
+            var subscribedTypes = GetSubscribedTypes(asset);
+
+            if (subscribedTypes.Contains(typeof(Tick)))
+            {
+                var trades = asset.Cache.GetAll<Tick>().Where(x => x.TickType == TickType.Trade && x.Price > 0);
+
+                foreach (var trade in trades)
+                {
+                    tradeHigh = Math.Max(tradeHigh, trade.Price);
+                    tradeLow = tradeLow == 0 ? trade.Price : Math.Min(tradeLow, trade.Price);
+                    endTimeUtc = trade.EndTime.ConvertToUtc(asset.Exchange.TimeZone);
+                }
+            }
+            else if (subscribedTypes.Contains(typeof(TradeBar)))
+            {
+                var tradeBar = asset.Cache.GetData<TradeBar>();
+
+                if (tradeBar != null)
+                {
+                    tradeHigh = tradeBar.High;
+                    tradeLow = tradeBar.Low;
+                    endTimeUtc = tradeBar.EndTime.ConvertToUtc(asset.Exchange.TimeZone);
+                }
+            }
 
             // do not fill on stale data
-            if (pricesEndTime <= order.Time) return fill;
+            if (endTimeUtc <= order.Time) return fill;
 
             //-> Valid Live/Model Order:
             switch (order.Direction)
             {
                 case OrderDirection.Buy:
                     //Buy limit seeks lowest price
-                    if (prices.Low < order.LimitPrice)
+                    if (tradeLow < order.LimitPrice)
                     {
-                        //Set order fill:
-                        fill.Status = OrderStatus.Filled;
-                        // fill at the worse price this bar or the limit price, this allows far out of the money limits
-                        // to be executed properly
-                        fill.FillPrice = Math.Min(prices.High, order.LimitPrice);
+                        // Fills with the limit price
+                        fill.FillPrice = order.LimitPrice;
                         // assume the order completely filled
+                        // TODO: Add separate DepthLimited fill partial order quantities based on tick quantity / bar.Volume available.
                         fill.FillQuantity = order.Quantity;
+                        fill.Status = OrderStatus.Filled;
                     }
                     break;
                 case OrderDirection.Sell:
                     //Sell limit seeks highest price possible
-                    if (prices.High > order.LimitPrice)
+                    if (tradeHigh > order.LimitPrice)
                     {
-                        fill.Status = OrderStatus.Filled;
-                        // fill at the worse price this bar or the limit price, this allows far out of the money limits
-                        // to be executed properly
-                        fill.FillPrice = Math.Max(prices.Low, order.LimitPrice);
-                        // assume the order completely filled
+                        // Fills with the limit price
+                        fill.FillPrice = order.LimitPrice;
+                        // Assume the order completely filled
+                        // TODO: Add separate DepthLimited fill partial order quantities based on tick quantity / bar.Volume available.
                         fill.FillQuantity = order.Quantity;
+                        fill.Status = OrderStatus.Filled;
                     }
                     break;
             }
