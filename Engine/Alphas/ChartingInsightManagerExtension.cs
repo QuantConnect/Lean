44d40446651a212edd1edae7321362ef7c46fa64
Martin-Molinero <martin@quantconnect.com>
2023-03-03 16:06:44 -0300

    Further insight chart cleanup

--- Engine/Alphas/ChartingInsightManagerExtension.cs
+++ Engine/Alphas/ChartingInsightManagerExtension.cs
@@ -16,7 +16,6 @@
 
 using System;
 using QuantConnect.Interfaces;
-using System.Collections.Generic;
 using QuantConnect.Algorithm.Framework.Alphas;
 using QuantConnect.Algorithm.Framework.Alphas.Analysis;
 
@@ -40,10 +39,6 @@ namespace QuantConnect.Lean.Engine.Alphas
         // Keep track, we only want to add the charts if the algorithm is producing insights
         private bool _chartsAdded;
         private IAlgorithm _algorithm;
-
-        // Heatmap chart
-        private readonly Chart _totalInsightCountPerSymbolChart = new Chart(AlphaAssets);          
-        private readonly Dictionary<Symbol, int> _totalInsightCountPerSymbol = new Dictionary<Symbol, int>();
 
         private readonly Chart _totalInsightCountChart = new Chart("Insight Count");
         private readonly Series _totalInsightCountSeries = new Series("Count", SeriesType.Bar, "#");
@@ -81,7 +76,6 @@ namespace QuantConnect.Lean.Engine.Alphas
             if (!_chartsAdded && _dailyInsightCount > 0)
             {
                 _algorithm.AddChart(_totalInsightCountChart);
-                _algorithm.AddChart(_totalInsightCountPerSymbolChart);
 
                 _chartsAdded = true;
             }
@@ -93,9 +87,6 @@ namespace QuantConnect.Lean.Engine.Alphas
 
                 // add sum of daily insight counts to the total insight count series
                 _totalInsightCountSeries.AddPoint(frontierTimeUtc.Date, _dailyInsightCount);
-
-                // Create the pie chart every minute or so
-                PopulateChartWithSeriesPerSymbol(_totalInsightCountPerSymbol, _totalInsightCountPerSymbolChart, SeriesType.Treemap, frontierTimeUtc);
 
                 // Resetting our storage
                 _dailyInsightCount = 0;
@@ -137,16 +128,6 @@ namespace QuantConnect.Lean.Engine.Alphas
         /// <param name="context">The newly generated insight analysis context</param>
         public void OnInsightGenerated(InsightAnalysisContext context)
         {
-            if (!_totalInsightCountPerSymbol.ContainsKey(context.Symbol))
-            {
-                _totalInsightCountPerSymbol[context.Symbol] = 1;
-            }
-            else
-            {
-                // track total count per symbol
-                _totalInsightCountPerSymbol[context.Symbol] += 1;
-            }
-
             _dailyInsightCount++;
         }
 
@@ -164,26 +145,6 @@ namespace QuantConnect.Lean.Engine.Alphas
         /// <param name="context">Context whose insight has just completed analysis</param>
         public void OnInsightAnalysisCompleted(InsightAnalysisContext context)
         {
-        }
-
-        /// <summary>
-        /// Creates series for each symbol and adds a value corresponding to the specified data
-        /// </summary>
-        private void PopulateChartWithSeriesPerSymbol(Dictionary<Symbol, int> data, Chart chart, SeriesType seriesType, DateTime frontierTimeUtc)
-        {
-            foreach (var kvp in data)
-            {
-                var symbol = kvp.Key;
-                var count = kvp.Value;
-
-                Series series;
-                if (!chart.Series.TryGetValue(symbol.Value, out series))
-                {
-                    series = new Series(symbol.Value, seriesType, null);
-                    chart.Series.Add(series.Name, series);
-                }
-                series.AddPoint(frontierTimeUtc, count);
-            }
         }
     }
 }
