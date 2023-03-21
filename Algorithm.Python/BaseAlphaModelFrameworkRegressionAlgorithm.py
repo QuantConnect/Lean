1b387b45285f2c79520b026cbcc149833e934e35
Alexandre Catarino <AlexCatarino@users.noreply.github.com>
2023-03-21 01:16:58 +0000

    Adds BaseAlphaModelFrameworkRegressionAlgorithm
    `BaseAlphaModelFrameworkRegressionAlgorithm` will be used to validate Alpha Model regression algorithm with the same universe.
    
    - HistoricalReturnsAlphaModelFrameworkAlgorithm
    - EmaCrossAlphaModelFrameworkAlgorithm
    - MacdAlphaModelFrameworkAlgorithm
    - RsiAlphaModelFrameworkAlgorithm
    - BasePairsTradingAlphaModelFrameworkAlgorithm

---
+++ Algorithm.Python/BaseAlphaModelFrameworkRegressionAlgorithm.py
@@ -0,0 +1,46 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from AlgorithmImports import *
+
+### <summary>
+### Abstract regression Framework algorithm used by <see cref="EmaCrossAlphaModelFrameworkAlgorithm"/>.
+### </summary>
+class BaseAlphaModelFrameworkRegressionAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2013, 10, 7)    #Set Start Date
+        self.SetEndDate(2013, 10, 11)      #Set End Date
+        
+        symbols = [Symbol.Create(ticker, SecurityType.Equity, Market.USA)
+            for ticker in ["SPY", "AIG", "BAC", "IBM"]]
+
+        # Manually add SPY and AIG when the algorithm starts
+        self.SetUniverseSelection(ManualUniverseSelectionModel(symbols[:2]))
+
+        # At midnight, add all securities every day except on the last data
+        # With this procedure, the Alpha Model will experience multiple universe changes
+        self.AddUniverseSelection(ScheduledUniverseSelectionModel(
+            self.DateRules.EveryDay(), self.TimeRules.Midnight,
+            lambda dt: symbols if dt < self.EndDate.astimezone(dt.tzinfo) - timedelta(1) else []))
+
+        self.SetAlpha(NullAlphaModel())
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
+        self.SetExecution(ImmediateExecutionModel())
+        self.SetRiskManagement(NullRiskManagementModel())
+
+    def OnEndOfAlgorithm(self):
+        # We have removed all securities from the universe. The Alpha Model should remove the consolidator
+        consolidatorCount = sum(s.Consolidators.Count for s in self.SubscriptionManager.Subscriptions)
+        if consolidatorCount > 0:
+            raise Exception(f"The number of consolidator is should be zero. Actual: {consolidatorCount}")
