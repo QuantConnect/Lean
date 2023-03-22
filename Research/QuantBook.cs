fcd07979390ab0a55065f9d9b9ed735171e029b1
Jhonathan Abreu <jdabreu25@gmail.com>
2023-03-22 14:37:18 -0400

    Use data end time to timestamp fundalmental data dictionaries

--- Research/QuantBook.cs
+++ Research/QuantBook.cs
@@ -852,17 +852,19 @@ namespace QuantConnect.Research
                 {
                     while (enumerator.MoveNext())
                     {
+                        var currentData = enumerator.Current;
+                        var time = currentData.EndTime;
                         var dataPoint = string.IsNullOrWhiteSpace(selector)
-                            ? enumerator.Current
-                            : GetPropertyValue(enumerator.Current, selector);
+                            ? currentData
+                            : GetPropertyValue(currentData, selector);
 
                         lock (data)
                         {
-                            if (!data.ContainsKey(enumerator.Current.Time))
+                            if (!data.ContainsKey(time))
                             {
-                                data[enumerator.Current.Time] = new DataDictionary<dynamic>(enumerator.Current.Time);
+                                data[time] = new DataDictionary<dynamic>(time);
                             }
-                            data[enumerator.Current.Time].Add(enumerator.Current.Symbol, dataPoint);
+                            data[time].Add(currentData.Symbol, dataPoint);
                         }
                     }
                 }
