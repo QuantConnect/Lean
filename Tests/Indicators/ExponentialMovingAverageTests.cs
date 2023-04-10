62ea5fa752f2af35b73354a460fe7135e7fdee6c
Jhonathan Abreu <jdabreu25@gmail.com>
2023-04-10 16:08:59 -0400

    Fix EMA indicator first value calculation
    As done by TALib and TradingView, the first EMA value after warmup is
    the SMA of the first period.

--- Tests/Indicators/ExponentialMovingAverageTests.cs
+++ Tests/Indicators/ExponentialMovingAverageTests.cs
@@ -1,11 +1,11 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,6 +14,7 @@
 */
 
 using System;
+using System.Linq;
 using NUnit.Framework;
 using QuantConnect.Indicators;
 
@@ -24,10 +25,10 @@ namespace QuantConnect.Tests.Indicators
     {
         protected override IndicatorBase<IndicatorDataPoint> CreateIndicator()
         {
-            return new ExponentialMovingAverage(14);
+             return new ExponentialMovingAverage(14);
         }
 
-        protected override string TestFileName => "spy_with_indicators.txt";
+        protected override string TestFileName => "spy_ema.csv";
 
         protected override string TestColumnName => "EMA14";
 
@@ -37,24 +38,25 @@ namespace QuantConnect.Tests.Indicators
         public void EmaComputesCorrectly()
         {
             const int period = 4;
-            decimal[] values = {1m, 10m, 100m, 1000m};
+            decimal[] values = { 1m, 10m, 100m, 1000m, 2000m, 3000m, 4000m, 5000m, 6000m, 7000m, 8000m, 9000m, 10000m };
             const decimal expFactor = 2m/(1m + period);
-            
+
             var ema4 = new ExponentialMovingAverage(period);
 
-            decimal current = 0m;
+            decimal expectedCurrent = 0m;
             for (int i = 0; i < values.Length; i++)
             {
                 ema4.Update(new IndicatorDataPoint(DateTime.UtcNow.AddSeconds(i), values[i]));
-                if (i == 0)
+                if (i == period - 1)
                 {
-                    current = values[i];
+                    // The indicator is ready after the first full period, the first value should be a SMA of the first period
+                    expectedCurrent = values.Take(period).Sum() / period;
                 }
-                else
+                if (i >= period)
                 {
-                    current = values[i]*expFactor + (1 - expFactor)*current;
+                    expectedCurrent = values[i] * expFactor + (1 - expFactor) * expectedCurrent;
                 }
-                Assert.AreEqual(current, ema4.Current.Value);
+                Assert.AreEqual(expectedCurrent, ema4.Current.Value, $"Index: {i}");
             }
         }
 
