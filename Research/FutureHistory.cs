8d54438eaca4a02649dea2c98d5c954d708ae721
Jhonathan Abreu <jdabreu25@gmail.com>
2023-04-11 17:54:58 -0400

    Add support for iterating FutureHistory and OptionHistory (#7176)
    This adds support for accessing the history slices in research
    environment

--- Research/FutureHistory.cs
+++ Research/FutureHistory.cs
@@ -1,11 +1,11 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 using Python.Runtime;
 using QuantConnect.Data;
 using QuantConnect.Python;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,7 +25,7 @@ namespace QuantConnect.Research
     /// <summary>
     /// Class to manage information from History Request of Futures
     /// </summary>
-    public class FutureHistory
+    public class FutureHistory : IEnumerable<Slice>
     {
         private IEnumerable<Slice> _data;
         private PandasConverter _converter;
@@ -71,5 +72,19 @@ namespace QuantConnect.Research
         {
             return _converter.ToString();
         }
+
+        #region IEnumerable implementation
+
+        public IEnumerator<Slice> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
     }
 }
