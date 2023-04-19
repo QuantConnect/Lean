ad57991707024329a76222b91755decd15ebe29b
Marinovsky <rmarino@unal.edu.co>
2023-04-19 17:50:55 -0500

    Implement `SecurityDataFilterPythonWrapper.cs`
    - Create `SecurityDataFilterPythonWrapper.cs`
    - Modifiy `Security.cs` to set a data filter, whether is a C#, Python derivative or custom data filter custom
    - Add Python and C# regression algorithms to assert the changes

---
+++ Algorithm.CSharp/CustomSecurityDataFilterRegressionAlgorithm.cs
@@ -0,0 +1,121 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+using QuantConnect.Securities;
+using QuantConnect.Securities.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    /// <summary>
+    /// Regression algorithm asserting we can specify a custom security data filter
+    /// </summary>
+    public class CustomSecurityDataFilterRegressionAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
+    {
+        private int _dataPoints;
+
+        public override void Initialize()
+        {
+            SetCash(2500000);
+            SetStartDate(2013, 10, 7);
+            SetEndDate(2013, 10, 7);
+
+            var security = AddSecurity(SecurityType.Equity, "SPY");
+            security.SetDataFilter(new CustomDataFilter());
+            _dataPoints = 0;
+        }
+
+        public override void OnData(Slice slice)
+        {
+            _dataPoints++;
+            SetHoldings("SPY", 0.2);
+            if (_dataPoints > 5)
+            {
+                throw new Exception($"There should not be more than 5 data points, but there were {_dataPoints}");
+            }
+        }
+
+        private class CustomDataFilter : ISecurityDataFilter
+        {
+            public bool Filter(Security vehicle, BaseData data)
+            {
+                // Skip data after 9:35am
+                if (data.Time >= new DateTime(2013, 10, 7, 9, 35, 0, 0))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This is used by the regression test system to indicate if the open source Lean repository has the required data to run this algorithm.
+        /// </summary>
+        public bool CanRunLocally { get; } = true;
+
+        /// <summary>
+        /// This is used by the regression test system to indicate which languages this algorithm is written in.
+        /// </summary>
+        public virtual Language[] Languages { get; } = { Language.CSharp, Language.Python};
+
+        /// <summary>
+        /// Data Points count of all timeslices of algorithm
+        /// </summary>
+        public long DataPoints => 25;
+
+        /// <summary>
+        /// Data Points count of the algorithm history
+        /// </summary>
+        public int AlgorithmHistoryDataPoints => 0;
+
+        /// <summary>
+        /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
+        /// </summary>
+        public virtual Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
+        {
+            {"Total Trades", "5"},
+            {"Average Win", "0%"},
+            {"Average Loss", "0%"},
+            {"Compounding Annual Return", "0%"},
+            {"Drawdown", "0%"},
+            {"Expectancy", "0"},
+            {"Net Profit", "0%"},
+            {"Sharpe Ratio", "0"},
+            {"Probabilistic Sharpe Ratio", "0%"},
+            {"Loss Rate", "0%"},
+            {"Win Rate", "0%"},
+            {"Profit-Loss Ratio", "0"},
+            {"Alpha", "0"},
+            {"Beta", "0"},
+            {"Annual Standard Deviation", "0"},
+            {"Annual Variance", "0"},
+            {"Information Ratio", "0"},
+            {"Tracking Error", "0"},
+            {"Treynor Ratio", "0"},
+            {"Total Fees", "$21.23"},
+            {"Estimated Strategy Capacity", "$22000000.00"},
+            {"Lowest Capacity Asset", "SPY R735QTJ8XC9X"},
+            {"Portfolio Turnover", "19.98%"},
+            {"OrderListHash", "632386514a9cf27653301bc01a190b8f"}
+        };
+    }
+}
---
+++ Common/Securities/SecurityDataFilterPythonWrapper.cs
@@ -0,0 +1,52 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using Python.Runtime;
+using QuantConnect.Data;
+using QuantConnect.Python;
+using QuantConnect.Securities.Interfaces;
+
+namespace QuantConnect.Securities
+{
+    /// <summary>
+    /// Python Wrapper for custom security data filters from Python
+    /// </summary>
+    public class SecurityDataFilterPythonWrapper : ISecurityDataFilter
+    {
+        private readonly dynamic _dataFilter;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="dataFilter">The Python class to wrapp</param>
+        public SecurityDataFilterPythonWrapper(PyObject dataFilter)
+        {
+            _dataFilter = dataFilter.ValidateImplementationOf<ISecurityDataFilter>();
+        }
+
+        /// <summary>
+        /// Performs Filter method from Python instance returning true to accept, or false to fail/reject the data point.
+        /// </summary>
+        /// <param name="data">BasData data object we're filtering</param>
+        /// <param name="vehicle">Security vehicle for filter</param>
+        public bool Filter(Security vehicle, BaseData data)
+        {
+            using (Py.GIL())
+            {
+                return (_dataFilter.Filter(vehicle, data) as PyObject).GetAndDispose<bool>();
+            }
+        }
+    }
+}
