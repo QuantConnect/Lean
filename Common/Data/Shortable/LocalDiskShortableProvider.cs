ef8f4ff403724cc7a683dbf14b5d762884494400
Martin-Molinero <martin@quantconnect.com>
2023-04-25 19:15:42 -0300

    Address review

--- Common/Data/Shortable/LocalDiskShortableProvider.cs
+++ Common/Data/Shortable/LocalDiskShortableProvider.cs
@@ -14,7 +14,6 @@
 */
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using QuantConnect.Configuration;
 using QuantConnect.Interfaces;
--- Common/Data/Shortable/NullShortableProvider.cs
+++ Common/Data/Shortable/NullShortableProvider.cs
@@ -14,7 +14,6 @@
 */
 
 using System;
-using System.Collections.Generic;
 using QuantConnect.Interfaces;
 
 namespace QuantConnect.Data.Shortable
@@ -25,16 +24,6 @@ namespace QuantConnect.Data.Shortable
     /// </summary>
     public class NullShortableProvider : IShortableProvider
     {
-        /// <summary>
-        /// Gets all shortable Symbols
-        /// </summary>
-        /// <param name="localTime">Time of the algorithm</param>
-        /// <returns>null indicating that all Symbols are shortable</returns>
-        public Dictionary<Symbol, long> AllShortableSymbols(DateTime localTime)
-        {
-            return null;
-        }
-
         /// <summary>
         /// Gets the quantity shortable for the Symbol at the given time.
         /// </summary>
