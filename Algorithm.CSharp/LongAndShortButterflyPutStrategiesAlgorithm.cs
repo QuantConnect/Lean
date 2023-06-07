4caeed535601113687a6b2d024578a01144b249e
Jhonathan Abreu <jdabreu25@gmail.com>
2023-06-07 16:26:00 -0400

    Add ShortButterflyCall and ShortButterflyPut strategies helper factory methods

---
+++ Algorithm.CSharp/LongAndShortButterflyPutStrategiesAlgorithm.cs
@@ -0,0 +1,210 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+using QuantConnect.Orders;
+using QuantConnect.Securities.Option;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    /// <summary>
+    /// This algorithm demonstrate how to use OptionStrategies helper class to batch send orders for common strategies.
+    /// In this case, the algorithm tests the Butterfly Put and Short Butterfly Put strategies.
+    /// </summary>
+    public class LongAndShortButterflyPutStrategiesAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
+    {
+        private Symbol _optionSymbol;
+        private OptionStrategy _butterflyPut;
+        private OptionStrategy _shortButterflyPut;
+
+        public override void Initialize()
+        {
+            SetStartDate(2015, 12, 24);
+            SetEndDate(2015, 12, 24);
+            SetCash(1000000);
+
+            var option = AddOption("GOOG");
+            _optionSymbol = option.Symbol;
+
+            option.SetFilter(-2, +2, 0, 180);
+
+            SetBenchmark("GOOG");
+        }
+
+        public override void OnData(Slice slice)
+        {
+            if (!Portfolio.Invested)
+            {
+                if (slice.OptionChains.TryGetValue(_optionSymbol, out var chain))
+                {
+                    var contractsByExpiry = chain.Where(x => x.Right == OptionRight.Put).GroupBy(x => x.Expiry);
+
+                    foreach (var group in contractsByExpiry)
+                    {
+                        var expiry = group.Key;
+                        var contracts = group.ToList();
+
+                        if (contracts.Count < 3)
+                        {
+                            continue;
+                        }
+
+                        var strikes = contracts.Select(x => x.Strike).OrderBy(x => x).ToArray();
+                        var atmStrike = contracts.MinBy(x => Math.Abs(x.Strike - chain.Underlying.Value)).Strike;
+                        var spread = Math.Min(atmStrike - strikes[0], strikes[^1] - atmStrike);
+                        var itmStrike = atmStrike + spread;
+                        var otmStrike = atmStrike - spread;
+
+                        if (strikes.Contains(otmStrike) && strikes.Contains(itmStrike))
+                        {
+                            // Ready to trade
+                            _butterflyPut = OptionStrategies.ButterflyPut(_optionSymbol, itmStrike, atmStrike, otmStrike, expiry);
+                            _shortButterflyPut = OptionStrategies.ShortButterflyPut(_optionSymbol, itmStrike, atmStrike, otmStrike, expiry);
+                            Buy(_butterflyPut, 2);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // Verify that the strategy was traded
+                var positionGroup = Portfolio.Positions.Groups.Single();
+
+                var buyingPowerModel = positionGroup.BuyingPowerModel as OptionStrategyPositionGroupBuyingPowerModel;
+                if (buyingPowerModel == null)
+                {
+                    throw new Exception($@"Expected position group buying power model type: {nameof(OptionStrategyPositionGroupBuyingPowerModel)
+                        }. Actual: {positionGroup.BuyingPowerModel.GetType()}");
+                }
+
+                if (positionGroup.Positions.Count() != 3)
+                {
+                    throw new Exception($"Expected position group to have 3 positions. Actual: {positionGroup.Positions.Count()}");
+                }
+
+                var higherStrike = _butterflyPut.OptionLegs.Max(leg => leg.Strike);
+                var higherStrikePosition = positionGroup.Positions
+                    .Single(x => x.Symbol.ID.OptionRight == OptionRight.Put && x.Symbol.ID.StrikePrice == higherStrike);
+
+                if (higherStrikePosition.Quantity != 2)
+                {
+                    throw new Exception($"Expected higher strike position quantity to be 2. Actual: {higherStrikePosition.Quantity}");
+                }
+
+                var lowerStrike = _butterflyPut.OptionLegs.Min(leg => leg.Strike);
+                var lowerStrikePosition = positionGroup.Positions
+                    .Single(x => x.Symbol.ID.OptionRight == OptionRight.Put && x.Symbol.ID.StrikePrice == lowerStrike);
+
+                if (lowerStrikePosition.Quantity != 2)
+                {
+                    throw new Exception($"Expected lower strike position quantity to be 2. Actual: {lowerStrikePosition.Quantity}");
+                }
+
+                var middleStrike = _butterflyPut.OptionLegs.Single(leg => leg.Strike < higherStrike && leg.Strike > lowerStrike).Strike;
+                var middleStrikePosition = positionGroup.Positions
+                    .Single(x => x.Symbol.ID.OptionRight == OptionRight.Put && x.Symbol.ID.StrikePrice == middleStrike);
+
+                if (middleStrikePosition.Quantity != -4)
+                {
+                    throw new Exception($"Expected middle strike position quantity to be -4. Actual: {middleStrikePosition.Quantity}");
+                }
+
+                // Now we should be able to close the position using the inverse strategy (a short butterfly put)
+                Buy(_shortButterflyPut, 2);
+
+                // We can quit now, no more testing required
+                Quit();
+            }
+        }
+
+        public override void OnEndOfAlgorithm()
+        {
+            if (Portfolio.Invested)
+            {
+                throw new Exception("Expected no holdings at end of algorithm");
+            }
+
+            var ordersCount = Transactions.GetOrders((order) => order.Status == OrderStatus.Filled).Count();
+            if (ordersCount != 6)
+            {
+                throw new Exception(
+                    "Expected 6 orders to have been submitted and filled, 3 for buying the butterfly put and 3 for the liquidation." +
+                    $" Actual {ordersCount}");
+            }
+        }
+
+        public override void OnOrderEvent(OrderEvent orderEvent)
+        {
+            Debug(orderEvent.ToString());
+        }
+
+        /// <summary>
+        /// This is used by the regression test system to indicate if the open source Lean repository has the required data to run this algorithm.
+        /// </summary>
+        public bool CanRunLocally { get; } = true;
+
+        /// <summary>
+        /// This is used by the regression test system to indicate which languages this algorithm is written in.
+        /// </summary>
+        public Language[] Languages { get; } = { Language.CSharp, Language.Python };
+
+        /// <summary>
+        /// Data Points count of all timeslices of algorithm
+        /// </summary>
+        public long DataPoints => 4494;
+
+        /// <summary>
+        /// Data Points count of the algorithm history
+        /// </summary>
+        public int AlgorithmHistoryDataPoints => 0;
+
+        /// <summary>
+        /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
+        /// </summary>
+        public Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
+        {
+            {"Total Trades", "6"},
+            {"Average Win", "0%"},
+            {"Average Loss", "0%"},
+            {"Compounding Annual Return", "0%"},
+            {"Drawdown", "0%"},
+            {"Expectancy", "0"},
+            {"Net Profit", "0%"},
+            {"Sharpe Ratio", "0"},
+            {"Probabilistic Sharpe Ratio", "0%"},
+            {"Loss Rate", "0%"},
+            {"Win Rate", "0%"},
+            {"Profit-Loss Ratio", "0"},
+            {"Alpha", "0"},
+            {"Beta", "0"},
+            {"Annual Standard Deviation", "0"},
+            {"Annual Variance", "0"},
+            {"Information Ratio", "0"},
+            {"Tracking Error", "0"},
+            {"Treynor Ratio", "0"},
+            {"Total Fees", "$6.00"},
+            {"Estimated Strategy Capacity", "$4000.00"},
+            {"Lowest Capacity Asset", "GOOCV 306CZL2DIL4G6|GOOCV VP83T1ZUHROL"},
+            {"Portfolio Turnover", "2.23%"},
+            {"OrderListHash", "53594646c4d8941941b95ebdd278a5ed"}
+        };
+    }
+}
---
+++ Algorithm.Python/LongAndShortButterflyCallStrategiesAlgorithm.py
@@ -0,0 +1,111 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from AlgorithmImports import *
+
+import itertools
+
+### <summary>
+### This algorithm demonstrate how to use OptionStrategies helper class to batch send orders for common strategies.
+### In this case, the algorithm tests the Butterfly Call and Short Butterfly Call strategies.
+### </summary>
+class LongAndShortButterflyCallStrategiesAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2015, 12, 24)
+        self.SetEndDate(2015, 12, 24)
+        self.SetCash(1000000)
+
+        option = self.AddOption("GOOG")
+        self._option_symbol = option.Symbol
+
+        option.SetFilter(-2, +2, 0, 180)
+
+        self.SetBenchmark("GOOG")
+
+    def OnData(self,slice):
+        if not self.Portfolio.Invested:
+            chain = slice.OptionChains.get(self._option_symbol)
+            if chain is not None:
+                callContracts = (contract for contract in chain if contract.Right == OptionRight.Call)
+
+                for expiry, group in itertools.groupby(callContracts, lambda x: x.Expiry):
+                    contracts = list(group)
+                    if len(contracts) < 3:
+                        continue
+
+                    strikes = sorted([contract.Strike for contract in contracts])
+                    atmStrike = min(strikes, key=lambda strike: abs(strike - chain.Underlying.Price))
+                    spread = min(atmStrike - strikes[0], strikes[-1] - atmStrike)
+                    itmStrike = atmStrike - spread
+                    otmStrike = atmStrike + spread
+
+                    if otmStrike in strikes and itmStrike in strikes:
+                        # Ready to trade
+                        self._butterfly_call = OptionStrategies.ButterflyCall(self._option_symbol, otmStrike, atmStrike, itmStrike, expiry)
+                        self._short_butterfly_call = OptionStrategies.ShortButterflyCall(self._option_symbol, otmStrike, atmStrike, itmStrike, expiry)
+                        self.Buy(self._butterfly_call, 2)
+        else:
+            # Verify that the strategy was traded
+            positionGroup = list(self.Portfolio.Positions.Groups)[0]
+
+            buyingPowerModel = positionGroup.BuyingPowerModel
+            if not isinstance(buyingPowerModel, OptionStrategyPositionGroupBuyingPowerModel):
+                raise Exception("Expected position group buying power model type: OptionStrategyPositionGroupBuyingPowerModel. "
+                                f"Actual: {type(positionGroup.BuyingPowerModel).__name__}")
+
+            positions = list(positionGroup.Positions)
+            if len(positions) != 3:
+                raise Exception(f"Expected position group to have 3 positions. Actual: {len(positions)}")
+
+            higherStrike = max(leg.Strike for leg in self._butterfly_call.OptionLegs)
+            higherStrikePosition = next((position for position in positions
+                                          if position.Symbol.ID.OptionRight == OptionRight.Call and position.Symbol.ID.StrikePrice == higherStrike),
+                                         None)
+
+            if higherStrikePosition.Quantity != 2:
+                raise Exception(f"Expected higher strike position quantity to be 2. Actual: {higherStrikePosition.Quantity}")
+
+            lowerStrike = min(leg.Strike for leg in self._butterfly_call.OptionLegs)
+            lowerStrikePosition = next((position for position in positions
+                                        if position.Symbol.ID.OptionRight == OptionRight.Call and position.Symbol.ID.StrikePrice == lowerStrike),
+                                       None)
+
+            if lowerStrikePosition.Quantity != 2:
+                raise Exception(f"Expected lower strike position quantity to be 2. Actual: {lowerStrikePosition.Quantity}")
+
+            middleStrike = [leg.Strike for leg in self._butterfly_call.OptionLegs if leg.Strike < higherStrike and leg.Strike > lowerStrike][0]
+            middleStrikePosition = next((position for position in positions
+                                         if position.Symbol.ID.OptionRight == OptionRight.Call and position.Symbol.ID.StrikePrice == middleStrike),
+                                        None)
+
+            if middleStrikePosition.Quantity != -4:
+                raise Exception(f"Expected middle strike position quantity to be -4. Actual: {middleStrikePosition.Quantity}")
+
+            # Now we should be able to close the position using the inverse strategy (a short butterfly call)
+            self.Buy(self._short_butterfly_call, 2);
+
+            # We can quit now, no more testing required
+            self.Quit();
+
+    def OnEndOfAlgorithm(self):
+        if self.Portfolio.Invested:
+            raise Exception("Expected no holdings at end of algorithm")
+
+        orders_count = len(list(self.Transactions.GetOrders(lambda order: order.Status == OrderStatus.Filled)))
+        if orders_count != 6:
+            raise Exception("Expected 6 orders to have been submitted and filled, 3 for buying the butterfly call and 3 for the liquidation. "
+                            f"Actual {orders_count}")
+
+    def OnOrderEvent(self, orderEvent):
+        self.Debug(str(orderEvent))
