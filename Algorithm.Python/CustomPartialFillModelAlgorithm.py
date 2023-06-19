ae93fc32e38e95c4830cec25de4b3e8813e0f25d
Marinovsky <rmarino@unal.edu.co>
2023-06-19 15:39:33 -0500

    Nit change

--- Algorithm.Python/CustomPartialFillModelAlgorithm.py
+++ Algorithm.Python/CustomPartialFillModelAlgorithm.py
@@ -68,6 +68,6 @@ class CustomPartialFillModel(FillModel):
             fill.Status = OrderStatus.PartiallyFilled
             self.absoluteRemainingByOrderId[order.Id] = absoluteRemaining - abs(fill.FillQuantity)
             price = fill.FillPrice
-            # self.algorithm.Debug(f"{self.algorithm.Time} - Partial Fill - Remaining {absoluteRemaining} Price - {price}")
+            # self.algorithm.Debug(f"{self.algorithm.Time} - Partial Fill - Remaining {self.absoluteRemainingByOrderId[order.Id]} Price - {price}")
 
         return fill
