e029b962298d84dcae66ec19f295ca51af197cd0
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2023-06-30 11:36:44 -0500

    Fix bug in VolumeWeightedAveragePriceIndicator (#7336)
    * Fix bug
    
    - Add `ResetHandler()` and `IsReset` field in `CompositeIndicator.cs`
    - Modify `CompositeIndicator.Reset()` to invoke the IsReset event
    - Modify `VolumeWeightedAveragePriceIndicator.Reset()` method to reset
      its composite indicator
    - Modify `IndicatorExtensions.WeightedBy()` to register an event to the
      returned composite indicator `IsReset` field
    
    * Enhance implementation
    
    * Address Requested changes
    
    * Nit changes
    
    * Nit change
    
    * Nit change
    
    * Address chages
    
    - Make ResetCompositeIndicator inherit from CompositeIndicator and
      override `Reset()` method
    - Add ResetCompositeIndicator unit tests
    - Modifiy IndicatorExtensions to keep returning the same type in
      `WeightedBy()` method
    
    * Avoid modify other IndicatorExtensions methods
    
    * Nit changes
    
    * Reset inner indicators and add more unit tests

--- Indicators/CompositeIndicator.cs
+++ Indicators/CompositeIndicator.cs
@@ -76,7 +76,7 @@ namespace QuantConnect.Indicators
         /// </summary>
         /// <param name="name">The name of this indicator</param>
         /// <param name="left">The left indicator for the 'composer'</param>
-        /// <param name="right">The right indidcator for the 'composoer'</param>
+        /// <param name="right">The right indicator for the 'composer'</param>
         /// <param name="composer">Function used to compose the left and right indicators</param>
         public CompositeIndicator(string name, IndicatorBase left, IndicatorBase right, IndicatorComposer composer)
             : base(name)
@@ -92,7 +92,7 @@ namespace QuantConnect.Indicators
         /// and producing a new value via the composer delegate specified
         /// </summary>
         /// <param name="left">The left indicator for the 'composer'</param>
-        /// <param name="right">The right indidcator for the 'composoer'</param>
+        /// <param name="right">The right indicator for the 'composer'</param>
         /// <param name="composer">Function used to compose the left and right indicators</param>
         public CompositeIndicator(IndicatorBase left, IndicatorBase right, IndicatorComposer composer)
             : this($"COMPOSE({left.Name},{right.Name})", left, right, composer)
