7330915d22296088ee6e6f4fd89d659c4d201d12
Jhonathan Abreu <jdabreu25@gmail.com>
2023-08-08 12:52:15 -0400

    Minor cleanup

--- Common/SeriesSampler.cs
+++ Common/SeriesSampler.cs
@@ -18,7 +18,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using QuantConnect.Util;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace QuantConnect
 {
