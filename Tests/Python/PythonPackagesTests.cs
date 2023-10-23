c181973d1768e29118096060b5a448b251d7bcda
Martin Molinero <martin.molinero1@gmail.com>
2023-10-23 12:25:40 -0300

    Reduce foundation size
    - Reduce foundation size and minor library update

--- Tests/Python/PythonPackagesTests.cs
+++ Tests/Python/PythonPackagesTests.cs
@@ -1106,23 +1106,6 @@ def RunTest():
             );
         }
 
-        [Test, Explicit("Installed in specific environment. Requires older numpy")]
-        public void PomegranateTest()
-        {
-            PythonInitializer.ActivatePythonVirtualEnvironment("/Foundation-Pomegranate");
-            AssertCode(
-                @"
-from pomegranate import *
-def RunTest():
-    d1 = NormalDistribution(5, 2)
-    d2 = LogNormalDistribution(1, 0.3)
-    d3 = ExponentialDistribution(4)
-    d = IndependentComponentsDistribution([d1, d2, d3])
-    X = [6.2, 0.4, 0.9]
-    return d.log_probability(X)"
-            );
-        }
-
         [Test]
         public void LightgbmTest()
         {
@@ -1280,46 +1263,6 @@ def RunTest():
 	y = solver.NumVar(0, 2, 'y')
 
 	print('Number of variables =', solver.NumVariables())");
-        }
-
-        [Test, Explicit("Installed in specific environment")]
-        public void AutoGluon()
-        {
-            PythonInitializer.ActivatePythonVirtualEnvironment("/Foundation-Pomegranate");
-            AssertCode(
-                @"
-from autogluon.tabular import TabularDataset, TabularPredictor
-
-def RunTest():
-    train_data = TabularDataset('https://autogluon.s3.amazonaws.com/datasets/Inc/train.csv')
-    test_data = TabularDataset('https://autogluon.s3.amazonaws.com/datasets/Inc/test.csv')
-    predictor = TabularPredictor(label='class').fit(train_data, time_limit=5)  # Fit models for 5s
-    leaderboard = predictor.leaderboard(test_data)");
-        }
-
-        [Test, Explicit("Installed in specific environment")]
-        public void Finrl()
-        {
-            PythonInitializer.ActivatePythonVirtualEnvironment("/Foundation-Pomegranate");
-            AssertCode(
-                @"
-import finrl
-
-def RunTest():
-    if finrl.__package__ != 'finrl':
-        raise ValueError('Unexpected package!')");
-        }
-
-        [Test, Explicit("Installed in specific environment. Requires older torch")]
-        public void Neuralprophet()
-        {
-            PythonInitializer.ActivatePythonVirtualEnvironment("/Foundation-Pomegranate");
-            AssertCode(
-                @"
-from neuralprophet import NeuralProphet
-
-def RunTest():
-    m = NeuralProphet()");
         }
 
         [Test]
@@ -2159,70 +2102,6 @@ def RunTest():
     pcmci = PCMCI(dataframe=dataframe, cond_ind_test=cond_ind_test)
     results = pcmci.run_pcmciplus(tau_min=0, tau_max=2, pc_alpha=0.01)
     pcmci.print_results(results, alpha_level=0.01)
-");
-        }
-
-        [Test, Explicit("Sometimes crashes when run along side the other tests")]
-        public void NBeatsTest()
-        {
-            PythonInitializer.ActivatePythonVirtualEnvironment("/Foundation-Pomegranate");
-            AssertCode(@"
-import warnings
-import numpy as np
-
-from nbeats_keras.model import NBeatsNet as NBeatsKeras
-from nbeats_pytorch.model import NBeatsNet as NBeatsPytorch
-
-warnings.filterwarnings(action='ignore', message='Setting attributes')
-
-def RunTest():
-    # https://keras.io/layers/recurrent/
-    # At the moment only Keras supports input_dim > 1. In the original paper, input_dim=1.
-    num_samples, time_steps, input_dim, output_dim = 50_000, 10, 1, 1
-
-    # This example is for both Keras and Pytorch. In practice, choose the one you prefer.
-    for BackendType in [NBeatsKeras, NBeatsPytorch]:
-        # NOTE: If you choose the Keras backend with input_dim>1, you have
-        # to set the value here too (in the constructor).
-        backend = BackendType(
-            backcast_length=time_steps, forecast_length=output_dim,
-            stack_types=(NBeatsKeras.GENERIC_BLOCK, NBeatsKeras.GENERIC_BLOCK),
-            nb_blocks_per_stack=2, thetas_dim=(4, 4), share_weights_in_stack=True,
-            hidden_layer_units=64
-        )
-
-        # Definition of the objective function and the optimizer.
-        backend.compile(loss='mae', optimizer='adam')
-
-        # Definition of the data. The problem to solve is to find f such as | f(x) - y | -> 0.
-        # where f = np.mean.
-        x = np.random.uniform(size=(num_samples, time_steps, input_dim))
-        y = np.mean(x, axis=1, keepdims=True)
-
-        # Split data into training and testing datasets.
-        c = num_samples // 10
-        x_train, y_train, x_test, y_test = x[c:], y[c:], x[:c], y[:c]
-        test_size = len(x_test)
-
-        # Train the model.
-        print('Training...')
-        backend.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=128)
-
-        # Save the model for later.
-        backend.save('n_beats_model.h5')
-
-        # Predict on the testing set (forecast).
-        predictions_forecast = backend.predict(x_test)
-        np.testing.assert_equal(predictions_forecast.shape, (test_size, backend.forecast_length, output_dim))
-
-        # Predict on the testing set (backcast).
-        predictions_backcast = backend.predict(x_test, return_backcast=True)
-        np.testing.assert_equal(predictions_backcast.shape, (test_size, backend.backcast_length, output_dim))
-
-        # Load the model.
-        model_2 = BackendType.load('n_beats_model.h5')
-
-        np.testing.assert_almost_equal(predictions_forecast, model_2.predict(x_test))
 ");
         }
 
