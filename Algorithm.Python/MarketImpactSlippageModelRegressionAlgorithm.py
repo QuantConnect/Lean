7d3733ccb79ee6456afe91ef549648cbe20b0d16
Louis Szeto <56447733+LouisSzeto@users.noreply.github.com>
2023-10-28 04:43:44 +0800

    Slippage model by market impact factors (#7513)
    * Add Market Impact Slippage Model
    
    * Add unit tests
    
    * Add regression tests and random seeds
    
    * Address peer review 1
    
    * Add non-negative logic choice
    
    * address peer review 2
    
    * timezone setting adjustment
    
    * Address peer review 2
    
    * address second review
    
    * minor updates
    
    * minor updates
    
    * Nit
    
    * fix regression tests
    
    * fix CI
    
    * Rebase and fix CI
    
    * regression test

---
+++ Algorithm.Python/MarketImpactSlippageModelRegressionAlgorithm.py
@@ -0,0 +1,35 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from AlgorithmImports import *
+
+class MarketImpactSlippageModelRegressionAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2013, 10, 7)
+        self.SetEndDate(2013, 10, 13)
+        self.SetCash(10000000)
+
+        spy = self.AddEquity("SPY", Resolution.Daily)
+        aapl = self.AddEquity("AAPL", Resolution.Daily)
+
+        spy.SetSlippageModel(MarketImpactSlippageModel(self))
+        aapl.SetSlippageModel(MarketImpactSlippageModel(self))
+
+    def OnData(self, data):
+        self.SetHoldings("SPY", 0.5)
+        self.SetHoldings("AAPL", -0.5)
+
+    def OnOrderEvent(self, orderEvent):
+        if orderEvent.Status == OrderStatus.Filled:
+            self.Debug(f"Price: {self.Securities[orderEvent.Symbol].Price}, filled price: {orderEvent.FillPrice}, quantity: {orderEvent.FillQuantity}")
