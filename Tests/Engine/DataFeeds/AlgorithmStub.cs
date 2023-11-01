54dfa68c7d10cae59a0638a9679364d62c14895e
Martin Molinero <martin.molinero1@gmail.com>
2023-11-01 16:05:04 -0300

    AlgorithmStub Lazy Pandas Converter Set

--- Tests/Engine/DataFeeds/AlgorithmStub.cs
+++ Tests/Engine/DataFeeds/AlgorithmStub.cs
@@ -32,6 +32,22 @@ namespace QuantConnect.Tests.Engine.DataFeeds
         public List<SecurityChanges> SecurityChangesRecord = new List<SecurityChanges>();
         public DataManager DataManager;
         public IDataFeed DataFeed;
+
+        /// <summary>
+        /// Lanzy PandasConverter only if used
+        /// </summary>
+        public override PandasConverter PandasConverter
+        {
+            get
+            {
+                if(base.PandasConverter == null)
+                {
+                    SetPandasConverter();
+                }
+                return base.PandasConverter;
+            }
+        }
+
         public AlgorithmStub(bool createDataManager = true)
         {
             if (createDataManager)
@@ -44,8 +60,6 @@ namespace QuantConnect.Tests.Engine.DataFeeds
             var orderProcessor = new FakeOrderProcessor();
             orderProcessor.TransactionManager = Transactions;
             Transactions.SetOrderProcessor(orderProcessor);
-            PythonInitializer.Initialize();
-            SetPandasConverter();
         }
 
         public AlgorithmStub(IDataFeed dataFeed)
@@ -54,7 +68,6 @@ namespace QuantConnect.Tests.Engine.DataFeeds
             DataManager = new DataManagerStub(dataFeed, this);
             SubscriptionManager.SetDataManager(DataManager);
             Transactions.SetOrderProcessor(new FakeOrderProcessor());
-            SetPandasConverter();
         }
 
         public void AddSecurities(Resolution resolution = Resolution.Second, List<string> equities = null, List<string> forex = null, List<string> crypto = null)
