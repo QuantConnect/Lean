f1627a9c961dcb4763558c5cc477e74cad0f651a
Martin-Molinero <martin@quantconnect.com>
2023-11-28 18:23:43 -0300

    Minor foundation update (#7602)
    * Minor foundation update
    
    * Add Xtransformers test

--- Tests/Python/PythonPackagesTests.cs
+++ Tests/Python/PythonPackagesTests.cs
@@ -17,13 +17,42 @@
 using System;
 using Python.Runtime;
 using NUnit.Framework;
-using QuantConnect.Python;
 
 namespace QuantConnect.Tests.Python
 {
     [TestFixture, Category("TravisExclude")]
     public class PythonPackagesTests
     {
+        [Test, Explicit("Needs to be run by itself to avoid hanging")]
+        public void XTransformers()
+        {
+            AssertCode(
+                @"
+import torch
+from x_transformers import XTransformer
+
+def RunTest():
+    model = XTransformer(
+        dim = 512,
+        enc_num_tokens = 256,
+        enc_depth = 6,
+        enc_heads = 8,
+        enc_max_seq_len = 1024,
+        dec_num_tokens = 256,
+        dec_depth = 6,
+        dec_heads = 8,
+        dec_max_seq_len = 1024,
+        tie_token_emb = True      # tie embeddings of encoder and decoder
+    )
+
+    src = torch.randint(0, 256, (1, 1024))
+    src_mask = torch.ones_like(src).bool()
+    tgt = torch.randint(0, 256, (1, 1024))
+
+    loss = model(src, tgt, mask = src_mask) # (1, 1024, 512)
+    loss.backward()");
+        }
+
         [Test]
         public void Functime()
         {
