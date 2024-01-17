9e24cce7dc956aef7d0a39e0f6eb9a632e744d86
Jhonathan Abreu <jdabreu25@gmail.com>
2024-01-17 08:47:44 -0400

    Backtest name and tags API calls
    Plus, first version of CI for API tests

--- Common/Api/Backtest.cs
+++ Common/Api/Backtest.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -135,5 +135,17 @@ namespace QuantConnect.Api
         /// </summary>
         [JsonProperty(PropertyName = "backtests")]
         public List<Backtest> Backtests { get; set; }
+    }
+
+    /// <summary>
+    /// Collection container for a list of backtest tags
+    /// </summary>
+    public class BacktestTags : RestResponse
+    {
+        /// <summary>
+        /// Collection of tags for a backtest
+        /// </summary>
+        [JsonProperty(PropertyName = "tags")]
+        public List<string> Tags { get; set; }
     }
 }
