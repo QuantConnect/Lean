65379f6badb5a293223fb4da848ae6790fb67919
Jhonathan Abreu <jdabreu25@gmail.com>
2024-01-26 10:16:38 -0400

    Minor fix

--- Tests/Common/Brokerages/BrokerageModelTests.cs
+++ Tests/Common/Brokerages/BrokerageModelTests.cs
@@ -410,6 +410,9 @@ class CustomSettlementModel:
     def Scan(self, parameters):
         raise ValueError(""Pepe2"")
 
+    def HasUnsettledFunds(self):
+        raise ValueError(""Pepe3"")
+
 class CustomBrokerageModel(DefaultBrokerageModel):
     def GetSettlementModel(self, securities):
         return CustomSettlementModel()
@@ -429,6 +432,9 @@ class CustomBrokerageModel(DefaultBrokerageModel):
                 ex = Assert.Throws<PythonException>(() => ((dynamic)settlementModel).Scan(scanParameters));
                 Assert.AreEqual("ValueError", ex.Type.Name);
                 Assert.AreEqual("Pepe2", ex.Message);
+                ex = Assert.Throws<PythonException>(() => ((dynamic)settlementModel).HasUnsettledFunds());
+                Assert.AreEqual("ValueError", ex.Type.Name);
+                Assert.AreEqual("Pepe3", ex.Message);
             }
         }
 
