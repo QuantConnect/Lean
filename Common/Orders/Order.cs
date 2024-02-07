0264d346e6c1d7780f1ffb83e31ccca880d38d35
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2024-02-07 15:50:03 -0500

    Add obsolete flag (#7768)

--- Common/Orders/Order.cs
+++ Common/Orders/Order.cs
@@ -165,9 +165,9 @@ namespace QuantConnect.Orders
         public decimal AbsoluteQuantity => Math.Abs(Quantity);
 
         /// <summary>
-        /// Gets the executed value of this order. If the order has not yet filled,
-        /// then this will return zero.
+        /// Deprecated
         /// </summary>
+        [Obsolete("Please use Order.GetValue(security) or security.Holdings.HoldingsValue")]
         public decimal Value => Quantity * Price;
 
         /// <summary>
