ae54edd96a20472537a30b338ba8caa68f929233
Jhonathan Abreu <jdabreu25@gmail.com>
2024-02-15 12:23:11 -0400

    Minor update

--- Algorithm.CSharp/RegressionTests/Universes/ETFConstituentUniverseMappedCompositeRegressionAlgorithm.cs
+++ Algorithm.CSharp/RegressionTests/Universes/ETFConstituentUniverseMappedCompositeRegressionAlgorithm.cs
@@ -157,7 +157,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 801;
+        public long DataPoints => 626;
 
         /// <summary>
         /// Data Points count of the algorithm history
