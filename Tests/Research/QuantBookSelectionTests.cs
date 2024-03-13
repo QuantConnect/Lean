9c5b3d851e9dd27749882f6d83f95545be6c44ab
Martin Molinero <martin.molinero1@gmail.com>
2024-03-13 10:41:23 -0300

    Backwards compatibility research universe history
    - Add backwards compatibility for research universe history api, adding
      unit tests

--- Tests/Research/QuantBookSelectionTests.cs
+++ Tests/Research/QuantBookSelectionTests.cs
@@ -151,6 +151,116 @@ class Test():
             }
         }
 
+        [TestCase(Language.CSharp)]
+        [TestCase(Language.Python)]
+        public void UniverseSelectionData_BackwardsCompatibility(Language language)
+        {
+            if (language == Language.CSharp)
+            {
+                var history = _qb.UniverseHistory<Fundamentals, Fundamental>(_start, _end).ToList();
+
+                // we asked for 2 weeks, 5 work days for each week expected
+                Assert.AreEqual(10, history.Count);
+                Assert.IsTrue(history.All(x => x.Count() > 7000));
+                Assert.IsTrue(history.All(x => x.All(fundamental => fundamental.GetType() == typeof(Fundamental))));
+            }
+            else
+            {
+                using (Py.GIL())
+                {
+                    var testModule = PyModule.FromString("testModule",
+                    @"
+from AlgorithmImports import *
+
+def getUniverseHistory(qb, start, end):
+    return qb.UniverseHistory(Fundamentals, start, end)
+                    ");
+
+                    dynamic getUniverse = testModule.GetAttr("getUniverseHistory");
+                    var pyHistory = getUniverse(_qb, _start, _end);
+
+                    Assert.AreEqual(10, pyHistory.__len__().AsManagedObject(typeof(int)));
+
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var index = pyHistory.index[i];
+                        var type = typeof(List<Fundamental>);
+                        var fundamental = (List<Fundamental>)pyHistory.loc[index].AsManagedObject(type);
+
+                        Assert.GreaterOrEqual(fundamental.Count, 7000);
+                    }
+                }
+            }
+        }
+
+        [TestCase(Language.CSharp, false)]
+        [TestCase(Language.Python, false)]
+        [TestCase(Language.CSharp, true)]
+        [TestCase(Language.Python, true)]
+        public void UniverseSelection_BackwardsCompatibility(Language language, bool useUniverseUnchanged)
+        {
+            var selectionState = false;
+            if (language == Language.CSharp)
+            {
+                var history = _qb.UniverseHistory<Fundamentals, Fundamental>(_start, _end, (fundamental) =>
+                {
+                    if (!useUniverseUnchanged || !selectionState)
+                    {
+                        selectionState = true;
+                        return new[] { Symbols.AAPL };
+                    }
+                    // after the first call we will return 'unchanged' if 'useUniverseUnchanged' is true
+                    return Universe.Unchanged;
+                }).ToList();
+
+                // we asked for 2 weeks, 5 work days for each week expected
+                Assert.AreEqual(10, history.Count);
+                Assert.IsTrue(history.All(x => x.Count() == 1));
+                Assert.IsTrue(history.All(x => x.Single().Symbol == Symbols.AAPL));
+                Assert.IsTrue(history.All(x => x.All(fundamental => fundamental.GetType() == typeof(Fundamental))));
+            }
+            else
+            {
+                using (Py.GIL())
+                {
+                    dynamic testModule = PyModule.FromString("testModule",
+                    @"
+from AlgorithmImports import *
+
+class Test():
+    def __init__(self, useUniverseUnchanged):
+        self.useUniverseUnchanged = useUniverseUnchanged
+        self.state = False
+
+    def selection(self, fundamentals):
+        if not self.useUniverseUnchanged or not self.state:
+            self.state = True
+            return [ x.Symbol for x in fundamentals if x.Symbol.Value == ""AAPL"" ]
+        return Universe.Unchanged
+
+    def getUniverseHistory(self, qb, start, end):
+        return qb.UniverseHistory(Fundamentals, start, end, self.selection)
+").GetAttr("Test");
+
+                    var instance = testModule(useUniverseUnchanged);
+                    var pyHistory = instance.getUniverseHistory(_qb, _start, _end);
+
+                    Assert.AreEqual(10, pyHistory.__len__().AsManagedObject(typeof(int)));
+
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var index = pyHistory.index[i];
+                        var series = pyHistory.loc[index];
+                        var type = typeof(Fundamental[]);
+                        var fundamental = (Fundamental[])series.AsManagedObject(type);
+
+                        Assert.GreaterOrEqual(fundamental.Length, 1);
+                        Assert.AreEqual(Symbols.AAPL, fundamental[0].Symbol);
+                    }
+                }
+            }
+        }
+
         private static string GetBaseImplementation(int expectedCount, string identation)
         {
             return @"
