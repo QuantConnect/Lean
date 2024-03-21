89904042da51d48dc80fb57e463a33483448f773
KMichaylov <krisidm24@gmail.com>
2024-03-21 20:00:39 +0100

    Add foundation for unit tests

---
+++ Tests/Indicators/DerivativeIndicatorOscillatorTest.cs
@@ -0,0 +1,17 @@
+﻿using QuantConnect.Indicators;
+
+namespace QuantConnect.Tests.Indicators;
+
+public class DerivativeIndicatorOscillatorTest : CommonIndicatorTests<IndicatorDataPoint>
+{
+    protected override IndicatorBase<IndicatorDataPoint> CreateIndicator()
+    {
+        return new IndicatorDerivativeOscillator("DO14", 14,5,3,9);
+    }
+
+    protected override string TestFileName => "spy_derivative_oscillator_daily_14_5_3_9.txt";
+
+    protected override string TestColumnName => "DO14";
+    
+    
+}
