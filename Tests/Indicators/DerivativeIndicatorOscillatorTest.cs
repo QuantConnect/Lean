42e845ad1a005c13b3341962b2fbe0267b7c3395
KMichaylov <krisidm24@gmail.com>
2024-03-24 20:16:22 +0100

    Add reset method for test

--- Tests/Indicators/DerivativeIndicatorOscillatorTest.cs
+++ Tests/Indicators/DerivativeIndicatorOscillatorTest.cs
@@ -1,4 +1,5 @@
-﻿using QuantConnect.Indicators;
+﻿using NUnit.Framework;
+using QuantConnect.Indicators;
 
 namespace QuantConnect.Tests.Indicators;
 
@@ -6,12 +7,35 @@ public class DerivativeIndicatorOscillatorTest : CommonIndicatorTests<IndicatorD
 {
     protected override IndicatorBase<IndicatorDataPoint> CreateIndicator()
     {
-        return new IndicatorDerivativeOscillator("DO14", 14,5,3,9);
+        return new IndicatorDerivativeOscillator("DO14", 14, 5, 3, 9);
     }
 
     protected override string TestFileName => "spy_derivative_oscillator_daily_14_5_3_9.txt";
 
     protected override string TestColumnName => "DO14";
-    
-    
+
+    [Test]
+    public void DoComputesCorrectly()
+    {
+        
+    }
+
+    public override void ResetsProperly()
+    {
+        // derivativeOscillator reset is just setting the value and samples back to 0
+        var derivativeOscillator = new IndicatorDerivativeOscillator("D014", 14, 5, 3, 9);
+
+        foreach (var data in TestHelper.GetDataStream(5))
+        {
+            derivativeOscillator.Update(data);
+        }
+
+        Assert.IsTrue(derivativeOscillator.IsReady);
+        Assert.AreNotEqual(0m, derivativeOscillator.Current.Value);
+        Assert.AreNotEqual(0, derivativeOscillator.Samples);
+
+        derivativeOscillator.Reset();
+
+        TestHelper.AssertIndicatorIsInDefaultState(derivativeOscillator);
+    }
 }
