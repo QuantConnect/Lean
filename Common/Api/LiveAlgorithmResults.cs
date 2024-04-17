622261ad06731d76586fb50b3e9a2a34be5000fc
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2024-04-17 09:00:46 -0500

    Add XML comments

--- Common/Api/LiveAlgorithmResults.cs
+++ Common/Api/LiveAlgorithmResults.cs
@@ -26,39 +26,75 @@ namespace QuantConnect.Api
     /// </summary>
     public class LiveAlgorithmResults : RestResponse
     {
+        /// <summary>
+        /// Algorithm message
+        /// </summary>
         [JsonProperty(PropertyName = "message")]
         public string Message { get; set; }
 
+        /// <summary>
+        /// Indicates the status of the algorihtm, i.e. 'Running', 'Stopped'
+        /// </summary>
         [JsonProperty(PropertyName = "status")]
         public string Status { get; set; }
 
+        /// <summary>
+        /// Algorithm deployment ID
+        /// </summary>
         [JsonProperty(PropertyName = "deployId")]
         public string DeployId { get; set; }
 
+        /// <summary>
+        /// The ID of the project this live algorithm was cloned
+        /// </summary>
         [JsonProperty(PropertyName = "cloneId")]
         public int CloneId { get; set; }
 
+        /// <summary>
+        /// Date the live algorithm was launched
+        /// </summary>
         [JsonProperty(PropertyName = "launched")]
         public DateTime Launched { get; set; }
 
+        /// <summary>
+        /// Date the live algorithm was stopped
+        /// </summary>
         [JsonProperty(PropertyName = "stopped")]
         public DateTime? Stopped { get; set; }
 
+        /// <summary>
+        /// Brokerage used in the live algorithm
+        /// </summary>
         [JsonProperty(PropertyName = "brokerage")]
         public string Brokerage { get; set; }
 
+        /// <summary>
+        /// Security types present in the live algorithm
+        /// </summary>
         [JsonProperty(PropertyName = "securityTypes")]
         public string SecurityTypes { get; set; }
 
+        /// <summary>
+        /// Name of the project the live algorithm is in
+        /// </summary>
         [JsonProperty(PropertyName = "projectName")]
         public string ProjectName { get; set; }
 
+        /// <summary>
+        /// Name of the data center
+        /// </summary>
         [JsonProperty(PropertyName = "datacenter")]
         public string Datacenter { get; set; }
 
+        /// <summary>
+        /// Indicates if the live algorithm is streaming to the public
+        /// </summary>
         [JsonProperty(PropertyName = "isPublicStreaming")]
         public bool IsPublicStreaming { get; set; }
 
+        /// <summary>
+        /// Files present in the project in which the algorithm is
+        /// </summary>
         [JsonProperty(PropertyName = "files")]
         public List<ProjectFile> Files { get; set; }
 
