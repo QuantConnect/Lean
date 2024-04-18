86b471083b485065d72f66ea1e8ca1b95f60d198
Jhonathan Abreu <jdabreu25@gmail.com>
2024-04-18 18:11:50 -0400

    Convert portfolio models to PEP8

--- Algorithm.Framework/Portfolio/ConfidenceWeightedPortfolioConstructionModel.py
+++ Algorithm.Framework/Portfolio/ConfidenceWeightedPortfolioConstructionModel.py
@@ -16,15 +16,15 @@ from InsightWeightingPortfolioConstructionModel import InsightWeightingPortfolio
 
 class ConfidenceWeightedPortfolioConstructionModel(InsightWeightingPortfolioConstructionModel):
     '''Provides an implementation of IPortfolioConstructionModel that generates percent targets based on the
-    Insight.Confidence. The target percent holdings of each Symbol is given by the Insight.Confidence from the last
+    Insight.CONFIDENCE. The target percent holdings of each Symbol is given by the Insight.CONFIDENCE from the last
     active Insight for that symbol.
-    For insights of direction InsightDirection.Up, long targets are returned and for insights of direction
-    InsightDirection.Down, short targets are returned.
+    For insights of direction InsightDirection.UP, long targets are returned and for insights of direction
+    InsightDirection.DOWN, short targets are returned.
     If the sum of all the last active Insight per symbol is bigger than 1, it will factor down each target
     percent holdings proportionally so the sum is 1.
-    It will ignore Insight that have no Insight.Confidence value.'''
+    It will ignore Insight that have no Insight.CONFIDENCE value.'''
 
-    def __init__(self, rebalance = Resolution.Daily, portfolioBias = PortfolioBias.LongShort):
+    def __init__(self, rebalance = Resolution.DAILY, portfolio_bias = PortfolioBias.LONG_SHORT):
         '''Initialize a new instance of ConfidenceWeightedPortfolioConstructionModel
         Args:
             rebalance: Rebalancing parameter. If it is a timedelta, date rules or Resolution, it will be converted into a function.
@@ -32,21 +32,21 @@ class ConfidenceWeightedPortfolioConstructionModel(InsightWeightingPortfolioCons
                               The function returns the next expected rebalance time for a given algorithm UTC DateTime.
                               The function returns null if unknown, in which case the function will be called again in the
                               next loop. Returning current time will trigger rebalance.
-            portfolioBias: Specifies the bias of the portfolio (Short, Long/Short, Long)'''
-        super().__init__(rebalance, portfolioBias)
+            portfolio_bias: Specifies the bias of the portfolio (Short, Long/Short, Long)'''
+        super().__init__(rebalance, portfolio_bias)
 
-    def ShouldCreateTargetForInsight(self, insight):
+    def should_create_target_for_insight(self, insight):
         '''Method that will determine if the portfolio construction model should create a
         target for this insight
         Args:
             insight: The insight to create a target for'''
         # Ignore insights that don't have Confidence value
-        return insight.Confidence is not None
+        return insight.confidence is not None
 
-    def GetValue(self, insight):
+    def get_value(self, insight):
         '''Method that will determine which member will be used to compute the weights and gets its value
         Args:
             insight: The insight to create a target for
         Returns:
             The value of the selected insight member'''
-        return insight.Confidence
+        return insight.confidence
