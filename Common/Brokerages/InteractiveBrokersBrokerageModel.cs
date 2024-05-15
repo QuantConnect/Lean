597c414baee5288954efa50e73dee4f02ee92575
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2024-05-15 13:47:25 -0500

    Nit changes

--- Common/Brokerages/InteractiveBrokersBrokerageModel.cs
+++ Common/Brokerages/InteractiveBrokersBrokerageModel.cs
@@ -145,13 +145,13 @@ namespace QuantConnect.Brokerages
 
                 return false;
             }
-            else if (order.Type == OrderType.MarketOnClose && (security.Type != SecurityType.Future && security.Type != SecurityType.Equity))
+            else if (order.Type == OrderType.MarketOnClose && security.Type != SecurityType.Future && security.Type != SecurityType.Equity)
             {
                 message = new BrokerageMessageEvent(BrokerageMessageType.Warning, $"Unsupported order type for {security.Type} security type",
                     "InteractiveBrokers does not support Market-on-Close orders for other security types different than: Futures and Equity.");
                 return false;
             }
-            else if (order.Type == OrderType.MarketOnOpen && (security.Type != SecurityType.Equity && !security.Type.IsOption()))
+            else if (order.Type == OrderType.MarketOnOpen && security.Type != SecurityType.Equity && !security.Type.IsOption())
             {
                 message = new BrokerageMessageEvent(BrokerageMessageType.Warning, $"Unsupported order type for {security.Type} security type",
                     "InteractiveBrokers does not support Market-on-Open orders for other security types different than: Equity and Options.");
