232fdc6c7cb0237bd354238ff67f357d7238d186
Romazes <romazes20@gmail.com>
2024-05-17 01:49:16 +0300

    feat: missed market TradeStation name

--- Common/Market.cs
+++ Common/Market.cs
@@ -69,6 +69,7 @@ namespace QuantConnect
             Tuple.Create(Bybit, 37),
             Tuple.Create(Coinbase, 38),
             Tuple.Create(InteractiveBrokers, 39),
+            Tuple.Create(TradeStation, 40),
         };
 
         static Market()
@@ -248,6 +249,11 @@ namespace QuantConnect
         /// InteractiveBrokers market
         /// </summary>
         public const string InteractiveBrokers = "interactivebrokers";
+
+        /// <summary>
+        /// Trade Station
+        /// </summary>
+        public const string TradeStation = "tradestation";
 
         /// <summary>
         /// Adds the specified market to the map of available markets with the specified identifier.
