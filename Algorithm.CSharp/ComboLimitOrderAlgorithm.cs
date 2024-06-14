4edf4e2e637b8ae546c9d519fc9d97b6891b25bd
Jhonathan Abreu <jdabreu25@gmail.com>
2024-06-14 17:54:40 -0400

    Updated options regression algorithms stats for new universe data

--- Algorithm.CSharp/ComboLimitOrderAlgorithm.cs
+++ Algorithm.CSharp/ComboLimitOrderAlgorithm.cs
@@ -136,7 +136,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public override long DataPoints => 471135;
+        public override long DataPoints => 15025;
 
         /// <summary>
         /// Data Points count of the algorithm history
