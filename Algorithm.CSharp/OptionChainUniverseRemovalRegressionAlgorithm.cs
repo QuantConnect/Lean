3ef8e0d16f9ed7c3d7a9e6075dfce950bf9a83eb
Jhonathan Abreu <jdabreu25@gmail.com>
2024-06-28 12:49:52 -0400

    Fix universe selection subscriptions start time to not include extended market hours

--- Algorithm.CSharp/OptionChainUniverseRemovalRegressionAlgorithm.cs
+++ Algorithm.CSharp/OptionChainUniverseRemovalRegressionAlgorithm.cs
@@ -204,7 +204,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 18266;
+        public long DataPoints => 17966;
 
         /// <summary>
         /// Data Points count of the algorithm history
