6c30157fabceee795f1c1fc22ab86dbc4c47a13d
Martin-Molinero <martin@quantconnect.com>
2024-07-08 11:58:50 -0300

    Remove universe selection on extended market dates (#8160)
    - Remove universe selection on dates with extended market hours only
    - Updating regression algorithms
    - Expand date & time rules API to support specifying whether extended
      market hours only dates are desired or not

--- Algorithm.CSharp/BasicTemplateFuturesWithExtendedMarketDailyAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateFuturesWithExtendedMarketDailyAlgorithm.cs
@@ -43,7 +43,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public override long DataPoints => 16265;
+        public override long DataPoints => 16161;
 
         /// <summary>
         /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
