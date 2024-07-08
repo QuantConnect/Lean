5c577173424f5ae24760f9d915e70844a413d085
Jhonathan Abreu <jdabreu25@gmail.com>
2024-07-08 16:29:03 -0400

    Update regression algorithm stats

--- Algorithm.CSharp/OptionEquityJellyRollRegressionAlgorithm.cs
+++ Algorithm.CSharp/OptionEquityJellyRollRegressionAlgorithm.cs
@@ -89,7 +89,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public override long DataPoints => 471135;
+        public override long DataPoints => 15023;
 
         /// <summary>
         /// Data Points count of the algorithm history
@@ -124,7 +124,7 @@ namespace QuantConnect.Algorithm.CSharp
             {"Tracking Error", "0"},
             {"Treynor Ratio", "0"},
             {"Total Fees", "$4.00"},
-            {"Estimated Strategy Capacity", "$160000.00"},
+            {"Estimated Strategy Capacity", "$180000.00"},
             {"Lowest Capacity Asset", "GOOCV W78ZERHAOVVQ|GOOCV VP83T1ZUHROL"},
             {"Portfolio Turnover", "4.70%"},
             {"OrderListHash", "34f1fe90151a1fc5dff43cd7b1205861"}
