88f04e4da6337b46d8d75c07622feef056331728
Jhonathan Abreu <jdabreu25@gmail.com>
2024-07-09 12:00:21 -0400

    Fix option chain provider history request

--- Algorithm.CSharp/IndexOptionPutOTMExpiryRegressionAlgorithm.cs
+++ Algorithm.CSharp/IndexOptionPutOTMExpiryRegressionAlgorithm.cs
@@ -174,7 +174,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of the algorithm history
         /// </summary>
-        public int AlgorithmHistoryDataPoints => 12;
+        public int AlgorithmHistoryDataPoints => 1;
 
         /// <summary>
         /// Final status of the algorithm
