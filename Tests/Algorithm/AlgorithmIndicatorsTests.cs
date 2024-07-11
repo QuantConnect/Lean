ee039901bb7dd1a91c45cfcc42dfe8e5d4b14531
Jhonathan Abreu <jdabreu25@gmail.com>
2024-07-11 18:20:11 -0400

    Unit test fixes

--- Tests/Algorithm/AlgorithmIndicatorsTests.cs
+++ Tests/Algorithm/AlgorithmIndicatorsTests.cs
@@ -387,9 +387,9 @@ class CustomSimpleMovingAverage(PythonIndicator):
             }
 
             Assert.IsTrue(indicator.IsReady);
-            Assert.AreEqual(0.994298416889621m, indicator.Current.Value);
-            Assert.AreEqual(0.351654399192164m, indicator.ImpliedVolatility.Current.Value);
-            Assert.AreEqual(390, indicatorValues.Count);
+            Assert.AreEqual(0.9942984m, indicator.Current.Value);
+            Assert.AreEqual(0.3516544m, indicator.ImpliedVolatility.Current.Value);
+            Assert.AreEqual(389, indicatorValues.Count);
 
             var lastData = indicatorValues.Current.Last();
             Assert.AreEqual(new DateTime(2014, 6, 6, 16, 0, 0), lastData.EndTime);
