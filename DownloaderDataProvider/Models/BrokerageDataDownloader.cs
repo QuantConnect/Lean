544dddff03834a84b43bf3b6ed92bf101a2fd5f5
Romazes <romazes20@gmail.com>
2024-07-31 01:44:22 +0300

    refactor: initialize in BrokerageDataDownloader

--- DownloaderDataProvider/Models/BrokerageDataDownloader.cs
+++ DownloaderDataProvider/Models/BrokerageDataDownloader.cs
@@ -15,9 +15,10 @@
 
 using QuantConnect.Util;
 using QuantConnect.Data;
-using QuantConnect.Queues;
+using QuantConnect.Packets;
 using QuantConnect.Interfaces;
 using QuantConnect.Securities;
+using QuantConnect.Configuration;
 
 namespace QuantConnect.DownloaderDataProvider.Launcher.Models
 {
@@ -35,13 +36,25 @@ namespace QuantConnect.DownloaderDataProvider.Launcher.Models
         /// Provides access to exchange hours and raw data times zones in various markets
         /// </summary>
         private readonly MarketHoursDatabase _marketHoursDatabase = MarketHoursDatabase.FromDataFolder();
-        
+
         /// <summary>
         /// Initializes a new instance of the <see cref="BrokerageDataDownloader"/> class.
         /// </summary>
         public BrokerageDataDownloader()
         {
-            var liveNodeConfiguration = JobQueueExtensions.GetLiveNodeConfigurations();
+            var liveNodeConfiguration = new LiveNodePacket() { Brokerage = Config.Get("data-downloader-brokerage") };
+
+            try
+            {
+                // import the brokerage data for the configured brokerage
+                var brokerageFactory = Composer.Instance.Single<IBrokerageFactory>(factory => factory.BrokerageType.MatchesTypeName(liveNodeConfiguration.Brokerage));
+                liveNodeConfiguration.BrokerageData = brokerageFactory.BrokerageData;
+            }
+            catch (InvalidOperationException error)
+            {
+                throw new InvalidOperationException($"{nameof(BrokerageDataDownloader)}.An error occurred while resolving brokerage data for a live job. Brokerage: {liveNodeConfiguration.Brokerage}.", error);
+            }
+
             _brokerage = Composer.Instance.GetExportedValueByTypeName<IBrokerage>(liveNodeConfiguration.Brokerage);
             ((IDataQueueHandler)_brokerage).SetJob(liveNodeConfiguration);
         }
@@ -63,7 +76,7 @@ namespace QuantConnect.DownloaderDataProvider.Launcher.Models
             var exchangeHours = _marketHoursDatabase.GetExchangeHours(symbol.ID.Market, symbol, symbol.SecurityType);
             var dataTimeZone = _marketHoursDatabase.GetDataTimeZone(symbol.ID.Market, symbol, symbol.SecurityType);
 
-            var historyRequest = new HistoryRequest(startUtc, endUtc, dataType, symbol, resolution, exchangeHours, dataTimeZone, resolution,
+            var historyRequest = new Data.HistoryRequest(startUtc, endUtc, dataType, symbol, resolution, exchangeHours, dataTimeZone, resolution,
                 true, false, DataNormalizationMode.Raw, tickType);
 
             var historyData = _brokerage.GetHistory(historyRequest);
