d4da6a1189239e87c3230dceb6ccfe83f256f90a
Jhonathan Abreu <jdabreu25@gmail.com>
2024-08-01 12:48:06 -0400

    Update default dividend yield provider depending on option type

--- Common/Data/DividendYieldProvider.cs
+++ Common/Data/DividendYieldProvider.cs
@@ -107,14 +107,25 @@ namespace QuantConnect.Data
         /// <summary>
         /// Creates a new instance for the given option symbol
         /// </summary>
-        public static DividendYieldProvider CreateForOption(Symbol optionSymbol)
+        public static IDividendYieldModel CreateForOption(Symbol optionSymbol)
         {
-            if (!optionSymbol.SecurityType.IsOption() || optionSymbol.SecurityType == SecurityType.Option)
+            if (optionSymbol.SecurityType == SecurityType.Option)
             {
                 return new DividendYieldProvider(optionSymbol.Underlying);
             }
 
-            return new DividendYieldProvider();
+            if (optionSymbol.SecurityType == SecurityType.IndexOption)
+            {
+                return optionSymbol.Value switch
+                {
+                    "SPX" => new DividendYieldProvider(Symbol.Create("SPY", SecurityType.Equity, QuantConnect.Market.USA)),
+                    "NDX" => new DividendYieldProvider(Symbol.Create("QQQ", SecurityType.Equity, QuantConnect.Market.USA)),
+                    "VIX" => new ConstantDividendYieldModel(0),
+                    _ => new DividendYieldProvider()
+                };
+            }
+
+            return new ConstantDividendYieldModel(0);
         }
 
         /// <summary>
