93590a0e68fda36e992ad40be15d4ef0d1cf550e
Alain Schaerer <aschaerer@pcatg.com>
2024-08-14 19:08:20 -0400

    Add failing test

--- Tests/Common/Statistics/MaximumRecoveryTests.cs
+++ Tests/Common/Statistics/MaximumRecoveryTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using QLNet;
 using QuantConnect.Statistics;
 
 namespace QuantConnect.Tests.Common.Statistics
@@ -25,6 +26,31 @@ namespace QuantConnect.Tests.Common.Statistics
         [Test]
         public void MaximumRecoveryTests_NoRecovery_ReturnZero()
         {
+            var startDate = DateTime.MinValue;
+            var equityOverTime = new SortedDictionary<DateTime, decimal>
+            {
+                { startDate, 100 },
+                { startDate.AddDays(1), 90 },
+                { startDate.AddDays(2), 98 },
+                { startDate.AddDays(3), 99 }
+            };
+            var maximumRecoveryTime = QuantConnect.Statistics.Statistics.MaximumRecoveryTime(equityOverTime);
+            Assert.AreEqual(TimeSpan.Zero, maximumRecoveryTime);
+        }
+
+        [Test]
+        public void MaximumRecoveryTests_BasicRecoveryWithin_ReturnRecoveryTime()
+        {
+            var startDate = DateTime.MinValue;
+            var equityOverTime = new SortedDictionary<DateTime, decimal>
+            {
+                { startDate, 100 },
+                { startDate.AddDays(1), 99 },
+                { startDate.AddDays(2), 98 },
+                { startDate.AddDays(3), 99 }
+            };
+            var maximumRecoveryTime = QuantConnect.Statistics.Statistics.MaximumRecoveryTime(equityOverTime);
+            Assert.AreEqual(TimeSpan.FromDays(1), maximumRecoveryTime);
 
         }
 
