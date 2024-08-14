434b80e10421e20f0ba1b74e2945e432c1842aad
Jhonathan Abreu <jdabreu25@gmail.com>
2024-08-14 15:33:54 -0400

    Regression tests updates after rebasing from master

--- Algorithm.CSharp/IndexOptionCallOTMExpiryDailyRegressionAlgorithm.cs
+++ Algorithm.CSharp/IndexOptionCallOTMExpiryDailyRegressionAlgorithm.cs
@@ -43,7 +43,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of the algorithm history
         /// </summary>
-        public override int AlgorithmHistoryDataPoints => 2;
+        public override int AlgorithmHistoryDataPoints => 1;
 
         /// <summary>
         /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
