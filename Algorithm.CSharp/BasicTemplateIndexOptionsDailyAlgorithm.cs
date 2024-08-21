8633714bd03370489be22770151a8a77f3deb159
Jhonathan Abreu <jdabreu25@gmail.com>
2024-08-21 17:19:00 -0400

    Update index options regression algorithms stats

--- Algorithm.CSharp/BasicTemplateIndexOptionsDailyAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateIndexOptionsDailyAlgorithm.cs
@@ -67,7 +67,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public override long DataPoints => 326;
+        public override long DataPoints => 356;
 
         /// <summary>
         /// Data Points count of the algorithm history
