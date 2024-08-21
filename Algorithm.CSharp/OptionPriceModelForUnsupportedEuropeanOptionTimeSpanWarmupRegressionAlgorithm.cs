3394b5dc52b2e01f7b3d550ccdc62661783aeeb9
Jhonathan Abreu <jdabreu25@gmail.com>
2024-08-21 17:19:00 -0400

    Update index options regression algorithms stats

--- Algorithm.CSharp/OptionPriceModelForUnsupportedEuropeanOptionTimeSpanWarmupRegressionAlgorithm.cs
+++ Algorithm.CSharp/OptionPriceModelForUnsupportedEuropeanOptionTimeSpanWarmupRegressionAlgorithm.cs
@@ -36,7 +36,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public override long DataPoints => 615;
+        public override long DataPoints => 639;
 
         /// <summary>
         /// Final status of the algorithm
