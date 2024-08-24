2b0bb2adcc4800c411b947777c9878cdc2f58701
Alain Schaerer <aschaerer@pcatg.com>
2024-08-24 12:34:29 -0400

    Issue #4581: Change algorithm so that when multiple maximum drawdowns occur, the longest of all recoveries is reported.

--- Common/Statistics/DrawdownDrawdownDateHighValueDTO.cs
+++ Common/Statistics/DrawdownDrawdownDateHighValueDTO.cs
@@ -9,13 +9,13 @@ namespace QuantConnect.Statistics
     internal class DrawdownDrawdownDateHighValueDTO
     {
         public decimal DrawdownPercent { get; }
-        public DateTime MaxDrawdownEndDate { get; }
+        public List<DateTime> MaxDrawdownEndDates { get; }
         public decimal HighPrice { get; }
 
-        public DrawdownDrawdownDateHighValueDTO(decimal drawdownPercent, DateTime maxDrawdownEndDate, decimal recoveryThresholdPrice)
+        public DrawdownDrawdownDateHighValueDTO(decimal drawdownPercent, List<DateTime> maxDrawdownEndDates, decimal recoveryThresholdPrice)
         {
             DrawdownPercent = drawdownPercent;
-            MaxDrawdownEndDate = maxDrawdownEndDate;
+            MaxDrawdownEndDates = maxDrawdownEndDates;
             HighPrice = recoveryThresholdPrice;
         }
     }
