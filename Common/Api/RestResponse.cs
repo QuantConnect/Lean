99ce7e88aa5aabaefa41fb671d0a47efe8f20052
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2024-10-03 17:56:29 -0500

    Nit change

--- Common/Api/RestResponse.cs
+++ Common/Api/RestResponse.cs
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-using Newtonsoft.Json;
+
 using System.Collections.Generic;
 
 namespace QuantConnect.Api
