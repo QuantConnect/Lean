cb255607ad8f84ee0401ec2d5edf5c74b97435a2
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2024-10-08 12:41:12 -0500

    Enhance implementation

--- Algorithm.Framework/Selection/OpenInterestFutureUniverseSelectionModel.cs
+++ Algorithm.Framework/Selection/OpenInterestFutureUniverseSelectionModel.cs
@@ -75,13 +75,7 @@ namespace QuantConnect.Algorithm.Framework.Selection
         protected override FutureFilterUniverse Filter(FutureFilterUniverse filter)
         {
             // Remove duplicated keys
-            var newFilter = filter;
-            var distinct = filter.Data.DistinctBy(x => x.Value);
-            if (filter.Data.Count() != distinct.Count())
-            {
-                newFilter = new FutureFilterUniverse(distinct, filter.LocalTime);
-            }
-            return newFilter.Contracts(FilterByOpenInterest(newFilter.ToDictionary(x => x, x => _marketHoursDatabase.GetEntry(x.ID.Market, x, x.ID.SecurityType))));
+            return filter.Contracts(FilterByOpenInterest(filter.DistinctBy(x => x).ToDictionary(x => x, x => _marketHoursDatabase.GetEntry(x.ID.Market, x, x.ID.SecurityType))));
         }
 
         /// <summary>
