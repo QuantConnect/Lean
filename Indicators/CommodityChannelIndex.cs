894a60eed263b9467dfeac98febf5b5b691df40c
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2024-11-07 09:07:00 -0500

    Use SafeDivision() method to avoid overflow exception

--- Indicators/CommodityChannelIndex.cs
+++ Indicators/CommodityChannelIndex.cs
@@ -106,7 +106,7 @@ namespace QuantConnect.Indicators
                 return 0.0m;
             }
 
-            return (typicalPrice - TypicalPriceAverage.Current.Value) / weightedMeanDeviation;
+            return Extensions.SafeDivision(typicalPrice - TypicalPriceAverage.Current.Value, weightedMeanDeviation, Current.Value);
         }
 
         /// <summary>
