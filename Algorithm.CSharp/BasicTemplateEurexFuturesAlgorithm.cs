7eea365aa17dde1556630cec4b8c1118319930cc
Jhonathan Abreu <jdabreu25@gmail.com>
2024-11-25 10:41:02 -0400

    Let delistings through for internal subscriptions (#8398)
    * Let delistings through for internal subscriptions
    
    Without it, previously mapped continuous future contracts for which a position is open never received a delisting event for them to be liquidated and marked as non-tradable
    
    * Update regression algorithm stats
    
    * Revert/remove transaction handler initialization parameters DTO class
    
    * Cleanup

--- Algorithm.CSharp/BasicTemplateEurexFuturesAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateEurexFuturesAlgorithm.cs
@@ -190,7 +190,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 133945;
+        public long DataPoints => 133947;
 
         /// <summary>
         /// Data Points count of the algorithm history
