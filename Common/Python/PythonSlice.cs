48610e50260aa5e6fff8f58a585e00f1239f9cd5
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-03 14:34:26 -0400

    Remove PythonSlice Data Python class
    Pythonnet handles dynamic objects behavior

--- Common/Python/PythonSlice.cs
+++ Common/Python/PythonSlice.cs
@@ -27,34 +27,6 @@ namespace QuantConnect.Python
     public class PythonSlice : Slice
     {
         private readonly Slice _slice;
-        private static readonly PyObject _converter;
-
-        static PythonSlice()
-        {
-            using (Py.GIL())
-            {
-                // Python Data class: Converts custom data (PythonData) into a python object'''
-                _converter = PyModule.FromString("converter",
-                    "from clr import AddReference\n" +
-                    "AddReference(\"Python.Runtime\")\n" +
-                    "from Python.Runtime import Util\n" +
-
-                    "class Data(object):\n" +
-                    "    def __init__(self, data):\n" +
-                    "        self.data = data\n" +
-                    "        members = [attr for attr in dir(data) if not callable(attr) and not attr.startswith(\"__\")]\n" +
-                    "        for member in members:\n" +
-                    "            setattr(self, member, getattr(data, member))\n" +
-                    "        for kvp in data.GetStorageDictionary():\n" +
-                    "           name = kvp.Key.replace('-', ' ').replace('.', ' ').title().replace(' ', '')\n" +
-                    "           snake_name = Util.ToSnakeCase(name)\n" +
-                    "           setattr(self, name, kvp.Value)\n" +
-                    "           setattr(self, snake_name, kvp.Value)\n" +
-
-                    "    def __str__(self):\n" +
-                    "        return self.data.ToString()");
-            }
-        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PythonSlice"/> class
@@ -127,24 +99,7 @@ namespace QuantConnect.Python
         {
             get
             {
-                var data = _slice[symbol];
-
-                var dynamicData = data as DynamicData;
-                if (dynamicData != null)
-                {
-                    try
-                    {
-                        using (Py.GIL())
-                        {
-                            return _converter.InvokeMethod("Data", new[] { dynamicData.ToPython() });
-                        }
-                    }
-                    catch
-                    {
-                        // NOP
-                    }
-                }
-                return data;
+                return _slice[symbol];
             }
         }
 
