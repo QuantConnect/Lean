e8645c018163b5973c1183556eed89c06bf2fa07
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-17 18:00:00 -0400

    Make Future universe selection file-based like option universe

--- Algorithm.CSharp/BasicTemplateFuturesFrameworkAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateFuturesFrameworkAlgorithm.cs
@@ -136,7 +136,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public virtual long DataPoints => 57759;
+        public virtual long DataPoints => 24906;
 
         /// <summary>
         /// Data Points count of the algorithm history
@@ -156,7 +156,7 @@ namespace QuantConnect.Algorithm.CSharp
             {"Total Orders", "2"},
             {"Average Win", "0%"},
             {"Average Loss", "0%"},
-            {"Compounding Annual Return", "-81.734%"},
+            {"Compounding Annual Return", "-79.830%"},
             {"Drawdown", "4.100%"},
             {"Expectancy", "0"},
             {"Start Equity", "100000"},
--- Algorithm.CSharp/BasicTemplateFuturesWithExtendedMarketDailyAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateFuturesWithExtendedMarketDailyAlgorithm.cs
@@ -36,40 +36,40 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public override long DataPoints => 14182;
+        public override long DataPoints => 9788;
 
         /// <summary>
         /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
         /// </summary>
         public override Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
         {
-            {"Total Orders", "32"},
+            {"Total Orders", "34"},
             {"Average Win", "0.33%"},
-            {"Average Loss", "-0.04%"},
-            {"Compounding Annual Return", "0.110%"},
+            {"Average Loss", "-0.03%"},
+            {"Compounding Annual Return", "0.124%"},
             {"Drawdown", "0.300%"},
-            {"Expectancy", "0.184"},
+            {"Expectancy", "0.215"},
             {"Start Equity", "1000000"},
-            {"End Equity", "1001108"},
-            {"Net Profit", "0.111%"},
-            {"Sharpe Ratio", "-1.688"},
-            {"Sortino Ratio", "-0.772"},
-            {"Probabilistic Sharpe Ratio", "14.944%"},
+            {"End Equity", "1001253.7"},
+            {"Net Profit", "0.125%"},
+            {"Sharpe Ratio", "-1.663"},
+            {"Sortino Ratio", "-0.78"},
+            {"Probabilistic Sharpe Ratio", "15.471%"},
             {"Loss Rate", "88%"},
             {"Win Rate", "12%"},
-            {"Profit-Loss Ratio", "8.47"},
+            {"Profit-Loss Ratio", "9.33"},
             {"Alpha", "-0.007"},
             {"Beta", "0.002"},
             {"Annual Standard Deviation", "0.004"},
             {"Annual Variance", "0"},
-            {"Information Ratio", "-1.353"},
+            {"Information Ratio", "-1.351"},
             {"Tracking Error", "0.089"},
-            {"Treynor Ratio", "-4.099"},
-            {"Total Fees", "$72.00"},
+            {"Treynor Ratio", "-4.101"},
+            {"Total Fees", "$76.30"},
             {"Estimated Strategy Capacity", "$0"},
             {"Lowest Capacity Asset", "ES VRJST036ZY0X"},
-            {"Portfolio Turnover", "0.87%"},
-            {"OrderListHash", "ea6fdf3133bde7063e4fc0fa809ae260"}
+            {"Portfolio Turnover", "0.92%"},
+            {"OrderListHash", "bf28172e7f5f4b046454e6c79adb0809"}
         };
     }
 }
