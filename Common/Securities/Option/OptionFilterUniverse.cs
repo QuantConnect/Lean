b7871538c09ea9fb5838b3d7d551f888adb2e46f
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-17 18:19:05 -0400

    Abstraction cleanup

--- Common/Securities/Option/OptionFilterUniverse.cs
+++ Common/Securities/Option/OptionFilterUniverse.cs
@@ -111,15 +111,6 @@ namespace QuantConnect.Securities
         }
 
         /// <summary>
-        /// Gets the symbol from the data
-        /// </summary>
-        /// <returns>The symbol that represents the datum</returns>
-        protected override Symbol GetSymbol(OptionUniverse data)
-        {
-            return data.Symbol;
-        }
-
-        /// <summary>
         /// Creates a new instance of the data type for the given symbol
         /// </summary>
         /// <returns>A data instance for the given symbol</returns>
