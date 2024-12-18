c9edca4fe5a2006e11702d1132564fc9bff91701
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-18 16:49:03 -0400

    Add FuturesChains API to QC algorithm
    Also refactor future chain provider to use the new FutureUniverse instead of zip file names

--- Common/Data/Market/FuturesContract.cs
+++ Common/Data/Market/FuturesContract.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -13,6 +13,9 @@
  * limitations under the License.
 */
 
+using QuantConnect.Interfaces;
+using QuantConnect.Python;
+using QuantConnect.Securities;
 using System;
 
 namespace QuantConnect.Data.Market
@@ -20,15 +23,21 @@ namespace QuantConnect.Data.Market
     /// <summary>
     /// Defines a single futures contract at a specific expiration
     /// </summary>
-    public class FuturesContract
+    public class FuturesContract : ISymbolProvider, ISymbol
     {
         /// <summary>
         /// Gets the futures contract's symbol
         /// </summary>
         public Symbol Symbol
         {
-            get; private set;
+            get; set;
         }
+
+        /// <summary>
+        /// The security identifier of the future contract symbol
+        /// </summary>
+        [PandasIgnore]
+        public SecurityIdentifier ID => Symbol.ID;
 
         /// <summary>
         /// Gets the underlying security's symbol
@@ -125,5 +134,62 @@ namespace QuantConnect.Data.Market
         /// A string that represents the current object.
         /// </returns>
         public override string ToString() => Symbol.Value;
+
+        /// <summary>
+        /// Updates the future contract with the new data, which can be a <see cref="Tick"/> or <see cref="TradeBar"/> or <see cref="QuoteBar"/>
+        /// </summary>
+        internal void Update(BaseData data)
+        {
+            switch (data)
+            {
+                case TradeBar tradeBar:
+                    if (tradeBar.Close != 0m)
+                    {
+                        LastPrice = tradeBar.Close;
+                        Volume = (long)tradeBar.Volume;
+                    }
+                   break;
+
+                case QuoteBar quoteBar:
+                    if (quoteBar.Ask != null && quoteBar.Ask.Close != 0m)
+                    {
+                        AskPrice = quoteBar.Ask.Close;
+                        AskSize = (long)quoteBar.LastAskSize;
+                    }
+                    if (quoteBar.Bid != null && quoteBar.Bid.Close != 0m)
+                    {
+                        BidPrice = quoteBar.Bid.Close;
+                        BidSize = (long)quoteBar.LastBidSize;
+                    }
+                    break;
+
+                case Tick tick:
+                    if (tick.TickType == TickType.Trade)
+                    {
+                        LastPrice = tick.Price;
+                    }
+                    else if (tick.TickType == TickType.Quote)
+                    {
+                        if (tick.AskPrice != 0m)
+                        {
+                            AskPrice = tick.AskPrice;
+                            AskSize = (long)tick.AskSize;
+                        }
+                        if (tick.BidPrice != 0m)
+                        {
+                            BidPrice = tick.BidPrice;
+                            BidSize = (long)tick.BidSize;
+                        }
+                    }
+                    else if (tick.TickType == TickType.OpenInterest)
+                    {
+                        if (tick.Value != 0m)
+                        {
+                            OpenInterest = tick.Value;
+                        }
+                    }
+                    break;
+            }
+        }
     }
 }
