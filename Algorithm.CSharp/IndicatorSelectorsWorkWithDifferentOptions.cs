55b951248695b8f97d3c4690430272d8b20cc198
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-24 11:26:30 -0400

    Minor fixes

--- Algorithm.CSharp/IndicatorSelectorsWorkWithDifferentOptions.cs
+++ Algorithm.CSharp/IndicatorSelectorsWorkWithDifferentOptions.cs
@@ -186,7 +186,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 454578;
+        public long DataPoints => 454503;
 
         /// <summary>
         /// Data Points count of the algorithm history
