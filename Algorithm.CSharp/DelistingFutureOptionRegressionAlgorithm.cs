bd01c40f2b8db2d47a2a907a43fd82a14566726a
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-26 14:44:33 -0400

    Reduce number of universe files in repo

--- Algorithm.CSharp/DelistingFutureOptionRegressionAlgorithm.cs
+++ Algorithm.CSharp/DelistingFutureOptionRegressionAlgorithm.cs
@@ -105,7 +105,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public virtual long DataPoints => 764120;
+        public virtual long DataPoints => 761073;
 
         /// <summary>
         /// Data Points count of the algorithm history
