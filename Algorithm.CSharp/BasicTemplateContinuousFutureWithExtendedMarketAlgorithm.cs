f082affdd58ec5ffb06a235f64285800f5c5bd76
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-26 14:44:33 -0400

    Reduce number of universe files in repo

--- Algorithm.CSharp/BasicTemplateContinuousFutureWithExtendedMarketAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateContinuousFutureWithExtendedMarketAlgorithm.cs
@@ -123,7 +123,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 505661;
+        public long DataPoints => 504483;
 
         /// <summary>
         /// Data Points count of the algorithm history
--- Algorithm.CSharp/FuturesExpiredContractRegression.cs
+++ Algorithm.CSharp/FuturesExpiredContractRegression.cs
@@ -89,7 +89,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 24974;
+        public long DataPoints => 24483;
 
         /// <summary>
         /// Data Points count of the algorithm history
