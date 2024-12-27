cbd343824df2d385de775c0c472c795bba6d99cb
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-27 08:08:31 -0400

    Minor data fixes

--- Algorithm.CSharp/ContinuousFutureRegressionAlgorithm.cs
+++ Algorithm.CSharp/ContinuousFutureRegressionAlgorithm.cs
@@ -173,7 +173,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 162530;
+        public long DataPoints => 162555;
 
         /// <summary>
         /// Data Points count of the algorithm history
