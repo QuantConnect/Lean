80fdfde5e368f94eeb2df08dcc1211a928180ebb
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-27 08:08:31 -0400

    Minor data fixes

--- Algorithm.CSharp/LimitOrdersAreFilledAfterHoursForFuturesRegressionAlgorithm.cs
+++ Algorithm.CSharp/LimitOrdersAreFilledAfterHoursForFuturesRegressionAlgorithm.cs
@@ -113,7 +113,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 52982;
+        public long DataPoints => 52987;
 
         /// <summary>
         /// Data Points count of the algorithm history
