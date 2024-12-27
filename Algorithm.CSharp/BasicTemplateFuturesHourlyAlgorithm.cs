04b49c50a37cf01f7f39d132ed9fa070d5474dde
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-27 08:08:31 -0400

    Minor data fixes

--- Algorithm.CSharp/BasicTemplateFuturesHourlyAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateFuturesHourlyAlgorithm.cs
@@ -36,7 +36,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public override long DataPoints => 25281;
+        public override long DataPoints => 25296;
 
         /// <summary>
         /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
