4d2487feeed03172a2c58457a790f28e08e5a89e
Jhonathan Abreu <jdabreu25@gmail.com>
2024-12-27 08:08:31 -0400

    Minor data fixes

--- Algorithm.CSharp/HistoryWithDifferentDataMappingModeRegressionAlgorithm.cs
+++ Algorithm.CSharp/HistoryWithDifferentDataMappingModeRegressionAlgorithm.cs
@@ -101,7 +101,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 953;
+        public long DataPoints => 968;
 
         /// <summary>
         /// Data Points count of the algorithm history
