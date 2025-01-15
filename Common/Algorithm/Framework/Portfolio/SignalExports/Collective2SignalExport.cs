54510e411d16ff4ffa054298ef60d6d354a68ccf
Jhonathan Abreu <jdabreu25@gmail.com>
2025-01-15 15:51:34 -0400

    Support Collective2 white-labe api usage

--- Common/Algorithm/Framework/Portfolio/SignalExports/Collective2SignalExport.cs
+++ Common/Algorithm/Framework/Portfolio/SignalExports/Collective2SignalExport.cs
@@ -83,18 +83,21 @@ namespace QuantConnect.Algorithm.Framework.Portfolio.SignalExports
         /// </summary>
         /// <param name="apiKey">API key provided by Collective2</param>
         /// <param name="systemId">Trading system's ID number</param>
-        public Collective2SignalExport(string apiKey, int systemId)
+        /// <param name="useWhiteLabelApi">Whether to use the white-label API instead of the general one</param>
+        public Collective2SignalExport(string apiKey, int systemId, bool useWhiteLabelApi = false)
         {
             _apiKey = apiKey;
             _systemId = systemId;
-            _destination = new Uri("https://api4-general.collective2.com/Strategies/SetDesiredPositions");
+            _destination = new Uri(useWhiteLabelApi
+                ? "https://api4-wl.collective2.com/Strategies/SetDesiredPositions"
+                : "https://api4-general.collective2.com/Strategies/SetDesiredPositions");
         }
 
         /// <summary>
         /// Creates a JSON message with the desired positions using the expected
         /// Collective2 API format and then sends it
         /// </summary>
-        /// <param name="parameters">A list of holdings from the portfolio 
+        /// <param name="parameters">A list of holdings from the portfolio
         /// expected to be sent to Collective2 API and the algorithm being ran</param>
         /// <returns>True if the positions were sent correctly and Collective2 sent no errors, false otherwise</returns>
         public override bool Send(SignalExportTargetParameters parameters)
@@ -120,7 +123,7 @@ namespace QuantConnect.Algorithm.Framework.Portfolio.SignalExports
         /// <summary>
         /// Converts a list of targets to a list of Collective2 positions
         /// </summary>
-        /// <param name="parameters">A list of targets from the portfolio 
+        /// <param name="parameters">A list of targets from the portfolio
         /// expected to be sent to Collective2 API and the algorithm being ran</param>
         /// <param name="positions">A list of Collective2 positions</param>
         /// <returns>True if the given targets could be converted to a Collective2Position list, false otherwise</returns>
@@ -333,7 +336,7 @@ namespace QuantConnect.Algorithm.Framework.Portfolio.SignalExports
         {
             /* Example:
 
-                    "ResponseStatus": 
+                    "ResponseStatus":
                     {
                       "ErrorCode": ""401",
                       "Message": ""Unauthorized",
