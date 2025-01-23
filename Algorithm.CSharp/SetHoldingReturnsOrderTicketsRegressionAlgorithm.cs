fa2341a855d80ea54992e68583fae5b019e855e7
Josue Nina <josue.nina.ponce@gmail.com>
2025-01-23 12:42:08 -0500

    Update name of regressionAlgorithm

--- Algorithm.CSharp/SetHoldingReturnsOrderTicketsRegressionAlgorithm.cs
+++ Algorithm.CSharp/SetHoldingReturnsOrderTicketsRegressionAlgorithm.cs
@@ -1,18 +1,29 @@
-using System;
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using QuantConnect.Algorithm.Framework.Portfolio;
 using QuantConnect.Data;
 using QuantConnect.Interfaces;
-using QuantConnect.Orders;
 
 namespace QuantConnect.Algorithm.CSharp
 {
     /// <summary>
     /// Validates that SetHoldings returns the correct number of order tickets on each execution.
     /// </summary>
-    public class SetHoldingReturnsOrderTickets : QCAlgorithm, IRegressionAlgorithmDefinition
+    public class SetHoldingReturnsOrderTicketsRegressionAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
     {
         private Symbol _spy;
         private Symbol _ibm;
@@ -37,7 +48,6 @@ namespace QuantConnect.Algorithm.CSharp
                 {
                     throw new RegressionTestException("Expected 2 tickets, got " + tickets.Count);
                 }
-                return;
             }
             // Validate that no tickets are returned on subsequent calls
             else if (tickets.Count != 0)
