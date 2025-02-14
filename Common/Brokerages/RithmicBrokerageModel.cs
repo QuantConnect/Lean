8bb0af19e1d6e97ee15afc92588f7322145726ce
Adam <adam.marquette@gmail.com>
2025-02-14 17:06:23 -0600

    Add initial Rithmic model and settings

---
+++ Common/Brokerages/RithmicBrokerageModel.cs
@@ -0,0 +1,13 @@
+using System;
+using QuantConnect.Benchmarks;
+using QuantConnect.Orders;
+using QuantConnect.Orders.Fees;
+using QuantConnect.Securities;
+using QuantConnect.Util;
+
+namespace QuantConnect.Brokerages
+{
+    public class RithmicBrokerageModel : DefaultBrokerageModel
+    {
+    }
+}
