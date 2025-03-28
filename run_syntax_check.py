5cbefcf45e3fc40ad156ba846d7a6453f109b169
Martin Molinero <martin.molinero1@gmail.com>
2025-03-28 11:08:57 -0300

    Ignore missing types in syntax check

--- run_syntax_check.py
+++ run_syntax_check.py
@@ -18,28 +18,28 @@ def adjust_file_contents(target_file: str):
     file_content = file.read_text()
     adjusted_import = 'from AlgorithmImports import *;from datetime import date, time, datetime, timedelta;import pandas as pd;import numpy as np;'
 
-    tmpFile = tempfile.NamedTemporaryFile(prefix=f"{file.name}_", delete=False)
-    Path(tmpFile.name).write_text("# mypy: disable-error-code=\"no-redef\"\n" + file_content.replace("from AlgorithmImports import *", adjusted_import))
-    return tmpFile
+    tmp_file = tempfile.NamedTemporaryFile(prefix=f"{file.name}_", delete=False)
+    Path(tmp_file.name).write_text("# mypy: disable-error-code=\"no-redef\"\n" + file_content.replace("from AlgorithmImports import *", adjusted_import))
+    return tmp_file
 
 def run_syntax_check(target_file: str):
-    tmpFile = adjust_file_contents(target_file)
+    tmp_file = adjust_file_contents(target_file)
     try:
-        result = run([sys.executable, "-m", "mypy", "--skip-cache-mtime-checks", "--skip-version-check", "--show-error-codes",
+        algorithm_result = run([sys.executable, "-m", "mypy", "--skip-cache-mtime-checks", "--skip-version-check", "--show-error-codes",
             "--no-error-summary", "--no-color-output", "--ignore-missing-imports", "--check-untyped-defs", "--cache-fine-grained", "--install-types",
-            "--non-interactive", "--cache-dir", "mypy_cache", tmpFile.name], capture_output=True, text=True)
+            "--non-interactive", "--cache-dir", "mypy_cache", tmp_file.name], capture_output=True, text=True)
 
-        print(result.stdout)
-        if result.stderr:
-            print(result.stderr)
+        print(algorithm_result.stdout)
+        if algorithm_result.stderr:
+            print(algorithm_result.stderr)
             return False
         return True
     except:
         import traceback
         print(f"{target_file} failed An exception occurred: {traceback.format_exc()}")
     finally:
-        tmpFile.close()
-        os.unlink(tmpFile.name)
+        tmp_file.close()
+        os.unlink(tmp_file.name)
     return False
 
 if __name__ == '__main__':
@@ -50,4 +50,5 @@ if __name__ == '__main__':
             target_files = [target for target in target_files if sys.argv[1] in target]
         result = pool.map(run_syntax_check, target_files)
         print(result)
+        print(f"SUCCESS RATE {round((sum(result) / len(result)) * 100, 1)}%")
         exit(0 if all(result) else 1)
