7985876377a747d27fcd9d6db1be69b1c2b3da16
Romazes <romazes20@gmail.com>
2025-04-04 00:38:57 +0300

    feat: missed order status in OrderTestParameters for different orders

--- Tests/Brokerages/StopMarketOrderTestParameters.cs
+++ Tests/Brokerages/StopMarketOrderTestParameters.cs
@@ -35,6 +35,7 @@ namespace QuantConnect.Tests.Brokerages
         {
             return new StopMarketOrder(Symbol, -Math.Abs(quantity), _lowLimit, DateTime.Now, properties: Properties)
             {
+                Status = OrderStatus.New,
                 OrderSubmissionData = OrderSubmissionData
             };
         }
@@ -43,6 +44,7 @@ namespace QuantConnect.Tests.Brokerages
         {
             return new StopMarketOrder(Symbol, Math.Abs(quantity), _highLimit, DateTime.Now, properties: Properties)
             {
+                Status = OrderStatus.New,
                 OrderSubmissionData = OrderSubmissionData
             };
         }
