5845f3cd8175b4b829ccb67545c8cadce4ce67af
Jhonathan Abreu <jdabreu25@gmail.com>
2025-04-21 16:27:37 -0400

    Use StubsIgnore attribute in some methods (#8708)
    IBaseData.Reader
    IBaseData.GetSource
    IndicatorBase.Update
    IAlgorithm.OnEndOfDay

--- Algorithm/QCAlgorithm.cs
+++ Algorithm/QCAlgorithm.cs
@@ -1139,6 +1139,7 @@ namespace QuantConnect.Algorithm
         /// and because Python does not support two methods with the same name</remarks>
         [Obsolete("This method is deprecated and will be removed after August 2021. Please use this overload: OnEndOfDay(Symbol symbol)")]
         [DocumentationAttribute(HandlingData)]
+        [StubsIgnore]
         public virtual void OnEndOfDay()
         {
 
@@ -1153,6 +1154,7 @@ namespace QuantConnect.Algorithm
         /// </remarks>
         /// <param name="symbol">Asset symbol for this end of day event. Forex and equities have different closing hours.</param>
         [DocumentationAttribute(HandlingData)]
+        [StubsIgnore]
         public virtual void OnEndOfDay(string symbol)
         {
         }
