3c22bd595572143665c3b61872ca6c9faac4d61e
Martin Molinero <martin.molinero1@gmail.com>
2025-04-23 16:05:10 -0300

    Fix sytanx check on algorithms importing algorithms

--- Algorithm.Python/QuitInInitializationRegressionAlgorithm.py
+++ Algorithm.Python/QuitInInitializationRegressionAlgorithm.py
@@ -1,4 +1,4 @@
-﻿# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
 # Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
