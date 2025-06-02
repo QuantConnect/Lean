7378119e915cac8d34977b83e5e5f14af978df70
Jhonathan Abreu <jdabreu25@gmail.com>
2025-06-02 16:35:35 -0400

    Add place order exception message to invalid order tag

--- Engine/TransactionHandlers/BrokerageTransactionHandler.cs
+++ Engine/TransactionHandlers/BrokerageTransactionHandler.cs
@@ -880,6 +880,7 @@ namespace QuantConnect.Lean.Engine.TransactionHandlers
 
             // set the order status based on whether or not we successfully submitted the order to the market
             bool orderPlaced;
+            Exception error = null;
             try
             {
                 orderPlaced = orders.All(o => _brokerage.PlaceOrder(o));
@@ -888,12 +889,13 @@ namespace QuantConnect.Lean.Engine.TransactionHandlers
             {
                 Log.Error(err);
                 orderPlaced = false;
+                error = err;
             }
 
             if (!orderPlaced)
             {
                 // we failed to submit the order, invalidate it
-                var errorMessage = $"Brokerage failed to place orders: [{string.Join(",", orders.Select(o => o.Id))}]";
+                var errorMessage = $"Brokerage failed to place orders: [{string.Join(",", orders.Select(o => o.Id))}]{(error == null ? "" : error.Message)}";
 
                 InvalidateOrders(orders, errorMessage);
                 _algorithm.Error(errorMessage);
