df2fa81161633035fb32724c55b85d270970b1c4
Jhonathan Abreu <jdabreu25@gmail.com>
2025-06-14 21:00:19 -0400

    Fill-forward last warmup data point to normal data feed

--- Engine/HistoricalData/SubscriptionDataReaderHistoryProvider.cs
+++ Engine/HistoricalData/SubscriptionDataReaderHistoryProvider.cs
@@ -165,7 +165,7 @@ namespace QuantConnect.Lean.Engine.HistoricalData
 
                 var readOnlyRef = Ref.CreateReadOnly(() => request.FillForwardResolution.Value.ToTimeSpan());
                 var exchange = GetSecurityExchange(security.Exchange, request.DataType, request.Symbol);
-                reader = new FillForwardEnumerator(reader, exchange, readOnlyRef, request.IncludeExtendedMarketHours, request.EndTimeLocal, config.Increment, config.DataTimeZone, useDailyStrictEndTimes, request.DataType);
+                reader = new FillForwardEnumerator(reader, exchange, readOnlyRef, request.IncludeExtendedMarketHours, request.StartTimeLocal, request.EndTimeLocal, config.Increment, config.DataTimeZone, useDailyStrictEndTimes, request.DataType);
             }
 
             // since the SubscriptionDataReader performs an any overlap condition on the trade bar's entire
