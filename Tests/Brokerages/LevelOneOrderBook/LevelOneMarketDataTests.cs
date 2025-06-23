6d86b852153f9988e6a58433811dde67efbfdf6e
Romazes <romazes20@gmail.com>
2025-06-23 16:52:21 +0300

    test:feat: validate UpdateLastTrade with IgnoreZeroSizeUpdates setting test:refactor: use global DateTime in testCases

--- Tests/Brokerages/LevelOneOrderBook/LevelOneMarketDataTests.cs
+++ Tests/Brokerages/LevelOneOrderBook/LevelOneMarketDataTests.cs
@@ -24,6 +24,8 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
     {
         private readonly Symbol _aapl = Symbols.AAPL;
 
+        private readonly DateTime _mockDateTime = new DateTime(2026, 06, 20);
+
         [TestCase(true)]
         [TestCase(false)]
         public void LevelOneMarketDataShouldNotRaiseEventWhenTimestampIsInvalid(bool useInvalidTimestamp)
@@ -46,13 +48,12 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
         [TestCase(true, 100)]
         public void UpdateOpenInterestShouldRaiseEventBasedOnValuePresence(bool shouldRaiseEvent, decimal? openInterest)
         {
-            var openInterestDateTime = new DateTime(2026, 06, 20);
             var levelOneMarketData = new LevelOneMarketData(_aapl);
             var eventRaised = default(bool);
 
             levelOneMarketData.BaseDataReceived += (_, e) => eventRaised = true;
 
-            levelOneMarketData.UpdateOpenInterest(openInterestDateTime, openInterest);
+            levelOneMarketData.UpdateOpenInterest(_mockDateTime, openInterest);
 
             Assert.AreEqual(shouldRaiseEvent, eventRaised, $"Expected event to {(shouldRaiseEvent ? "" : "not ")}be raised for open interest value: {openInterest?.ToStringInvariant() ?? "null"}");
         }
@@ -63,13 +64,12 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
         [TestCase(true, 1, 1)]
         public void UpdateLastTradeShouldRaiseEventBasedOnPriceAndQuantity(bool shouldRaiseEvent, decimal? lastQuantity, decimal? lastPrice)
         {
-            var lastTradeDateTime = new DateTime(2026, 06, 20);
             var levelOneMarketData = new LevelOneMarketData(_aapl);
             var eventRaised = default(bool);
 
             levelOneMarketData.BaseDataReceived += (_, e) => eventRaised = true;
 
-            levelOneMarketData.UpdateLastTrade(lastTradeDateTime, lastQuantity, lastPrice);
+            levelOneMarketData.UpdateLastTrade(_mockDateTime, lastQuantity, lastPrice);
 
             Assert.AreEqual(shouldRaiseEvent, eventRaised, $"Expected event {(shouldRaiseEvent ? "" : "not ")}to be raised for (Qty={lastQuantity}, Price={lastPrice}).");
         }
@@ -80,13 +80,12 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
         [TestCase(false, 1, 1, null, null)]
         public void UpdateLastTradeShouldTrackLatestTradeCorrectlyOnMultipleUpdates(bool shouldRaiseEvent, decimal? firstLastQuantity, decimal? firstLastPrice, decimal? secondLastQuantity, decimal? secondLastPrice)
         {
-            var lastTradeDateTime = new DateTime(2026, 06, 20);
             var levelOneMarketData = new LevelOneMarketData(_aapl);
             var eventRaised = default(bool);
 
             levelOneMarketData.BaseDataReceived += (_, e) => eventRaised = true;
 
-            levelOneMarketData.UpdateLastTrade(lastTradeDateTime, firstLastQuantity, firstLastPrice);
+            levelOneMarketData.UpdateLastTrade(_mockDateTime, firstLastQuantity, firstLastPrice);
             Assert.IsTrue(eventRaised, "Expected event to be raised on first update.");
             Assert.AreEqual(firstLastQuantity, levelOneMarketData.LastTradeSize, "First update: unexpected LastTradeSize.");
             Assert.AreEqual(firstLastPrice, levelOneMarketData.LastTradePrice, "First update: unexpected LastTradePrice.");
@@ -94,7 +93,7 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
             // Reset event flag for second update
             eventRaised = false;
 
-            levelOneMarketData.UpdateLastTrade(lastTradeDateTime, secondLastQuantity, secondLastPrice);
+            levelOneMarketData.UpdateLastTrade(_mockDateTime, secondLastQuantity, secondLastPrice);
 
             Assert.AreEqual(shouldRaiseEvent, eventRaised, $"Second update: Expected event {(shouldRaiseEvent ? "" : "not ")}to be raised.");
 
@@ -112,13 +111,12 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
         [TestCase(true, 1, 1, 1, 0)]
         public void UpdateQuoteShouldRaiseEventBasedOnAskAndBid(bool shouldRaiseEvent, decimal? bidPrice, decimal? bidSize, decimal? askPrice, decimal? askSize)
         {
-            var lastQuoteDateTime = new DateTime(2026, 06, 20);
             var levelOneMarketData = new LevelOneMarketData(_aapl);
             var eventRaised = default(bool);
 
             levelOneMarketData.BaseDataReceived += (_, e) => eventRaised = true;
 
-            levelOneMarketData.UpdateQuote(lastQuoteDateTime, bidPrice, bidSize, askPrice, askSize);
+            levelOneMarketData.UpdateQuote(_mockDateTime, bidPrice, bidSize, askPrice, askSize);
 
             Assert.AreEqual(shouldRaiseEvent, eventRaised);
         }
@@ -131,19 +129,18 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
             decimal? firstBidPrice, decimal? firstBidSize, decimal? firstAskPrice, decimal? firstAskSize,
             decimal? secondBidPrice, decimal? secondBidSize, decimal? secondAskPrice, decimal? secondAskSize)
         {
-            var lastQuoteDateTime = new DateTime(2026, 06, 20);
             var levelOneMarketData = new LevelOneMarketData(_aapl);
             var eventRaised = default(bool);
 
             levelOneMarketData.BaseDataReceived += (_, e) => eventRaised = true;
 
-            levelOneMarketData.UpdateQuote(lastQuoteDateTime, firstBidPrice, firstBidSize, firstAskPrice, firstAskSize);
+            levelOneMarketData.UpdateQuote(_mockDateTime, firstBidPrice, firstBidSize, firstAskPrice, firstAskSize);
             Assert.IsTrue(eventRaised);
 
             // Reset event flag for second update
             eventRaised = false;
 
-            levelOneMarketData.UpdateQuote(lastQuoteDateTime, secondBidPrice, secondBidSize, secondAskPrice, secondAskSize);
+            levelOneMarketData.UpdateQuote(_mockDateTime, secondBidPrice, secondBidSize, secondAskPrice, secondAskSize);
 
             Assert.AreEqual(shouldRaiseEvent, eventRaised, $"Second update: Expected event {(shouldRaiseEvent ? "" : "not ")}to be raised.");
         }
@@ -152,7 +149,6 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
         [TestCase(false, 2, 0, 2, 0)]
         public void UpdateQuoteShouldIgnoreZeroSizeUpdatesCorrectly(bool ignoreZeroSizeUpdates, decimal? bidPrice, decimal? bidSize, decimal? askPrice, decimal? askSize)
         {
-            var lastQuoteDateTime = new DateTime(2026, 06, 20);
             var levelOneMarketData = new LevelOneMarketData(_aapl)
             {
                 IgnoreZeroSizeUpdates = ignoreZeroSizeUpdates
@@ -161,9 +157,9 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
             var expectedBestBidSize = 1;
             var expectedBestAskSize = 1;
 
-            levelOneMarketData.UpdateQuote(lastQuoteDateTime, 1, expectedBestBidSize, 1, expectedBestAskSize);
+            levelOneMarketData.UpdateQuote(_mockDateTime, 1, expectedBestBidSize, 1, expectedBestAskSize);
 
-            levelOneMarketData.UpdateQuote(lastQuoteDateTime, bidPrice, bidSize, askPrice, askSize);
+            levelOneMarketData.UpdateQuote(_mockDateTime, bidPrice, bidSize, askPrice, askSize);
 
             Assert.AreEqual(bidPrice, levelOneMarketData.BestBidPrice, "Bid price should update.");
             Assert.AreEqual(askPrice, levelOneMarketData.BestAskPrice, "Ask price should update.");
@@ -177,6 +173,32 @@ namespace QuantConnect.Tests.Brokerages.LevelOneOrderBook
             {
                 Assert.AreEqual(bidSize, levelOneMarketData.BestAskSize, "Bid size should be overwritten with 0.");
                 Assert.AreEqual(askSize, levelOneMarketData.BestBidSize, "Ask size should be overwritten with 0.");
+            }
+        }
+
+        [TestCase(true, 0, 2)]
+        [TestCase(false, 0, 2)]
+        public void UpdateLastTradeShouldIgnoreZeroSizeUpdatesCorrectly(bool ignoreZeroSizeUpdates, decimal? lastQuantity, decimal? lastPrice)
+        {
+            var levelOneMarketData = new LevelOneMarketData(_aapl)
+            {
+                IgnoreZeroSizeUpdates = ignoreZeroSizeUpdates
+            };
+
+            var expectedLastQuantity = 1;
+            levelOneMarketData.UpdateLastTrade(_mockDateTime, expectedLastQuantity, 1);
+
+            levelOneMarketData.UpdateLastTrade(_mockDateTime, lastQuantity, lastPrice);
+
+            Assert.AreEqual(lastPrice, levelOneMarketData.LastTradePrice, "LastTradePrice should update.");
+
+            if (ignoreZeroSizeUpdates)
+            {
+                Assert.AreEqual(expectedLastQuantity, levelOneMarketData.LastTradeSize);
+            }
+            else
+            {
+                Assert.AreEqual(lastQuantity, levelOneMarketData.LastTradeSize);
             }
         }
     }
