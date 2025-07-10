ce2a9e761511d055273edb584dcadd036fc1948f
Josue Nina <josue.nina.ponce@gmail.com>
2025-07-10 17:38:48 -0500

    Add TryGet to safely get the key: MaximumDrawdownRecovery

--- Messaging/Messaging.cs
+++ Messaging/Messaging.cs
@@ -97,18 +97,27 @@ namespace QuantConnect.Messaging
 
                     if (result.Progress == 1)
                     {
-                        // Generate order list hash and store MaximumDrawdownRecovery value temporarily
+                        // Generate unique hash of the order list
                         var orderHash = result.Results.Orders.GetHash();
                         var maxDrawdownRecoveryKey = PerformanceMetrics.MaximumDrawdownRecovery;
-                        var maxDrawdownRecoveryValue = result.Results.Statistics[maxDrawdownRecoveryKey];
 
-                        // Reorder statistics: 
-                        // 1. Remove MaximumDrawdownRecovery to reposition it
-                        // 2. Add OrderListHash 
-                        // 3. Restore MaximumDrawdownRecovery as last metric
-                        result.Results.Statistics.Remove(maxDrawdownRecoveryKey);
-                        result.Results.Statistics.Add("OrderListHash", orderHash);
-                        result.Results.Statistics.Add(maxDrawdownRecoveryKey, maxDrawdownRecoveryValue);
+                        // Safely try to get the MaximumDrawdownRecovery value
+                        if (result.Results.Statistics.TryGetValue(maxDrawdownRecoveryKey, out var maxDrawdownRecoveryValue))
+                        {
+                            // Reorder statistics: 
+                            // 1. Remove MaximumDrawdownRecovery to reposition it
+                            // 2. Add OrderListHash 
+                            // 3. Restore MaximumDrawdownRecovery as last metric
+                            result.Results.Statistics.Remove(maxDrawdownRecoveryKey);
+                            result.Results.Statistics.Add("OrderListHash", orderHash);
+                            result.Results.Statistics.Add(maxDrawdownRecoveryKey, maxDrawdownRecoveryValue);
+                        }
+                        else
+                        {
+                            // If key doesn't exist, just add the OrderListHash
+                            result.Results.Statistics.Add("OrderListHash", orderHash);
+                            Log.Trace("STATISTICS:: MaximumDrawdownRecovery not found.");
+                        }
 
                         var statisticsStr = $"{Environment.NewLine}" +
                             $"{string.Join(Environment.NewLine, result.Results.Statistics.Select(x => $"STATISTICS:: {x.Key} {x.Value}"))}";
