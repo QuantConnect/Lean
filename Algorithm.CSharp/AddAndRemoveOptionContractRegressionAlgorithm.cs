0a333cf2ba93e727870c5e2185a9886cf991dc82
Jhonathan Abreu <jdabreu25@gmail.com>
2025-07-11 16:06:58 -0400

    Minor regression algorithm fix
    AddAndRemoveOptionContractRegressionAlgorithm to not depend on universe file entries ordering

--- Algorithm.CSharp/AddAndRemoveOptionContractRegressionAlgorithm.cs
+++ Algorithm.CSharp/AddAndRemoveOptionContractRegressionAlgorithm.cs
@@ -41,9 +41,11 @@ namespace QuantConnect.Algorithm.CSharp
             var aapl = QuantConnect.Symbol.Create("AAPL", SecurityType.Equity, Market.USA);
 
             _contract = OptionChain(aapl)
-                .OrderBy(x => x.ID.Symbol)
-                .FirstOrDefault(optionContract => optionContract.ID.OptionRight == OptionRight.Call
-                    && optionContract.ID.OptionStyle == OptionStyle.American);
+                .OrderBy(symbol => symbol.ID.OptionRight)
+                .ThenBy(symbol => symbol.ID.StrikePrice)
+                .ThenBy(symbol => symbol.ID.Date)
+                .ThenBy(symbol => symbol.ID)
+                .FirstOrDefault(optionContract => optionContract.ID.OptionRight == OptionRight.Call);
             AddOptionContract(_contract);
         }
 
