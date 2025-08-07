12926f0adedbb4ee6dbc5d056afe676dfd8111ac
Jhonathan Abreu <jdabreu25@gmail.com>
2025-08-07 17:55:42 -0400

    More fixes

--- Algorithm.Python/BasicTemplateIntrinioEconomicData.py
+++ Algorithm.Python/BasicTemplateIntrinioEconomicData.py
@@ -13,7 +13,7 @@
 
 from AlgorithmImports import *
 
-from QuantConnect.Data.Custom import Intrinio
+from QuantConnect.Data.Custom.Intrinio import *
 
 class BasicTemplateIntrinioEconomicData(QCAlgorithm):
 
@@ -25,12 +25,12 @@ class BasicTemplateIntrinioEconomicData(QCAlgorithm):
         self.set_cash(100000)           #Set Strategy Cash
 
         # Set your Intrinio user and password.
-        Intrinio.IntrinioConfig.set_user_and_password("intrinio-username", "intrinio-password")
+        IntrinioConfig.set_user_and_password("intrinio-username", "intrinio-password")
         # The Intrinio user and password can be also defined in the config.json file for local backtest.
 
         # Set Intrinio config to make 1 call each minute, default is 1 call each 5 seconds.
         #(1 call each minute is the free account limit for historical_data endpoint)
-        Intrinio.IntrinioConfig.set_time_interval_between_calls(timedelta(minutes = 1))
+        IntrinioConfig.set_time_interval_between_calls(timedelta(minutes = 1))
 
         # United States Oil Fund LP
         self.uso = self.add_equity("USO", Resolution.DAILY).symbol
@@ -39,8 +39,8 @@ class BasicTemplateIntrinioEconomicData(QCAlgorithm):
         self.bno = self.add_equity("BNO", Resolution.DAILY).symbol
         self.securities[self.bno].set_leverage(2)
 
-        self.add_data(Intrinio.IntrinioEconomicData, "$DCOILWTICO", Resolution.DAILY)
-        self.add_data(Intrinio.IntrinioEconomicData, "$DCOILBRENTEU", Resolution.DAILY)
+        self.add_data(IntrinioEconomicData, "$DCOILWTICO", Resolution.DAILY)
+        self.add_data(IntrinioEconomicData, "$DCOILBRENTEU", Resolution.DAILY)
 
         self.ema_wti = self.ema("$DCOILWTICO", 10)
 
