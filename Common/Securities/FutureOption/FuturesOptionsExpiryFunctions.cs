e75bbf96157c8fa5ce3d3ef624918f1729bbc345
Martin-Molinero <martin@quantconnect.com>
2025-09-26 17:58:36 -0300

    Fix soybean meal & oil FOP expiration (#8995)
    * Fix soybean meanl FOP expiration
    
    * Fix Soybean oil FOPs

--- Common/Securities/FutureOption/FuturesOptionsExpiryFunctions.cs
+++ Common/Securities/FutureOption/FuturesOptionsExpiryFunctions.cs
@@ -26,11 +26,14 @@ namespace QuantConnect.Securities.FutureOption
     {
         private static readonly Symbol _lo = Symbol.CreateCanonicalOption(Symbol.Create("CL", SecurityType.Future, Market.NYMEX));
         private static readonly Symbol _on = Symbol.CreateCanonicalOption(Symbol.Create("NG", SecurityType.Future, Market.NYMEX));
+        private static readonly Symbol _ozm = Symbol.CreateCanonicalOption(Symbol.Create("ZM", SecurityType.Future, Market.CBOT));
         private static readonly Symbol _ozb = Symbol.CreateCanonicalOption(Symbol.Create("ZB", SecurityType.Future, Market.CBOT));
         private static readonly Symbol _ozc = Symbol.CreateCanonicalOption(Symbol.Create("ZC", SecurityType.Future, Market.CBOT));
         private static readonly Symbol _ozn = Symbol.CreateCanonicalOption(Symbol.Create("ZN", SecurityType.Future, Market.CBOT));
+        private static readonly Symbol _otn = Symbol.CreateCanonicalOption(Symbol.Create("TN", SecurityType.Future, Market.CBOT));
         private static readonly Symbol _ozs = Symbol.CreateCanonicalOption(Symbol.Create("ZS", SecurityType.Future, Market.CBOT));
         private static readonly Symbol _ozt = Symbol.CreateCanonicalOption(Symbol.Create("ZT", SecurityType.Future, Market.CBOT));
+        private static readonly Symbol _ozl = Symbol.CreateCanonicalOption(Symbol.Create("ZL", SecurityType.Future, Market.CBOT));
         private static readonly Symbol _ozw = Symbol.CreateCanonicalOption(Symbol.Create("ZW", SecurityType.Future, Market.CBOT));
         private static readonly Symbol _hxe = Symbol.CreateCanonicalOption(Symbol.Create("HG", SecurityType.Future, Market.COMEX));
         private static readonly Symbol _og = Symbol.CreateCanonicalOption(Symbol.Create("GC", SecurityType.Future, Market.COMEX));
@@ -61,9 +64,12 @@ namespace QuantConnect.Securities.FutureOption
             { _ozb, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozb.Underlying, expiryMonth) },
             { _ozc, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozc.Underlying, expiryMonth) },
             { _ozn, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozn.Underlying, expiryMonth) },
+            { _otn, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_otn.Underlying, expiryMonth) },
             { _ozs, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozs.Underlying, expiryMonth) },
             { _ozt, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozt.Underlying, expiryMonth) },
             { _ozw, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozw.Underlying, expiryMonth) },
+            { _ozl, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozl.Underlying, expiryMonth) },
+            { _ozm, expiryMonth => FridayBeforeTwoBusinessDaysBeforeEndOfMonth(_ozm.Underlying, expiryMonth) },
             { _hxe, expiryMonth => FourthLastBusinessDayInPrecedingMonthFromContractMonth(_hxe.Underlying, expiryMonth, 12, 0) },
             { _og, expiryMonth => FourthLastBusinessDayInPrecedingMonthFromContractMonth(_og.Underlying, expiryMonth, 12, 30) },
             { _so, expiryMonth => FourthLastBusinessDayInPrecedingMonthFromContractMonth(_so.Underlying, expiryMonth, 12, 25) },
--- Tests/Common/Securities/FutureOption/FuturesOptionsExpiryFunctionsTests.cs
+++ Tests/Common/Securities/FutureOption/FuturesOptionsExpiryFunctionsTests.cs
@@ -59,6 +59,33 @@ namespace QuantConnect.Tests.Common.Securities.FutureOption
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase("ZM", 12, "20251121", "20251212")]
+        [TestCase("ZM", 11, "20251024", "20251212")]
+        [TestCase("ZL", 12, "20251121", "20251212")]
+        [TestCase("ZL", 11, "20251024", "20251212")]
+        [TestCase("TN", 12, "20251121", "20251222")]
+        [TestCase("TN", 11, "20251024", "20251222")]
+        public void SoybeanMealMapping(string futureTicker, int month, string expectedFop, string expectedFuture)
+        {
+            var referenceDate = new DateTime(2025, 9, 1);
+            var market = Market.CBOT;
+            var contractMonth = new DateTime(2025, month, 1);
+            var canonicalFuture = Symbol.Create(futureTicker, SecurityType.Future, market);
+            var canonicalFutureOption = Symbol.CreateOption(
+                canonicalFuture,
+                market,
+                default,
+                default,
+                default,
+                SecurityIdentifier.DefaultDate);
+            var futureOptionExpiry = FuturesOptionsExpiryFunctions.FuturesOptionExpiry(canonicalFutureOption, contractMonth);
+            Assert.AreEqual(Time.ParseDate(expectedFop), futureOptionExpiry);
+
+            var underlyingFuture = FuturesOptionsUnderlyingMapper.GetUnderlyingFutureFromFutureOption(canonicalFutureOption.ID.Symbol, market, futureOptionExpiry, referenceDate);
+
+            Assert.AreEqual(Time.ParseDate(expectedFuture), underlyingFuture.ID.Date.Date);
+        }
+
         [Test]
         public void ExpiryFunctionsReturnExpectedResultWhenExpiryIsAHoliday()
         {
