d65cf33a6b2a31d0097e157655c86eeeb36a5a69
cdiaz9719 <cdiaznicaragua97@gmail.com>
2025-10-06 14:47:34 -0600

    Update DailySentimentScoreAlgorithm.cs

--- Algorithm.CSharp/CustomData/DailySentimentScoreAlgorithm.cs
+++ Algorithm.CSharp/CustomData/DailySentimentScoreAlgorithm.cs
@@ -1,1 +1,35 @@
 
+using QuantConnect;
+using QuantConnect.Algorithm;
+using QuantConnect.Data;
+using QuantConnect.Data.Custom;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class DailySentimentScoreAlgorithm : QCAlgorithm
+    {
+        private Symbol _aapl;
+
+        public override void Initialize()
+        {
+            SetStartDate(2024, 1, 2);
+            SetEndDate(2024, 1, 31);
+            SetCash(100000);
+
+            _aapl = AddEquity("AAPL", Resolution.Daily).Symbol;
+            AddData<DailySentimentScore>(_aapl, Resolution.Daily);
+        }
+
+        public override void OnData(Slice data)
+        {
+            var sentiment = data.Get<DailySentimentScore>(_aapl);
+            if (sentiment != null)
+            {
+                if (sentiment.Score > 0 && !Portfolio[_aapl].Invested)
+                    SetHoldings(_aapl, 1.0);
+                else if (sentiment.Score <= 0 && Portfolio[_aapl].Invested)
+                    Liquidate(_aapl);
+            }
+        }
+    }
+}
