401988c2ea9afe3517c04bff7ba89f5667972546
Josue Nina <josue.nina.ponce@gmail.com>
2025-10-15 17:22:07 -0500

    Solve review comments

--- Algorithm.CSharp/CustomDataSecurityCacheGetDataRegressionAlgorithm.cs
+++ Algorithm.CSharp/CustomDataSecurityCacheGetDataRegressionAlgorithm.cs
@@ -13,18 +13,20 @@
  * limitations under the License.
 */
 
-using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using Newtonsoft.Json;
 using QuantConnect.Data;
 using QuantConnect.Interfaces;
 using QuantConnect.Securities;
+using static QuantConnect.Algorithm.CSharp.CustomDataRegressionAlgorithm;
 
 
 namespace QuantConnect.Algorithm.CSharp
 {
+    /// <summary>
+    /// Regression algorithm used to verify that GetData<T> correctly retrieves 
+    /// the latest custom data stored in the security cache.
+    /// </summary>
     public class CustomDataSecurityCacheGetDataRegressionAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
     {
         public override void Initialize()
@@ -115,124 +117,5 @@ namespace QuantConnect.Algorithm.CSharp
             {"Drawdown Recovery", "0"},
             {"OrderListHash", "d41d8cd98f00b204e9800998ecf8427e"}
         };
-
-        /// <summary>
-        /// Custom Data Type: Bitcoin data from Quandl - http://www.quandl.com/help/api-for-bitcoin-data
-        /// </summary>
-        public class Bitcoin : BaseData
-        {
-            [JsonProperty("timestamp")]
-            public int Timestamp { get; set; }
-            [JsonProperty("open")]
-            public decimal Open { get; set; }
-            [JsonProperty("high")]
-            public decimal High { get; set; }
-            [JsonProperty("low")]
-            public decimal Low { get; set; }
-            public decimal Mid { get; set; }
-
-            [JsonProperty("last")]
-            public decimal Close { get; set; }
-            [JsonProperty("bid")]
-            public decimal Bid { get; set; }
-            [JsonProperty("ask")]
-            public decimal Ask { get; set; }
-            [JsonProperty("vwap")]
-            public decimal WeightedPrice { get; set; }
-            [JsonProperty("volume")]
-            public decimal VolumeBTC { get; set; }
-
-            /// <summary>
-            /// The end time of this data. Some data covers spans (trade bars)
-            /// and as such we want to know the entire time span covered
-            /// </summary>
-            /// <remarks>
-            /// This property is overriden to allow different values for Time and EndTime
-            /// if they are set in the Reader. In the base implementation EndTime equals Time
-            /// </remarks>
-            public override DateTime EndTime { get; set; }
-
-            /// <summary>
-            /// 1. DEFAULT CONSTRUCTOR: Custom data types need a default constructor.
-            /// We search for a default constructor so please provide one here. It won't be used for data, just to generate the "Factory".
-            /// </summary>
-            public Bitcoin()
-            {
-                Symbol = "BTC";
-            }
-
-            /// <summary>
-            /// 2. RETURN THE STRING URL SOURCE LOCATION FOR YOUR DATA:
-            /// This is a powerful and dynamic select source file method. If you have a large dataset, 10+mb we recommend you break it into smaller files. E.g. One zip per year.
-            /// We can accept raw text or ZIP files. We read the file extension to determine if it is a zip file.
-            /// </summary>
-            /// <param name="config">Configuration object</param>
-            /// <param name="date">Date of this source file</param>
-            /// <param name="isLiveMode">true if we're in live mode, false for backtesting mode</param>
-            /// <returns>String URL of source file.</returns>
-            public override SubscriptionDataSource GetSource(SubscriptionDataConfig config, DateTime date, bool isLiveMode)
-            {
-                if (isLiveMode)
-                {
-                    return new SubscriptionDataSource("https://www.bitstamp.net/api/ticker/", SubscriptionTransportMedium.Rest);
-                }
-
-                //return "http://my-ftp-server.com/futures-data-" + date.ToString("Ymd") + ".zip";
-                // OR simply return a fixed small data file. Large files will slow down your backtest
-                return new SubscriptionDataSource("https://www.quantconnect.com/api/v2/proxy/nasdaq/api/v3/datatables/QDL/BITFINEX.csv?code=BTCUSD&api_key=qAWKpUfmSVFnU3bRQwKy")
-                {
-                    Sort = true
-                };
-            }
-
-            /// <summary>
-            /// 3. READER METHOD: Read 1 line from data source and convert it into Object.
-            /// Each line of the CSV File is presented in here. The backend downloads your file, loads it into memory and then line by line
-            /// feeds it into your algorithm
-            /// </summary>
-            /// <param name="line">string line from the data source file submitted above</param>
-            /// <param name="config">Subscription data, symbol name, data type</param>
-            /// <param name="date">Current date we're requesting. This allows you to break up the data source into daily files.</param>
-            /// <param name="isLiveMode">true if we're in live mode, false for backtesting mode</param>
-            /// <returns>New Bitcoin Object which extends BaseData.</returns>
-            public override BaseData Reader(SubscriptionDataConfig config, string line, DateTime date, bool isLiveMode)
-            {
-                var coin = new Bitcoin();
-                if (isLiveMode)
-                {
-                    //Example Line Format:
-                    //{"high": "441.00", "last": "421.86", "timestamp": "1411606877", "bid": "421.96", "vwap": "428.58", "volume": "14120.40683975", "low": "418.83", "ask": "421.99"}
-                    try
-                    {
-                        coin = JsonConvert.DeserializeObject<Bitcoin>(line);
-                        coin.EndTime = DateTime.UtcNow.ConvertFromUtc(config.ExchangeTimeZone);
-                        coin.Value = coin.Close;
-                    }
-                    catch { /* Do nothing, possible error in json decoding */ }
-                    return coin;
-                }
-
-                //Example Line Format:
-                // code    date        high     low      mid      last     bid      ask      volume
-                // BTCUSD  2024-10-08  63248.0  61940.0  62246.5  62245.0  62246.0  62247.0  477.91102114
-                try
-                {
-                    string[] data = line.Split(',');
-                    coin.Time = DateTime.Parse(data[1], CultureInfo.InvariantCulture);
-                    coin.EndTime = coin.Time.AddDays(1);
-                    coin.High = Convert.ToDecimal(data[2], CultureInfo.InvariantCulture);
-                    coin.Low = Convert.ToDecimal(data[3], CultureInfo.InvariantCulture);
-                    coin.Mid = Convert.ToDecimal(data[4], CultureInfo.InvariantCulture);
-                    coin.Close = Convert.ToDecimal(data[5], CultureInfo.InvariantCulture);
-                    coin.Bid = Convert.ToDecimal(data[6], CultureInfo.InvariantCulture);
-                    coin.Ask = Convert.ToDecimal(data[7], CultureInfo.InvariantCulture);
-                    coin.VolumeBTC = Convert.ToDecimal(data[8], CultureInfo.InvariantCulture);
-                    coin.Value = coin.Close;
-                }
-                catch { /* Do nothing, skip first title row */ }
-
-                return coin;
-            }
-        }
     }
 }
--- Algorithm.Python/CustomDataSecurityCacheGetDataRegressionAlgorithm.py
+++ Algorithm.Python/CustomDataSecurityCacheGetDataRegressionAlgorithm.py
@@ -13,7 +13,12 @@
 
 import json
 from AlgorithmImports import *
+from CustomDataRegressionAlgorithm import Bitcoin
 
+### <summary>
+### Regression algorithm used to verify that get_data(type) correctly retrieves
+### the latest custom data stored in the security cache.
+### </summary>
 class CustomDataSecurityCacheGetDataRegressionAlgorithm(QCAlgorithm):
 
     def initialize(self) -> None:
@@ -35,70 +40,3 @@ class CustomDataSecurityCacheGetDataRegressionAlgorithm(QCAlgorithm):
         bitcoin_from_slice = list(data.get(Bitcoin).values())[0]
         if bitcoin_from_slice != bitcoin:
             raise RegressionTestException("Expected cached Bitcoin to match the one from Slice")
-
-
-class Bitcoin(PythonData):
-    '''Custom Data Type: Bitcoin data from Quandl - https://data.nasdaq.com/databases/BCHAIN'''
-
-    def get_source(self, config: SubscriptionDataConfig, date: datetime, is_live_mode: bool) -> SubscriptionDataSource:
-        if is_live_mode:
-            return SubscriptionDataSource("https://www.bitstamp.net/api/ticker/", SubscriptionTransportMedium.REST)
-
-        #return "http://my-ftp-server.com/futures-data-" + date.to_string("Ymd") + ".zip"
-        # OR simply return a fixed small data file. Large files will slow down your backtest
-        subscription = SubscriptionDataSource("https://www.quantconnect.com/api/v2/proxy/nasdaq/api/v3/datatables/QDL/BITFINEX.csv?code=BTCUSD&api_key=WyAazVXnq7ATy_fefTqm")
-        subscription.sort = True
-        return subscription
-
-    def reader(self, config: SubscriptionDataConfig, line: str, date: datetime, is_live_mode: bool) -> DynamicData:
-        coin = Bitcoin()
-        coin.symbol = config.symbol
-
-        if is_live_mode:
-            # Example Line Format:
-            # {"high": "441.00", "last": "421.86", "timestamp": "1411606877", "bid": "421.96", "vwap": "428.58", "volume": "14120.40683975", "low": "418.83", "ask": "421.99"}
-            try:
-                live_btc = json.loads(line)
-
-                # If value is zero, return coin
-                value = live_btc["last"]
-                if value == 0:
-                    return coin
-
-                coin.time = datetime.now()
-                coin.value = value
-                coin["Open"] = float(live_btc["open"])
-                coin["High"] = float(live_btc["high"])
-                coin["Low"] = float(live_btc["low"])
-                coin["Close"] = float(live_btc["last"])
-                coin["Ask"] = float(live_btc["ask"])
-                coin["Bid"] = float(live_btc["bid"])
-                coin["VolumeBTC"] = float(live_btc["volume"])
-                coin["WeightedPrice"] = float(live_btc["vwap"])
-                return coin
-            except ValueError:
-                # Do nothing, possible error in json decoding
-                return coin
-
-        # Example Line Format:
-        # code    date        high     low      mid      last     bid      ask      volume
-        # BTCUSD  2024-10-08  63248.0  61940.0  62246.5  62245.0  62246.0  62247.0  477.91102114
-        if not (line.strip() and line[7].isdigit()): return coin
-
-        try:
-            data = line.split(',')
-            coin.time = datetime.strptime(data[1], "%Y-%m-%d")
-            coin.end_time = coin.time + timedelta(days=1)
-            coin.value = float(data[5])
-            coin["High"] = float(data[2])
-            coin["Low"] = float(data[3])
-            coin["Mid"] = float(data[4])
-            coin["Close"] = float(data[5])
-            coin["Bid"] = float(data[6])
-            coin["Ask"] = float(data[7])
-            coin["VolumeBTC"] = float(data[8])
-            return coin
-
-        except ValueError:
-            # Do nothing, possible error in json decoding
-            return coin
