13bb7e0217e0ffcf8b75fe263dd6e7224830c8f6
Martin-Molinero <martin.molinero1@gmail.com>
2025-10-16 10:36:48 -0300

    Update CustomDataSecurityCacheGetDataRegressionAlgorithm.py

--- Algorithm.Python/CustomDataSecurityCacheGetDataRegressionAlgorithm.py
+++ Algorithm.Python/CustomDataSecurityCacheGetDataRegressionAlgorithm.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 from AlgorithmImports import *
 from CustomDataRegressionAlgorithm import Bitcoin
 
