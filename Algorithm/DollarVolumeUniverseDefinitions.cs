c2406a9d45d98dc4f8469c8c8e17ea629226a030
yyxxddjj <2505553080@qq.com>
2025-11-10 16:44:38 +0800

    Docs(Algorithm): Add XML code references
    Adds XML documentation tags (<see/>, <paramref/>) to code
    references within the Algorithm folder. This improves code
    navigation, IntelliSense, and helps the stubs generator tool
    to correctly map C# elements.
    
    Resolves #9016

--- Algorithm/DollarVolumeUniverseDefinitions.cs
+++ Algorithm/DollarVolumeUniverseDefinitions.cs
@@ -40,13 +40,13 @@ namespace QuantConnect.Algorithm
         }
 
         /// <summary>
-        /// Creates a new coarse universe that contains the top count of stocks
+        /// Creates a new coarse <see cref="Universe"/> that contains the top count of stocks
         /// by daily dollar volume
         /// </summary>
         /// <param name="count">The number of stock to select</param>
         /// <param name="universeSettings">The settings for stocks added by this universe.
         /// Defaults to <see cref="QCAlgorithm.UniverseSettings"/></param>
-        /// <returns>A new coarse universe for the top count of stocks by dollar volume</returns>
+        /// <returns>A new coarse <see cref="Universe"/> for the top count of stocks by dollar volume</returns>
         [Obsolete("This method is deprecated. Use method `Universe.DollarVolume.Top(...)` instead")]
         public Universe Top(int count, UniverseSettings universeSettings = null)
         {
