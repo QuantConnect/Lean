683bfe007a401b131d9135cd62322d67c5fb231f
Jhonathan Abreu <jdabreu25@gmail.com>
2025-12-05 10:05:40 -0400

    Avoid emitting overlapping fill forwarded data (#9107)
    * Avoid emitting overlapping fill forward data
    
    * Cleanup

--- Algorithm.CSharp/IndicatorSelectorsWorkWithDifferentOptions.cs
+++ Algorithm.CSharp/IndicatorSelectorsWorkWithDifferentOptions.cs
@@ -186,7 +186,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public long DataPoints => 455431;
+        public long DataPoints => 455371;
 
         /// <summary>
         /// Data Points count of the algorithm history
