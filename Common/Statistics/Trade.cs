6f33572417e7f8f4b04d06579d69cec3f7088fe1
Jhonathan Abreu <jdabreu25@gmail.com>
2026-01-15 13:47:02 -0400

    Deprecated Trade.Symbol in favor of new Trade.Symbols

--- Common/Statistics/Trade.cs
+++ Common/Statistics/Trade.cs
@@ -13,6 +13,7 @@
  * limitations under the License.
 */
 
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
@@ -23,10 +24,41 @@ namespace QuantConnect.Statistics
     /// </summary>
     public class Trade
     {
+        private List<Symbol> _symbols;
+
         /// <summary>
         /// The symbol of the traded instrument
         /// </summary>
-        public Symbol Symbol { get; set; }
+        [Obsolete("Use Symbols property instead")]
+        [JsonIgnore]
+        public Symbol Symbol
+        {
+            get
+            {
+                return _symbols != null && _symbols.Count > 0 ? _symbols[0] : Symbol.Empty;
+            }
+            set
+            {
+                if (_symbols == null)
+                {
+                    _symbols = new List<Symbol>() { value };
+                }
+                else
+                {
+                    _symbols.Clear();
+                    _symbols.Add(value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The symbol associated to the traded instruments
+        /// </summary>
+        public List<Symbol> Symbols
+        {
+            get { return _symbols; }
+            set { _symbols = value; }
+        }
 
         /// <summary>
         /// The date and time the trade was opened
