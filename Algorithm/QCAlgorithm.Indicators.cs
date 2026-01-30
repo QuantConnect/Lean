a21c6785085a2d9bfcdeedf090fb5458d05245f4
Josue Nina <josue.nina.ponce@gmail.com>
2026-01-30 11:08:37 -0500

    Solve review comments

--- Algorithm/QCAlgorithm.Indicators.cs
+++ Algorithm/QCAlgorithm.Indicators.cs
@@ -4237,23 +4237,23 @@ namespace QuantConnect.Algorithm
                 {
                     if (resolution.HasValue)
                     {
-                        if (subscription.ExtendedMarketHours && !Settings.DailyConsolidationUseExtendedMarketHours)
-                        {
-                            // Show this warning only once
-                            if (!_hasShownDailyConsolidationWarning)
-                            {
-                                Debug($"Warning: The subscription for {symbol} has ExtendedMarketHours=true, " +
-                                    $"but Settings.DailyConsolidationUseExtendedMarketHours=false. " +
-                                    $"Daily consolidations will exclude extended market hours. " +
-                                    $"Enable algorithm.Settings.DailyConsolidationUseExtendedMarketHours to include them."
-                                );
-
-                                _hasShownDailyConsolidationWarning = true;
-                            }
-                        }
-
                         if (resolution.Value == Resolution.Daily)
                         {
+                            if (subscription.ExtendedMarketHours && !Settings.DailyConsolidationUseExtendedMarketHours)
+                            {
+                                // Show this warning only once
+                                if (!_hasShownDailyConsolidationWarning)
+                                {
+                                    Debug($"Warning: The subscription for {symbol} has ExtendedMarketHours=true, " +
+                                        $"but Settings.DailyConsolidationUseExtendedMarketHours=false. " +
+                                        $"Daily consolidations will exclude extended market hours. " +
+                                        $"Enable algorithm.Settings.DailyConsolidationUseExtendedMarketHours to include them."
+                                    );
+
+                                    _hasShownDailyConsolidationWarning = true;
+                                }
+                            }
+
                             consolidator = new MarketHourAwareConsolidator(Settings.DailyPreciseEndTime, resolution.Value, subscription.Type, subscription.TickType,
                                 Settings.DailyConsolidationUseExtendedMarketHours && subscription.ExtendedMarketHours);
                         }
