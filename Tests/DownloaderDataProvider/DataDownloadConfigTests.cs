0212acf927e19270924d36c978822353ffa0e5f1
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2026-02-13 18:45:10 +0200

    Feat: support downloading single contract in `DownloaderProvider` (#9285)
    * feat: improve symbol parsing for futures/options in downloader
    
    * refactor: symbol parsing and unify ticker handling
    
    * refactor: symbol parsing and loading in BaseDataDownloadConfig

--- Tests/DownloaderDataProvider/DataDownloadConfigTests.cs
+++ Tests/DownloaderDataProvider/DataDownloadConfigTests.cs
@@ -14,6 +14,8 @@
  *
 */
 
+using System;
+using System.Linq;
 using NUnit.Framework;
 using QuantConnect.Configuration;
 using QuantConnect.DownloaderDataProvider.Launcher.Models;
@@ -52,6 +54,48 @@ namespace QuantConnect.Tests.DownloaderDataProvider
             Assert.That(dataDownloadConfig.MarketName, Is.EqualTo(expectedMarket));
 
             Config.Reset();
+        }
+
+        [TestCase(Market.CME, Securities.Futures.Indices.SP500EMini + "H6", SecurityType.Future, false, 0, "2026/03/20")]
+        [TestCase(Market.CME, Securities.Futures.Indices.SP500EMini, SecurityType.Future, true, 0, "2026/03/20")]
+        [TestCase(Market.CME, "E", SecurityType.Future, true, 0, "2026/03/20")]
+        [TestCase(Market.USA, "AAPL", SecurityType.Option, true, 0, "2026/03/20")]
+        [TestCase(Market.USA, "AAPL260213C00262500", SecurityType.Option, false, 262.5, "2026/02/13")]
+        [TestCase(Market.USA, "AAPL  260213C00262500", SecurityType.Option, false, 262.5, "2026/02/13")]
+        [TestCase(Market.USA, "SPXW260213C06050000", SecurityType.IndexOption, false, 6050, "2026/02/13")]
+        [TestCase(Market.USA, "SPXW  260213C06050000", SecurityType.IndexOption, false, 6050, "2026/02/13")]
+        [TestCase(Market.CME, "ESH6 C7000", SecurityType.FutureOption, false, 7000, "2026/03/20")]
+        [TestCase(Market.COMEX, "OGJ6 C4985", SecurityType.FutureOption, false, 4985, "2026/03/26")]
+        [TestCase(Market.CME, "GFH6 C368.5", SecurityType.FutureOption, false, 368.5, "2026/03/26")]
+        public void ShouldParseSymbolContractAndDownload(string expectedMarket, string ticker, SecurityType securityType, bool expectedIsCanonical, decimal expectedStrike, DateTime expectedExpiry)
+        {
+            Config.Set("data-type", "Trade");
+            Config.Set("resolution", "Daily");
+            Config.Set("security-type", $"{securityType}");
+            Config.Set("tickers", $"{{\"{ticker}\": \"\"}}");
+            Config.Set("start-date", "20260201");
+            Config.Set("end-date", "20260213");
+            Config.Set("market", expectedMarket);
+
+            var dataDownloadConfig = new DataDownloadConfig();
+
+            Assert.IsNotEmpty(dataDownloadConfig.Symbols);
+            Assert.AreEqual(1, dataDownloadConfig.Symbols.Count);
+
+            var symbol = dataDownloadConfig.Symbols.First();
+            Assert.IsNotNull(symbol);
+            Assert.AreEqual(expectedIsCanonical, symbol.IsCanonical());
+            Assert.AreEqual(symbol.ID.Market, expectedMarket);
+
+            if (!symbol.IsCanonical())
+            {
+                Assert.AreEqual(expectedExpiry, symbol.ID.Date.Date);
+            }
+
+            if (securityType.IsOption())
+            {
+                Assert.AreEqual(expectedStrike, symbol.ID.StrikePrice);
+            }
         }
     }
 }
