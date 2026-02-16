4d61f42323bbfb0aa12f2fac7cd3e04886600231
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2026-02-16 16:28:41 +0200

    Refactor: error/log message (#9297)
    * feat: improve error logging
    
    * ref: error msg

--- Brokerages/Authentication/OAuthTokenHandler.cs
+++ Brokerages/Authentication/OAuthTokenHandler.cs
@@ -87,7 +87,9 @@ namespace QuantConnect.Brokerages.Authentication
                     }
                 }
 
-                throw new InvalidOperationException(string.Join(",", response.Errors));
+                Logging.Log.Error($"{nameof(OAuthTokenHandler<TRequest, TResponse>)}.{nameof(GetAccessToken)}: Failed to retrieve access token. Response: {response}. Last known expiration: {_accessTokenMetaData?.Expiration.ToStringInvariant() ?? "Not requested yet"}.");
+                throw new InvalidOperationException($"Authentication failed. " +
+                    $"Details: {(response?.Errors?.Count > 0 ? string.Join(",", response.Errors) : "empty")}");
             }
             catch (Exception ex)
             {
