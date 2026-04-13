f0aefdb51060fb91f63741291bed411214385a5b
pınar aksoy <rpain2367@gmail.com>
2026-04-13 09:40:40 -0700

    Refactor SwissArmyKnife to compute all tools as sub-indicators (#9370)
    Previously, each SwissArmyKnife instance could only compute a single
    tool. To use multiple tools, users had to create separate instances.
    Now all five tools (Gauss, Butter, HighPass, TwoPoleHighPass, BandPass)
    are computed simultaneously and exposed as public properties.
    
    Resolves #3270

--- Tests/Indicators/SwissArmyKnifeTests.cs
+++ Tests/Indicators/SwissArmyKnifeTests.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -32,7 +32,7 @@ namespace QuantConnect.Tests.Indicators
         protected override string TestColumnName => "Gauss";
 
         protected override Action<IndicatorBase<IndicatorDataPoint>, double> Assertion =>
-            (indicator, expected) => 
+            (indicator, expected) =>
                 Assert.AreEqual(expected, (double) indicator.Current.Value, 0.01);
 
         [Test]
@@ -79,6 +79,92 @@ namespace QuantConnect.Tests.Indicators
         {
             var indicator = new SwissArmyKnife(20, 0.1, SwissArmyKnifeTool.Butter);
             RunTestIndicator(indicator, "Butter", 0.01m);
+        }
+
+        [Test]
+        public void SubIndicatorsComputeAllToolsSimultaneously()
+        {
+            var sak = new SwissArmyKnife(20, 0.1, SwissArmyKnifeTool.Gauss);
+
+            var gaussStandalone = new SwissArmyKnife(20, 0.1, SwissArmyKnifeTool.Gauss);
+            var butterStandalone = new SwissArmyKnife(20, 0.1, SwissArmyKnifeTool.Butter);
+            var hpStandalone = new SwissArmyKnife(20, 0.1, SwissArmyKnifeTool.HighPass);
+            var tphpStandalone = new SwissArmyKnife(20, 0.1, SwissArmyKnifeTool.TwoPoleHighPass);
+            var bpStandalone = new SwissArmyKnife(20, 0.1, SwissArmyKnifeTool.BandPass);
+
+            foreach (var data in TestHelper.GetDataStream(25))
+            {
+                sak.Update(data);
+                gaussStandalone.Update(data);
+                butterStandalone.Update(data);
+                hpStandalone.Update(data);
+                tphpStandalone.Update(data);
+                bpStandalone.Update(data);
+
+                Assert.AreEqual(gaussStandalone.Current.Value, sak.Gauss.Current.Value);
+                Assert.AreEqual(butterStandalone.Current.Value, sak.Butter.Current.Value);
+                Assert.AreEqual(hpStandalone.Current.Value, sak.HighPass.Current.Value);
+                Assert.AreEqual(tphpStandalone.Current.Value, sak.TwoPoleHighPass.Current.Value);
+                Assert.AreEqual(bpStandalone.Current.Value, sak.BandPass.Current.Value);
+            }
+        }
+
+        [Test]
+        public void SubIndicatorsResetProperly()
+        {
+            var sak = new SwissArmyKnife(4, 0.1, SwissArmyKnifeTool.Gauss);
+
+            foreach (var data in TestHelper.GetDataStream(5))
+            {
+                sak.Update(data);
+            }
+
+            Assert.IsTrue(sak.Gauss.IsReady);
+            Assert.IsTrue(sak.Butter.IsReady);
+            Assert.IsTrue(sak.HighPass.IsReady);
+            Assert.IsTrue(sak.TwoPoleHighPass.IsReady);
+            Assert.IsTrue(sak.BandPass.IsReady);
+
+            sak.Reset();
+
+            Assert.IsFalse(sak.Gauss.IsReady);
+            Assert.IsFalse(sak.Butter.IsReady);
+            Assert.IsFalse(sak.HighPass.IsReady);
+            Assert.IsFalse(sak.TwoPoleHighPass.IsReady);
+            Assert.IsFalse(sak.BandPass.IsReady);
+        }
+
+        [Test]
+        public void PrimaryToolMatchesCurrentValue()
+        {
+            foreach (SwissArmyKnifeTool tool in Enum.GetValues(typeof(SwissArmyKnifeTool)))
+            {
+                var sak = new SwissArmyKnife(20, 0.1, tool);
+
+                foreach (var data in TestHelper.GetDataStream(25))
+                {
+                    sak.Update(data);
+
+                    switch (tool)
+                    {
+                        case SwissArmyKnifeTool.Gauss:
+                            Assert.AreEqual(sak.Gauss.Current.Value, sak.Current.Value);
+                            break;
+                        case SwissArmyKnifeTool.Butter:
+                            Assert.AreEqual(sak.Butter.Current.Value, sak.Current.Value);
+                            break;
+                        case SwissArmyKnifeTool.HighPass:
+                            Assert.AreEqual(sak.HighPass.Current.Value, sak.Current.Value);
+                            break;
+                        case SwissArmyKnifeTool.TwoPoleHighPass:
+                            Assert.AreEqual(sak.TwoPoleHighPass.Current.Value, sak.Current.Value);
+                            break;
+                        case SwissArmyKnifeTool.BandPass:
+                            Assert.AreEqual(sak.BandPass.Current.Value, sak.Current.Value);
+                            break;
+                    }
+                }
+            }
         }
 
         private void RunTestIndicator(IndicatorBase<IndicatorDataPoint> indicator, string field, decimal variance)
