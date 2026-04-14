7602c5bde06bde4204699ac96cc22284fc44c30f
JosueNina <36119850+JosueNina@users.noreply.github.com>
2026-04-14 10:32:47 -0500

    Fix DataManager creating invalid data type subscriptions (#9379)
    * Fix DataManager skipping invalid data type subscriptions
    
    * Update regression algorithms
    
    * Use effective resolution when looking up subscription data types

--- Algorithm.CSharp/EquityOptionsUniverseSettingsRegressionAlgorithm.cs
+++ Algorithm.CSharp/EquityOptionsUniverseSettingsRegressionAlgorithm.cs
@@ -103,7 +103,7 @@ namespace QuantConnect.Algorithm.CSharp
         /// <summary>
         /// Data Points count of all timeslices of algorithm
         /// </summary>
-        public virtual long DataPoints => 4276;
+        public virtual long DataPoints => 4275;
 
         /// <summary>
         /// Data Points count of the algorithm history
