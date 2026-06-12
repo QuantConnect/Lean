38911b5254967d6ccaa8ec898faa3c4f4512165f
Josue Nina <josue.nina.ponce@gmail.com>
2026-06-12 14:02:03 -0500

    Move MarketHourAwareConsolidator to Data.Consolidators namespace

--- Algorithm.CSharp/SecuritySessionWithOptionRegressionAlgorithm.cs
+++ Algorithm.CSharp/SecuritySessionWithOptionRegressionAlgorithm.cs
@@ -17,7 +17,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using QuantConnect.Data;
-using QuantConnect.Data.Common;
+using QuantConnect.Data.Consolidators;
 using QuantConnect.Data.Market;
 using QuantConnect.Securities;
 
