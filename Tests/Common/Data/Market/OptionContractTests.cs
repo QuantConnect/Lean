aab3a6fc431d124c743858f0e185eac58f2c6515
Jhonathan Abreu <jdabreu25@gmail.com>
2026-07-01 14:13:48 -0400

    Add Price, Value and Close aliases to BaseContract
    Expose Price, Value and Close on BaseContract as aliases of LastPrice to
    mimic the BaseData/TradeBar API without inheriting BaseData. Marked with
    PandasIgnore to avoid duplicating the LastPrice column in chain DataFrames.

---
+++ Tests/Common/Data/Market/OptionContractTests.cs
@@ -0,0 +1,61 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using NUnit.Framework;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Securities;
+using QuantConnect.Securities.Option;
+
+namespace QuantConnect.Tests.Common.Data.Market
+{
+    [TestFixture]
+    public class OptionContractTests
+    {
+        private static Option CreateOption(Symbol symbol)
+        {
+            return new Option(
+                SecurityExchangeHours.AlwaysOpen(TimeZones.NewYork),
+                new SubscriptionDataConfig(typeof(TradeBar), symbol, Resolution.Minute, TimeZones.NewYork, TimeZones.NewYork, true, true, true),
+                new Cash(Currencies.USD, 0, 1m),
+                new OptionSymbolProperties(SymbolProperties.GetDefault(Currencies.USD)),
+                ErrorCurrencyConverter.Instance,
+                RegisteredSecurityDataTypesProvider.Null
+            );
+        }
+
+        [Test]
+        public void PriceValueAndCloseAliasLastPrice()
+        {
+            var symbol = Symbols.SPY_C_192_Feb19_2016;
+            var contract = new OptionContract(CreateOption(symbol)) { Time = new DateTime(2016, 02, 16) };
+
+            // No data yet, all aliases default to zero
+            Assert.AreEqual(0, contract.LastPrice);
+            Assert.AreEqual(contract.LastPrice, contract.Price);
+            Assert.AreEqual(contract.LastPrice, contract.Value);
+            Assert.AreEqual(contract.LastPrice, contract.Close);
+
+            var tradeBar = new TradeBar(new DateTime(2016, 02, 16), symbol, 1, 2, 3, 4, 5);
+            contract.Update(tradeBar);
+
+            Assert.AreEqual(4, contract.LastPrice);
+            Assert.AreEqual(contract.LastPrice, contract.Price);
+            Assert.AreEqual(contract.LastPrice, contract.Value);
+            Assert.AreEqual(contract.LastPrice, contract.Close);
+        }
+    }
+}
