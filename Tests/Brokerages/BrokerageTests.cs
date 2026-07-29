7f9b4415f7e2fa0fdc8f0a4a46b22a7a7b78bad4
Romazes <romazes20@gmail.com>
2026-07-29 14:57:24 +0300

    restore: BrokerageTest file to master

--- Tests/Brokerages/BrokerageTests.cs
+++ Tests/Brokerages/BrokerageTests.cs
@@ -37,10 +37,6 @@ namespace QuantConnect.Tests.Brokerages
         // ideally this class would be abstract, but I wanted to keep the order test cases here which use the
         // various parameters required from derived types
 
-        // shared across tests: NUnitLogHandler is stateless, and reusing one instance avoids
-        // an undisposed local when TryAddHandler rejects a duplicate (CA2000)
-        private static readonly NUnitLogHandler NUnitHandler = new();
-
         private IBrokerage _brokerage;
         private OrderProvider _orderProvider;
         private SecurityProvider _securityProvider;
@@ -54,21 +50,6 @@ namespace QuantConnect.Tests.Brokerages
         [SetUp]
         public void Setup()
         {
-            switch (Log.LogHandler)
-            {
-                case NUnitLogHandler:
-                    break;
-                case null:
-                    Log.LogHandler = NUnitHandler;
-                    break;
-                case CompositeLogHandler composite:
-                    composite.TryAddHandler(NUnitHandler);
-                    break;
-                default:
-                    Log.LogHandler = new CompositeLogHandler(Log.LogHandler, NUnitHandler);
-                    break;
-            }
-
             Log.Trace("");
             Log.Trace("");
             Log.Trace("--- SETUP ---");
