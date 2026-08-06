de8d2f8c97fdf48cabbc3093c0ec5bc543157a7b
桌子潔 <bm1016bm@gmail.com>
2026-08-07 02:27:16 +0800

    Update US equity market hours through 2028 (#9646)

--- Tests/Common/Securities/MarketHoursDatabaseTests.cs
+++ Tests/Common/Securities/MarketHoursDatabaseTests.cs
@@ -128,6 +128,36 @@ namespace QuantConnect.Tests.Common.Securities
             Assert.AreEqual(earlyCloseTime, equityHours.EarlyCloses[date]);
         }
 
+        [Test]
+        public void CorrectlyReadsUsEquity2028SpecialHours()
+        {
+            var exchangeHours = MarketHoursDatabase.FromDataFolder()
+                .GetExchangeHours(Market.USA, null, SecurityType.Equity);
+
+            var holidays = new[]
+            {
+                new DateTime(2028, 1, 17),
+                new DateTime(2028, 2, 21),
+                new DateTime(2028, 4, 14),
+                new DateTime(2028, 5, 29),
+                new DateTime(2028, 6, 19),
+                new DateTime(2028, 7, 4),
+                new DateTime(2028, 9, 4),
+                new DateTime(2028, 11, 23),
+                new DateTime(2028, 12, 25)
+            };
+
+            foreach (var holiday in holidays)
+            {
+                Assert.IsTrue(exchangeHours.Holidays.Contains(holiday));
+            }
+            Assert.IsFalse(exchangeHours.Holidays.Contains(new DateTime(2028, 1, 1)));
+
+            var earlyCloseTime = new TimeSpan(13, 0, 0);
+            Assert.AreEqual(earlyCloseTime, exchangeHours.EarlyCloses[new DateTime(2028, 7, 3)]);
+            Assert.AreEqual(earlyCloseTime, exchangeHours.EarlyCloses[new DateTime(2028, 11, 24)]);
+        }
+
         [TestCase("AUP", Market.COMEX, true)]
         [TestCase("AA6", Market.NYMEX, true)]
         [TestCase("6A", Market.CME, false)]
